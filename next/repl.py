88639c56f791a7c2ebb134d97af5919f1a2d7fb9
John Mercouris <jmercouris@gmail.com>
2017-04-08 15:48:30 +0200

    Working on Lisp Lexer

--- next/repl.py
+++ next/repl.py
@@ -1,11 +1,6 @@
 import interpreter.lex as lex
-import interpreter.yacc as yacc
+# import interpreter.yacc as yacc
 
-# -----------------------------------------------------------------------------
-# calc.py
-#
-# A simple calculator with variables.
-# -----------------------------------------------------------------------------
 
 tokens = ('NAME', 'NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
           'EQUALS', 'LPAREN', 'RPAREN')
@@ -42,78 +37,20 @@ def t_error(t):
 
 
 # Build the lexer
-lex.lex()
+lexer = lex.lex()
 
-
-# Precedence rules for the arithmetic operators
-precedence = (
-    ('left', 'PLUS', 'MINUS'),
-    ('left', 'TIMES', 'DIVIDE'),
-    ('right', 'UMINUS'))
-
-# dictionary of names (for storing variables)
-names = {}
-
-
-def p_statement_assign(p):
-    'statement : NAME EQUALS expression'
-    names[p[1]] = p[3]
-
-
-def p_statement_expr(p):
-    'statement : expression'
-    print(p[1])
-
-
-def p_expression_binop(p):
-    '''expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression TIMES expression
-                  | expression DIVIDE expression'''
-    if p[2] == '+':
-        p[0] = p[1] + p[3]
-    elif p[2] == '-':
-        p[0] = p[1] - p[3]
-    elif p[2] == '*':
-        p[0] = p[1] * p[3]
-    elif p[2] == '/':
-        p[0] = p[1] / p[3]
-
-
-def p_expression_uminus(p):
-    'expression : MINUS expression %prec UMINUS'
-    p[0] = -p[2]
-
-
-def p_expression_group(p):
-    'expression : LPAREN expression RPAREN'
-    p[0] = p[2]
-
-
-def p_expression_number(p):
-    'expression : NUMBER'
-    p[0] = p[1]
-
-
-def p_expression_name(p):
-    'expression : NAME'
-    try:
-        p[0] = names[p[1]]
-    except LookupError:
-        print("Undefined name '%s'" % p[1])
-        p[0] = 0
-
-
-def p_error(p):
-    print("Syntax error at '%s'" % p.value)
-
-
-# yacc
-yacc.yacc()
 
 while True:
     try:
-        s = input('calc > ')   # use input() on Python 3
+        s = input('nlisp > ')   # use input() on Python 3
     except EOFError:
         break
-    yacc.parse(s)
+    lexer.input(s)
+    
+    # iterate through lexer tokens
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        print(' type {:<7} value {:<7} line {:<7} pos {:<7}'.format(
+            tok.type, tok.value, tok.lineno, tok.lexpos))
