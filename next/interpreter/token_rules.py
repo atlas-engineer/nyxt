34f408d1b9b593559dea13299bf1b988e377cd4c
John Mercouris <jmercouris@gmail.com>
2017-04-08 17:01:52 +0200

    Token rules moved to separate module

---
+++ next/interpreter/token_rules.py
@@ -0,0 +1,31 @@
+op_tokens = ('PLUS', 'MINUS', 'TIMES', 'DIVIDE')
+tokens = ('SYMBOL', 'NUMBER', 'LPAREN', 'RPAREN') + op_tokens
+
+# Tokens
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_SYMBOL = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+# Ignored characters
+t_ignore = " \t"
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
--- next/repl.py
+++ next/repl.py
@@ -1,42 +1,8 @@
 import interpreter.lex as lex
-# import interpreter.yacc as yacc
-
-
-op_tokens = ('PLUS', 'MINUS', 'TIMES', 'DIVIDE')
-tokens = ('SYMBOL', 'NUMBER', 'LPAREN', 'RPAREN') + op_tokens
-
-# Tokens
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_SYMBOL = r'[a-zA-Z_][a-zA-Z0-9_]*'
-
-
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-
-
-# Ignored characters
-t_ignore = " \t"
-
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += t.value.count("\n")
-
-
-def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
-
+import interpreter.token_rules
 
 # Build the lexer
-lexer = lex.lex()
+lexer = lex.lex(module=interpreter.token_rules)
 
 
 while True:
