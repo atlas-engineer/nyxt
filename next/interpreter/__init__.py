1fb613ec4a87b3241f748dfe00cf07fb66833d2d
John Mercouris <john@mercouris.email>
2017-04-15 16:32:51 +0200

    Changed to parsing expression grammar

--- next/interpreter/__init__.py
+++ next/interpreter/__init__.py
@@ -1,8 +1,21 @@
+from parsimonious.grammar import Grammar
 
 
 class Interpreter(object):
     """
     
     """
+    def __init__(self):
+        self.grammar = Grammar(
+            """
+            bold_text  = bold_open text bold_close
+            text       = ~"[A-Z 0-9]*"i
+            bold_open  = "(("
+            bold_close = "))"
+            """)
+    
+    def parse(self, expression):
+        print(self.grammar.parse(expression))
+
     def evaluate(self, expression):
         print('eval')
--- next/repl.py
+++ next/repl.py
@@ -1,12 +1,6 @@
 import cmd
-import interpreter.lex as lex
-
-import interpreter.token_rules
-from interpreter.parse_table import yacc
-
-
-# Build the lexer
-lexer = lex.lex(module=interpreter.token_rules)
+from interpreter import Interpreter
+instance = Interpreter()
 
 
 class nLisp(cmd.Cmd):
@@ -15,13 +9,7 @@ class nLisp(cmd.Cmd):
         self.prompt = "nlisp> "
   
     def default(self, line):
-        lexer.input(line)
-        for token in lexer:
-            print(' type {:<7} value {:<7} line {:<7} pos {:<7}'.format(
-                token.type, token.value, token.lineno, token.lexpos))
-            
-        result = yacc.parse(line)
-        print(result)
+        instance.parse(line)
 
 
 if __name__ == '__main__':
