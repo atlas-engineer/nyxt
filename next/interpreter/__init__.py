611b784d1ead3d9db685f388b0dc3b2bc41c5410
John Mercouris <john@mercouris.email>
2017-04-15 19:23:39 +0200

    Changed to parsimonious parser

--- next/interpreter/__init__.py
+++ next/interpreter/__init__.py
@@ -1,21 +1,33 @@
+"""
+ s_expression = atomic / "(" s_expression "."s_expression ")" / list
+ list = "(" s_expression < s_expression > ")"
+ letter = "a" / "b" / " ..." / "z"
+ number = "1" / "2" / " ..." / "9"
+ empty = " "
+"""
+
 from parsimonious.grammar import Grammar
 
 
 class Interpreter(object):
-    """
+    """(+ 1 (* a b))
     
     """
     def __init__(self):
         self.grammar = Grammar(
             """
-            bold_text  = bold_open text bold_close
-            text       = ~"[A-Z 0-9]*"i
-            bold_open  = "(("
-            bold_close = "))"
+            EXPRESSION    = ATOM / LIST
+            LIST          = LPAREN EXPRESSION* RPAREN
+            ATOM          = SYMBOL / NUMBER / EMPTY
+            SYMBOL        = ~r"[A-Za-z]+"
+            NUMBER        = ~r"[0-9]+"
+            LPAREN        = "("
+            RPAREN        = ")"
+            EMPTY         = " "
             """)
     
     def parse(self, expression):
         print(self.grammar.parse(expression))
-
+    
     def evaluate(self, expression):
         print('eval')
