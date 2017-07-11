168bfff6368e936e315413de39e158df46d6cd6b
John Mercouris <john@mercouris.email>
2017-07-12 00:35:47 +0200

    Keymap basic detection in place

--- next/lisp/keymap.lisp
+++ next/lisp/keymap.lisp
@@ -1,24 +1,79 @@
 (in-package :next)
+(ql:quickload :cl-strings)
+(use-package :cl-strings)
 
 (qadd-event-filter nil |QEvent.KeyPress| 'key-press)
 (qadd-event-filter nil |QEvent.KeyRelease| 'key-release)
 
-(defvar *control-modifier* nil
-  "Is the control modifier active")
+(defstruct key
+  character
+  control-modifier
+  meta-modifier)
 
-(defun kbd (keys)
-  "Convert KEYS to the internal next representation")
+(defvar *control-modifier* nil
+  "A variable to store the status of the control key")
+(defvar *meta-modifier* nil
+  "A variable to store the status of the alt/meta key")
+
+(defparameter global-map (make-hash-table :test 'equalp)
+  "A global key map for all keys")
+(defparameter *key-sequence-stack* ()
+  "A buffer that keeps track of the keys a user has inputted")
+
+(defun push-key-chord (key-character-string)
+  (let ((key-chord (make-key)))
+    (if *control-modifier*
+	(setf (key-control-modifier key-chord) t))
+    
+    (setf (key-character key-chord) key-character-string)
+    (push key-chord *key-sequence-stack*)))
+
+(defun consume-key-sequence ()
+  (print *key-sequence-stack*)
+  ;; If key recognized, execute function
+  (if (gethash *key-sequence-stack* global-map)
+      (progn
+	(funcall (gethash *key-sequence-stack* global-map))
+	(setf *key-sequence-stack* ()))
+      ;; If key not recognized, print message
+      (progn
+	(print "Key Undefined")
+	(setf *key-sequence-stack* ()))))
 
 (defun key-press (obj event)
   (case (|key| event)
     (#.|Qt.Key_Control|
        (setf *control-modifier* t))
     (t ; all other keys
-     (print (|text| event)))))
+     (print (|text| event))
+     (push-key-chord (|text| event))
+     (consume-key-sequence))))
 
 (defun key-release (obj event)
   (case (|key| event)
     (#.|Qt.Key_Control|
        (setf *control-modifier* nil))
     (t (return-from key-release))))
+
+(defun define-key (mode-map key-sequence function)
+  (setf (gethash key-sequence mode-map) function))
+
+(defun kbd (key-sequence-string)
+  (let ((key-sequence ()))
+    ;; Iterate through all key chords (space delimited)
+    (loop for key-chord-string in (split key-sequence-string " ")
+       ;; Iterate through all keys in chord (hyphen delimited)
+       do (let ((key-chord (make-key)))
+	    (loop for key-character-string in (split key-chord-string "-")
+	       do (cond
+		    ((equal "C" key-character-string) (setf (key-control-modifier key-chord) t))
+		    (t (setf (key-character key-chord) key-character-string))))
+	    (push key-chord key-sequence)))
+    key-sequence))
+
+(defun print-hey ()
+  (print "hey"))
+
+(define-key global-map (kbd "a") #'print-hey)
+
 
