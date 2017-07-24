7372ab82f83df30d47ced8b1d372b2bf3ce42c09
John Mercouris <john@mercouris.email>
2017-07-24 11:44:01 +0300

    build.sh commented

--- next/build.sh
+++ next/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# Use this script to generate an application for distribution
+
+# Delete tmp if exists to force recompilation
 rm -rf ./tmp
+
+# Make the application bundle
 eql5 make
 qmake
 make
+
+# Clean up temporary directories
 rm -rf ./tmp
+rm libnext.a
