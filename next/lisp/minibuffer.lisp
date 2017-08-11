b71804cd88b4589f19c0934cbafa886d52c39d9e
John Mercouris <john@mercouris.email>
2017-08-11 15:20:20 +0200

    minibuffer input example
    created first working version of minibuffer input system, need
    to add support for all letters of the alphabet as input and a callback
    to bring the original buffer in focus after receiving input

--- next/lisp/minibuffer.lisp
+++ next/lisp/minibuffer.lisp
@@ -9,21 +9,23 @@
 (defparameter *minibuffer-completion-callback* nil
   "A variable to store the function upon completion of the minibuffer read")
 
-(defun insert-a ()
-  (setf *minibuffer-input-string* (concatenate 'string *minibuffer-input-string* "a")))
-(define-key minibuffer-mode-map (kbd "a") #'insert-a)
-
-(defun insert-b ()
-  (setf *minibuffer-input-string* (concatenate 'string *minibuffer-input-string* "b")))
-(define-key minibuffer-mode-map (kbd "b") #'insert-b)
-
 (defun return-input ()
   (funcall *minibuffer-completion-callback* *minibuffer-input-string*)
-  (setf *minibuffer-input-string* nil))
+  (setf *minibuffer-input-string* nil)
+  (update-display))
 (define-key minibuffer-mode-map (kbd "Return") #'return-input)
 
 (defun input (callback-function)
   (setf *minibuffer-completion-callback* callback-function))
+
+(defun update-display ()
+  (|setHtml| (buffer-view *mini-buffer*) *minibuffer-input-string*))
+
+(defun minibuffer-mode ()
+  "Base mode for input"
+  (make-mode
+   :name "Minibuffer-Mode"
+   :keymap minibuffer-mode-map))
 
 (defun printy-text (text)
   (print text))
@@ -32,9 +34,18 @@
   (input #'printy-text))
 (define-key minibuffer-mode-map (kbd "C-a") #'print-text-read)
 
-(defun minibuffer-mode ()
-  "Base mode for input"
-  
-  (make-mode
-   :name "Minibuffer-Mode"
-   :keymap minibuffer-mode-map))
+;; define input keys
+(defun insert-a ()
+  (setf *minibuffer-input-string* (concatenate 'string *minibuffer-input-string* "a"))
+  (update-display))
+(define-key minibuffer-mode-map (kbd "a") #'insert-a)
+
+(defun insert-b ()
+  (setf *minibuffer-input-string* (concatenate 'string *minibuffer-input-string* "b"))
+  (update-display))
+(define-key minibuffer-mode-map (kbd "b") #'insert-b)
+
+(defun insert-c ()
+  (setf *minibuffer-input-string* (concatenate 'string *minibuffer-input-string* "c"))
+  (update-display))
+(define-key minibuffer-mode-map (kbd "c") #'insert-c)
