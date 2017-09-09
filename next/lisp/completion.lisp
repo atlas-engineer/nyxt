2ce35eb85f45dd7811cd50b2566228417abbc21a
John Mercouris <john@mercouris.email>
2017-09-09 03:03:08 -0500

    completion fuzzy-match function implemented

---
+++ next/lisp/completion.lisp
@@ -0,0 +1,24 @@
+;;;; completion.lisp --- completion matchers
+
+;;;; completion matchers are functions that accept an input string and
+;;;; a list of strings, and given some logic, return an ordered list
+;;;; of strings matched based on probability
+;;;;
+;;;; as an example, consider the possible implementation
+;;;; "fuzzy_matcher" with input string "so str" and a list of strings:
+;;;; "some string", "strike" we would expect the return to be "some
+;;;; string", "strike", because it is more probable that the user is
+;;;; looking for "some string"
+
+(defun fuzzy-match (string string-list)
+  (let ((regex
+	 (with-output-to-string (stream)
+	   (loop for char across string do
+		(princ char stream)
+		(princ #\. stream)
+		(princ #\* stream))))
+	(results nil))
+    (loop for element in string-list do
+	 (when (match-re regex element)
+	   (push element results)))
+    results))
