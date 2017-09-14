b262fe1b7658cb24202ff2165e5d2ca6807551d5
John Mercouris <john@mercouris.email>
2017-09-13 21:59:53 -0500

    added hooks to loadFinished in document-mode to add to history tree

--- next/lisp/document-mode.lisp
+++ next/lisp/document-mode.lisp
@@ -5,13 +5,18 @@
 (defvar document-mode-hook nil)
 (defvar document-mode-map (make-hash-table :test 'equalp))
 
-(defclass document-mode (mode) ())
+(defclass document-mode (mode)
+  ((history-tree :accessor mode-history-tree :initform ())))
 
 (defun scroll-down ()
   (|scroll| (|mainFrame| (|page| (buffer-view *active-buffer*))) 0 30))
 
 (defun scroll-up ()
   (|scroll| (|mainFrame| (|page| (buffer-view *active-buffer*))) 0 -30))
+
+(defun add-element-history (mode)
+  (push (|toString| (|url| (mode-view mode))) (mode-history-tree mode))
+  (print (mode-history-tree mode)))
 
 (defun set-url-new-buffer (input-url)
   (let ((new-buffer (generate-new-buffer "default" (document-mode))))
@@ -28,10 +33,14 @@
 
 (defun document-mode ()
   "Base mode for interacting with documents"
-  (make-instance 'document-mode
-   :name "Document-Mode"
-   :keymap document-mode-map
-   :view (qnew "QWebView")))
+  (let ((mode 
+	 (make-instance 'document-mode
+			:name "Document-Mode"
+			:keymap document-mode-map
+			:view (qnew "QWebView"))))
+    (qconnect (|mainFrame| (|page| (mode-view mode))) "loadFinished(bool)" (lambda (ok) (add-element-history mode)))
+    ;; return instance of mode
+    mode))
 
 (define-key document-mode-map (kbd "C-p") #'scroll-up)
 (define-key document-mode-map (kbd "C-n") #'scroll-down)
