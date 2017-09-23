b201a52c3a854735968e90eba96ce29ff76ec9bc
John Mercouris <john@mercouris.email>
2017-09-22 19:15:05 -0500

    commented purpose of input-complete macro

--- next/lisp/macro.lisp
+++ next/lisp/macro.lisp
@@ -2,9 +2,13 @@
 
 (in-package :next)
 
-;; used to provide input to buffers
+;; used to provide input to buffers, "function" must accept input from
+;; the minibuffer
 (defmacro :input (function)
   `#'(lambda () (input #',function)))
 
+;; used to provide input to buffers with an optional completion
+;; function, the completion function must narrow a list of candidates
+;; when given input
 (defmacro :input-complete (function completion)
   `#'(lambda () (input #',function #',completion)))
