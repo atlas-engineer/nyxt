d1148a2a8cd5d6cf4711324cb6ec63d9caff0e2a
John Mercouris <john@mercouris.email>
2017-10-26 01:16:19 +0200

    updated "run" script for simplification

--- next/run.lisp
+++ next/run.lisp
@@ -1,12 +1,8 @@
 ;;; run.lisp --- helper runner program to run from the command line
-;;; to launch nEXT simply `eql5 run.lisp`
 (require "asdf")
 
-(push "./" asdf:*central-registry*)
+(push (directory-namestring *load-pathname*) asdf:*central-registry*)
 (ql:quickload "next")
-(asdf:load-system "next")
-
-(in-package #:next)
 
 ;; start nEXT
-(start)
+(next:start)
