bd4322ba20b5f8c9ddf6275683107cdee4aa5b7b
John Mercouris <john@mercouris.email>
2017-11-01 11:36:37 +0100

    changed run.lisp to load.lisp to load system without starting nEXT

--- next/load.lisp
+++ next/load.lisp
@@ -1,8 +1,5 @@
-;;; run.lisp --- helper runner program to run from the command line
+;;; load.lisp --- helper runner program to load system from the command line
 (require "asdf")
 
 (push (directory-namestring *load-pathname*) asdf:*central-registry*)
 (ql:quickload "next")
-
-;; start nEXT
-(next:start)
