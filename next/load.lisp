b7ecd4fd4e2884e7e3bf701c5bae1abf59a5a283
John Mercouris <john@mercouris.email>
2017-11-01 20:26:32 +0100

    deleted gratuitous load.lisp file
    user can manually load system via repl, additionally relies on
    undefined behavior in implementation

--- next/load.lisp
+++
@@ -1,5 +0,0 @@
-;;; load.lisp --- helper runner program to load system from the command line
-(require "asdf")
-
-(push (directory-namestring *load-pathname*) asdf:*central-registry*)
-(ql:quickload "next")
