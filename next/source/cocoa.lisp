f850cbbdd5bc887816777518bf3369b4020489e6
John Mercouris <john@mercouris.email>
2017-11-04 14:28:33 +0100

    rearrange magic

--- next/source/cocoa.lisp
+++ next/source/cocoa.lisp
@@ -29,10 +29,6 @@
      character)
     (call-next-method event)))
 
-(defun url-from-string (s)
-  (ccl::with-autorelease-pool
-    (#/retain (#/URLWithString: ns:ns-url (ccl::%make-nsstring (string s))))))
-
 (defun make-window ()
   (gui::assume-cocoa-thread)
   ;; Content rect for window, bounds rect for view.
@@ -60,9 +56,6 @@
 (defun kill ()
   (quit))
 
-(defun get-active-view ()
-  (print (objc:send ns:ns-view 'focus-view)))
-
 (defun set-visible-view (view)
   (on-main-thread
    (#/setContentView: *window* view)))
@@ -73,15 +66,16 @@
 
 (defun make-web-view ()
   (on-main-thread
-   (ns:with-ns-rect (r 100.0 100.0 1024.0 768.0)
-		    (let ((view
-			   (make-instance
-			    'ns:web-view
-			    :with-frame r
-			    :frame-name #@"frame"
-			    :group-name #@"group")))
-		      (lisp-views:constrain-to-natural-size view)
-		      view))))
+   (let ((view
+	  (make-instance
+	   'ns:web-view
+	   :frame-name #@"frame"
+	   :group-name #@"group")))
+     view)))
+
+(defun url-from-string (s)
+  (ccl::with-autorelease-pool
+    (#/retain (#/URLWithString: ns:ns-url (ccl::%make-nsstring (string s))))))
 
 (defun web-view-set-url (view url)
   (on-main-thread
