ca347ebf156f90e6067fc3aed3f60a5b37012def
John Mercouris <john@mercouris.email>
2017-11-22 17:52:06 +0100

    remove unnecessary prefix ps: from defun

--- next/source/link-hint.lisp
+++ next/source/link-hint.lisp
@@ -4,18 +4,18 @@
 (in-package :next)
 
 (defparen add-link-hints
-  (ps:defun qsa (context selector)
+  (defun qsa (context selector)
     "Alias of document.querySelectorAll"
     (ps:chain context (query-selector-all selector)))
-  (ps:defun remove-if-not (predicate sequence)
+  (defun remove-if-not (predicate sequence)
     "Small reimplementation of remove-if"
-    (ps:loop for el in sequence
+    (loop for el in sequence
 	     when (predicate el)
 	     collect el))
-  (ps:defun code-char (n)
+  (defun code-char (n)
     "Alias of String.fromCharCode"
     (ps:chain -string (from-char-code n)))
-  (ps:defun is-in-viewport (el)
+  (defun is-in-viewport (el)
     "Finds out if an element is in the viewport"
     (ps:let ((rect (ps:chain el (get-bounding-client-rect))))
       (and
@@ -25,11 +25,11 @@
 				  (ps:@ document document-element client-height)))
        (<= (ps:@ rect right) (or (ps:@ window inner-width)
 				 (ps:@ document document-element client-width))))))
-  (ps:defun hint-determine-position (rect)
+  (defun hint-determine-position (rect)
     "Determines the position of a hint according to the link"
     (ps:create :top  (ps:@ rect top)
 	       :left (- (ps:@ rect left) 20)))
-  (ps:defun hint-create-element (link hint)
+  (defun hint-create-element (link hint)
     "Creates a DOM element to be used as a hint"
     (ps:let* ((rect (ps:chain link (get-bounding-client-rect)))
 	      (position (hint-determine-position rect))
@@ -38,7 +38,7 @@
 	(setf (ps:@ position left) (+ (ps:@ position left) 20)))
       (when (< (ps:@ position top) 0)
 	(setf (ps:@ position top) (+ (ps:@ position top) 20)))
-      (setf (ps:@ el class-name) "lispkit-link-hints")
+      (setf (ps:@ el class-name) "next-link-hint")
       (setf (ps:@ el style background) "yellow")
       (setf (ps:@ el style border) "1px solid #ccc")
       (setf (ps:@ el style position) "absolute")
@@ -48,36 +48,36 @@
       (setf (ps:@ el style top) (+ (ps:@ position top) "px"))
       (setf (ps:@ el text-content) hint)
       el))
-  (ps:defun hint-add (link hint)
+  (defun hint-add (link hint)
     "Adds a hint on a single link"
     (ps:let ((hint-element (hint-create-element link hint)))
       (ps:chain document body (append-child hint-element))
       hint-element))
-  (ps:defun hints-add (links)
+  (defun hints-add (links)
     "Adds hints on links"
     (ps:let* ((links-length (length links))
 	   (hints (hints-generate links-length)))
-      (ps:loop for i from 0 to (- links-length 1)
+      (loop for i from 0 to (- links-length 1)
 	 collect (ps:create :link (elt links i) :hint (hint-add (elt links i) (elt hints i))))))
-  (ps:defun hints-determine-chars-length (length)
+  (defun hints-determine-chars-length (length)
     "Finds out how many chars long the hints must be"
     (ps:let ((i 1))
       ;; 26 chars in alphabet
-      (ps:loop while (> length (expt 26 i))
+      (loop while (> length (expt 26 i))
 	 do (incf i))
       i))
-  (ps:defun hints-generate (length)
+  (defun hints-generate (length)
     "Generates hints that will appear on the links"
     (strings-generate length (hints-determine-chars-length length)))
-  (ps:defun links-find (window document)
+  (defun links-find (window document)
     "Finds all the links within the viewport"
     (remove-if-not #'is-in-viewport (qsa document "a")))
-  (ps:defun strings-generate (length chars-length)
+  (defun strings-generate (length chars-length)
     "Generates strings of specified length"
     (ps:let ((minimum (1+ (ps:chain -math (pow 26 (- chars-length 1))))))
-      (ps:loop for i from minimum to (+ minimum length)
+      (loop for i from minimum to (+ minimum length)
 	 collect (string-generate i))))
-  (ps:defun string-generate (n)
+  (defun string-generate (n)
     "Generates a string from a number"
     (if (>= n 0)
 	(+ (string-generate (floor (- (/ n 26) 1)))
@@ -90,12 +90,12 @@
   (interface:web-view-execute (view *active-buffer*) add-link-hints))
 
 (defparen remove-link-hints
-  (ps:defun qsa (context selector)
+  (defun qsa (context selector)
     "Alias of document.querySelectorAll"
     (ps:chain context (query-selector-all selector)))
-  (ps:defun hints-remove-all ()
+  (defun hints-remove-all ()
     "Removes all the links"
-    (ps:dolist (el (qsa document ".lispkit-link-hints"))
+    (ps:dolist (el (qsa document ".next-link-hint"))
       (ps:chain el (remove))))
   (hints-remove-all))
 
