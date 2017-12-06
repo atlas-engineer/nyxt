23cbb8073c54f6b50f28db12583fe7993cc7f0c0
John Mercouris <john@mercouris.email>
2017-12-06 16:30:15 +0100

    home page shows hotkeys for new users

--- next/source/base.lisp
+++ next/source/base.lisp
@@ -14,7 +14,8 @@
   ;; create the default buffers
   (setf *minibuffer*
 	(make-instance 'buffer :name "minibuffer" :mode (minibuffer-mode)))
-  (set-visible-active-buffer (generate-new-buffer "default" (document-mode))))
+  (set-visible-active-buffer (generate-new-buffer "default" (document-mode)))
+  (set-url *start-page-url*))
 
 (defun initialize-default-key-bindings ()
   (define-key *global-map* (kbd "C-x C-c")
--- next/source/global.lisp
+++ next/source/global.lisp
@@ -3,12 +3,14 @@
 (in-package :next)
 
 (defvar *global-map* (make-hash-table :test 'equalp)
-  "A global key map, available in every mode/buffer")
+  "A global key map, available in every mode/buffer.")
 (defvar *active-buffer* ()
-  "The currently active buffer, do not modify this variable directly")
+  "The currently active buffer, do not modify this variable directly.")
 (defvar *minibuffer* nil
-  "A variable to store the mini-buffer")
+  "A variable to store the minibuffer.")
 (defvar *buffers* ()
-  "A list of all existing buffers")
+  "A list of all existing buffers.")
 (defvar *scroll-distance* 15
   "The distance scroll-down or scroll-up will scroll.")
+(defvar *start-page-url* "https://next-browser.github.io/start.html"
+  "The url of the first buffer opened by nEXT when started.")
