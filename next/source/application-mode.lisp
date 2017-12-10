9174f03ec94ba6d714f03bc8ebb12037fcff2564
John Mercouris <john@mercouris.email>
2017-12-10 21:55:19 +0100

    application mode

---
+++ next/source/application-mode.lisp
@@ -0,0 +1,14 @@
+;;; application-mode.lisp --- major mode for "web applications"
+;;; has no history or bindings to avoid interference with "web applications"'
+
+(in-package :next)
+
+(defvar *application-mode-map* (make-hash-table :test 'equalp))
+
+(defclass application-mode (mode) ())
+
+(defun application-mode ()
+  (make-instance 'application-mode
+		 :name "Application-Mode"
+		 :keymap *application-mode-map*
+		 :view (interface:make-web-view)))
