3584b1bc5c4d6a1ac46b2974903ac8626c12ae4e
John Mercouris <john@mercouris.email>
2017-12-13 21:32:50 +0100

    rudimentary regex/search

--- next/source/search-buffer.lisp
+++ next/source/search-buffer.lisp
@@ -2,5 +2,15 @@
 
 (in-package :next)
 
-(defparen add-search-boxes
-    )
+(defparen add-search-boxes (search-string)
+  (let* ((regex-string (ps:lisp (concatenate 'string search-string "[A-Za-z]*")))
+         (regex-flags "gi")
+         (matcher (ps:new (-reg-exp regex-string regex-flags)))
+         (stringy "some magical string breadfish stop")
+         (matches ())
+         (last-match t))
+    (ps:chain -j-s-o-n
+              (stringify
+               (loop do last-match do
+                    (setf last-match (ps:chain matcher (exec stringy)))
+                  collect last-match)))))
