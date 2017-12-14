a74fc42ef540e0f51f613d89f20883e0058881d4
John Mercouris <john@mercouris.email>
2017-12-14 17:38:01 +0100

    searching individual buffers working

--- next/source/search-buffer.lisp
+++ next/source/search-buffer.lisp
@@ -2,7 +2,9 @@
 
 (in-package :next)
 
-(defparen add-search-boxes (search-string)
+(defparenstatic initialize-search-buffer
+    (ps:defvar current-search 0)
+  (ps:defvar match-count)
   (defun insert (str index value)
     (+ (ps:chain str (substr 0 index)) value (ps:chain str (substr index))))
   (defun create-search-span (index)
@@ -13,7 +15,10 @@
       (setf (ps:@ el style font-weight) "bold")
       (setf (ps:@ el style text-align) "center")
       (setf (ps:@ el text-content) index)
-      el))
+      (setf (ps:@ el id) index)
+      el)))
+
+(defparen add-search-boxes (search-string)
   (let* ((regex-string (ps:lisp (concatenate 'string search-string "[A-Za-z]*")))
          (regex-flags "gi")
          (matcher (ps:new (-reg-exp regex-string regex-flags)))
@@ -21,10 +26,11 @@
          (last-match t)
          (matches (loop while (setf last-match (ps:chain matcher (exec body)))
                      collect (ps:chain last-match index))))
+    (setf match-count (length matches))
     (setf matches (ps:chain matches (reverse)))
-    (loop for i from 0 to (- (length matches) 1)
+    (loop for i from 0 to (- match-count 1)
        do (setf body (insert body (ps:elt matches i)
-                             (ps:chain (create-search-span (- (length matches) i)) outer-h-t-m-l))))
+                             (ps:chain (create-search-span (- match-count i)) outer-h-t-m-l))))
     (setf (ps:chain document body inner-h-t-m-l) body))
   nil)
 
@@ -37,3 +43,15 @@
     (ps:dolist (el (qsa document ".next-search-hint"))
       (ps:chain el (remove))))
   (search-hints-remove-all))
+
+(defparenstatic next-search-hint
+    (when (> match-count current-search)
+      (setf current-search (+ current-search 1)))
+  (let ((element (ps:chain document (get-element-by-id current-search))))
+    (ps:chain element (scroll-into-view t))))
+
+(defparenstatic previous-search-hint
+    (when (> current-search 0)
+      (setf current-search (- current-search 1)))
+  (let ((element (ps:chain document (get-element-by-id current-search))))
+    (ps:chain element (scroll-into-view t))))
