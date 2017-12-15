44f8af1f549f091e8ce81871b6b9eae1b4a0b027
John Mercouris <john@mercouris.email>
2017-12-15 18:39:12 +0100

    return all heading as json

--- next/source/jump-heading.lisp
+++ next/source/jump-heading.lisp
@@ -2,3 +2,11 @@
 
 (in-package :next)
 
+(defparenstatic get-headings
+  (defun qsa (context selector)
+    "Alias of document.querySelectorAll"
+    (ps:chain context (query-selector-all selector)))
+  (let ((headings (qsa document "h1, h2, h3, h4, h5, h6")))
+    (ps:chain -j-s-o-n (stringify
+                        (loop for heading in headings
+                           collect (ps:chain heading inner-text))))))
