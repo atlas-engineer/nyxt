38ca3d632a39fd49e8f8341b0b475a79ca3d6e1a
John Mercouris <john@mercouris.email>
2017-12-15 18:55:44 +0100

    jump to heading based on inner text implemented

--- next/source/jump-heading.lisp
+++ next/source/jump-heading.lisp
@@ -10,3 +10,12 @@
     (ps:chain -j-s-o-n (stringify
                         (loop for heading in headings
                            collect (ps:chain heading inner-text))))))
+
+(defparen jump-to-heading (heading-inner-text)
+  (defun qsa (context selector)
+    "Alias of document.querySelectorAll"
+    (ps:chain context (query-selector-all selector)))
+  (let ((headings (qsa document "h1, h2, h3, h4, h5, h6")))
+    (loop for heading in headings do
+         (when (equal (ps:lisp heading-inner-text) (ps:chain heading inner-text))
+           (ps:chain heading (scroll-into-view t))))))
