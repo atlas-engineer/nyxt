b855b773ab49b5dde3398cb537e191962f6fc1e9
Devin Lehmacher <lehmacdj@gmail.com>
2018-01-13 16:31:51 -0500

    add utility functions for getting xdg spec paths
    Because we will need these functions while defining global variables we
    need to define them here rather than in utility.lisp because otherwise
    they would be undefined in global.lisp.

--- next/source/global.lisp
+++ next/source/global.lisp
@@ -2,6 +2,14 @@
 
 (in-package :next)
 
+;; utility functions for getting paths from the xdg directory specification in
+;; a namespaced directory for next
+(defun xdg-data-home (&optional (rel ""))
+  (uiop:xdg-data-home (concatenate 'string "next/" rel)))
+
+(defun xdg-config-home (&optional (rel ""))
+  (uiop:xdg-config-home (concatenate 'string "next/" rel)))
+
 (defvar *global-map* (make-hash-table :test 'equalp)
   "A global key map, available in every mode/buffer.")
 (defvar *active-buffer* ()
