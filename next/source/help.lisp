46dfb44afa571c31c4638573a397c39f03703c12
John Mercouris <john@mercouris.email>
2018-02-05 23:14:58 +0100

    fix typo

--- next/source/help.lisp
+++ next/source/help.lisp
@@ -35,7 +35,7 @@
            (help-contents (concatenate 'string "<h1>" (symbol-name input) "</h1>"
                                        (documentation input 'variable)
                                        "<h2>Current Value:</h2>"
-                                       (write-to-string (sypmbol-value input))))
+                                       (write-to-string (symbol-value input))))
            (insert-help (ps:ps (setf (ps:@ document Body inner-H-T-M-L) (ps:lisp help-contents)))))
       (interface:web-view-execute (view help-buffer) insert-help)
       (set-visible-active-buffer help-buffer))))
