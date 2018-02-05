176900d75ff1756f9243fd1aa8334795cb7f9e8b
John Mercouris <john@mercouris.email>
2018-02-05 22:45:56 +0100

    basic hook implementation

--- next/source/hook.lisp
+++ next/source/hook.lisp
@@ -2,8 +2,21 @@
 
 (in-package :next)
 
-(defun add-hook (hook-name function function-designator))
+(defun add-function-to-hook (hook-name function-key function)
+  (let ((hook-functions-hash (alexandria:ensure-gethash
+                              hook-name
+                              *available-hooks*
+                              (make-hash-table :test #'equalp))))
+    (setf (gethash function-key hook-functions-hash) function)))
 
-(defun run-hook (hook-name))
+(defun execute-entry (key value)
+  (declare (ignore key))
+  (funcall value))
 
-(defun remove-hook ((hook-name function-designator)))
+(defun run-hook (hook-name)
+  (let ((hook-functions-hash (gethash hook-name *available-hooks*)))
+    (maphash 'execute-entry hook-functions-hash)))
+
+(defun remove-hook (hook-name function-key)
+  (let ((hook-functions-hash (gethash hook-name *available-hooks*)))
+    (remhash function-key hook-functions-hash)))
