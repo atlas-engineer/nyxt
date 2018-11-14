c56e6364712873d55f5850f30c5241e656bc36e1
Pierre Neidhardt <mail@ambrevar.xyz>
2018-11-14 15:23:54 +0100

    ports/gtk-webkit: Use libsoup instead of xmlrpc-c

--- ports/gtk-webkit/next-gtk-webkit.c
+++ ports/gtk-webkit/next-gtk-webkit.c
@@ -10,16 +10,9 @@ Use of this file is governed by the license that can be found in LICENSE.
 #include <gtk/gtk.h>
 #include <webkit2/webkit2.h>
 
-#include <xmlrpc-c/base.h>
-#include <xmlrpc-c/server.h>
-#include <xmlrpc-c/server_abyss.h>
-
-#include "message-queue.h"
+#include <libsoup/soup.h>
 
 #define APPNAME "Next"
-
-// TODO: Make local.
-GAsyncQueue *main_queue;
 
 static void destroy_window(GtkWidget *widget, GtkWidget *window) {
 	gtk_main_quit();
@@ -37,7 +30,8 @@ static char *window_make_gui() {
 	gtk_window_set_default_size(GTK_WINDOW(main_window), 800, 600);
 	// TODO: Make title customizable from Lisp.
 	gtk_window_set_title(GTK_WINDOW(main_window), APPNAME);
-	gtk_window_set_wmclass(GTK_WINDOW(w), wmstr, APPNAME);
+	// TODO: Deprecated?
+	/* gtk_window_set_wmclass(GTK_WINDOW(main_window), APPNAME, APPNAME); */
 
 	// Create a browser instance
 	WebKitWebView *web_view = WEBKIT_WEB_VIEW(webkit_web_view_new());
@@ -64,92 +58,57 @@ static char *window_make_gui() {
 	return "foo";
 }
 
-static xmlrpc_value *window_make_request(xmlrpc_env *const envP,
-	xmlrpc_value *const paramArrayP, void *const serverInfo,
-	void *const channelInfo) {
-	g_async_queue_push(main_queue, &window_make_gui);
-	// TODO: Need to trigger Glib to process the async queue, otherwise it will
-	// wait until next keyboard event, timeout, etc.
+static void server_callback(SoupServer *server, SoupMessage *msg,
+	const char *path, GHashTable *query,
+	SoupClientContext *context, gpointer data) {
+	SoupMessageHeadersIter iter;
+	const char *name, *value;
 
-	// TODO: Get operation result.
-	return xmlrpc_build_value(envP, "s", "result");
-}
+	g_debug("%s %s HTTP/1.%d", msg->method, path,
+		soup_message_get_http_version(msg));
+	soup_message_headers_iter_init(&iter, msg->request_headers);
+	while (soup_message_headers_iter_next(&iter, &name, &value)) {
+		g_debug("%s: %s", name, value);
+	}
 
-static void server_start() {
-	struct xmlrpc_method_info3 const window_make = {
-		"window.make", &window_make_request
-	};
+	if (msg->request_body->length == 0) {
+		g_warning("Empty HTTP request");
+		return;
+	}
+
+	g_debug("%s", msg->request_body->data);
+	SoupXMLRPCParams *params = NULL;
+	GError *error = NULL;
+	char *method_name = soup_xmlrpc_parse_request(msg->request_body->data,
+			msg->request_body->length,
+			&params,
+			&error);
+	if (error) {
+		g_warning("Malformed XMLRPC request: %s", error->message);
+		return;
+	}
+
+	g_debug("Method name: %s", method_name);
+
+	if (strcmp(method_name, "window.make") == 0) {
+		window_make_gui();
+	}
+
 	/*
-	struct xmlrpc_method_info3 const windowDelete = {
-	"window.delete", &window_delete,};
-	struct xmlrpc_method_info3 const windowActive = {
-	"window.active", &window_active,};
-	struct xmlrpc_method_info3 const windowSetActiveBuffer = {
-	"window.set.active.buffer", &window_set_active_buffer,};
-	struct xmlrpc_method_info3 const bufferMake = {
-	"buffer.make", &buffer_make,};
-	struct xmlrpc_method_info3 const bufferExecuteJavaScript = {
-	"buffer.execute.javascript", &buffer_execute_javascript,};
-	struct xmlrpc_method_info3 const minibufferSetHeight = {
-	"minibuffer.set.height", &minibuffer_set_height,};
-	struct xmlrpc_method_info3 const minibufferExecuteJavascript = {
-	"minibuffer.execute.javascript", &minibuffer_execute_javascript,};
+	GVariant *variant = soup_xmlrpc_params_parse(params, NULL, &error);
+	if (error) {
+	        g_warning("Malformed method parameters: %s\n", error->message);
+	}
 	*/
 
-	xmlrpc_server_abyss_parms serverparm;
-	xmlrpc_registry *registryP;
-	xmlrpc_env env;
-	xmlrpc_env_init(&env);
+	soup_xmlrpc_params_free(params);
 
-	registryP = xmlrpc_registry_new(&env);
-	if (env.fault_occurred) {
-		printf("xmlrpc_registry_new() failed: %s\n", env.fault_string);
-		exit(1);
+	soup_xmlrpc_message_set_response(msg, g_variant_new_string("foo"), &error);
+	if (error) {
+		g_warning("Failed to set XMLRPC response: %s", error->message);
 	}
 
-	xmlrpc_registry_add_method3(&env, registryP, &window_make);
-	/*
-	xmlrpc_registry_add_method3(&env, registryP, &windowDelete);
-	xmlrpc_registry_add_method3(&env, registryP, &windowActive);
-	xmlrpc_registry_add_method3(&env, registryP, &windowSetActiveBuffer);
-	xmlrpc_registry_add_method3(&env, registryP, &bufferMake);
-	xmlrpc_registry_add_method3(&env, registryP,
-	&bufferExecuteJavaScript);
-	xmlrpc_registry_add_method3(&env, registryP, &minibufferSetHeight);
-	xmlrpc_registry_add_method3(&env, registryP,
-	&minibufferExecuteJavascript);
-	*/
-
-	if (env.fault_occurred) {
-		printf("xmlrpc_registry_add_method3() failed.  %s\n",
-			env.fault_string);
-		exit(1);
-	}
-
-	serverparm.config_file_name = NULL; /* Select the modern normal API */
-	serverparm.registryP = registryP;
-	serverparm.port_number = 8082;
-	// TODO: Use .log extension?
-	serverparm.log_file_name = "/tmp/next_xmlrpc_log";
-
-	g_message("Starting xmlrpc server");
-	xmlrpc_server_abyss(&env, &serverparm, XMLRPC_APSIZE(log_file_name));
-	g_message("Stopping xmlrpc server");
-	if (env.fault_occurred) {
-		printf("xmlrpc_server_abyss() failed.  %s\n", env.fault_string);
-		exit(1);
-	}
-}
-
-typedef char * (*GuiCallback) ();
-gboolean main_queue_callback(gpointer message, gpointer user_data) {
-	g_message("Callback");
-	GuiCallback callback = (GuiCallback)message;
-	// TODO: Get return value.
-	callback();
-
-	// Return TRUE to keep the GSource alive.
-	return TRUE;
+	g_debug("Response: %d %s", msg->status_code, msg->reason_phrase);
 }
 
 int main(int argc, char *argv[]) {
@@ -158,18 +117,22 @@ int main(int argc, char *argv[]) {
 
 	// TODO: Start the xmlrpc server first?  If GUI is started, then we can
 	// report xmlrpc startup issue graphically.
-	GThread *server_thread = g_thread_new("server", (GThreadFunc) &server_start, NULL);
+	// TODO: Server logging?
+	// TODO: libsoup's examples don't unref the server.  Should we?
+	SoupServer *server = soup_server_new(
+		/* SOUP_SERVER_SERVER_HEADER, APPNAME, */
+		NULL);
 
-	main_queue = g_async_queue_new();
-	GSource *message_queue_source = message_queue_source_new(main_queue, NULL, NULL);
-	g_source_set_callback(message_queue_source, (GSourceFunc) &main_queue_callback, NULL, NULL);
-	g_source_attach(message_queue_source, NULL);
+	GError *error = NULL;
+	soup_server_listen_all(server, 8082, 0, &error);
+	if (error) {
+		g_printerr("Unable to create server: %s\n", error->message);
+		exit(1);
+	}
+	g_debug("Starting XMLRPC server");
+	soup_server_add_handler(server, NULL, server_callback, NULL, NULL);
+
 	gtk_main();
-
-	// TODO: Kill xmlrpc server cleanly.
-	// We can use xmlrpc_server_abyss_terminate:
-	// http://xmlrpc-c.sourceforge.net/doc/libxmlrpc_server_abyss.html#server_abyss_terminate
-	g_thread_join(server_thread);
 
 	return EXIT_SUCCESS;
 }
