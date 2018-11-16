54fe9bb828e7ee66b552bf9fe28c045d10ff0091
John Mercouris <john@mercouris.email>
2018-11-16 21:08:55 +0100

    check asynchronously if key was not consumed
    if the key was not consumed, dispatch it to the main thread and pass it
    a long

--- ports/cocoa-webkit/cocoa-webkit/NextApplication.m
+++ ports/cocoa-webkit/cocoa-webkit/NextApplication.m
@@ -7,24 +7,12 @@
 //
 
 #import "NextApplication.h"
-
 #include <xmlrpc-c/base.h>
 #include <xmlrpc-c/client.h>
 #include <xmlrpc-c/config.h>
-
-#define NAME "Next"
-#define VERSION "0.1"
+#include "Global.h"
 
 @implementation NextApplication
-
-static void
-dieIfFaultOccurred (xmlrpc_env * const envP) {
-    if (envP->fault_occurred) {
-        fprintf(stderr, "ERROR: %s (%d)\n",
-                envP->fault_string, envP->fault_code);
-        exit(1);
-    }
-}
 
 - (void)sendEvent:(NSEvent *)event
 {
@@ -36,18 +24,11 @@ dieIfFaultOccurred (xmlrpc_env * const envP) {
         bool commandPressed = (modifierFlags & NSEventModifierFlagCommand);
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-            xmlrpc_env env;
+            xmlrpc_env env = [[Global sharedInstance] getXMLRPCEnv];
             xmlrpc_value * resultP;
             xmlrpc_int consumed;
             const char * const serverUrl = "http://localhost:8081/RPC2";
             const char * const methodName = "PUSH-KEY-CHORD";
-            
-            // Initialize our error-handling environment.
-            xmlrpc_env_init(&env);
-            
-            // Start up our XML-RPC client library.
-            xmlrpc_client_init2(&env, XMLRPC_CLIENT_NO_FLAGS, NAME, VERSION, NULL, 0);
-            dieIfFaultOccurred(&env);
             
             // Make the remote procedure call
             resultP = xmlrpc_client_call(&env, serverUrl, methodName,
@@ -56,24 +37,14 @@ dieIfFaultOccurred (xmlrpc_env * const envP) {
                                          (xmlrpc_bool) alternatePressed,
                                          (xmlrpc_bool) commandPressed,
                                          (xmlrpc_int) characterCodePressed);
-            dieIfFaultOccurred(&env);
             
             xmlrpc_read_int(&env, resultP, &consumed);
-            dieIfFaultOccurred(&env);
-            printf("The sequence was consumed: %d\n", consumed);
-            
-            // Dispose of our result value.
-            xmlrpc_DECREF(resultP);
-            xmlrpc_env_clean(&env);
-            xmlrpc_client_cleanup();
-            
-            if (consumed == 0) {
+            if (!consumed) {
                 dispatch_sync(dispatch_get_main_queue(), ^{
                     [super sendEvent:event];
                 });
             }
         });
-        
     } else {
         [super sendEvent:event];
     }
