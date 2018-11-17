f680630554638b83c8c180f99df224e18bb9e59e
John Mercouris <john@mercouris.email>
2018-11-17 14:42:29 +0100

    help.lisp: update to work with the new API

--- source/help.lisp
+++ source/help.lisp
@@ -26,31 +26,36 @@
 
 (define-command variable-inspect ()
   "Inspect a variable and show it in a help buffer."
+  (load-package-globals)
   (with-result (input (read-from-minibuffer
                        *minibuffer*
                        :completion-function 'variable-complete
-                       :setup-function 'load-package-globals))
-    (let* ((help-buffer (generate-new-buffer
-                         (concatenate 'string "HELP-" (symbol-name input)) (document-mode)))
-           (help-contents (concatenate 'string "<h1>" (symbol-name input) "</h1>"
-                                       (documentation input 'variable)
-                                       "<h2>Current Value:</h2>"
-                                       (write-to-string (symbol-value input))))
-           (insert-help (ps:ps (setf (ps:@ document Body inner-H-T-M-L) (ps:lisp help-contents)))))
-      (buffer-evaluate-javascript *interface* (view help-buffer) insert-help)
+                       :input-prompt "Inspect variable:"))
+    (let* ((help-buffer (make-buffer
+                         (concatenate 'string "HELP-" (symbol-name input))))
+           (help-contents (cl-markup:markup
+                           (:h1 (symbol-name input))
+                           (:p (documentation input 'variable))
+                           (:h2 "Current Value:")
+                           (:p (write-to-string (symbol-value input)))))
+           (insert-help (ps:ps (setf (ps:@ document Body |innerHTML|)
+                                     (ps:lisp help-contents)))))
+      (buffer-evaluate-javascript *interface* help-buffer insert-help)
       (set-active-buffer *interface* help-buffer))))
 
 (define-command command-inspect ()
   "Inspect a function and show it in a help buffer."
   (with-result (input (read-from-minibuffer
                        *minibuffer*
+                       :input-prompt "Inspect command:"
                        :completion-function 'function-complete))
-    (let* ((help-buffer (generate-new-buffer
-                         (concatenate 'string "HELP-" input) (document-mode)))
-           (help-contents (concatenate 'string "<h1>" input "</h1>"
-                                       (documentation input 'variable)
-                                       "<h2>Documentation:</h2>"
-                                       (write-to-string (doc (gethash input *available-commands*)))))
-           (insert-help (ps:ps (setf (ps:@ document Body inner-H-T-M-L) (ps:lisp help-contents)))))
-      (buffer-evaluate-javascript *interface* (view help-buffer) insert-help)
+    (let* ((help-buffer (make-buffer
+                         (concatenate 'string "HELP-" input)))
+           (help-contents (cl-markup:markup
+                           (:h1 input)
+                           (:h2 "Documentation")
+                           (:p (write-to-string (doc (gethash input *available-commands*))))))
+           (insert-help (ps:ps (setf (ps:@ document Body |innerHTML|)
+                                     (ps:lisp help-contents)))))
+      (buffer-evaluate-javascript *interface* help-buffer insert-help)
       (set-active-buffer *interface* help-buffer))))
