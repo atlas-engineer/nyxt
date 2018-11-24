fc3576be0e8ecf9791d1ce6af29610e8a123ed5d
John Mercouris <john@mercouris.email>
2018-11-24 01:42:16 +0100

    ports/cocoa: release starts application in new thread

--- source/ports/cocoa-release.lisp
+++ source/ports/cocoa-release.lisp
@@ -4,4 +4,8 @@
   #+sbcl(loop (sb-sys:serve-all-events)))
 
 (defun run-program ()
-  (print (uiop/image:argv0)))
+  (let* ((path (uiop/image:argv0))
+                (path (cl-strings:shorten path (- (length path) 4) :truncate-string ""))
+         (path (concatenate 'string path "cocoa-webkit")))
+    (bt:make-thread
+     (lambda () (uiop:run-program path)))))
