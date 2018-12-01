952c75111920eb8b9018de7426e8883aa5aacf8c
Pierre Neidhardt <mail@ambrevar.xyz>
2018-12-01 13:10:42 +0100

    gtk-webkit.lisp: Set variables for the executable, arguments and log

--- source/ports/gtk-webkit.lisp
+++ source/ports/gtk-webkit.lisp
@@ -1,5 +1,13 @@
 (in-package :next)
 
+(defvar *gtk-webkit-command* "next-gtk-webkit"
+  "Path to the GTK-Webkit platform port executable.")
+(defvar *gtk-webkit-args* (list "--port" (write-to-string (getf *platform-port-socket* :port))
+                                "--core-socket" (format nil "http://localhost:~a/RPC2" *core-port*))
+  "Arguments to pass to the GTK-Webkit platform port executable.")
+(defvar *gtk-webkit-log* #P"/tmp/next-gtk-webkit.log"
+  "Where to store the log of the GTK-Webkit platform port.")
+
 (defmethod set-conversion-table ((port port))
   (setf (gethash "SPACE" *character-conversion-table*) " ")
   (setf (gethash "BACKSPACE" *character-conversion-table*) "")
@@ -12,7 +20,7 @@
   #+sbcl(loop (sb-sys:serve-all-events)))
 
 (defmethod run-program ((port port))
-  ;; TODO: Make path configurable from the build system.
-  (let* ((path  "next-gtk-webkit"))
-    (bt:make-thread
-     (lambda () (uiop:run-program path)))))
+  (bt:make-thread
+   (lambda ()
+     (uiop:run-program (cons *gtk-webkit-command* *gtk-webkit-args*)
+                       :output *gtk-webkit-log*))))
