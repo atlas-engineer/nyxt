b9471db9703dd9efb544de1e31a793ae8621d730
Pierre Neidhardt <mail@ambrevar.xyz>
2018-12-02 13:43:13 +0100

    gtk-webkit.lisp: Implement kill-program

--- source/ports/gtk-webkit.lisp
+++ source/ports/gtk-webkit.lisp
@@ -8,6 +8,9 @@
 (defvar *gtk-webkit-log* #P"/tmp/next-gtk-webkit.log"
   "Where to store the log of the GTK-Webkit platform port.")
 
+(defclass port ()
+  ((running-process :accessor running-process)))
+
 (defmethod set-conversion-table ((port port))
   (setf (gethash "SPACE" *character-conversion-table*) " ")
   (setf (gethash "BACKSPACE" *character-conversion-table*) "")
@@ -17,10 +20,16 @@
   (setf (gethash "ESCAPE" *character-conversion-table*) ""))
 
 (defmethod run-loop ((port port))
-  #+sbcl(loop (sb-sys:serve-all-events)))
+  (uiop:wait-process (running-process port)))
 
 (defmethod run-program ((port port))
-  (bt:make-thread
-   (lambda ()
-     (uiop:run-program (cons *gtk-webkit-command* *gtk-webkit-args*)
-                       :output *gtk-webkit-log*))))
+  (setf (running-process port)
+        (uiop:launch-program (cons *gtk-webkit-command* *gtk-webkit-args*)
+                             :output *gtk-webkit-log*)))
+
+(defmethod kill-program ((port port))
+  (uiop:run-program
+   (list "kill" "-15"
+         (write-to-string
+          (uiop/launch-program:process-info-pid
+           (running-process port))))))
