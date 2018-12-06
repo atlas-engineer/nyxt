47fdfe5380a849211e8df2b878956ece9e850e00
John Mercouris <john@mercouris.email>
2018-12-06 14:56:46 +0100

    ports/cocoa: printing a help string when flag specified

--- ports/cocoa-webkit/cocoa-webkit/main.m
+++ ports/cocoa-webkit/cocoa-webkit/main.m
@@ -6,12 +6,17 @@
 int main(int argc, const char * argv[])
 {
     NSArray *arguments = NSProcessInfo.processInfo.arguments;
-    NSLog(@"%@", arguments);
+//  NSLog(@"%@", arguments);
+
+    if ([arguments containsObject:@"-h"] || [arguments containsObject:@"--help"])
+    {
+        printf("%s\n", [@"Help String!" UTF8String]);
+    }
 
     NextApplication *app = [NextApplication sharedApplication];
     [app setDelegate:[[NextApplicationDelegate alloc] init]];
     [app setActivationPolicy:NSApplicationActivationPolicyRegular];
     [app run];
-    
+
     return 0;
 }
