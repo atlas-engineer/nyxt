53248e4735e26804e039a58b3c06c5eecef2c0cd
John Mercouris <john@mercouris.email>
2018-12-07 23:27:58 +0100

    simplify command line argument parsing

--- ports/cocoa-webkit/cocoa-webkit/main.m
+++ ports/cocoa-webkit/cocoa-webkit/main.m
@@ -3,44 +3,39 @@
 #import "NextApplicationDelegate.h"
 #import "NextApplication.h"
 #import "Global.h"
+#include "argparse.h"
+
+static const char *const usages[] = {
+    "cocoa-webkit [options...]",
+    NULL,
+};
 
 int main(int argc, const char * argv[])
 {
+    int port = 8082;
+    const char *coreSocket = "http://localhost:8081/RPC2";
+    
+    struct argparse_option options[] = {
+        OPT_HELP(),
+        OPT_GROUP("Basic options"),
+        OPT_INTEGER('p', "port", &port, "selected integer", NULL, 0, 0),
+        OPT_STRING('s', "core-socket", &coreSocket, "XML RPC Address of Lisp Core", NULL, 0, 0),
+        OPT_END(),
+    };
+    
+    struct argparse argparse;
+    argparse_init(&argparse, options, usages, 0);
+    argparse_describe(&argparse,
+                      "\ncoca-Webkit is the platform front end for the Lisp core.",NULL);
+    argc = argparse_parse(&argparse, argc, argv);
+    if (coreSocket != NULL)
+        printf("path: %s\n", coreSocket);
+    if (port != 0)
+        printf("int_num: %d\n", port);
+    
     Global *global = [Global sharedInstance];
-    NSArray *arguments = NSProcessInfo.processInfo.arguments;
-
-    if ([arguments containsObject:@"-h"] || [arguments containsObject:@"--help"])
-    {
-        printf("%s\n", [@"Usage:"
-                         "\n cocoa-webkit [OPTION...]"
-                         "\n Help Options:"
-                         "\n   -h, --help                                       Show help options"
-                         "\n "
-                         "\n Application Options:"
-                         "\n  -p, --port=8082                                  Port the XML-RPC server listens to"
-                         "\n  -s, --core-socket=http://localhost:8081/RPC2     Socket of the Lisp core"
-                        UTF8String]);
-    }
-    if ([arguments containsObject:@"-p"])
-    {
-        [global setPort:[arguments objectAtIndex: 1 + [arguments indexOfObject:@"-p"]]];
-        NSLog(@"%@", [global port]);
-    }
-    if ([arguments containsObject:@"--port"])
-    {
-        [global setPort:[arguments objectAtIndex: 1 + [arguments indexOfObject:@"--port"]]];
-        NSLog(@"%@", [global port]);
-    }
-    if ([arguments containsObject:@"-s"])
-    {
-        [global setCoreSocket:[arguments objectAtIndex: 1 + [arguments indexOfObject:@"-s"]]];
-        NSLog(@"%@", [global coreSocket]);
-    }
-    if ([arguments containsObject:@"--core-socket"])
-    {
-        [global setCoreSocket:[arguments objectAtIndex: 1 + [arguments indexOfObject:@"--core-socket"]]];
-        NSLog(@"%@", [global coreSocket]);
-    }
+    [global setCoreSocket:[NSString stringWithUTF8String:coreSocket]];
+    [global setPort:[@(port) stringValue]];
     
     NSLog(@"Starting server with: Port: %@ Core Socket: %@", [global port], [global coreSocket]);
     
