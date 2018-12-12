c454eac5fab10c61f2c4c6e759e2ef3a752200a0
John Mercouris <john@mercouris.email>
2018-12-12 15:35:37 +0100

    ports/cocoa: returning results for operations

--- ports/cocoa-webkit/CocoaWebkit/Server.m
+++ ports/cocoa-webkit/CocoaWebkit/Server.m
@@ -27,9 +27,7 @@
                  __block NSString* operationResult = @"";
                  dispatch_sync(dispatch_get_main_queue(), ^{
                      NextApplicationDelegate *delegate = [NSApp delegate];
-                     operationResult = [delegate windowMake];
-                     NSLog(@"Operation Result: %@", operationResult);
-                 });
+                     operationResult = [delegate windowMake];});
                  responseEncoder = [[XMLRPCResponseEncoder alloc]
                                     initWithParameters:@[operationResult]];
                  return [GCDWebServerDataResponse responseWithText:[responseEncoder body]];
@@ -38,75 +36,70 @@
                  dispatch_sync(dispatch_get_main_queue(), ^{
                      NextApplicationDelegate *delegate = [NSApp delegate];
                      [delegate window: [parameters objectAtIndex:0]
-                             setTitle: [parameters objectAtIndex:1]];
-                 });
+                             setTitle: [parameters objectAtIndex:1]];});
+                 responseEncoder = [[XMLRPCResponseEncoder alloc]
+                                    initWithParameters:@[@YES]];
+                 return [GCDWebServerDataResponse responseWithText:[responseEncoder body]];
              }
              else if ([method isEqualToString:@"window.delete"]) {
                  dispatch_sync(dispatch_get_main_queue(), ^{
                      NextApplicationDelegate *delegate = [NSApp delegate];
-                     [delegate windowDelete: [parameters objectAtIndex:0]];
-                 });
+                     [delegate windowDelete: [parameters objectAtIndex:0]];});
              }
              else if ([method isEqualToString:@"window.active"]) {
                  __block NSString* operationResult = @"";
                  dispatch_sync(dispatch_get_main_queue(), ^{
                      NextApplicationDelegate *delegate = [NSApp delegate];
-                     operationResult = [delegate windowActive];
-                 });
+                     operationResult = [delegate windowActive];});
              }
              else if ([method isEqualToString:@"window.exists"]) {
                  __block bool operationResult;
                  dispatch_sync(dispatch_get_main_queue(), ^{
                      NextApplicationDelegate *delegate = [NSApp delegate];
-                     operationResult = [delegate windowExists: [parameters objectAtIndex:0]];
-                 });
+                     operationResult = [delegate windowExists: [parameters objectAtIndex:0]];});
              }
              else if ([method isEqualToString:@"window.set.active.buffer"]) {
                  dispatch_sync(dispatch_get_main_queue(), ^{
                      NextApplicationDelegate *delegate = [NSApp delegate];
                      [delegate setActiveBufferForWindow: [parameters objectAtIndex:0]
-                                                 buffer: [parameters objectAtIndex:1]];
-                 });
+                                                 buffer: [parameters objectAtIndex:1]];});
              }
              else if ([method isEqualToString:@"window.set.minibuffer.height"]) {
                  __block NSInteger operationResult = 0;
                  dispatch_sync(dispatch_get_main_queue(), ^{
                      NextApplicationDelegate *delegate = [NSApp delegate];
                      operationResult = [delegate setMinibufferHeightForWindow: [parameters objectAtIndex:0]
-                                                                       height: [parameters objectAtIndex:1]];
-                 });
+                                                                       height: [parameters objectAtIndex:1]];});
              }
              else if ([method isEqualToString:@"buffer.make"]) {
                  __block NSString* operationResult = @"";
                  dispatch_sync(dispatch_get_main_queue(), ^{
                      NextApplicationDelegate *delegate = [NSApp delegate];
-                     operationResult = [delegate bufferMake];
-                 });
+                     operationResult = [delegate bufferMake];});
              }
              else if ([method isEqualToString:@"buffer.delete"]) {
                  dispatch_sync(dispatch_get_main_queue(), ^{
                      NextApplicationDelegate *delegate = [NSApp delegate];
-                     [delegate bufferDelete: [parameters objectAtIndex:0]];
-                 });
+                     [delegate bufferDelete: [parameters objectAtIndex:0]];});
              }
              else if ([method isEqualToString:@"buffer.evaluate.javascript"]) {
                  __block NSString* operationResult = @"";
                  dispatch_sync(dispatch_get_main_queue(), ^{
                      NextApplicationDelegate *delegate = [NSApp delegate];
                      operationResult = [delegate bufferEvaluateJavaScript:[parameters objectAtIndex:0]
-                                                               javaScript:[parameters objectAtIndex:1]];
-                 });
+                                                               javaScript:[parameters objectAtIndex:1]];});
              }
              else if ([method isEqualToString:@"minibuffer.evaluate.javascript"]) {
                  __block NSString* operationResult = @"";
                  dispatch_sync(dispatch_get_main_queue(), ^{
                      NextApplicationDelegate *delegate = [NSApp delegate];
                      operationResult = [delegate minibufferEvaluateJavaScript:[parameters objectAtIndex:0]
-                                                                   javaScript:[parameters objectAtIndex:1]];
-                 });
+                                                                   javaScript:[parameters objectAtIndex:1]];});
              }
              
-             return [GCDWebServerDataResponse responseWithText:@"<xml></xml>"];
+             responseEncoder = [[XMLRPCResponseEncoder alloc]
+                                initWithParameters:@[@"Unsupported operation!"]];
+             return [GCDWebServerDataResponse responseWithText:[responseEncoder body]];
          }];
     }
     return self;
