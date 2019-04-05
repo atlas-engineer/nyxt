e3c878bae7573f6bdae80b325fbd995a68a24304
John Mercouris <john@mercouris.email>
2019-04-05 13:45:20 +0200

    cocoa-webkit.lisp: remove uneeded functions

--- source/ports/cocoa-webkit.lisp
+++ source/ports/cocoa-webkit.lisp
@@ -1,31 +1,12 @@
 (in-package :next)
 
-(defclass port ()
-  ((running-process :accessor running-process)))
-
-(defmethod set-conversion-table ((port port))
-  (setf (gethash "SPACE" *character-conversion-table*) " ")
-  (setf (gethash "BACKSPACE" *character-conversion-table*) "")
-  (setf (gethash "RETURN" *character-conversion-table*) "")
-  (setf (gethash "HYPHEN" *character-conversion-table*) "-")
-  (setf (gethash "ESCAPE" *character-conversion-table*) "")
-  (setf (gethash "TAB" *character-conversion-table*) "	"))
-
-(defmethod run-loop ((port port))
-  (uiop:wait-process (running-process port)))
+(setf *port* (make-instance 'port))
 
 (defmethod run-program ((port port))
   (let* ((path (uiop/image:argv0))
                 (path (cl-strings:shorten path (- (length path) 4) :truncate-string ""))
          (path (concatenate 'string path "cocoa-webkit")))
     (setf (running-process port) (uiop:launch-program path))))
-
-(defmethod kill-program ((port port))
-  (uiop:run-program
-   (list "kill" "-15"
-         (write-to-string
-          (uiop/launch-program:process-info-pid
-           (running-process port))))))
 
 (define-key *global-map* (key "S-t") 'make-visible-new-buffer)
 (define-key *global-map* (key "S-n") 'make-window)
