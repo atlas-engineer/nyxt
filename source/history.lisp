44703de3a531127f767d68797fac851d21bf91ca
Pierre Neidhardt <mail@ambrevar.xyz>
2019-05-07 16:17:06 +0200

    Fix history database update of "visits" column

--- source/history.lisp
+++ source/history.lisp
@@ -3,40 +3,40 @@
 (in-package :next)
 
 (defun %initialize-history-db (path)
-  "Create a database file if necessary and make a table for bookmarks"
-  (if (probe-file path)
-      (let ((db (sqlite:connect
-                 (truename (probe-file path)))))
-        ;; Make sure new fields exist.
-        (unless (string= "visits" (cadar (last (sqlite:execute-to-list db "pragma table_info(typed)"))))
-          (sqlite:execute-non-query db "alter table typed add visits integer default 1")))
-      (progn
-        (close (open (ensure-parent-exists path)
-                     :direction :probe :if-does-not-exist :create))
-        (let ((db (sqlite:connect
-                   (truename (probe-file path)))))
-          (sqlite:execute-non-query
-           db "create table history (id integer primary key, url text not null)")
-          (sqlite:execute-non-query
-           db "create table typed (id integer primary key, url text not null, visits integer default 1, unique (url) on conflict replace)")
-          (sqlite:execute-non-query
-           db "insert into history (url) values (?)" "about:blank")
-          (sqlite:execute-non-query
-           db "insert into typed (url) values (?)" "about:blank")
-          (sqlite:disconnect db)))))
+  "Create a database file if necessary and make a table for bookmarks."
+  (close (open (ensure-parent-exists path)
+               :direction :probe :if-does-not-exist :create))
+  (let ((db (sqlite:connect
+             (truename (probe-file path)))))
+    (sqlite:execute-non-query
+     db "create table history (id integer primary key, url text not null)")
+    (sqlite:execute-non-query
+     db "create table typed (id integer primary key, url text not null, visits integer default 1, unique (url) on conflict replace)")
+    (sqlite:execute-non-query
+     db "insert into history (url) values (?)" "about:blank")
+    (sqlite:execute-non-query
+     db "insert into typed (url) values (?)" "about:blank")
+    (sqlite:disconnect db)))
 
 (defun ensure-history-db ()
-  "Returns the pathname of the history database"
-  (ensure-file-exists
-   (anaphora:aif (%%window-active *interface*)
-                 (history-db-path anaphora:it)
-                 ;; This additional window fallback should not be necessary
-                 ;; anymore now that `%%window-make' sets `last-active-window'
-                 ;; so that `%%window-active' always returns a result.
-                 (some (lambda (window)
-                         (history-db-path window))
-                       (alexandria:hash-table-values (windows *interface*))))
-   #'%initialize-history-db))
+  "Return the pathname of the history database."
+  (let ((path (anaphora:aif (%%window-active *interface*)
+                            (history-db-path anaphora:it)
+                            ;; This additional window fallback should not be necessary
+                            ;; anymore now that `%%window-make' sets `last-active-window'
+                            ;; so that `%%window-active' always returns a result.
+                            (some (lambda (window)
+                                    (history-db-path window))
+                                  (alexandria:hash-table-values (windows *interface*))))))
+    (if (probe-file path)
+        (let ((db (sqlite:connect
+                   (truename (probe-file path)))))
+          ;; Make sure new fields exist.
+          (unless (string= "visits" (cadar (last (sqlite:execute-to-list db "pragma table_info(typed)"))))
+            (sqlite:execute-non-query db "alter table typed add visits integer default 1")
+            (sqlite:disconnect db))
+          path)
+        (ensure-file-exists path #'%initialize-history-db))))
 
 (defun history-add (url)
   (let ((db (sqlite:connect (ensure-history-db))))
