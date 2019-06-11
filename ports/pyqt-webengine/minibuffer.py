a3ec53c8ae3a70c87b6d167ef409e397ea0125e1
John Mercouris <john@mercouris.email>
2019-06-11 17:51:08 +0200

    pyqt-webengine:minibuffer.py: expand upon evaluate javascript

--- ports/pyqt-webengine/minibuffer.py
+++ ports/pyqt-webengine/minibuffer.py
@@ -6,6 +6,7 @@ class MiniBuffer():
 
     """
     view = None
+    scripts = {}
 
     def __init__(self):
         super(MiniBuffer, self).__init__()
@@ -13,6 +14,10 @@ class MiniBuffer():
         self.set_html("<style>body{padding:0;margin:0;}</style>Unset.")
 
     def evaluate_javascript(self, script):
+        # This method should return an identifier to the LISP
+        # core. Upon completion of the javascript script, the platform
+        # port will make a call to the LISP core with the results of
+        # that computation, and the associated identifier.
         self.view.page().runJavaScript(script)
 
     def set_height(self, height):
