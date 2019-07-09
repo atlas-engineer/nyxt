c4f7d4e1bbf2d8742bffa54d02c0a1127c8b1ac8
John Mercouris <john@mercouris.email>
2019-07-09 16:18:35 +0200

    buffers.py: capture callback in a closure

--- ports/pyqt-webengine/buffers.py
+++ ports/pyqt-webengine/buffers.py
@@ -1,6 +1,5 @@
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
-import dbus
 import logging
 
 import core_interface
@@ -9,27 +8,13 @@ import core_interface
 #  buffer (Buffer).
 BUFFERS = {}
 
+
 def get_buffer(identifier):
     buffer = BUFFERS.get(identifier)
     if buffer:
         return buffer
     else:
         raise Exception("Buffer ID: {} not found!".format(identifier))
-
-
-class BufferInfo():
-    # use another structure than Buffer for these settings to prevent
-    # race conditions (see buffer.h).
-    buffer = None
-    callback_id = 0
-
-    def __init__(self, buffer, callback_id=0):
-        """
-        - buffer: buffer object
-        - callback_id: int
-        """
-        self.buffer = buffer
-        self.callback_id = callback_id
 
 
 class Buffer():
@@ -40,13 +25,11 @@ class Buffer():
     scripts = {}
     identifier = "0"
     callback_count = 0
-    buffer_info = None
 
     def __init__(self, identifier=None):
         super(Buffer, self).__init__()
         self.view = QWebEngineView()
         self.identifier = identifier
-        self.buffer_info = BufferInfo(self)
 
     def evaluate_javascript(self, script):
         """
@@ -57,24 +40,24 @@ class Buffer():
 
         Return: a callback_id (str).
         """
-        self.buffer_info.callback_id = self.callback_count + 1
+        self.callback_count += 1
 
         # doc: "When the script has been executed, resultCallback is called
         # with the result of the last executed statement".
         logging.info("evaluate_javascript: {}".format(script))
-        self.view.page().runJavaScript(script, self.javascript_callback)
-        return str(self.buffer_info.callback_id)
+        self.view.page().runJavaScript(
+            script,
+            lambda x: self.javascript_callback(x, str(self.callback_count)))
+        return str(self.callback_count)
 
-    def javascript_callback(self, res):
+    def javascript_callback(self, res, callback_id):
         logging.debug("JS result is: {}".format(res))
         if res is None:
             return
 
         logging.info("calling buffer_javascript_call_back with id '{}', callback id '{}'".format(
-            self.identifier, self.buffer_info.callback_id))
-        core_interface.buffer_javascript_call_back(str(self.identifier),
-                                                  res,
-                                                  str(self.buffer_info.callback_id))
+            self.identifier, callback_id))
+        core_interface.buffer_javascript_call_back(str(self.identifier), res, callback_id)
 
     def set_height(self, height):
         self.view.setFixedHeight(height)
