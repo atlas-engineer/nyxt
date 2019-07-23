892cfc66bde7a1ad31c1c4ebc6c88ed9ee041f9b
John Mercouris <john@mercouris.email>
2019-07-23 12:52:41 +0200

    remote.lisp: remove deprecated comment

--- source/remote.lisp
+++ source/remote.lisp
@@ -273,7 +273,6 @@ For an array of string, that would be \"as\"."
 (defmethod %rpc-send ((interface remote-interface) (method string) &rest args)
   ;; TODO: Make %rpc-send asynchronous?
   ;; If the platform port ever hangs, the next %rpc-send will hang the Lisp core too.
-  ;; Always ensure we send the auth token as the first argument
   ;; TODO: Catch connection errors and execution errors.
   (dbus:with-open-bus (bus (session-server-addresses))
     (dbus:with-introspected-object (platform-port bus +platform-port-object-path+ +platform-port-name+)
