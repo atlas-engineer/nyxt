0d19c1c5336fb8a53916729f29fb656ec28ea895
John Mercouris <john@mercouris.email>
2019-07-26 17:24:08 +0200

    pyqt-webengine.lisp: defmethod path to find the platform port in an app bundle
    this will look for the platform port within the Next.app

--- source/ports/pyqt-webengine.lisp
+++ source/ports/pyqt-webengine.lisp
@@ -1,3 +1,10 @@
 (in-package :next)
 
 (setf (get-default 'port 'name) "next-pyqt-webengine")
+
+(defmethod path ((port port))
+  "Calculate the path of the port relative to the application bundle."
+  (let* ((path (uiop/image:argv0))
+         (path (cl-strings:shorten path (- (length path) 4) :truncate-string ""))
+         (path (concatenate 'string path "next-pyqt-webengine")))
+    path))
