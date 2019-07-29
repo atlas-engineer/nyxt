0850a01fbebbecb5befc7b41e4c564692dbb99a2
vindarel <vindarel@mailz.org>
2019-07-29 22:27:29 +0200

    Improve fuzzy search: order not important, consider input words
    Major improvement: the order of input words is not important:
    
    M-x "swit buf" gives the same results as "buf swit"
    
    Candidates are sorted by "best matching" with the levenshtein
    distance (not helping much ?).
    
    Candidates that start with the first written word come first:
    
    M-x "de" => destructor, delete-window, delete-buffer, help-mode, …
    
    HOWEVER
    
    unlike the previous method, the search terms currently must be
    separated by a space to match:
    
    M-x "switbuf" doesn't return "switch-buffer".

--- next.asd
+++ next.asd
@@ -11,6 +11,7 @@
                :cl-strings
                :str
                :cl-string-match
+               :mk-string-metrics
                :quri
                :sqlite
                :parenscript
---
+++ tests/test-utility.lisp
@@ -0,0 +1,64 @@
+(defpackage :next.tests
+  (:use :common-lisp
+        :next
+        :prove))
+
+(in-package :next.tests)
+
+(plan nil)
+
+(setf next::*interface* (make-instance 'next::remote-interface))
+
+(defparameter *candidates* '("link-hints" "active-history-node" "history-backwards"
+                             "did-finish-navigation" "history-forwards"
+                             "history-forwards-query" "copy-title" "did-commit-navigation"
+                             "copy-url" "add-or-traverse-history" "set-url-new-buffer"
+                             "noscript-mode" "help" "jump-to-heading" "next-search-hint"
+                             "bookmark-current-page" "new-buffer" "command-inspect"
+                             "add-search-hints" "kill" "remove-search-hints" "load-file"
+                             "keymap" "next-version" "name" "scroll-left" "activate"
+                             "scroll-page-down" "scroll-right" "destructor"
+                             "scroll-to-bottom" "switch-buffer-next" "command-evaluate"
+                             "did-finish-navigation" "bookmark-anchor" "scroll-down"
+                             "scroll-up" "vi-button1" "reload-current-buffer"
+                             "copy-anchor-url" "bookmark-delete" "go-anchor-new-buffer"
+                             "zoom-out-page" "keymap-schemes" "buffer" "new-window"
+                             "execute-command" "make-visible-new-buffer" "download-url"
+                             "switch-buffer" "application-mode" "delete-buffer"
+                             "start-swank" "did-commit-navigation" "delete-window"
+                             "bookmark-url" "unzoom-page" "load-init-file"
+                             "download-anchor-url" "zoom-in-page" "document-mode"
+                             "scroll-to-top" "vi-insert-mode" "help-mode" "vi-normal-mode"
+                             "minibuffer-mode" "proxy-mode" "blocker-mode"
+                             "delete-current-buffer" "scroll-page-up"
+                             "set-url-from-bookmark" "switch-buffer-previous"
+                             "download-list" "download-mode" "set-url-current-buffer"
+                             "about" "variable-inspect" "go-anchor" "previous-search-hint"
+                             "go-anchor-new-buffer-focus")
+  "existing next commands.")
+
+(subtest "Fuzzy match"
+  (is "help" (first (fuzzy-match "hel"
+                                 '("help-mode" "help" "foo-help" "help-foo-bar"))))
+  (is "help" (first (fuzzy-match "hel"
+                                 *candidates*))
+      "match 'help' with real candidates list")
+  (is "switch-buffer" (first (fuzzy-match "swit buf"
+                                          '("about" "switch-buffer-next" "switch-buffer"
+                                            "delete-buffer")))
+      "match 'swit buf' (small list)")
+  (is "switch-buffer" (first (fuzzy-match "swit buf"
+                                          *candidates*))
+      "match 'swit buf' with real candidates list")
+  (is "switch-buffer" (first (fuzzy-match "buf swit"
+                                          '("about" "switch-buffer-next" "switch-buffer"
+                                            "delete-buffer")))
+      "reverse match 'buf swit' (small list)")
+  (is "switch-buffer" (first (fuzzy-match "buf swit"
+                                          *candidates*))
+      "reverse match 'buf swit' with real candidates list")
+
+  (is "delete-foo" (first (fuzzy-match "de"
+                                       '("some-mode" "delete-foo")))
+      "candidates beginning with the first word appear first")
+  )
