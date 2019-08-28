be7c96ade3ce80542ff6963326bf0e4d5de5530b
Solomon Bloch <solomonbloch@gmail.com>
2019-08-28 12:02:43 -0400

    Fix implementation of RING-INSERT to work correctly.

--- source/ring.lisp
+++ source/ring.lisp
@@ -6,25 +6,43 @@
           :initform (make-array 1000 :initial-element nil)
           :type array)
    (item-count :accessor item-count
-               :initform 0)))
+               :initform 0)
+   (head-index :accessor head-index
+               :initform 0
+               :documentation "Index of oldest item.")))
 
 (defmethod ring-size ((ring ring))
   "Return the maximum number of elements it can contain."
   (length (items ring)))
 
+(defmethod +1-ring-index ((ring ring) index)
+  "Add one to index, with wraparound."
+  (let ((new-index (1+ index)))
+    (if (eq new-index (length (items ring)))
+        0 new-index)))
+
+(defmethod -1-ring-index ((ring ring) index)
+  "Minus one from index, with wraparound."
+  (let ((new-index (1- index)))
+    (if (eq new-index -1)
+        (1- (length (items ring))) new-index)))
+
 (defmethod ring-index ((ring ring) index)
   "Convert to internal ring index, where items are ordered from newest to oldest."
-  (mod (1- (- (item-count ring) index))
+  (mod (1- (+ (head-index ring)
+              (- (item-count ring) index)))
        (ring-size ring)))
 
 (defmethod ring-insert ((ring ring) new-item)
   "Insert item into RING.
 If RING is full, replace the oldest item."
-  (setf (aref (items ring)
-              (mod (item-count ring)
-                   (ring-size ring)))
-        new-item)
-  (incf (item-count ring)))
+  (prog1 (setf (aref (items ring)
+               (mod (+ (head-index ring) (item-count ring))
+                    (ring-size ring)))
+               new-item)
+    (if (= (item-count ring) (length (items ring)))
+        (setf (head-index ring) (+1-ring-index ring (head-index ring)))
+        (incf (item-count ring)))))
 
 (defmethod ring-ref ((ring ring) index)
   "Return value from items by INDEX where 0 INDEX is most recent."
@@ -34,7 +52,7 @@ If RING is full, replace the oldest item."
   "Return list of items ordered by most recent."
   (loop for index from 0 below (ring-size ring)
         for item = (ring-ref ring index)
-        if (not (null item)) collect item))
+        unless (null item) collect item))
 
 (defmethod ring-insert-clipboard ((ring ring))
   "Check if clipboard-content is most recent entry in RING.
