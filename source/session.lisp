9b883aaa5c1c152c5d0a7f79b15aa50b5a60f54c
Pierre Neidhardt <mail@ambrevar.xyz>
2019-09-16 15:33:01 +0200

    Include version number in session.

--- source/session.lisp
+++ source/session.lisp
@@ -9,14 +9,15 @@
   "Return the data that needs to be serialized.
 This data can be used to restore the session later, e.g. when starting a new
 instance of Next."
-  (delete-if #'null (mapcar #'buffer-history
-                            (alexandria:hash-table-values (buffers *interface*)))))
+  ;; TODO: What should the session root be?  A hash-table?  A plist?
+  ;; For now, it's just a simple list where the first element is the version.
+  ;; Maybe not future-proof, but it's unclear that session structure will ever
+  ;; change much.
+  (list +version+
+        (delete-if #'null (mapcar #'buffer-history
+                                  (alexandria:hash-table-values (buffers *interface*))))))
 
-;; TODO: How can we identify dead buffers?  Maybe with a nil ID?  Or maybe a
-;; dead-buffer is just a buffer history.
-;; TODO: Use dead buffers for undo.
-
-;; TODO: Include version number in session and warn when not matching.
+;; TODO: Pretty-print the session.
 ;; TODO: Make sure URLs are persisted when set from C-l.
 (defun store-sexp-session ()
   "Store the current Next session to the last window's `session-path'.
@@ -35,27 +36,33 @@ Currently we store the list of current URLs of all buffers."
 (defun restore-sexp-session ()
   "Store the current Next session to the last window `session-path'.
 Currently we store the list of current URLs of all buffers."
-  (let ((buffer-histories
-         (with-open-file (file (session-path (last-active-window *interface*))
-                               :direction :input
-                               :if-does-not-exist nil)
-           (when file
-             (s-serialization:deserialize-sexp
-              file)))))
-    (when buffer-histories
-      (log:info "Restoring ~a" (mapcar #'next/document-mode:node-data buffer-histories))
-      ;; Delete the old buffers?
-      ;; (maphash (lambda (id buffer)
-      ;;            (declare (ignore id))
-      ;;            (rpc-buffer-delete *interface* buffer))
-      ;;          buffers)
-      ;; Make the new ones.
-      ;; TODO: Add function `make-buffer-from-history' to document-mode?
-      (loop for history in buffer-histories
-            for buffer = (make-buffer)
-            for mode = (find-mode buffer 'document-mode)
-            do (set-url (next/document-mode:node-data history) :buffer buffer)
-            do (setf (next/document-mode:active-history-node mode) history))
-      ;; TODO: Switch to the last active buffer.  We probably need to serialize *interface*.
-      ;; Or else we could include `access-time' in the buffer class.
-      )))
+  (let ((session-path (session-path *interface*)))
+    (handler-case
+        (match (with-open-file (file session-path
+                                     :direction :input
+                                     :if-does-not-exist nil)
+                 (when file
+                   (s-serialization:deserialize-sexp
+                    file)))
+          ((list version buffer-histories)
+           (unless (string= version +version+)
+             (log:warn "Session version ~s differs from current version ~s" version +version+))
+           (when buffer-histories
+             (log:info "Restoring ~a" (mapcar #'next/document-mode:node-data buffer-histories))
+             ;; Delete the old buffers?
+             ;; (maphash (lambda (id buffer)
+             ;;            (declare (ignore id))
+             ;;            (rpc-buffer-delete *interface* buffer))
+             ;;          buffers)
+             ;; Make the new ones.
+             ;; TODO: Replace the loop with a function `make-buffer-from-history' in document-mode?
+             (loop for history in buffer-histories
+                   for buffer = (make-buffer)
+                   for mode = (find-mode buffer 'document-mode)
+                   do (set-url (next/document-mode:node-data history) :buffer buffer)
+                   do (setf (next/document-mode:active-history-node mode) history))
+             ;; TODO: Switch to the last active buffer.  We probably need to serialize *interface*.
+             ;; Or else we could include `access-time' in the buffer class.
+             )))
+      (error (c)
+        (log:warn "Failed to restore session from ~a: ~a" session-path c)))))
