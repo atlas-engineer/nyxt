1df811d0927af3e9c7906e20f952592215fc561b
Pierre Neidhardt <mail@ambrevar.xyz>
2019-09-25 15:10:55 +0200

    About: Include list of contributors.

--- source/about.lisp
+++ source/about.lisp
@@ -7,6 +7,12 @@
                   :modes (cons 'help-mode
                                (get-default 'buffer 'default-modes))))
          (contents (cl-markup:markup
+                    (:h1 "Contributors")
+                    (:p "Let us know if you've contributed to the development of
+                    Next and would like to be included on this list.")
+                    (:ul
+                     (:li "Adom Hartell (@4t0m)")
+                     (:li "Solomon Bloch (@noogie13)"))
                     (:h1 "Crowdfunding backers")
                     (:p "Thank you to all who have supported and made Next possible!")
                     (:h2 "2018-11 campaign: *NIX Support")
