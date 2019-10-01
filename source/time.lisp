94896441629b4d572abdb26daf4f8a0b5b08367e
Pierre Neidhardt <mail@ambrevar.xyz>
2019-10-01 18:47:07 +0200

    Fix type inference with asctime->timestamp.

--- source/time.lisp
+++ source/time.lisp
@@ -7,18 +7,21 @@
 (defun asctime->timestamp (asc-timestring)
   "Convert ASC-TIMESTRING to a timestamp.
 An ASC-TIMESTRING is in the form of `Tue Oct 1 15:55:09 2019'."
-  (handler-case
-      (let* ((timestring (str:split " " asc-timestring :omit-nulls t))
-             (time (str:split ":" (nth 3 timestring)))
-             (second (parse-integer (nth 2 time)))
-             (minute (parse-integer (nth 1 time)))
-             (hour (parse-integer (nth 0 time)))
-             (day (parse-integer (nth 2 timestring)))
-             (month (position (nth 1 timestring) local-time:+short-month-names+
-                              :test #'string-equal))
-             (year (parse-integer (nth 4 timestring))))
-        (local-time:universal-to-timestamp
-         (encode-universal-time second minute hour day month year)))
-    (error (c)
-      (log:debug "Error creating timestamp: ~a" c)
-      nil)))
+  (let ((timestamp nil))
+    (handler-case
+        (let* ((timestring (str:split " " asc-timestring :omit-nulls t))
+               (time (str:split ":" (nth 3 timestring)))
+               (second (parse-integer (nth 2 time)))
+               (minute (parse-integer (nth 1 time)))
+               (hour (parse-integer (nth 0 time)))
+               (day (parse-integer (nth 2 timestring)))
+               (month (position (nth 1 timestring) local-time:+short-month-names+
+                                :test #'string-equal))
+               (year (parse-integer (nth 4 timestring))))
+          (setf timestamp
+                (local-time:universal-to-timestamp
+                 (encode-universal-time second minute hour day month year))))
+      (error (c)
+        (log:debug "Error creating timestamp: ~a" c)
+        nil))
+    timestamp))
