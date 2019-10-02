85f3a5cc89a14715afdc35d7b4ac59d3014974db
Pierre Neidhardt <mail@ambrevar.xyz>
2019-10-02 12:30:28 +0200

    Sort candidates by the position of their exact substring matches.
    For instance, "sw" now returns "switch-buffer" first instead of (copy-password,
    save-new-password, start-swank, switch-buffer).

--- source/fuzzy.lisp
+++ source/fuzzy.lisp
@@ -32,6 +32,22 @@ Only substrings of 3 characters or more are considered."
            (length long-substrings))
         0)))
 
+(defun exact-match-norm (candidate input)
+  "Average of the normalized position of all the exact matches in input substrings.
+If 0 exact matches, return 0."
+  (let* ((exactly-matching-substrings (find-exactly-matching-substrings
+                                       input
+                                       (list candidate))))
+    (if exactly-matching-substrings
+        (let ((positions (mapcar (lambda (p) (- (length candidate) p))
+                                 (delete-if #'null
+                                            (mapcar (lambda (substring)
+                                                      (search substring candidate))
+                                                    exactly-matching-substrings)))))
+          (/ (apply #'+ positions)
+                  (* (length positions) (length candidate))))
+        0)))
+
 (defun to-unicode (input)
   "Convert INPUT to (simple-array character) type."
   (if (typep input 'base-string)
@@ -44,8 +60,9 @@ A higher score means the candidate comes first."
   ;; The Jaccard metric seems to provide much better results than, say,
   ;; Damerau-Levensthein.
   ;; TODO: Check out fzf for a possibly good scoring algorithm.
-  (+ (* 1.0 (mk-string-metrics:jaccard input candidate))
-     (* 1.0 (substring-norm candidate (str:split " " input)))))
+  (+ (* 1.0 (mk-string-metrics:jaccard candidate input))
+     (* 1.0 (substring-norm candidate (str:split " " input)))
+     (* 1.0 (exact-match-norm candidate input))))
 
 (defun sort-candidates (input candidate-pairs)
   "Sort CANDIDATE-PAIRS, the pair closest to INPUT in the levenshtein distance comes first.
@@ -62,24 +79,52 @@ more details."
           (> (score-candidate input (first x))
              (score-candidate input (first y))))))
 
+(defun find-exactly-matching-substrings (input candidates)
+  "Return the list of input substrings that match at least one candidate."
+  (let ((input-strings (str:split " " input :omit-nulls t)))
+    (delete-duplicates
+     (loop for candidate in candidates
+           append (remove-if
+                   (lambda (i)
+                     (not (search i candidate)))
+                   input-strings))
+     :test #'string=)))
+
 (defun keep-exact-matches-in-candidates (input candidate-pairs)
   "Destructively filter out non-exact matches from candidates.
 If any input substring matches exactly (but not necessarily a whole word),
 then all candidates that are not exactly matched by at least one substring are removed."
-  (let* ((input-strings (str:split " " input :omit-nulls t))
-         (exactly-matching-substrings (delete-duplicates
-                                       (loop for candidate-pair in candidate-pairs
-                                             append (remove-if
-                                                     (lambda (i)
-                                                       (not (search i (first candidate-pair))))
-                                                     input-strings))
-                                       :test #'string=)))
+  (let* ((exactly-matching-substrings (find-exactly-matching-substrings
+                                       input
+                                       (mapcar #'first candidate-pairs))))
     (if exactly-matching-substrings
         (setf candidate-pairs
               (delete-if (lambda (candidate-pair)
                            (not (loop for i in exactly-matching-substrings
                                       always (search i (first candidate-pair)))))
                          candidate-pairs))
+        candidate-pairs)))
+
+(defun sort-exact-matches-in-candidates (input candidate-pairs)
+  "Destructively sort candidates by the position of the exact match.
+The earlier the match in the candidate, the first in the list.
+The comparition is done against the first exact match, then the second, etc. "
+  (let* ((exactly-matching-substrings (find-exactly-matching-substrings
+                                       input
+                                       (mapcar #'first candidate-pairs))))
+    (if exactly-matching-substrings
+        (setf candidate-pairs
+              (sort candidate-pairs
+                    (lambda (candidate-pair1 candidate-pair2)
+                      (loop for substring in exactly-matching-substrings
+                            for pos1 = (search substring (first candidate-pair1))
+                            for pos2 = (search substring (first candidate-pair2))
+                            when (and pos1 (not pos2))
+                              return t
+                            when (and (not pos1) pos2)
+                              return nil
+                            when (and pos1 pos2)
+                              return (< pos1 pos2)))))
         candidate-pairs)))
 
 @export
@@ -97,6 +142,7 @@ The match is case-sensitive if INPUT contains at least one uppercase character."
                         (mapcar (lambda (p) (list (string-downcase (first p)) (second p))) pairs)
                         pairs))
              (pairs (keep-exact-matches-in-candidates input pairs))
+             ;; (pairs (sort-exact-matches-in-candidates input pairs)) ; TODO: Is this useful when we have the exact-match-norm?
              (pairs (sort-candidates input pairs)))
         (log:debug "~a" (mapcar (lambda (c) (list (first c)
                                                   (score-candidate (to-unicode input) (first c))))
