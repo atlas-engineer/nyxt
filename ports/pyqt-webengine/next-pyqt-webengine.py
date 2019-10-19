3b63627bdae5256081bc5207af8334e32b342eaa
Taylor Viti <tviti@hawaii.edu>
2019-10-18 15:27:12 -1000

    DBusWindowAdaptor: Make dbus func sigs. more consistent w/ gtk port.

--- ports/pyqt-webengine/next-pyqt-webengine.py
+++ ports/pyqt-webengine/next-pyqt-webengine.py
@@ -44,66 +44,77 @@ class DBusWindowAdaptor(QtDBus.QDBusAbstractAdaptor):
                 """
 <interface name="engineer.atlas.next.platform">
     <method name="window_make">
-      <arg direction="in"  type="s" name="window_id" />
+        <arg direction="in"  type="s" name="id" />
+        <arg direction="out" type="s" name="new_id" />
     </method>
     <method name="window_set_title">
-      <arg direction="in"  type="s" name="window_id" />
-      <arg direction="in"  type="s" name="title" />
+        <arg direction="in"  type="s" name="id" />
+        <arg direction="in"  type="s" name="title" />
+        <arg direction="out" type="b" name="status" />
     </method>
     <method name="window_delete">
-      <arg direction="in"  type="s" name="window_id" />
+        <arg direction="in"  type="s" name="id" />
+        <arg direction="out" type="b" name="status" />
     </method>
     <method name="window_active">
-      <arg direction="out" type="s" name="id" />
+        <arg direction="out" type="s" name="id" />
     </method>
     <method name="window_exists">
-      <arg direction="in"  type="s" name="window_id" />
+        <arg direction="in"  type="s" name="id" />
+        <arg direction="out" type="b" name="status" />
     </method>
     <method name="window_set_active_buffer">
-      <arg direction="in"  type="s" name="window_id" />
-      <arg direction="in"  type="s" name="buffer_id" />
+        <arg direction="in"  type="s" name="window_id" />
+        <arg direction="in"  type="s" name="buffer_id" />
+        <arg direction="out" type="b" name="status" />
     </method>
     <method name="window_set_minibuffer_height">
-      <arg direction="in"  type="s" name="window_id" />
-      <arg direction="in"  type="i" name="height" />
+        <arg direction="in"  type="s" name="window_id" />
+        <arg direction="in"  type="i" name="height" />
+        <arg direction="out" type="x" name="new_height" />
     </method>
     <method name="buffer_make">
-      <arg direction="in"  type="s" name="buffer_id" />
+        <arg direction="in"  type="s" name="id" />
+        <arg direction="in"  type="a{ss}" name="options" />
+        <arg direction="out" type="s" name="new_id" />
     </method>
     <method name="buffer_delete">
-      <arg direction="in"  type="s" name="buffer_id" />
+        <arg direction="in"  type="s" name="id" />
+        <arg direction="out" type="b" name="status" />
     </method>
     <method name="buffer_load">
-      <arg direction="in"  type="s" name="buffer_id" />
-      <arg direction="in"  type="s" name="url" />
+        <arg direction="in"  type="s" name="buffer_id" />
+        <arg direction="in"  type="s" name="url" />
+        <arg direction="out" type="b" name="status" />
     </method>
     <method name="buffer_evaluate_javascript">
-      <arg direction="in"  type="s" name="buffer_id" />
-      <arg direction="in"  type="s" name="javascript" />
-      <arg direction="out" type="s" name="callback_id"/>
+        <arg direction="in"  type="s" name="buffer_id" />
+        <arg direction="in"  type="s" name="javascript" />
+        <arg direction="out" type="s" name="callback_id"/>
     </method>
     <method name="minibuffer_evaluate_javascript">
-      <arg direction="in"  type="s" name="window_id" />
-      <arg direction="in"  type="s" name="javascript" />
-      <arg direction="out"  type="s" name="callback_id" />
+        <arg direction="in"  type="s" name="window_id" />
+        <arg direction="in"  type="s" name="javascript" />
+        <arg direction="out" type="s" name="callback_id" />
     </method>
     <method name="set_proxy">
-      <arg direction="in"  type="as" name="buffer_ids" />
-      <arg direction="in"  type="s" name="mode" />
-      <arg direction="in"  type="s" name="address" />
-      <arg direction="in"  type="as" name="whitelist" />
-      <arg direction="out" type="b" />
+        <arg direction="in"  type="as" name="buffer_ids" />
+        <arg direction="in"  type="s"  name="mode" />
+        <arg direction="in"  type="s"  name="address" />
+        <arg direction="in"  type="as" name="whitelist" />
+        <arg direction="out" type="b"  name="status" />
     </method>
     <method name="get_proxy">
-      <arg direction="out" type="s" />
+        <arg direction="out" type="s" />
     </method>
     <method name="generate_input_event">
-      <arg direction="in"  type="s" name="window_id" />
-      <arg direction="in"  type="i" name="key_code" />
-      <arg direction="in"  type="as" name="modifiers" />
-      <arg direction="in"  type="i" name="low_level_data" />
-      <arg direction="in"  type="d" name="x" />
-      <arg direction="in"  type="d" name="y" />
+        <arg direction="in"  type="s"  name="window_id" />
+        <arg direction="in"  type="i"  name="key_code" />
+        <arg direction="in"  type="as" name="modifiers" />
+        <arg direction="in"  type="i"  name="low_level_data" />
+        <arg direction="in"  type="d"  name="x" />
+        <arg direction="in"  type="d"  name="y" />
+        <arg direction="out" type="b"  name="status" />
     </method>
     <method name="window_killall">
     </method>
@@ -111,7 +122,7 @@ class DBusWindowAdaptor(QtDBus.QDBusAbstractAdaptor):
     </method>
     <method name="buffer_list">
     </method>
-  </interface>
+</interface>
                 """)
 
     def __init__(self, parent):
@@ -119,43 +130,51 @@ class DBusWindowAdaptor(QtDBus.QDBusAbstractAdaptor):
         self.setAutoRelaySignals(True)
         return
 
-    @pyqtSlot(str)
+    @pyqtSlot(str, result=str)
     def window_make(self, window_id):
         return self.parent().window_make(window_id)
 
-    @pyqtSlot(str, str)
+    @pyqtSlot(str, str, result=bool)
     def window_set_title(self, window_id, title):
-        return self.parent().window_set_title(window_id, title)
+        self.parent().window_set_title(window_id, title)
+        return True
 
-    @pyqtSlot(str)
+    @pyqtSlot(str, result=bool)
     def window_delete(self, window_id):
         return self.parent().window_delete(window_id)
 
-    @pyqtSlot()
+    @pyqtSlot(result=str)
     def window_active(self):
         return self.parent().window_active()
 
-    @pyqtSlot(str)
+    @pyqtSlot(str, result=bool)
     def window_exists(self, window_id):
         return self.parent().window_exists(window_id)
 
-    @pyqtSlot(str, str)
+    @pyqtSlot(str, str, result=bool)
     def window_set_active_buffer(self, window_id, buffer_id):
         return self.parent().window_set_active_buffer(window_id, buffer_id)
 
-    @pyqtSlot(str, int)
+    @pyqtSlot(str, int, result=int)
     def window_set_minibuffer_height(self, window_id, height):
         return self.parent().window_set_minibuffer_height(window_id, height)
 
-    @pyqtSlot(str)
-    def buffer_make(self, buffer_id):
-        return self.parent().buffer_make(buffer_id)
+    # It's easier to pass structs over dbus using QDBusMessage, than to specify
+    # args + types in the decorator. See
+    # https://www.riverbankcomputing.com/static/Docs/PyQt5/dbus.html
+    @pyqtSlot(QtDBus.QDBusMessage, result=str)
+    def buffer_make(self, msg):
+        args = msg.arguments()
+        buffer_id = args[0]
+        options = args[1]
 
-    @pyqtSlot(str)
+        return self.parent().buffer_make(buffer_id, options)
+
+    @pyqtSlot(str, result=bool)
     def buffer_delete(self, buffer_id):
         return self.parent().buffer_delete(buffer_id)
 
-    @pyqtSlot(str, str)
+    @pyqtSlot(str, str, result=bool)
     def buffer_load(self, buffer_id, url):
         return self.parent().buffer_load(buffer_id, url)
 
@@ -163,7 +182,7 @@ class DBusWindowAdaptor(QtDBus.QDBusAbstractAdaptor):
     def buffer_evaluate_javascript(self, buffer_id, script):
         return self.parent().buffer_evaluate_javascript(buffer_id, script)
 
-    @pyqtSlot(str, str)
+    @pyqtSlot(str, str, result=str)
     def minibuffer_evaluate_javascript(self, window_id, script):
         return self.parent().minibuffer_evaluate_javascript(window_id, script)
 
@@ -230,7 +249,7 @@ class DBusWindow(QObject):
         _window = window.get_window(window_id)
         return _window.set_minibuffer_height(height)
 
-    def buffer_make(self, buffer_id):
+    def buffer_make(self, buffer_id, options):
         return buffers.make(buffer_id)
 
     def buffer_delete(self, buffer_id):
