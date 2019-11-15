061fd74a89c9b4f1455b384e13c5ffe39506e283
Pierre Neidhardt <mail@ambrevar.xyz>
2019-11-15 16:38:46 +0100

    VI-mode: Fix indentation.

--- source/vi-mode.lisp
+++ source/vi-mode.lisp
@@ -1,53 +1,53 @@
 (uiop:define-package :next/vi-mode
-    (:use :common-lisp :trivia :next)
+  (:use :common-lisp :trivia :next)
   (:documentation "VI-style bindings."))
 (in-package :next/vi-mode)
 
 (define-mode vi-normal-mode ()
-    "Enable VI-style modal bindings (normal mode)"
-    ((keymap-schemes
-      :initform
-      (let ((map (make-keymap)))
-        (define-key :keymap map
-          "i" #'vi-insert-mode
-          "button1" #'vi-button1)
-        (list :vi-normal map)))
-     (destructor
-      :initform
-      (lambda (mode)
-        (setf (current-keymap-scheme (buffer mode))
-              (get-default 'buffer 'current-keymap-scheme))
-        (setf (forward-input-events-p (buffer mode))
-              (get-default 'buffer 'current-keymap-scheme))))
-     (constructor
-      :initform
-      (lambda (mode)
-        (let ((active-buffer (buffer mode)))
-          (vi-insert-mode :activate nil :buffer active-buffer)
-          (setf (current-keymap-scheme active-buffer) :vi-normal)
-          (setf (forward-input-events-p active-buffer) nil))))))
+  "Enable VI-style modal bindings (normal mode)"
+  ((keymap-schemes
+    :initform
+    (let ((map (make-keymap)))
+      (define-key :keymap map
+        "i" #'vi-insert-mode
+        "button1" #'vi-button1)
+      (list :vi-normal map)))
+   (destructor
+    :initform
+    (lambda (mode)
+      (setf (current-keymap-scheme (buffer mode))
+            (get-default 'buffer 'current-keymap-scheme))
+      (setf (forward-input-events-p (buffer mode))
+            (get-default 'buffer 'current-keymap-scheme))))
+   (constructor
+    :initform
+    (lambda (mode)
+      (let ((active-buffer (buffer mode)))
+        (vi-insert-mode :activate nil :buffer active-buffer)
+        (setf (current-keymap-scheme active-buffer) :vi-normal)
+        (setf (forward-input-events-p active-buffer) nil))))))
 
 ;; TODO: Move ESCAPE binding to the override map?
 (define-mode vi-insert-mode ()
-    "Enable VI-style modal bindings (insert mode)"
-    ((keymap-schemes
-      :initform
-      (let ((map (make-keymap)))
-        (define-key :keymap map
-          "ESCAPE" #'vi-normal-mode
-          "button1" #'vi-button1)
-        (list :vi-insert map)))
-     (destructor
-      :initform
-      (lambda (mode)
-        (setf (current-keymap-scheme (buffer mode))
-              (get-default 'buffer 'current-keymap-scheme))))
-     (constructor
-      :initform
-      (lambda (mode)
-        (let ((active-buffer (buffer mode)))
-          (vi-normal-mode :activate nil :buffer active-buffer)
-          (setf (current-keymap-scheme active-buffer) :vi-insert))))))
+  "Enable VI-style modal bindings (insert mode)"
+  ((keymap-schemes
+    :initform
+    (let ((map (make-keymap)))
+      (define-key :keymap map
+        "ESCAPE" #'vi-normal-mode
+        "button1" #'vi-button1)
+      (list :vi-insert map)))
+   (destructor
+    :initform
+    (lambda (mode)
+      (setf (current-keymap-scheme (buffer mode))
+            (get-default 'buffer 'current-keymap-scheme))))
+   (constructor
+    :initform
+    (lambda (mode)
+      (let ((active-buffer (buffer mode)))
+        (vi-normal-mode :activate nil :buffer active-buffer)
+        (setf (current-keymap-scheme active-buffer) :vi-insert))))))
 
 (define-parenscript %clicked-in-input? ()
   (ps:chain document active-element tag-name))
@@ -61,9 +61,9 @@
   "Enable VI insert mode when focus is on an input element on the web page."
   (let ((root-mode (find-mode buffer 'root-mode)))
     (rpc-generate-input-event
-                              (rpc-window-active)
-                              (first (last-key-chords
-                                      (buffer root-mode))))
+     (rpc-window-active)
+     (first (last-key-chords
+             (buffer root-mode))))
     (%clicked-in-input?
      :callback (lambda (response)
                  (cond
