7f7b5aabb5280a327f2d6c7dcd3645e63e81d9ec
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2019-11-26 15:13:45 +0100

    Clip only first line when getting password from password-store

--- libraries/password-manager/password-pass.lisp
+++ libraries/password-manager/password-pass.lisp
@@ -12,9 +12,17 @@
             raw-list)))
 
 (defmethod clip-password ((password-interface password-store-interface) password-name)
-  (clip-password-string (uiop:run-program (list *password-store-program* "show"
-                                                password-name)
-                                          :output '(:string :stripped t))))
+  (clip-password-string
+   ;; The common way to store secret in password-store is to use the first line
+   ;; for the secret; there's no standard for how to encode anything else.
+   ;; Because there's no support in `password` for having additional fields
+   ;; (e.g. username or email address for autofilling input fields), we'll keep
+   ;; it simple and just return the first line of the password file.
+   (first
+    (cl-ppcre:split #\newline
+                    (uiop:run-program (list *password-store-program* "show"
+                                            password-name)
+                                      :output '(:string :stripped t))))))
 
 (defmethod save-password ((password-interface password-store-interface)
                           password-name password)
