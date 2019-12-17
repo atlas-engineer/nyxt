665cf6204547da71d47637c7c639fcc21d9c6c55
Pierre Neidhardt <mail@ambrevar.xyz>
2019-12-17 20:44:52 +0100

    libraries/hooks: Export forgotten symbols.

--- libraries/hooks/package.lisp
+++ libraries/hooks/package.lisp
@@ -11,15 +11,8 @@
    #:run-hook-with-args
    #:run-hook-with-args-until-failure
    #:run-hook-with-args-until-success
-   #:handler
-   #:name
-   #:description
-   #:handler-type
-   #:place
-   #:value
    #:make-handler
    #:equals
-   #:hook
    #:default-combine-hook
    #:combine-hook-until-failure
    #:combine-hook-until-success
@@ -30,11 +23,28 @@
    #:define-hook
    #:find-hook
    #:define-hook-type
-   ;; Pre-generated types
-   ;; TODO: Add the rest.
+   ;; Handler class:
+   #:handler
+   #:name
+   #:description
+   #:handler-type
+   #:place
+   #:value
+   ;; Hook class:
+   #:hook
+   #:handlers
+   #:disabled-handlers
+   #:combination
+   ;; Pre-generated types:
+   #:make-handler-void
+   #:handler-void
+   #:hook-void
    #:make-handler-string->string
    #:handler-string->string
    #:hook-string->string
-   #:make-handler-void
-   #:handler-void
-   #:hook-void))
+   #:make-handler-number->number
+   #:handler-number->number
+   #:hook-number->number
+   #:make-handler-any
+   #:handler-any
+   #:hook-any))
