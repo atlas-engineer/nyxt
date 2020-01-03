8822c2fa9bc528aa7f6724a469f87b2e8c5ee720
vindarel <vindarel@mailz.org>
2020-01-03 17:08:27 +0100

    add --eval CLI argument
    fixes #437

--- source/start.lisp
+++ source/start.lisp
@@ -18,10 +18,14 @@
     (:name :verbose
            :short #\v
            :long "verbose"
-     :description "Print debugging information to stdout.")
+           :description "Print debugging information to stdout.")
     (:name :version
            :long "version"
            :description "Print version and exit.")
+    (:name :eval
+           :long "eval"
+           :arg-parser #'identity
+           :description "Eval the Lisp expression.")
     (:name :init-file
            :short #\i
            :long "init-file"
@@ -79,6 +83,7 @@ Set to '-' to read standard input instead.")
 
 @export
 (defun entry-point ()
+  "Read the CLI arguments and start the plateform port."
   (multiple-value-bind (options free-args)
       (parse-cli-args)
     (when (getf options :help)
@@ -86,19 +91,30 @@ Set to '-' to read standard input instead.")
 
 next [options] [urls]")
       (uiop:quit))
+
     (when (getf options :version)
       (format t "Next ~a~&" +version+)
       (uiop:quit))
+
     (when (getf options :verbose)
       (set-debug-level :debug)
       (format t "Arguments parsed: ~a and ~a~&" options free-args))
+
+    (when (getf options :eval)
+      (unless (getf options :no-init)
+        (load-lisp-file (init-file-path) :interactive nil))
+      (eval-expr (getf options :eval))
+      (uiop:quit))
+
     (when (getf options :session)
       (when (string-equal (getf options :session) "nil")
         (setf next:*use-session* nil)))
+
     (setf *options* options
           *free-args* free-args)
     (start :urls free-args
            :non-interactive t))
+
   (handler-case (progn (run-loop (port *interface*))
                        (kill-interface *interface*))
     ;; Catch a C-c, don't print a full stacktrace.
@@ -196,6 +212,15 @@ If :running, show an error but don't quit the Lisp process."
 If INTERACTIVE is t, allow the debugger on errors.
 If :running, show an error but don't quit the Lisp process."
   (load-lisp-file init-file :interactive interactive))
+
+(defun eval-expr (expr)
+  "Evaluate the form EXPR (string) and print its result.
+EXPR must contain one single Lisp form. Use `progn' if needed."
+  (handler-case
+      (print (eval (read-from-string expr)))
+    (error (c)
+      (format *error-output* "~%~a~&~a~&" (cl-ansi-text:red "Evaluation error:") c)
+      (uiop:quit 1))))
 
 (defun default-startup (&optional urls)
   "Make a window and load URLS in new buffers.
