7d6e04120247f80c08ac9a84e24aeff73ed37178
John Mercouris <john@mercouris.email>
2020-01-20 18:25:45 +0100

    interprocess-communication.lisp: comment %rpc-send functions

--- source/interprocess-communication.lisp
+++ source/interprocess-communication.lisp
@@ -28,54 +28,52 @@
     (gir:invoke (foreign-window-object 'show-all))
     window))
 
-(declaim (ftype (function (window string)) rpc-window-set-title))
 @export
 (defun rpc-window-set-title (window title)
   "Set the title for a given window."
-  (%rpc-send "window_set_title" (id window) title))
+  ; (%rpc-send "window_set_title" (id window) title)
+  )
 
-(declaim (ftype (function (window)) rpc-window-delete))
 @export
 (defun rpc-window-delete (window)
   "Delete a window object and remove it from the hash of windows.
 Once deleted, the `window-will-close' RPC endpoint will be called, running
 INTERFACE's `window-delete-hook' over WINDOW."
-  (%rpc-send "window_delete" (id window)))
+  ;(%rpc-send "window_delete" (id window))
+  )
 
 @export
 (defun rpc-window-active ()
   "Return the window object for the currently active window."
-  (let ((window (gethash (%rpc-send "window_active")
+  (let ((window (gethash "goldfish"; (%rpc-send "window_active")
                          (windows *interface*))))
     (when window
       (setf (last-active-window *interface*) window))
     (last-active-window *interface*)))
 
-(declaim (ftype (function (window)) rpc-window-exists))
 @export
 (defun rpc-window-exists (window)
   "Return if a window exists."
-  (%rpc-send "window_exists" (id window)))
+  ;(%rpc-send "window_exists" (id window))
+  )
 
-(declaim (ftype (function (window buffer)) rpc-window-set-active-buffer))
 @export
 (defun rpc-window-set-active-buffer (window buffer)
   "Set INTERFACE's WINDOW buffer to BUFFER.
 Run WINDOW's `window-set-active-buffer-hook' over WINDOW and BUFFER before
 proceeding."
   (next-hooks:run-hook (window-set-active-buffer-hook window) window buffer)
-  (%rpc-send "window_set_active_buffer" (id window) (id buffer))
+  ; (%rpc-send "window_set_active_buffer" (id window) (id buffer))
   (setf (active-buffer window) buffer)
   (when (and window buffer)
     (setf (last-active-buffer *interface*) buffer))
   buffer)
 
-(declaim (ftype (function (window integer)) rpc-window-set-minibuffer-height))
 @export
 (defun rpc-window-set-minibuffer-height (window height)
-  (%rpc-send "window_set_minibuffer_height" (id window) height))
+  ; (%rpc-send "window_set_minibuffer_height" (id window) height)
+  )
 
-(declaim (ftype (function (&key (:title string) (:default-modes list) (:dead-buffer buffer))) rpc-buffer-make))
 @export
 (defun rpc-buffer-make (&key title default-modes dead-buffer)
   "Make buffer with title TITLE and modes DEFAULT-MODES.
@@ -92,8 +90,8 @@ If DEAD-BUFFER is a dead buffer, recreate its web view and give it a new ID."
       (ensure-parent-exists (cookies-path buffer)))
     (setf (gethash (id buffer) (buffers *interface*)) buffer)
     (incf (total-buffer-count *interface*))
-    (%rpc-send "buffer_make" (id buffer)
-               `(("cookies-path" ,(namestring (cookies-path buffer)))))
+    ;; (%rpc-send "buffer_make" (id buffer)
+    ;;            `(("cookies-path" ,(namestring (cookies-path buffer)))))
     (unless (last-active-buffer *interface*)
       ;; When starting from a REPL, it's possible that the window is spawned in
       ;; the background and current-buffer would then return nil.
@@ -106,18 +104,17 @@ If DEAD-BUFFER is a dead buffer, recreate its web view and give it a new ID."
     (initialize-modes buffer)
     buffer))
 
-(declaim (ftype (function (buffer)) rpc-buffer-delete))
 @export
 (defun rpc-buffer-delete (buffer)
   "Delete BUFFER from `*interface*'.
-Run BUFFER's `buffer-delete-hook' over BUFFER before deleting it."
+   Run BUFFER's `buffer-delete-hook' over BUFFER before deleting it."
   (next-hooks:run-hook (buffer-delete-hook buffer) buffer)
   (let ((parent-window (find-if
                         (lambda (window) (eql (active-buffer window) buffer))
                         (alexandria:hash-table-values (windows *interface*))))
         (replacement-buffer (or (%get-inactive-buffer)
                                 (rpc-buffer-make))))
-    (%rpc-send "buffer_delete" (id buffer))
+    ; (%rpc-send "buffer_delete" (id buffer))
     (when parent-window
       (window-set-active-buffer parent-window replacement-buffer))
     (remhash (id buffer) (buffers *interface*))
@@ -128,29 +125,27 @@ Run BUFFER's `buffer-delete-hook' over BUFFER before deleting it."
        (when *use-session*
          (funcall f))))))
 
-(declaim (ftype (function (buffer string)) rpc-buffer-load))
 @export
 (defun rpc-buffer-load (buffer uri)
-  (%rpc-send "buffer_load" (id buffer) uri))
+  ;(%rpc-send "buffer_load" (id buffer) uri)
+  )
 
-(declaim (ftype (function (buffer string &key (:callback function))) rpc-buffer-evaluate-javascript))
 @export
 (defun rpc-buffer-evaluate-javascript (buffer javascript &key callback)
-  (let ((callback-id
-          (%rpc-send "buffer_evaluate_javascript" (id buffer) javascript)))
+  (let ((callback-id 0
+          ; (%rpc-send "buffer_evaluate_javascript" (id buffer) javascript)
+          ))
     (setf (gethash callback-id (callbacks buffer)) callback)
     callback-id))
 
-(declaim (ftype (function (window string &key (:callback function))) rpc-minibuffer-evaluate-javascript))
 @export
 (defun rpc-minibuffer-evaluate-javascript (window javascript &key callback)
-  ;; JS example: document.body.innerHTML = 'hello'
-  (let ((callback-id
-          (%rpc-send "minibuffer_evaluate_javascript" (id window) javascript)))
+  (let ((callback-id 0
+          ;(%rpc-send "minibuffer_evaluate_javascript" (id window) javascript)
+          ))
     (setf (gethash callback-id (minibuffer-callbacks window)) callback)
     callback-id))
 
-(declaim (ftype (function (window key-chord)) rpc-generate-input-event))
 @export
 (defun rpc-generate-input-event (window event)
   "For now, we only generate keyboard events.
@@ -163,45 +158,46 @@ events."
               (key-chord-low-level-data event)
               (key-chord-position event))
              (id window))
-  (%rpc-send "generate_input_event"
-             (id window)
-             (key-chord-key-code event)
-             (or (key-chord-modifiers event) (list ""))
-             (key-chord-low-level-data event)
-             (float (or (first (key-chord-position event)) -1.0))
-             (float (or (second (key-chord-position event)) -1.0))))
+  ;; (%rpc-send "generate_input_event"
+  ;;            (id window)
+  ;;            (key-chord-key-code event)
+  ;;            (or (key-chord-modifiers event) (list ""))
+  ;;            (key-chord-low-level-data event)
+  ;;            (float (or (first (key-chord-position event)) -1.0))
+  ;;            (float (or (second (key-chord-position event)) -1.0)))
+  )
 
-(declaim (ftype (function (buffer &optional string list)) rpc-set-proxy))
 @export
 (defun rpc-set-proxy (buffer &optional (proxy-uri "") (ignore-hosts (list nil)))
   "Redirect network connections of BUFFER to proxy server PROXY-URI.
-Hosts in IGNORE-HOSTS (a list of strings) ignore the proxy.
-For the user-level interface, see `proxy-mode'.
+   Hosts in IGNORE-HOSTS (a list of strings) ignore the proxy.
+   For the user-level interface, see `proxy-mode'.
 
-Note: WebKit supports three proxy \"modes\": default (the system proxy),
-custom (the specified proxy) and none.
-TODO: We don't use \"none\" here, but it could be useful to expose it to the
-user."
-  (%rpc-send "set_proxy" (list (id buffer))
-             (if (string= proxy-uri "")
-                 "default"
-                 "custom")
-             proxy-uri ignore-hosts))
+   Note: WebKit supports three proxy \"modes\": default (the system proxy),
+   custom (the specified proxy) and none.
+   TODO: We don't use \"none\" here, but it could be useful to expose it to the
+   user."
+  ;; (%rpc-send "set_proxy" (list (id buffer))
+  ;;            (if (string= proxy-uri "")
+  ;;                "default"
+  ;;                "custom")
+  ;;            proxy-uri ignore-hosts)
+  )
 
-(declaim (ftype (function (buffer)) rpc-get-proxy))
 @export
 (defun rpc-get-proxy (buffer)
-  "Return (MODE ADDRESS WHITELISTED-ADDRESSES...) of the active proxy configuration.
-MODE is one of \"default\" (use system configuration), \"custom\" or \"none\".
-ADDRESS is in the form PROTOCOL://HOST:PORT."
-  (%rpc-send "get_proxy" (id buffer)))
+  "Return (MODE ADDRESS WHITELISTED-ADDRESSES...) of the active proxy
+   configuration.
+   MODE is one of \"default\" (use system configuration), \"custom\"
+   or \"none\".
+   ADDRESS is in the form PROTOCOL://HOST:PORT."
+  ;(%rpc-send "get_proxy" (id buffer))
+  )
 
-(declaim (ftype (function (buffer string boolean)) rpc-buffer-set))
 @export
 (defun rpc-buffer-set (buffer setting value)
   "Set SETTING to VALUE for BUFFER.
-The valid SETTINGs are specified by the platform, e.g. for WebKitGTK it is
-https://webkitgtk.org/reference/webkit2gtk/stable/WebKitSettings.html.
-
-TODO: Only booleans are supported for now."
-  (%rpc-send "buffer_set" (id buffer) setting value))
+   Specification:
+   https://webkitgtk.org/reference/webkit2gtk/stable/WebKitSettings.html."
+  ;(%rpc-send "buffer_set" (id buffer) setting value)
+  )
