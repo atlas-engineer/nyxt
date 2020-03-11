6c01705483221f11de633e40cb64963942b7e96b
Pierre Neidhardt <mail@ambrevar.xyz>
2020-03-11 17:43:32 +0100

    keymap/tests: Test "Make key".

--- libraries/keymap/tests/tests.lisp
+++ libraries/keymap/tests/tests.lisp
@@ -2,5 +2,18 @@
 
 (prove:plan nil)
 
+(prove:subtest "Make key"
+  (let* ((key (keymap:make-key :code 38 :value "a" :modifiers '("C")))
+         (mod (first (fset:convert 'list (keymap:key-modifiers key)))))
+    (prove:is (keymap:key-code key)
+              38)
+    (prove:is (keymap:key-value key)
+              "a")
+    (prove:is (keymap:modifier= mod "C")
+              t)
+    (prove:is (keymap:modifier= mod "control")
+              t)
+    (prove:is (keymap:modifier= mod keymap:+control+)
+              t)))
 
 (prove:finalize)
