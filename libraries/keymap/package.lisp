f151b903e97ba71c10c30736de3002a5f33a7141
Pierre Neidhardt <mail@ambrevar.xyz>
2020-03-12 20:34:45 +0100

    keymap: Add package documentation.

--- libraries/keymap/package.lisp
+++ libraries/keymap/package.lisp
@@ -39,4 +39,17 @@
    translate-shifted-control-combinations
 
    *print-shortcut*
-   keys->keyspecs))
+   keys->keyspecs)
+  (:documentation "
+The workflow goes as follows:
+- Make a keymap with `make-keymap'.
+- Define a binding on it with `define-key'.
+- Lookup this binding with `lookup-key'.
+
+Some globals can be tweaked to customize the library to your needs:
+
+- `*modifier-list*': List of known keyboard modifiers like `+control+'.
+- `*default-translator*': The default function to infer the right binding when
+  the exact binding hits nothing.
+- `*print-shortcuts*': Print modifiers using their short form instead of the
+  full name, e.g. \"C\" instead of \"control\"."))
