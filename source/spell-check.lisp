f7082ee7881c5d3b86e724e4c229eafdcdb6b84f
John Mercouris <john@mercouris.email>
2020-04-13 17:05:51 +0200

    spell-check.lisp: prompt the user when a highlighted word is incorrect

--- source/spell-check.lisp
+++ source/spell-check.lisp
@@ -16,19 +16,22 @@
               (echo "~a is incorrect." word)))))
 
 (define-command spell-check-highlighted-word ()
-  "Spell check a highlighted word."
+  "Spell check a highlighted word. If a word is incorrectly spelled,
+pull up a prompt of suggestions."
   (with-result (word (%copy))
     (if (spell-check-word :word word)
         (echo "Highlighted word: ~a, spelled correctly." word)
-        (echo "Highlighted word: ~a, spelled incorrectly." word))))
+        (progn (echo "Highlighted word: ~a, spelled incorrectly." word)
+               (spell-check-suggest-word :word word)))))
 
-(define-command spell-check-suggest-word ()
+(define-command spell-check-suggest-word (&key word)
   "Suggest a spelling for a given word."
   (with-result (selected-word (read-from-minibuffer
                                (make-minibuffer
+                                :input-buffer word
                                 :input-prompt "Suggest spelling (3+ characters)"
                                 :completion-function 'enchant-completion)))
-    selected-word))
+    (trivial-clipboard:text selected-word)))
 
 (defun enchant-completion (input)
   (when (> (length input) 2)
