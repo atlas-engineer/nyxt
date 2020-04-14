8952d1fa3839a9249962968516a2cc15cf1d31d3
John Mercouris <john@mercouris.email>
2020-04-14 14:27:28 +0200

    global.lisp: set-up a *renderer-class* for usage in define-configuration macro
    also set-up a list of known renderer-browser-classes
    
    This reverts commit 7cce2df6ba45de86b8b37b9e57f0f71b7ca0a275.

--- source/global.lisp
+++ source/global.lisp
@@ -3,6 +3,14 @@
 
 (in-package :next)
 
+(defvar +renderer-browser-classes+ '(gtk-browser qt-browser)
+  "List of known browser/renderer classes, used to figure out what the
+  renderers have been loaded.")
+
+(defvar *renderer-class* 'gtk-browser
+  "The renderer class used when constructing the define-configuration
+  macro.")
+
 (defvar *options* '()
   "The list of command line options.")
 
