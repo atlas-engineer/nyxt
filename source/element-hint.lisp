20e95d3084861f4ba7d3a1da8e6ac2243d2d7f0b
John Mercouris <john@mercouris.email>
2020-04-25 17:26:41 +0200

    element-hint.lisp: support multiple buffers in follow-hint-new-buffer

--- source/element-hint.lisp
+++ source/element-hint.lisp
@@ -327,7 +327,8 @@ currently active buffer."
 (define-command follow-hint-new-buffer (&key annotate-full-document)
   "Show a set of element hints, and open the user inputted one in a new
 buffer (not set to visible active buffer)."
-  (query-hints "Open element in new buffer:" '%follow-hint-new-buffer
+  (query-hints "Open element in new buffer:" (lambda (result) (mapcar #'%follow-hint-new-buffer result))
+               :multi-selection-p t
                :annotate-full-document annotate-full-document))
 
 (define-command follow-hint-new-buffer-focus (&key annotate-full-document)
