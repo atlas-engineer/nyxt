85be63703e7910095099461f64a2518b5945f76d
Pierre Neidhardt <mail@ambrevar.xyz>
2020-04-30 18:33:36 +0200

    Print TLS error in same buffer.

--- source/renderer-gtk.lisp
+++ source/renderer-gtk.lisp
@@ -352,14 +352,18 @@ Warning: This behaviour may change in the future."
   "Return nil to propagate further (i.e. raise load-failed signal), T otherwise."
   (let* ((context (webkit:webkit-web-view-web-context (gtk-object buffer)))
          (host (quri:uri-host (quri:uri url))))
-    (when (and (certificate-whitelist buffer)
-               (member-string host (certificate-whitelist buffer)))
-      (webkit:webkit-web-context-allow-tls-certificate-for-host
-       context
-       (gobject:pointer certificate)
-       host)
-      (set-url* url :buffer buffer)
-      t)))
+    (if (and (certificate-whitelist buffer)
+             (member-string host (certificate-whitelist buffer)))
+        (progn
+          (webkit:webkit-web-context-allow-tls-certificate-for-host
+           context
+           (gobject:pointer certificate)
+           host)
+          (set-url* url :buffer buffer)
+          t)
+        (progn
+          (tls-help buffer url)
+          t))))
 
 (defmethod on-signal-decide-policy ((buffer gtk-buffer) response-policy-decision policy-decision-type-response)
   (let ((is-new-window nil) (is-known-type t) (event-type nil)
@@ -515,8 +519,13 @@ Warning: This behaviour may change in the future."
    (gtk-object buffer) "load-failed-with-tls-errors"
    (lambda (web-view failing-uri certificate errors)
      (declare (ignore web-view errors))
-     (on-signal-load-failed-with-tls-errors buffer certificate failing-uri)
-     (tls-help buffer failing-uri)))
+     (on-signal-load-failed-with-tls-errors buffer certificate failing-uri)))
+  (gobject:g-signal-connect
+   (gtk-object buffer) "load-failed"
+   (lambda (web-view load-event failing-uri error)
+     (declare (ignore web-view load-event failing-uri error))
+     ;; TODO: Print a better error page.
+     (log:debug "Load failed")))
   (gobject:g-signal-connect
    (gtk-object buffer) "notify::uri"
    (lambda (web-view param-spec)
