2a2263cdd59f8bca2c4d88607628cf4745cedd27
John Mercouris <john@mercouris.email>
2020-05-04 14:58:01 +0200

    analysis.lisp: collapse whitespaces in tokens

--- libraries/text-analysis/analysis.lisp
+++ libraries/text-analysis/analysis.lisp
@@ -3,7 +3,7 @@
 (in-package :text-analysis)
 
 (defun tokenize-string (string &key (remove-stop-words t) (stem nil) (down-case t))
-  (let* ((tokens (str:split " " string))
+  (let* ((tokens (str:split " " (str:collapse-whitespaces string)))
          (tokens (if remove-stop-words
                      (delete-if (lambda (x) (gethash (string-downcase  x) (stop-words-lookup *language-data*))) tokens)
                      tokens))
