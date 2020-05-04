cebc78752af9a7bb01fa864c50848129c392bfa7
Pierre Neidhardt <mail@ambrevar.xyz>
2020-05-04 12:18:22 +0200

    Implement application-mode.

--- source/application-mode.lisp
+++ source/application-mode.lisp
@@ -1,11 +1,31 @@
-;;; application-mode.lisp --- major mode for "web applications"
-;;; has no history or bindings to avoid interference with "web applications"'
+(uiop:define-package :next/application-mode
+    (:use :common-lisp :trivia :next)
+  (:documentation "Forward all keybindings to the web view except those in the `override-map'."))
+(in-package :next/application-mode)
 
-(in-package :next)
-
-;; TODO: Finish it!  We can move the active modes to a list of backed up modes,
-;; and have one "escape key" bound to a function that would restore the backup modes.
+;; Moving modes out of the `modes' slot is a bad idea: too many parts rely on
+;; the presence of the `modes' slot.
 
 (define-mode application-mode ()
-    "Mode that forwards all keys to the renderer."
-  ())
+  "Mode that forwards all keys to the renderer."
+  ((destructor
+    :initform
+    (lambda (mode)
+      (hooks:remove-hook (current-keymaps-hook (buffer mode))
+                         'keep-override-map)))
+   (constructor
+    :initform
+    (lambda (mode)
+      (if (current-keymaps-hook (buffer mode))
+          (hooks:add-hook (current-keymaps-hook (buffer mode))
+                          (make-handler-keymaps-buffer #'keep-override-map))
+          (make-hook-keymaps-buffer
+             :combination #'hooks:combine-composed-hook
+             :handlers (list #'keep-override-map)))))))
+
+(declaim (ftype (function (list-of-keymaps buffer) (values list-of-keymaps buffer))
+                keep-override-map))
+(defun keep-override-map (keymaps buffer)
+  (if (active-minibuffers (current-window))
+      (values keymaps buffer)
+      (values (list (override-map buffer)) buffer)))
