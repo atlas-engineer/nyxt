51c7e4daaafc30a282e1c582c148494b6f94bbe8
John Mercouris <john@mercouris.email>
2020-05-26 16:19:07 +0200

    darwin-gtk.lisp: copy webkit-executables

--- source/darwin-gtk.lisp
+++ source/darwin-gtk.lisp
@@ -2,3 +2,13 @@
 
 (in-package :next)
 
+(deploy:define-hook (:deploy copy-webkit-executables) (directory)
+  (let ((webkit-executables (list "WebKitNetworkProcess"
+                                  "WebKitPluginProcess"
+                                  "WebKitWebProcess"
+                                  "jsc"))
+        (webkit-executable-path (or (uiop:getenv "WEBKIT_EXEC_PATH")
+                                    "/opt/local/libexec/webkit2gtk-4.0/")))
+    (loop for executable in webkit-executables do
+      (uiop:copy-file (format nil "~a~a" webkit-executable-path executable)
+                      (format nil "~a/~a" directory executable)))))
