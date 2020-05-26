96787b5d50cff8b62bba98392dcfd8fee9f1e264
John Mercouris <john@mercouris.email>
2020-05-26 16:19:25 +0200

    darwin-gtk.lisp: set webkit executable environment variables

--- source/darwin-gtk.lisp
+++ source/darwin-gtk.lisp
@@ -2,6 +2,7 @@
 
 (in-package :next)
 
+
 (deploy:define-hook (:deploy copy-webkit-executables) (directory)
   (let ((webkit-executables (list "WebKitNetworkProcess"
                                   "WebKitPluginProcess"
@@ -12,3 +13,9 @@
     (loop for executable in webkit-executables do
       (uiop:copy-file (format nil "~a~a" webkit-executable-path executable)
                       (format nil "~a/~a" directory executable)))))
+
+(deploy:define-hook (:boot set-environment-paths) (directory)
+  (unless (uiop:getenv "WEBKIT_EXEC_PATH")
+    (setf (uiop:getenv "WEBKIT_EXEC_PATH") (format nil "~a../Resources" directory)))
+  (unless (uiop:getenv "FONTCONFIG_PATH")
+    (setf (uiop:getenv "FONTCONFIG_PATH") "/opt/X11/lib/X11/fontconfig")))
