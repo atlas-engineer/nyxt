d84dc0ba04258810b563acba2ed265870b214e72
John Mercouris <john@mercouris.email>
2020-06-02 15:41:01 +0200

    external-editor.lisp: create comand to call external editor

--- source/external-editor.lisp
+++ source/external-editor.lisp
@@ -9,3 +9,15 @@ is blocking, invoke on a separate thread when possible."
       (log:debug "Visual Editor: ~a opening: ~a" (visual-editor *browser*) p)
       (uiop:run-program (list visual-editor (uiop:native-namestring p)))
       (uiop:read-file-string p))))
+
+(define-command fill-input-from-external-editor ()
+  "This command will open your editor specified by your VISUAL-EDITOR
+  of the BROWSER class, if unset, it will default to your VISUAL
+  environment variable. It will then capture whatever text you enter
+  and save in your editor."
+  (bt:make-thread
+   (lambda ()
+     (let ((text (get-text-from-external-editor)))
+       (ffi-within-renderer-thread
+        *browser*
+        (lambda () (%paste :input-text text)))))))
