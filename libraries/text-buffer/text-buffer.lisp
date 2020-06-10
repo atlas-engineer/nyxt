3cd7d88ea472c06514e0adb9e771cbd39e66caef
John Mercouris <john@mercouris.email>
2020-06-10 16:58:31 +0200

    text-buffer.lisp: implement movement commands

--- libraries/text-buffer/text-buffer.lisp
+++ libraries/text-buffer/text-buffer.lisp
@@ -2,13 +2,10 @@
 
 (defclass text-buffer (cluffer-simple-line:line) ())
 
-(defclass cursor (cluffer-simple-line::right-sticky-cursor) ())
-
-(defmethod move-forward-word ((cursor cursor))
-  (print "moving forwards by a word"))
-
-(defmethod move-backward-word ((cursor cursor))
-  (print "moving backwards by a word"))
+(defclass cursor (cluffer-simple-line::right-sticky-cursor)
+  ((word-separation-characters
+    :accessor word-separation-characters
+    :initform '(":" "/" "-" "." " " " "))))
 
 (defmethod string-representation ((buffer text-buffer))
   (with-output-to-string (out)
@@ -16,6 +13,71 @@
                (write-string string out))
          (cluffer:items buffer))))
 
-(defmethod delete-item-backwards ((cursor cursor))
-  (cluffer:backward-item cursor)
-  (cluffer:delete-item cursor))
+(defmethod safe-forward ((cursor cursor))
+  (unless (cluffer:end-of-line-p cursor)
+    (cluffer:forward-item cursor)
+    t))
+
+(defmethod safe-backward ((cursor cursor))
+  (unless (cluffer:beginning-of-line-p cursor)
+    (cluffer:backward-item cursor)
+    t))
+
+(defmethod delete-item-backward ((cursor cursor))
+  (when (safe-backward cursor)
+    (cluffer:delete-item cursor)
+    t))
+
+(defmethod delete-item-forward ((cursor cursor))
+  (unless (cluffer:end-of-line-p cursor)
+    (cluffer:delete-item cursor)
+    t))
+
+(defmethod move-forward-word ((cursor cursor))
+  (unless (cluffer:end-of-line-p cursor)
+    (if (intersection (word-separation-characters cursor)
+                      (list (cluffer:item-after-cursor cursor))
+                      :test #'equal)
+        (loop while (intersection (word-separation-characters cursor)
+                                  (list (cluffer:item-after-cursor cursor))
+                                  :test #'equal)
+              do (cluffer:forward-item cursor)
+              until (cluffer:end-of-line-p cursor))
+        (loop while (not (intersection (word-separation-characters cursor)
+                                       (list (cluffer:item-after-cursor cursor))
+                                       :test #'equal))
+              do (cluffer:forward-item cursor)
+              until (cluffer:end-of-line-p cursor))))
+  (cluffer:cursor-position cursor))
+
+(defmethod move-backward-word ((cursor cursor))
+  (unless (cluffer:beginning-of-line-p cursor)
+    (if (intersection (word-separation-characters cursor)
+                      (list (cluffer:item-before-cursor cursor))
+                      :test #'equal)
+        (loop while (intersection (word-separation-characters cursor)
+                                  (list (cluffer:item-before-cursor cursor))
+                                  :test #'equal)
+              do (cluffer:backward-item cursor)
+              until (cluffer:beginning-of-line-p cursor))
+        (loop while (not (intersection (word-separation-characters cursor)
+                                       (list (cluffer:item-before-cursor cursor))
+                                       :test #'equal))
+              do (cluffer:backward-item cursor)
+              until (cluffer:beginning-of-line-p cursor))))
+  (cluffer:cursor-position cursor))
+
+(defmethod delete-backward-word ((cursor cursor))
+  (dotimes (i (- (cluffer:cursor-position cursor) (move-backward-word cursor)))
+    (cluffer:delete-item cursor)))
+
+(defmethod delete-forward-word ((cursor cursor))
+  (dotimes (i (abs (- (cluffer:cursor-position cursor) (move-forward-word cursor))))
+    (delete-item-backward cursor)))
+
+(defmethod kill-forward-line ((cursor cursor))
+  (loop while (delete-item-forward cursor)))
+
+(defmethod insert-string ((cursor cursor) string)
+  (loop for char across string do
+        (cluffer:insert-item cursor (string char))))
