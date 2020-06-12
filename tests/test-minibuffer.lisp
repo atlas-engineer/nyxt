5b810fd93b691583dc7722553e6667a68b2e9fab
Pierre Neidhardt <mail@ambrevar.xyz>
2020-06-12 16:57:23 +0200

    tests/test-minibuffer: Fix minibuffer-mode package.

--- tests/test-minibuffer.lisp
+++ tests/test-minibuffer.lisp
@@ -9,32 +9,32 @@
 
 (subtest "Backwards word"
   (is 0
-      (nyxt::backwards-word-position "hello" 4))
+      (nyxt/minibuffer-mode::backwards-word-position "hello" 4))
   (is 0
-      (nyxt::backwards-word-position "a b/c d:hello" 1))
+      (nyxt/minibuffer-mode::backwards-word-position "a b/c d:hello" 1))
   (is 0
-      (nyxt::backwards-word-position "a////" 4)
+      (nyxt/minibuffer-mode::backwards-word-position "a////" 4)
       "move past many word delimiters when we are on it.")
   (is 0
-      (nyxt::backwards-word-position "a////" 5)
+      (nyxt/minibuffer-mode::backwards-word-position "a////" 5)
       "move past many word delimiters when we are ahead of it (real world).")
   (is 4
-      (nyxt::backwards-word-position "a///b:: " 8)
+      (nyxt/minibuffer-mode::backwards-word-position "a///b:: " 8)
       "watch out single letters between delimiters.")
   (is 0
-      (nyxt::backwards-word-position "a///b:: " 4)
+      (nyxt/minibuffer-mode::backwards-word-position "a///b:: " 4)
       "watch out single letters between delimiters.")
   (is 2
-      (nyxt::backwards-word-position "a/hello" 5))
+      (nyxt/minibuffer-mode::backwards-word-position "a/hello" 5))
   (is 2
-      (nyxt::backwards-word-position "a/hello" 100))
+      (nyxt/minibuffer-mode::backwards-word-position "a/hello" 100))
   (is 4
-      (nyxt::backwards-word-position "a/b/hello" 7))
+      (nyxt/minibuffer-mode::backwards-word-position "a/b/hello" 7))
   (is 4
-      (nyxt::backwards-word-position "a:b hello" 7)))
+      (nyxt/minibuffer-mode::backwards-word-position "a:b hello" 7)))
 
 (subtest "Delete backwards word"
-  (is "foo/" (nyxt::%delete-backwards-word "foo/bar" 7))
-  (is "foo/" (nyxt::%delete-backwards-word "foo/bar//" 9)))
+  (is "foo/" (nyxt/minibuffer-mode::%delete-backwards-word "foo/bar" 7))
+  (is "foo/" (nyxt/minibuffer-mode::%delete-backwards-word "foo/bar//" 9)))
 
 (finalize)
