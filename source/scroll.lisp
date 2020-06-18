d0d9c552135d01071ba05aca3346707274eb4aa5
John Mercouris <john@mercouris.email>
2020-06-18 16:32:23 +0200

    scroll.lisp: move top-level parenscript functions to inline pflet

--- source/scroll.lisp
+++ source/scroll.lisp
@@ -1,65 +1,57 @@
 (in-package :nyxt/web-mode)
 
-(define-parenscript %scroll-to-top ()
-  (ps:chain window (scroll-by 0 (- (ps:chain document body scroll-height)))))
-
-(define-parenscript %scroll-to-bottom ()
-  (ps:chain window (scroll-by 0 (ps:chain document body scroll-height))))
-
-(define-parenscript %scroll-down (&key (scroll-distance (scroll-distance (current-buffer))))
-  (ps:chain window (scroll-by 0 (ps:lisp scroll-distance))))
-
-(define-parenscript %scroll-up (&key (scroll-distance (scroll-distance (current-buffer))))
-  (ps:chain window (scroll-by 0 (ps:lisp (- scroll-distance)))))
-
-(define-parenscript %scroll-left (&key (horizontal-scroll-distance
-                                        (horizontal-scroll-distance (current-buffer))))
-  (ps:chain window (scroll-by (ps:lisp (- horizontal-scroll-distance)) 0)))
-
-(define-parenscript %scroll-right (&key (horizontal-scroll-distance
-                                         (horizontal-scroll-distance (current-buffer))))
-  (ps:chain window (scroll-by (ps:lisp horizontal-scroll-distance) 0)))
-
 (define-command scroll-to-top ()
   "Scroll to the top of the current page."
-  (%scroll-to-top))
+  (pflet ((scroll-to-top ()
+            (ps:chain window (scroll-by 0 (- (ps:chain document body scroll-height))))))
+    (scroll-to-top)))
 
 (define-command scroll-to-bottom ()
   "Scroll to the bottom of the current page."
-  (%scroll-to-bottom))
+  (pflet ((scroll-to-bottom ()
+            (ps:chain window (scroll-by 0 (ps:chain document body scroll-height)))))
+    (scroll-to-bottom)))
 
-(define-command scroll-down ()
+(define-command scroll-down (&key (scroll-distance (scroll-distance (current-buffer))))
   "Scroll down the current page.
 The amount scrolled is determined by the buffer's `scroll-distance'."
-  (%scroll-down))
+  (pflet ((scroll-down ()
+            (ps:chain window (scroll-by 0 (ps:lisp scroll-distance)))))
+    (scroll-down)))
 
-(define-command scroll-up ()
+(define-command scroll-up (&key (scroll-distance (scroll-distance (current-buffer))))
   "Scroll up the current page.
 The amount scrolled is determined by the buffer's `scroll-distance'."
-  (%scroll-up))
+  (pflet ((scroll-up ()
+            (ps:chain window (scroll-by 0 (ps:lisp (- scroll-distance))))))
+    (scroll-up)))
 
-(define-command scroll-left ()
+(define-command scroll-left (&key (horizontal-scroll-distance
+                                   (horizontal-scroll-distance (current-buffer))))
   "Scroll left the current page.
 The amount scrolled is determined by the buffer's `horizontal-scroll-distance'."
-  (%scroll-left))
+  (pflet ((scroll-left ()
+            (ps:chain window (scroll-by (ps:lisp (- horizontal-scroll-distance)) 0))))
+    (scroll-left)))
 
-(define-command scroll-right ()
+(define-command scroll-right (&key (horizontal-scroll-distance
+                                    (horizontal-scroll-distance (current-buffer))))
   "Scroll right the current page.
 The amount scrolled is determined by the buffer's `horizontal-scroll-distance'."
-  (%scroll-right))
-
-(define-parenscript %scroll-page-down ()
-  (ps:chain window (scroll-by 0 (* (ps:lisp (page-scroll-ratio (current-buffer)))
-                                   (ps:@ window inner-height)))))
-
-(define-parenscript %scroll-page-up ()
-  (ps:chain window (scroll-by 0 (* (ps:lisp (page-scroll-ratio (current-buffer)))
-                                   (- (ps:@ window inner-height))))))
+  (pflet ((scroll-right ()
+            (ps:chain window (scroll-by (ps:lisp horizontal-scroll-distance) 0))))
+    (scroll-right)))
 
 (define-command scroll-page-down ()
   "Scroll down by one page height."
-  (%scroll-page-down))
+  (pflet ((scroll-page-down ()
+            (ps:chain window (scroll-by 0 (* (ps:lisp (page-scroll-ratio (current-buffer)))
+                                             (ps:@ window inner-height))))))
+    (scroll-page-down)))
 
 (define-command scroll-page-up ()
   "Scroll up by one page height."
-  (%scroll-page-up))
+  (pflet ((scroll-page-up ()
+            (ps:chain window (scroll-by 0 (* (ps:lisp (page-scroll-ratio (current-buffer)))
+                                             (- (ps:@ window inner-height)))))))
+    (scroll-page-up)))
