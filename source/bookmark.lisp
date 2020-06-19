f47f1af643e54392f030a973e82be930e11fb538
John Mercouris <john@mercouris.email>
2020-06-19 14:11:06 +0200

    bookmark.lisp: fix fuzzy-match of tags

--- source/bookmark.lisp
+++ source/bookmark.lisp
@@ -164,7 +164,7 @@ This can be useful to let the user select no tag when returning directly."
     (when with-empty-tag
       (push "" tags))
     (lambda (minibuffer)
-      (fuzzy-match (word-at-cursor minibuffer) tags))))
+      (fuzzy-match (text-buffer::word-at-cursor (input-cursor minibuffer)) tags))))
 
 (define-command insert-tag (&optional (minibuffer (current-minibuffer)))
   "Replace current work with selected tag."
