c1656decd22f0d18f48730a06179570eea0de221
Pierre Neidhardt <mail@ambrevar.xyz>
2020-07-08 13:14:20 +0200

    reading-line-mode: Compute z-index value logically instead of hardcoding it.

--- source/reading-line-mode.lisp
+++ source/reading-line-mode.lisp
@@ -27,7 +27,7 @@ track of their reading position."
                         :left "0"
                         :width "100%"
                         :background-color "gray"
-                        :z-index "2147483647" ; 32 bit signed integer max
+                        :z-index #.(format nil "~s" (1- (expt 2 31))) ; 32 bit signed integer max
                         :opacity "15%"
                         :height "20px")))
           :documentation "The CSS applied to the reading line.")
