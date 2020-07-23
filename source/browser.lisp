58c1a451c36ec826ecf5a071dc852445371bcd5f
John Mercouris <john@mercouris.email>
2020-07-23 16:32:15 +0200

    browser.lisp: use flet for lisp-url
    this makes it clearer what the URL is supposed to do when invoked

--- source/browser.lisp
+++ source/browser.lisp
@@ -695,25 +695,26 @@ The following example does a few things:
   (echo-warning "JavaScript error: ~a" condition))
 
 (defun format-status (window)
-  (let ((buffer (current-buffer window)))
-    (str:concat
-     (markup:markup
-      (:b (format nil "[~{~a~^ ~}]"
-                  (mapcar (lambda (m) (str:replace-all "-mode" ""
-                                                       (str:downcase
-                                                        (class-name (class-of m)))))
-                          (modes buffer))))
-      (:span :class "status-menu"
-             (:a :class "button" :title "Back" :href "lisp://%28nyxt%2Fweb-mode%3Ahistory-backwards%29" "←")
-             (:a :class "button" :title "Refresh" :href "lisp://%28nyxt%3Areload-current-buffer%29" "↺")
-             (:a :class "button" :title "Forwards" :href "lisp://%28nyxt%2Fweb-mode%3Ahistory-forwards%29" "→")))
-     (:span :class (when (eq (slot-value buffer 'load-status) :loading) "loader") "")
-     (:span (if (eq (slot-value buffer 'load-status) :loading) "Loading: " ""))
-     (:span
-      (format nil " ~a — ~a"
-              (object-display (url buffer))
-              (title buffer))))))
-
+  (flet ((lisp-url (data)
+           (str:concat "lisp://" (quri:url-encode data))))
+    (let ((buffer (current-buffer window)))
+      (str:concat
+       (markup:markup
+        (:span (format nil "[~{~a~^ ~}]"
+                       (mapcar (lambda (m) (str:replace-all "-mode" ""
+                                                            (str:downcase
+                                                             (class-name (class-of m)))))
+                               (modes buffer))))
+        (:span :class "status-menu"
+               (:a :class "button" :title "Backwards" :href (lisp-url "(nyxt/web-mode:history-backwards)") "←")
+               (:a :class "button" :title "Reload" :href (lisp-url "(nyxt:reload-current-buffer)") "↺")
+               (:a :class "button" :title "Forwards" :href (lisp-url "(nyxt/web-mode:history-forwards)") "→"))
+        (:span :class (when (eq (slot-value buffer 'load-status) :loading) "loader") "")
+        (:span (if (eq (slot-value buffer 'load-status) :loading) "Loading: " ""))
+        (:span
+         (format nil " ~a — ~a"
+                 (object-display (url buffer))
+                 (title buffer))))))))
 
 (defun print-message (message &optional window)
   (let ((window (or window (current-window))))
