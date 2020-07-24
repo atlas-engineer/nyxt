07b90b46780dff755323439fb6c9b283b730f44e
Pierre Neidhardt <mail@ambrevar.xyz>
2020-07-24 09:18:20 +0200

    Allow defaulting to "no" in with-confirm.

--- source/macro.lisp
+++ source/macro.lisp
@@ -103,27 +103,28 @@ Example:
        (with-result* ,(rest bindings) ,@body))))
 
 (export-always '*yes-no-choices*)
-(defparameter *yes-no-choices* '("yes" "no")
+(defparameter *yes-no-choices* '(:yes "yes" :no "no")
   "The suggestions when asking the user for a yes/no choice.
-The first one is the 'yes' choice, the second is 'no'.")
+See `with-confirm'.
+The first suggestion poses as a default.")
 
 (defun yes-no-suggestion-filter ()
   (lambda (minibuffer)
-    (fuzzy-match (input-buffer minibuffer) *yes-no-choices*)))
+    (fuzzy-match (input-buffer minibuffer) (sera:plist-values *yes-no-choices*))))
 
 (defun confirmed-p (answer)
-  (string-equal answer (first *yes-no-choices*)))
+  (string-equal answer (getf *yes-no-choices* :yes)))
 
 (export-always 'with-confirm)
 (defmacro with-confirm (prompt &body body)
   "Ask the user for confirmation before executing BODY.
 PROMPT is a list fed to `format nil'.
 
-Example usage:
+Example usage defaulting to \"no\":
 
+\(let ((*yes-no-choices* '(:no \"no\" :yes \"yes\")))
   (with-confirm (\"Are you sure to kill ~a buffers?\" count)
-     (delete-buffers))
-"
+     (delete-buffers)))"
   `(with-result (answer (read-from-minibuffer
                          (make-minibuffer
                           :input-prompt (format nil ,@prompt)
