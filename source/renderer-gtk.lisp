c69893eda6ea4e8a42f104e233ba7414b0b4e870
Pierre Neidhardt <mail@ambrevar.xyz>
2020-08-06 18:55:54 +0200

    GTK: Remove upstreamed g-tls-certificate-flags FFI.

--- source/renderer-gtk.lisp
+++ source/renderer-gtk.lisp
@@ -870,18 +870,3 @@ As a second value, return the current buffer index starting from 0."
   (webkit:webkit-web-view-go-to-back-forward-list-item
    (gtk-object buffer)
    (webkit-history-entry-gtk-object history-entry)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; See https://github.com/Ferada/cl-cffi-gtk/issues/37.
-(in-package :gio)
-(define-g-flags "GTlsCertificateFlags" g-tls-certificate-flags
-   (:export t
-    :type-initializer "g_tls_certificate_flags_get_type")
-   (:unknown-ca 1)
-   (:bad-identity 2)
-   (:not-activated 4)
-   (:expired 8)
-   (:revoked 16)
-   (:insecure 32)
-   (:generic-error 64)
-   (:validate-all 128))
