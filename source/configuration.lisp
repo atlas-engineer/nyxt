098d7359e0648ce5745031c93d5ccc0caec63010
Pierre Neidhardt <mail@ambrevar.xyz>
2020-08-12 12:41:14 +0200

    Remove unused export-class.

--- source/configuration.lisp
+++ source/configuration.lisp
@@ -20,29 +20,6 @@ HyperSpec:
      (deftype ,from () ',to)
      (find-class ',from)))
 
-;; While we don't use this macro yet, it could prove useful if we decide to use
-;; a `defclass' macro other than serapeum's.
-(defmacro export-class (class-symbol &optional
-                                       (package nil package-supplied?)
-                                       (accessor-fn #'identity))
-  "Export class and all its readers, writers and accessors.
-Accessor symbols are derived from ACCESSOR-FN applied to the slot name."
-  (let* ((slot-definitions (closer-mop:class-direct-slots (find-class class-symbol)))
-         (slot-readers (mapcar #'car (mapcar #'closer-mop:slot-definition-readers slot-definitions)))
-         (slot-writers (mapcar #'cadar (mapcar #'closer-mop:slot-definition-writers slot-definitions)))
-         (slot-accessors (delete-if-not (lambda (sym)
-                                          (and (typep (symbol-function sym) 'generic-function)
-                                               (some (lambda (method)
-                                                       (equal (closer-mop:method-lambda-list method)
-                                                              (list class-symbol)))
-                                                     (closer-mop:generic-function-methods (symbol-function 'active-buffer)))))
-                                        (mapcar accessor-fn (mapcar #'closer-mop:slot-definition-name slot-definitions))))
-         (syms (delete-duplicates (append slot-readers slot-writers slot-accessors))))
-    `(progn
-       (eval-when (:compile-toplevel :load-toplevel :execute)
-         (export ',class-symbol)
-         (export ',syms ,@(and package-supplied? (list package)))))))
-
 (defmacro with-class ((class-sym override-sym) &body body)
   "Dynamically override the class corresponding to CLASS-SYM by OVERRIDE-SYM.
 The class is restored when exiting BODY."
