ab6b954c484399d1698c7130661750a5bed3b587
Pierre Neidhardt <mail@ambrevar.xyz>
2020-08-29 12:30:05 +0200

    GTK: Fix duplicate web-context accessor.

--- source/renderer-gtk.lisp
+++ source/renderer-gtk.lisp
@@ -36,6 +36,7 @@ want to change the behaviour of modifiers, for instance swap 'control' and
   ((modifier-translator #'my-translate-modifiers)))")
    (web-context nil
                 :type t
+                :accessor nil
                 :export nil
                 :documentation "Single instantiation of our custom web context.")
    (data-manager-path (make-instance 'data-manager-path
