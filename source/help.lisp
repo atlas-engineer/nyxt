27777516ce4a41255da8169c90a23c5146018bb1
John Mercouris <john@mercouris.email>
2020-09-09 14:38:52 +0200

    help.lisp: write config.lisp

--- source/help.lisp
+++ source/help.lisp
@@ -217,8 +217,9 @@ A command is a special kind of function that can be called with
   (with-result (input (read-from-minibuffer
                        (make-minibuffer
                         :input-prompt (format nil "Configure slot value ~a" slot))))
-    (eval `(define-configuration ,class
-             ((,slot (read-from-string ,input)))))))
+    (let ((configuration-form `(define-configuration ,class
+                                 ((,slot (read-from-string ,input))))))
+      (eval configuration-form))))
 
 (define-command describe-slot ()
   "Inspect a slot and show it in a help buffer."
