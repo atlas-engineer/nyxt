b25fd7f279ded38f66f79b0665114b372880ee36
John Mercouris <john@mercouris.email>
2020-09-21 16:22:01 +0200

    system.lisp: add simple list-systems command

--- source/system.lisp
+++ source/system.lisp
@@ -2,3 +2,20 @@
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
 (in-package :nyxt)
+
+(define-command list-systems ()
+  "List systems available."
+  (let ((buffer (make-internal-buffer :title "*Systems*")))
+    (let* ((content
+             (markup:markup
+              (:style (style buffer))
+              (:h1 "Systems")
+              (:body
+               (loop for message in (ql:system-list)
+                     collect (markup:markup (:div
+                                             (:p message)))))))
+           (insert-content (ps:ps (setf (ps:@ document body |innerHTML|)
+                                        (ps:lisp content)))))
+      (ffi-buffer-evaluate-javascript buffer insert-content))
+    (set-current-buffer buffer)
+    buffer))
