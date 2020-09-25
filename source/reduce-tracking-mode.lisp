263fc6ddd523210dcd030dc9b339c1b3e0679454
Pierre Neidhardt <mail@ambrevar.xyz>
2020-09-25 16:07:49 +0200

    Add reduce-tracking-mode.

---
+++ source/reduce-tracking-mode.lisp
@@ -0,0 +1,26 @@
+;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
+(uiop:define-package :nyxt/reduce-tracking-mode
+  (:use :common-lisp :trivia :nyxt)
+  (:documentation "Mode to mitigate fingerprinting."))
+(in-package :nyxt/reduce-tracking-mode)
+
+(define-mode reduce-tracking-mode ()
+  "Set specific settings in the web view in order to mitigate fingerprinting,
+(how third-party trackers attempt to indentify you."
+  ((preferred-languages '("en_US")
+                        :type list-of-strings
+                        :documentation "The list of languages that will be sent as
+part of the Accept-Language HTTP header.")
+   (destructor
+    (lambda (mode)
+      (ffi-set-preferred-languages (buffer mode)
+                                   (list (first
+                                          (str:split
+                                           "."
+                                           (or (uiop:getenv "LANG") "")))))))
+   (constructor
+    (lambda (mode)
+      (ffi-set-preferred-languages (buffer mode)
+                                   (preferred-languages mode))))))
