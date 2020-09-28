731833001ec2d74500af9c11192998a1dfbb6c7a
Pierre Neidhardt <mail@ambrevar.xyz>
2020-09-28 13:01:49 +0200

    Work around page load issues when calling unzoom-page.

--- source/web-mode.lisp
+++ source/web-mode.lisp
@@ -431,8 +431,10 @@ Otherwise go forward to the only child."
   ;; can only be done after the URL has been loaded which is a bit of a
   ;; kludge.  Instead we could add an FFI endpoint,
   ;; e.g. webkit_web_view_set_zoom_level.
-  (unzoom-page :buffer (buffer mode)
-               :ratio (current-zoom-ratio (buffer mode)))
+
+  ;; TODO: Temporarily commented out to work with synchronous minibuffers.
+  ;; (unzoom-page :buffer (buffer mode)
+  ;;              :ratio (current-zoom-ratio (buffer mode)))
   url)
 
 (defmethod nyxt:object-string ((node htree:node))
