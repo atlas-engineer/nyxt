6582d615d484037cee3c360c6d8ae25ba0492c71
Pierre Neidhardt <mail@ambrevar.xyz>
2020-10-03 13:24:31 +0200

    Move *default-proxy* to the `proxy' proxy-mode slot.

--- source/proxy-mode.lisp
+++ source/proxy-mode.lisp
@@ -6,15 +6,6 @@
   (:documentation "Proxy mode (e.g. Tor et al.)"))
 (in-package :nyxt/proxy-mode)
 
-(trivial-package-local-nicknames:add-package-local-nickname :sera :serapeum)
-
-(serapeum:export-always '*default-proxy*)
-(defparameter *default-proxy*
-  (make-instance 'proxy
-                 :server-address (quri:uri "socks5://localhost:9050")
-                 :allowlist '("localhost" "localhost:8080")
-                 :proxied-downloads-p t))
-
 (define-mode proxy-mode ()
   "Enable forwarding of all network requests to a specific host.
 As for every mode, it only applies to the current buffer.  If you want to enable
@@ -22,15 +13,18 @@ a proxy for all buffers, add it to the list of default modes.
 
 Example to use Tor as a proxy both for browsing and downloading:
 
-\(setf nyxt/proxy-mode:*default-proxy*
-  (make-instance 'proxy
-                 :server-address (quri:uri \"socks5://localhost:9050\")
-                 :allowlist '(\"localhost\" \"localhost:8080\")
-                 :proxied-downloads-p t))
+\(define-configuration nyxt/proxy-mode:proxy-mode
+  ((proxy (make-instance 'proxy
+                         :server-address (quri:uri \"socks5://localhost:9050\")
+                         :allowlist '(\"localhost\" \"localhost:8080\")
+                         :proxied-downloads-p t))))
 
 \(define-configuration buffer
   ((default-modes (append '(proxy-mode) %slot-default))))"
-  ((proxy *default-proxy*
+  ((proxy (make-instance 'proxy
+                 :server-address (quri:uri "socks5://localhost:9050")
+                 :allowlist '("localhost" "localhost:8080")
+                 :proxied-downloads-p t)
           :type proxy)
    (destructor
     (lambda (mode)
