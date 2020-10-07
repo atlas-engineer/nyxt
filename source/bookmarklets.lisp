3950cf016f129046df15a250ef81f68a061a91fd
John Mercouris <john@mercouris.email>
2020-10-07 16:48:41 +0200

    bookmarklets.lisp: refine bookmarklet macro

--- source/bookmarklets.lisp
+++ source/bookmarklets.lisp
@@ -12,22 +12,21 @@
   (:documentation "Bookmarkets with an origin outside of Nyxt are
   sourced from here."))
 
-(in-package :nyxt/bookmarklets)
+
+(in-package :nyxt)
 
 (defmacro define-bookmarklet (name text)
   `(defparameter ,name ,text "Bookmarklet."))
 
-(define-bookmarklet color-internal-external-links
-  "(function(){var i,x; for (i=0;x=document.links[i];++i)x.style.color=['blue','red','orange'][sim(x,location)]; function sim(a,b) { if (a.hostname!=b.hostname) return 0; if (fixPath(a.pathname)!=fixPath(b.pathname) || a.search!=b.search) return 1; return 2; } function fixPath(p){ p = (p.charAt(0)=='/' ? '' : '/') + p;/*many browsers*/ p=p.split('?')[0];/*opera*/ return p; } })()")
-
-(in-package :nyxt)
-
 (defmacro define-bookmarklet-command (name documentation source)
-  `(define-command ,name (&optional (buffer (current-buffer)))
-     ,documentation
-     (ffi-buffer-evaluate-javascript-async buffer ,source)))
+  (let ((namespaced-name (intern (string name) (find-package 'nyxt/bookmarklets))))
+    `(progn
+       (define-bookmarklet ,namespaced-name ,source)
+       (define-command ,name (&optional (buffer (current-buffer)))
+         ,documentation
+         (ffi-buffer-evaluate-javascript-async buffer ,namespaced-name)))))
 
 (define-bookmarklet-command color-internal-external-links
   "Color internal links red, external links blue, and in-page links orange."
-  nyxt/bookmarklets::color-internal-external-links)
+  "(function(){var i,x; for (i=0;x=document.links[i];++i)x.style.color=['blue','red','orange'][sim(x,location)]; function sim(a,b) { if (a.hostname!=b.hostname) return 0; if (fixPath(a.pathname)!=fixPath(b.pathname) || a.search!=b.search) return 1; return 2; } function fixPath(p){ p = (p.charAt(0)=='/' ? '' : '/') + p;/*many browsers*/ p=p.split('?')[0];/*opera*/ return p; } })()")
 
