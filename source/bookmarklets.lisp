b83f4317bc2655a813088c6a48fd5f5125299d4e
John Mercouris <john@mercouris.email>
2020-10-08 14:02:02 +0200

    bookmarkletslisp: fix typo

--- source/bookmarklets.lisp
+++ source/bookmarklets.lisp
@@ -35,7 +35,7 @@
   "(function(){var s,F,j,f,i; s = \"\"; F = document.forms; for(j=0; j<F.length; ++j) { f = F[j]; for (i=0; i<f.length; ++i) { if (f[i].type.toLowerCase() == \"password\") s += f[i].value + \"\n\"; } } if (s) alert(\"Passwords in forms on this page:\n\n\" + s); else alert(\"There are no passwords in forms on this page.\");})();")
 
 (define-bookmarklet-command show-hidden-form-elements
-  "Sohw hidden form elements."
+  "Show hidden form elements."
   "(function(){var i,f,j,e,div,label,ne; for(i=0;f=document.forms[i];++i)for(j=0;e=f[j];++j)if(e.type==\"hidden\"){ D=document; function C(t){return D.createElement(t);} function A(a,b){a.appendChild(b);} div=C(\"div\"); label=C(\"label\"); A(div, label); A(label, D.createTextNode(e.name + \": \")); e.parentNode.insertBefore(div, e); e.parentNode.removeChild(e); ne=C(\"input\");/*for ie*/ ne.type=\"text\"; ne.value=e.value; A(label, ne); label.style.MozOpacity=\".6\"; --j;/*for moz*/}})()")
 
 (define-bookmarklet-command enlarge-textareas
