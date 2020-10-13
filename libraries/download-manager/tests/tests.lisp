a1cd16162755d1a1c15cc86fe2968ffadeb8273d
Pierre Neidhardt <mail@ambrevar.xyz>
2020-10-13 17:55:55 +0200

    libraries/download-manager: Fix test.

--- libraries/download-manager/tests/tests.lisp
+++ libraries/download-manager/tests/tests.lisp
@@ -3,14 +3,16 @@
 
 (in-package :cl-user)
 
-(let ((uris '("https://abcl.org"
-              "http://slack.net"
-              "https://duckduckgo.com"
-              "https://atlas.engineer")))
-  (prove:plan (length uris))
-  (dolist (uri uris)
-    (prove:ok
-     (download-manager:resolve uri)
-     (format nil "Able to download <~a>~%" uri))))
+(prove:plan nil)
+
+(prove:subtest "Simple HTTP/HTTPS downloads"
+  (let ((uris '("https://abcl.org"
+                "http://slack.net"
+                "https://duckduckgo.com"
+                "https://atlas.engineer")))
+    (dolist (uri uris)
+      (prove:ok
+       (download-manager:resolve (quri:uri uri))
+       (format nil "Able to download <~a>~%" uri)))))
 
 (prove:finalize)
