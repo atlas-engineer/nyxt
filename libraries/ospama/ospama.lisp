7b59d3278d14b89a32acf0eb0b8a585015e66e33
Pierre Neidhardt <mail@ambrevar.xyz>
2020-10-19 10:44:28 +0200

    libraries/ospama: Detect manager and create `manager' class.

--- libraries/ospama/ospama.lisp
+++ libraries/ospama/ospama.lisp
@@ -3,6 +3,12 @@
 
 (in-package :ospama)
 
+(define-class manager ()
+  ((path ""))
+  (:export-class-name-p t)
+  (:export-accessor-names-p t)
+  (:accessor-name-transformer #'class*:name-identity))
+
 (define-class os-package ()
   ((name "")
    (version "")
@@ -15,12 +21,30 @@
   (:accessor-name-transformer #'class*:name-identity))
 
 (export-always '*manager*)
-(defvar *manager* :guix ;; (serapeum:resolve-executable "guix") ; TODO: Set this automatically.
-  "The currently selected package manager.")
+(defvar *manager* nil
+  "The currently selected package manager.
+If unset, it will default to the first `*detected-managers*'.")
+
+(export-always '*detected-managers*)
+(defvar *detected-managers* '()
+  "The list of keywords corresponding to the detected managers.")
+
+(defun detect-manager (name class-sym)
+  "Prepend the CLASS-SYM instance to `*detected-managers*' if NAME is an
+executable that can be found.
+NAME can be also be a path."
+  (serapeum:and-let* ((path (serapeum:resolve-executable name)))
+    (push (make-instance class-sym :path path)
+          *detected-managers*)))
+
+(defun manager ()
+  (unless *manager*
+    (setf *manager* (first *detected-managers*)))
+  *manager*)
 
 (defun run-over-packages (argument-method package-list)
   (multiple-value-bind (pre-args post-args)
-      (funcall argument-method *manager*)
+      (funcall argument-method (manager))
     (let ((command (append
                     pre-args
                     (mapcar #'name package-list)
@@ -31,28 +55,28 @@
 
 (export-always 'list-packages)
 (defun list-packages (&optional profile)
-  (manager-list-packages *manager* profile))
+  (manager-list-packages (manager) profile))
 
 (export-always 'find-os-package)
 (defun find-os-package (name)
-  (manager-find-os-package *manager* name))
+  (manager-find-os-package (manager) name))
 
 (export-always 'list-profiles)
 (defun list-profiles ()
-  (manager-list-profiles *manager*))
+  (manager-list-profiles (manager)))
 
 (export-always 'install)
 (defun install (package-list &optional profile)
-  (manager-install *manager* package-list profile))
+  (manager-install (manager) package-list profile))
 
-(defmethod manager-install ((manager t) package-list &optional profile)
+(defmethod manager-install ((manager manager) package-list &optional profile)
   (run-over-packages (lambda (manager) (install-command manager profile)) package-list))
 
 (export-always 'uninstall)
 (defun uninstall (package-list &optional profile)
-  (manager-uninstall *manager* package-list profile))
+  (manager-uninstall (manager) package-list profile))
 
-(defmethod manager-uninstall ((manager t) package-list &optional profile)
+(defmethod manager-uninstall ((manager manager) package-list &optional profile)
   (run-over-packages (lambda (manager) (uninstall-command manager profile)) package-list))
 
 ;; (defmethod list-files ((manager (eql t)) package-list)
