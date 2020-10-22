1bf93457fe4928e04ed84b5e3d9437571a382963
Pierre Neidhardt <mail@ambrevar.xyz>
2020-10-22 18:40:41 +0200

    libraries/ospama: Declare generics.

--- libraries/ospama/ospama.lisp
+++ libraries/ospama/ospama.lisp
@@ -3,7 +3,6 @@
 
 (in-package :ospama)
 
-;; TODO: Declare defgenerics.
 ;; TODO: Add more typing.
 
 (define-class manager ()
@@ -63,42 +62,62 @@ NAME can be also be a path."
     (setf *manager* (first *detected-managers*)))
   *manager*)
 
+(declaim (ftype (function (list) uiop/launch-program::process-info) run))
 (defun run (command)
-  "Return a PROCESS-INFO of the command."
-  (uiop:launch-program command
-                       :output :stream
-                       :error-output :output))
-
-(defun run-over-packages (argument-method package-list) ; TODO: This is a bit convoluted.  Simplify?
-  "Return a PROCESS-INFO of the command."
-  (multiple-value-bind (pre-args post-args)
-      (funcall argument-method (manager))
-    (let ((command (append
-                    pre-args
-                    (mapcar #'name package-list)
-                    post-args)))
-      (run command))))
-
+  "Run COMMAND over ARGS in the background.
+Use the returned `process-info' to check the output and the process termination."
+  (the (values uiop/launch-program::process-info &optional)
+       (uiop:launch-program command
+                            :output :stream
+                            :error-output :output)))
 (export-always 'list-packages)
 (defun list-packages (&optional profile)
   (manager-list-packages (manager) profile))
+
+(export-always 'manager-list-packages)
+(defgeneric manager-list-packages (manager &optional profile)
+  (:method ((manager manager) &optional profile)
+    (declare (ignorable profile))
+    (error "Unspecified manager method"))
+  (:documentation "Return the list of packages in PROFILE.
+If PROFILE is nil, return all packages."))
 
 (export-always 'list-package-outputs)
 (defun list-package-outputs ()
   (manager-list-package-outputs (manager)))
 
+(export-always 'manager-list-package-outputs)
+(defgeneric manager-list-package-outputs (manager)
+  (:method ((manager manager))
+    (error "Unspecified manager method"))
+  (:documentation "Return the list of all package outputs.
+This only needs to be implemented for package managers that support outputs."))
+
 (export-always 'find-os-package)
 (defun find-os-package (name)
   (manager-find-os-package (manager) name))
+
+(export-always 'manager-find-os-package)
+(defgeneric manager-find-os-package (manager name)
+  (:method ((manager manager) name)
+    (error "Unspecified manager method"))
+  (:documentation "Return the package matching NAME."))
 
 (export-always 'list-profiles)
 (defun list-profiles ()
   (manager-list-profiles (manager)))
 
+(export-always 'manager-list-profiles)
+(defgeneric manager-list-profiles (manager)
+  (:method ((manager manager))
+    (error "Unspecified manager method"))
+  (:documentation "Return all profiles as a list of strings."))
+
 (export-always 'list-manifests)
 (defun list-manifests ()
   (manager-list-manifests (manager)))
 
+(export-always 'manager-list-manifests)
 (defmethod manager-list-manifests ((manager manager))
   (uiop:directory-files (uiop:ensure-directory-pathname manifest-directory)))
 
@@ -106,26 +125,48 @@ NAME can be also be a path."
 (defun install (package-list &optional profile)
   (manager-install (manager) package-list profile))
 
+(export-always 'manager-install)
 (defmethod manager-install ((manager manager) package-list &optional profile)
-  (run-over-packages (lambda (manager) (install-command manager profile)) package-list))
+  (run (append (install-command manager profile) (mapcar #'name package-list))))
+
+(export-always 'install-command)
+(defgeneric install-command (manager profile)
+  (:method ((manager manager) profile)
+    (error "Unspecified manager method"))
+  (:documentation "Return the package manager command (as a list of string)
+to install a manifest."))
 
 (export-always 'install-manifest)
 (defun install-manifest (manifest &optional profile)
   (manager-install-manifest (manager) manifest profile))
 
-(defmethod install-manifest-command ((manager manager)) ; TODO: Define all defgenerics?
-  (error "Unspecified manager command"))
-
+(export-always 'manager-install-manifest)
 (defmethod manager-install-manifest ((manager manager) manifest &optional profile)
-  (run (list (lambda (manager) (install-manifest-command manager profile))
-             manifest)))
+  (run (append (install-manifest-command manager profile)
+               (list manifest))))
+
+(export-always 'install-manifest-command)
+(defgeneric install-manifest-command (manager profile)
+  (:method ((manager manager) profile)
+    (error "Unspecified manager method"))
+  (:documentation "Return the package manager command (as a list of string)
+to install a manifest."))
 
 (export-always 'uninstall)
 (defun uninstall (package-list &optional profile)
   (manager-uninstall (manager) package-list profile))
 
+(export-always 'manager-uninstall)
 (defmethod manager-uninstall ((manager manager) package-list &optional profile)
-  (run-over-packages (lambda (manager) (uninstall-command manager profile)) package-list))
+  (run (append (uninstall-command manager profile)
+               (mapcar #'name package-list))))
+
+(export-always 'uninstall-command)
+(defgeneric uninstall-command (manager profile)
+  (:method ((manager manager) profile)
+    (error "Unspecified manager method"))
+  (:documentation "Return the package manager command (as a list of string)
+to uninstall packages"))
 
 (defun list-files-recursively (directory)
   (let ((result '()))
