525145cc000e765b345ecd2420a5931ea68da566
Pierre Neidhardt <mail@ambrevar.xyz>
2020-10-23 12:19:01 +0200

    libraries/ospama: Finish Guix tests.

--- libraries/ospama/tests/test-guix.lisp
+++ libraries/ospama/tests/test-guix.lisp
@@ -2,18 +2,60 @@
 
 (prove:plan nil)
 
-(setf ospama:*manager* (make-instance 'ospama:guix-manager :path "guix"))
+(defvar *test-package-name* "hello")
+(defvar *test-profile* (uiop:resolve-absolute-location ; TODO: Can we generate a temp dir in Common Lisp?
+                        (list (uiop:temporary-directory) "ospama-tests/profile")))
+(defvar *test-manifest-file* (uiop:resolve-absolute-location 
+                              (list (uiop:temporary-directory) "ospama-tests/manifest.scm")))
 
-(prove:subtest "Manager detection"
-  (prove:is (ospama:path (ospama:manager))
-            "guix"))
+(defvar *test-manifest* "(specifications->manifest '(\"hello\"))")
 
 (prove:subtest "Package list"
-  (prove:ok (< 0 (length (ospama:list-packages (ospama:manager)))))
-  (prove:ok (typep (first (ospama:list-packages)) 'ospama:guix-package)))
+  (prove:ok (< 0 (length (ospama:list-packages))))
+  (prove:ok (typep (first (ospama:list-packages)) 'ospama:os-package)))
 
 (prove:subtest "Find package"
-  (prove:is (ospama:name (ospama:find-os-package "nyxt"))
-            "nyxt"))
+  (prove:is (ospama:name (ospama:find-os-package *test-package-name*))
+            *test-package-name*))
 
+(prove:subtest "List profiles"
+  (prove:ok (uiop:directory-exists-p (first (ospama:list-profiles)))))
+
+(prove:subtest "Install to temp profile"
+  (uiop:ensure-all-directories-exist
+   (list (uiop:pathname-parent-directory-pathname *test-profile*)))
+  (let ((process-info (ospama:install (list (ospama:find-os-package *test-package-name*))
+                                      *test-profile*)))
+    (uiop:wait-process process-info)
+    (prove:is (ospama:name (first (ospama:list-packages *test-profile*)))
+              *test-package-name*)
+    (setf process-info (ospama:uninstall (list (ospama:find-os-package *test-package-name*))
+                                         *test-profile*))
+    (uiop:wait-process process-info)
+    ;; TODO: Delete *test-profile* afterwards?
+    (prove:ok (null (ospama:list-packages *test-profile*)))))
+
+(prove:subtest "Install manifest to temp profile"
+  (uiop:ensure-all-directories-exist
+   (list (uiop:pathname-parent-directory-pathname *test-profile*)
+         (uiop:pathname-parent-directory-pathname *test-manifest-file*)))
+  (uiop:with-output-file (f *test-manifest-file* :if-exists :overwrite)
+    (write-string *test-manifest* f))
+  (let ((process-info (ospama:install-manifest *test-manifest-file*
+                                               *test-profile*)))
+    (uiop:wait-process process-info)
+    ;; TODO: Delete *test-profile* afterwards?
+    (prove:is (ospama:name (first (ospama:list-packages *test-profile*)))
+              *test-package-name*)))
+
+(prove:subtest "List files"
+  (let* ((output-list (ospama:outputs (ospama:find-os-package *test-package-name*)))
+         (file-list (ospama:list-files
+                     (list (first output-list)))))
+    (prove:is (pathname-name (first file-list))
+              "hello")
+    (prove:is (first (last (pathname-directory (first file-list))))
+              "bin")))
+
+;; TODO: Only run tests when manager is found?
 (prove:finalize)
