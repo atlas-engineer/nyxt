5d408fc848c136a3f42af8709829b38b86dc2d23
Pierre Neidhardt <mail@ambrevar.xyz>
2020-11-02 18:27:41 +0100

    CI: Build .deb package on tag push.

---
+++ build-scripts/build-ubuntu-package.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Inspired by https://gitlab.com/ralt/linux-packaging/-/blob/eae586eaad5d6448121c53412ff3f2de712b24ca/.ci/build.sh.
+
+set -xe
+
+gem install --no-document fpm &> /dev/null
+
+export PATH=~/.gem/ruby/$(ls ~/.gem/ruby)/bin:$PATH
+
+git clone --depth=1 --branch=sbcl-2.0.10 https://github.com/sbcl/sbcl.git ~/sbcl &> /dev/null
+(
+    cd ~/sbcl
+    set +e
+    sh make.sh --fancy --with-sb-linkable-runtime --with-sb-dynamic-core &> sbcl-build.log
+    code=$?
+    set -e
+    test $code = 0 || (cat sbcl-build.log && exit 1)
+
+    sh install.sh &> /dev/null
+)
+
+export SBCL_HOME=/usr/local/lib/sbcl
+
+curl -O https://beta.quicklisp.org/quicklisp.lisp && sbcl --load quicklisp.lisp --eval '(quicklisp-quickstart:install)' --eval '(ql:add-to-init-file)' --quit &> /dev/null
+rm quicklisp.lisp
+
+mkdir -p ~/common-lisp
+git clone --depth=1 https://gitlab.com/ralt/linux-packaging.git ~/common-lisp/linux-packaging/ &> /dev/null
+## Modern ASDF needed.
+git clone --depth=1 https://gitlab.common-lisp.net/asdf/asdf.git ~/common-lisp/asdf/ &> /dev/null
+
+mkdir -p ~/.config/common-lisp/source-registry.conf.d/
+echo "(:tree \"$PWD/\")" >> ~/.config/common-lisp/source-registry.conf.d/linux-packaging.conf
+
+## Prepare desktop file.
+sbcl --eval '(require "asdf")' \
+		 --load ~/quicklisp/setup.lisp \
+		 --eval "(ql:quickload :nyxt)" \
+		 --eval '(with-open-file (stream "version" :direction :output :if-exists :supersede) (format stream "~a" nyxt:+version+))' \
+		 --quit
+sed -i "s/VERSION/$(cat version)/" assets/nyxt.desktop
+
+sbcl \
+    --eval '(setf *debugger-hook* (lambda (c h) (declare (ignore h)) (format t "~A~%" c) (sb-ext:quit :unix-status -1)))' \
+    --load ~/quicklisp/setup.lisp \
+    --eval "(ql:quickload :linux-packaging)" \
+    --eval "(ql:quickload :nyxt)" \
+    --eval "(ql:quickload :nyxt/ubuntu-package)" \
+    --eval "(asdf:make :nyxt/ubuntu-package)" \
+    --quit
--- nyxt.asd
+++ nyxt.asd
@@ -1,6 +1,8 @@
 (asdf:defsystem :nyxt
   :version "2" ; Pre-release 3
   :author "Atlas Engineer LLC"
+  :homepage "https://nyxt.atlas.engineer"
+  :description "Extensible web-browser in Common Lisp"
   :license "BSD 3-Clause"
   :serial t
   :depends-on (:alexandria
@@ -291,3 +293,25 @@
   :depends-on (nyxt/class-star prove)
   :perform (asdf:test-op (op c)
                          (nyxt-run-test c "libraries/class-star/tests/")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsystem "nyxt/ubuntu-package"
+  :defsystem-depends-on ("linux-packaging")
+  :class "linux-packaging:deb"
+  :build-operation "linux-packaging:build-op"
+  :depends-on ("nyxt/gtk")
+  :package-name "nyxt"
+  :additional-dependencies ("glib-networking"
+                            "gsettings-desktop-schemas"
+                            "xclip"
+                            "enchant"
+                            ;; TODO: Remove notify-osd?
+                            "notify-osd")
+  :additional-files (("assets/nyxt.desktop" . "usr/share/applications/")
+                     ("assets/nyxt_16x16.png" . #p"usr/share/icons/hicolor/16x16/apps/nyxt.png")
+                     ("assets/nyxt_32x32.png" . #p"usr/share/icons/hicolor/32x32/apps/nyxt.png")
+                     ("assets/nyxt_128x128.png" . #p"usr/share/icons/hicolor/128x128/apps/nyxt.png")
+                     ("assets/nyxt_256x256.png" . #p"usr/share/icons/hicolor/256x256/apps/nyxt.png")
+                     ("assets/nyxt_512x512.png" . #p"usr/share/icons/hicolor/512x512/apps/nyxt.png"))
+  :build-pathname "nyxt"
+  :entry-point "nyxt:entry-point")
