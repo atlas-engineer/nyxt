2ed878cf85e0c68405e6dfc82ee992e12d8d40a2
Pierre Neidhardt <mail@ambrevar.xyz>
2020-11-07 11:52:51 +0100

    source/.dir-locals.el: Add Nyxt "define-*" to list of imenu keywords.

---
+++ source/.dir-locals.el
@@ -0,0 +1,24 @@
+((lisp-mode
+  . ((eval . (cl-flet ((enhance-imenu-lisp
+                        (&rest keywords)
+                        (dolist (keyword keywords)
+                          (add-to-list
+                           'lisp-imenu-generic-expression
+                           (list (purecopy (concat (capitalize keyword)
+                                                   (if (string= (substring-no-properties keyword -1) "s")
+                                                       "es"
+                                                       "s")))
+                                 (purecopy (concat "^\\s-*("
+                                                   (regexp-opt
+                                                    (list (concat "define-" keyword))
+                                                    t)
+                                                   "\\s-+\\(" lisp-mode-symbol-regexp "\\)"))
+                                 2)))))
+               ;; This adds the argument to the list of imenu known keywords.
+               (enhance-imenu-lisp
+                "bookmarklet-command"
+                "class"
+                "command"
+                "mode"
+                "parenscript"
+                "user-class"))))))
