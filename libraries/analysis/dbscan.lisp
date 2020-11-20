294cd690c6827fbc91f88648f1011be781bb3f99
John Mercouris <john@mercouris.email>
2020-11-20 23:17:51 +0100

    dbscan.lisp: vector distance calculation

--- libraries/analysis/dbscan.lisp
+++ libraries/analysis/dbscan.lisp
@@ -5,3 +5,11 @@
 
 ;;; dbscan.lisp -- implementation of Density-based spatial clustering
 ;;; of applications with noise (DBSCAN) algorithm
+
+;; Vector distance
+
+(defmethod distance ((vector-1 t) (vector-2 t))
+  "v = (list 1 0 5)
+   w = (list 0 2 4)
+   d(v, w) = (sqrt (1-0)^2 + (0-2)^2 + (5-4)^2)"
+  )
