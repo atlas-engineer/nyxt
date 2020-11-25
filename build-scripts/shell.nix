5b37f846853e2ab327ad116ac1d1009a93856811
John Mercouris <john@mercouris.email>
2020-11-25 13:26:54 +0100

    shell.nix: reindent, remove lispPackages

--- build-scripts/shell.nix
+++ build-scripts/shell.nix
@@ -3,7 +3,7 @@ with builtins;
 let inherit (pkgs) stdenv; in
 with pkgs;
 stdenv.mkDerivation {
-  name = "next-dev";
+  name = "nyxt-dev";
 
   nativeBuildInputs = [
     pkgs.libressl.out
@@ -11,45 +11,12 @@ stdenv.mkDerivation {
     pkgs.sbcl
   ];
 
-  # ++ (with pkgs.lispPackages; [
-  #   prove-asdf
-  #   trivial-features
-  #   alexandria
-  #   bordeaux-threads
-  #   cl-css
-  #   cl-json
-  #   cl-markup
-  #   cl-ppcre
-  #   cl-ppcre-unicode
-  #   cl-prevalence
-  #   closer-mop
-  #   dexador
-  #   enchant
-  #   iolib
-  #   local-time
-  #   log4cl
-  #   mk-string-metrics
-  #   parenscript
-  #   quri
-  #   serapeum
-  #   str
-  #   plump
-  #   swank
-  #   trivia
-  #   trivial-clipboard
-  #   trivial-features
-  #   trivial-types
-  #   unix-opts
-  #   usocket
-  # ]);
-
   buildInputs = [
     pkgs.enchant.out
     pkgs.gsettings-desktop-schemas.out
     pkgs.glib-networking.out
     pkgs.pango.out
     pkgs.cairo.out
-    pkgs.gtkd.out
     pkgs.gdk-pixbuf.out
     pkgs.gtk3.out
     pkgs.glib.out
@@ -70,7 +37,6 @@ stdenv.mkDerivation {
     pkgs.glib-networking.out
     pkgs.pango.out
     pkgs.cairo.out
-    pkgs.gtkd.out
     pkgs.gdk-pixbuf.out
     pkgs.gtk3.out
     pkgs.glib.out
@@ -78,7 +44,17 @@ stdenv.mkDerivation {
     pkgs.webkitgtk
   ];
 
-  LD_LIBRARY_PATH = with stdenv.lib; "${makeLibraryPath [ pkgs.gsettings-desktop-schemas.out pkgs.enchant.out pkgs.glib-networking.out pkgs.webkitgtk pkgs.gtk3 pkgs.pango.out pkgs.cairo.out pkgs.gdk-pixbuf.out pkgs.gtkd.out pkgs.glib.out pkgs.libfixposix.out pkgs.libressl.out ]};";
+  LD_LIBRARY_PATH = with stdenv.lib; "${makeLibraryPath [ pkgs.gsettings-desktop-schemas.out
+                                                          pkgs.enchant.out
+                                                          pkgs.glib-networking.out
+                                                          pkgs.webkitgtk
+                                                          pkgs.gtk3
+                                                          pkgs.pango.out
+                                                          pkgs.cairo.out
+                                                          pkgs.gdk-pixbuf.out
+                                                          pkgs.glib.out
+                                                          pkgs.libfixposix.out
+                                                          pkgs.libressl.out ]};";
 
   GIO_MODULE_DIR = "${pkgs.glib-networking.out}/lib/gio/modules/";
   GIO_EXTRA_MODULES = "${pkgs.glib-networking.out}/lib/gio/modules/";
