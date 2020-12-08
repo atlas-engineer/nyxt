78d3398a1fe225a5c1e9876159505d281bf92380
Tony Olagbaiye <me@fron.io>
2020-12-08 05:20:17 +0000

    nix: fix build

--- default.nix
+++ default.nix
@@ -1,15 +1,28 @@
 { pkgs ? import <nixpkgs> {}
 , src ? ./.
-, version ? "nix"
+, version ? "git"
 , ... }:
 
-pkgs.nyxt.overrideAttrs (drv: {
+let
+  nix-lisp = import (pkgs.fetchFromGitHub {
+    owner = "nix-lisp";
+    repo = "lisp-overlay";
+    rev = "99c738ad1c309b8fde6c95d62b7dab0092d0f23b";
+    sha256 = "1rb5yygsyzzc6byajzyf78dp86p9k2igqpsk561k6276mgkrfijw";
+    # date = 2020-12-08T04:57:09+00:00;
+  }) {};
+in pkgs.nyxt.overrideAttrs (drv: {
   src = drv.src.overrideAttrs (drv: {
     inherit src;
     name = pkgs.lib.replaceStrings [drv.meta.version] [version] drv.name;
 
     # Pending nixpkgs changes
     installPhase = builtins.replaceStrings ["nyxt-ext"] ["nyxt"] drv.installPhase;
+
+    # Package polyfills
+    propagatedBuildInputs = drv.propagatedBuildInputs ++ [
+      nix-lisp.calispel
+    ];
   });
   inherit version;
 })
