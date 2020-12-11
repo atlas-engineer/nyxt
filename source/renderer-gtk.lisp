a03ea7cb3e0bb4dd9892d7e7e82511171707df71
Pierre Neidhardt <mail@ambrevar.xyz>
2020-12-11 15:41:51 +0100

    Draft: Try using dummy text widget to accept input methods.  Does not work.
    Pressing control-shift-u correctly inputs an underlined "u" in the dummy text
    widget, however it will be erased on next character press, instead of expanding
    into a unicode character.

--- source/renderer-gtk.lisp
+++ source/renderer-gtk.lisp
@@ -211,7 +211,8 @@ Such contexts are not needed for internal buffers."
                   minibuffer-container minibuffer-view
                   status-buffer status-container
                   message-container message-view
-                  id key-string-buffer) window
+                  id key-string-buffer
+                  dummy-text-input) window
        (setf id (get-unique-window-identifier *browser*))
        (setf gtk-object (make-instance 'gtk:gtk-window
                                        :type :toplevel
@@ -240,6 +241,9 @@ Such contexts are not needed for internal buffers."
        (gtk:gtk-box-pack-start message-container message-view :expand t)
        (setf (gtk:gtk-widget-size-request message-container)
              (list -1 (message-buffer-height window)))
+
+       (setf dummy-text-input (make-instance 'gtk:gtk-entry))
+       (gtk:gtk-box-pack-end box-layout dummy-text-input :expand nil)
 
        (setf status-buffer (make-instance 'user-status-buffer))
        (gtk:gtk-box-pack-end box-layout status-container :expand nil)
@@ -718,6 +722,9 @@ Warning: This behaviour may change in the future."
   buffer)
 
 (define-ffi-method ffi-window-set-minibuffer-height ((window gtk-window) height)
+  (if (zerop height)
+      (gtk:gtk-widget-grab-focus (dummy-text-input window))
+      (gtk:gtk-widget-grab-focus (gtk-object window)))
   (setf (gtk:gtk-widget-size-request (minibuffer-container window))
         (list -1 height)))
 
