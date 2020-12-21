c1359b89510abdc3b00b7371f2bedb49d413165c
Pierre Neidhardt <mail@ambrevar.xyz>
2020-12-21 18:09:46 +0100

    Draft: prompt-buffer: Prepare and fix compilation.

--- source/prompt-buffer.lisp
+++ source/prompt-buffer.lisp
@@ -3,77 +3,89 @@
 
 (in-package :nyxt)
 
-(define-class prompt-buffer (user-internal-buffer)
-  ((channel (error "Channel required"))
-   (interrupt-channel (error "Interrupt channel required"))
-   (default-modes '(prompt-buffer-mode))
-   ;; TODO: Need a changed-callback?
-   ;; TODO: Need a invisible-input-p slot?
-   ;; (invisible-input-p nil
-;;                       :documentation "If non-nil, input is replaced by placeholder character.
+(defmacro define-function (name args docstring &body body)
+  "Eval ARGS and DOCSTRING then define function over the resulting lambda list
+and string.
+All ARGS are declared as `ignorable'."
+  (let ((evaluated-args (eval args))
+        (evaluated-docstring (eval docstring)))
+    `(defun ,name ,evaluated-args
+       ,evaluated-docstring
+       (declare (ignorable ,@(set-difference (mapcar (lambda (arg) (if (listp arg) (first arg) arg))
+                                                     evaluated-args)
+                                             lambda-list-keywords)))
+       ,@body)))
+
+(sera:eval-always
+  (define-class prompt-buffer (user-internal-buffer)
+    ((prompter (error "Prompter required") ; TODO: Inherit instead?
+               :type prompter:prompter)
+     (default-modes '(prompt-buffer-mode))
+     ;; TODO: Need a changed-callback?
+     ;; TODO: Need a invisible-input-p slot?
+     (invisible-input-p nil
+                        :documentation "If non-nil, input is replaced by placeholder character.
 ;; This is useful to conceal passwords.")
-   (history (minibuffer-generic-history *browser*)
-            :type (or containers:ring-buffer-reverse null)
-            :documentation "History of inputs for the minibuffer.
-If nil, no history is used.")
-   (hide-suggestion-count-p nil
-                            :documentation "Show the number of chosen suggestions
-inside brackets. It can be useful to disable, for instance for a yes/no question.")
-   (suggestion-head 0
-                    :export nil)
-   (suggestion-cursor 0
-                      :export nil)
-   (content ""
-            :accessor nil
-            :export nil
-            :documentation "The HTML content of the minibuffer.")
-   ;; TODO: Need max-lines?
-   ;; (max-lines 10
-;;               :documentation "Max number of suggestion lines to show.
-;; You will want edit this to match the changes done to `style'.")
-   (style #.(cl-css:css
-             '((* :font-family "monospace,monospace"
-                  :font-size "14px"
-                  :line-height "18px")
-               (body :border-top "4px solid dimgray"
-                     :margin "0"
-                     :padding "0 6px")
-               ("#container" :display "flex"
-                             :flex-flow "column"
-                             :height "100%")
-               ("#input" :padding "6px 0"
-                         :border-bottom "solid 1px lightgray")
-               ("#suggestions" :flex-grow "1"
-                               :overflow-y "auto"
-                               :overflow-x "auto")
-               ("#cursor" :background-color "gray"
+     (hide-suggestion-count-p nil       ; TODO: Move to `prompter' library?
+                              :documentation "Show the number of chosen suggestions
+;; inside brackets. It can be useful to disable, for instance for a yes/no question.")
+     ;; TODO: If we move selection cursor to `prompter' library, then it can be
+     ;; restored when resuming.
+     ;; (suggestion-head 0
+     ;;                  :export nil)
+     ;; (suggestion-cursor 0
+     ;;                    :export nil)
+     (content ""
+              :accessor nil
+              :export nil
+              :documentation "The HTML content of the minibuffer.")
+     ;; TODO: Need max-lines?
+     ;; (max-lines 10
+     ;;               :documentation "Max number of suggestion lines to show.
+     ;; You will want edit this to match the changes done to `style'.")
+     (style #.(cl-css:css
+               '((* :font-family "monospace,monospace"
+                    :font-size "14px"
+                    :line-height "18px")
+                 (body :border-top "4px solid dimgray"
+                       :margin "0"
+                       :padding "0 6px")
+                 ("#container" :display "flex"
+                               :flex-flow "column"
+                               :height "100%")
+                 ("#input" :padding "6px 0"
+                           :border-bottom "solid 1px lightgray")
+                 ("#suggestions" :flex-grow "1"
+                                 :overflow-y "auto"
+                                 :overflow-x "auto")
+                 ("#cursor" :background-color "gray"
+                            :color "white")
+                 ("#prompt" :padding-right "4px"
+                            :color "dimgray")
+                 (ul :list-style "none"
+                     :padding "0"
+                     :margin "0")
+                 (li :padding "2px")
+                 (.marked :background-color "darkgray"
+                          :font-weight "bold"
                           :color "white")
-               ("#prompt" :padding-right "4px"
-                          :color "dimgray")
-               (ul :list-style "none"
-                   :padding "0"
-                   :margin "0")
-               (li :padding "2px")
-               (.marked :background-color "darkgray"
-                        :font-weight "bold"
-                        :color "white")
-               (.selected :background-color "gray"
-                          :color "white")))
-          :documentation "The CSS applied to a minibuffer when it is set-up.")
-   (override-map (let ((map (make-keymap "overide-map")))
-                   (define-key map
-                     "escape"
-                     ;; We compute symbol at runtime because
-                     ;; nyxt/minibuffer-mode does not exist at
-                     ;; compile-time since it's loaded afterwards.
-                     (find-symbol (string 'cancel-input)
-                                  (find-package 'nyxt/minibuffer-mode))))
-                 :type keymap:keymap
-                 :documentation "Keymap that takes precedence over all modes' keymaps."))
-  (:export-class-name-p t)
-  (:export-accessor-names-p t)
-  (:accessor-name-transformer #'class*:name-identity)
-  (:documentation "The prompt buffer is the interface for user interactions.
+                 (.selected :background-color "gray"
+                            :color "white")))
+            :documentation "The CSS applied to a minibuffer when it is set-up.")
+     (override-map (let ((map (make-keymap "overide-map")))
+                     (define-key map
+                       "escape"
+                       ;; We compute symbol at runtime because
+                       ;; nyxt/minibuffer-mode does not exist at
+                       ;; compile-time since it's loaded afterwards.
+                       (find-symbol (string 'cancel-input)
+                                    (find-package 'nyxt/minibuffer-mode))))
+                   :type keymap:keymap
+                   :documentation "Keymap that takes precedence over all modes' keymaps."))
+    (:export-class-name-p t)
+    (:export-accessor-names-p t)
+    (:accessor-name-transformer #'class*:name-identity)
+    (:documentation "The prompt buffer is the interface for user interactions.
 Each prompt spawns a new object: this makes it possible to nest prompts , such
 as invoking `prompt-history'.
 
@@ -84,22 +96,37 @@ A prompt query is typically done as follows:
               :default-modes '(set-tag-mode minibuffer-mode)
               :suggestion-function (tag-suggestion-filter))))
   ;; Write form here in which `tags' is bound to the resulting element(s).
-  )"))
+  )")))
 
 (define-user-class prompt-buffer)
 
-(defmethod initialize-instance :after ((prompt-buffer prompt-buffer) &key)
+(defmethod initialize-instance :after ((prompt-buffer prompt-buffer) &key) ; TODO: Merge in `make-prompt-buffer'?
   (hooks:run-hook (prompt-buffer-make-hook *browser*) prompt-buffer)
   ;; We don't want to show the input in the suggestion list when invisible.
-  (unless (suggestion-function prompt-buffer)
-    ;; If we have no suggestion function, then we have no suggestions beside
-    ;; immediate input, so we must allow them as valid suggestion.
-    (setf (must-match-p prompt-buffer) nil))
   (setf (must-match-p prompt-buffer)
         (if (invisible-input-p prompt-buffer)
             t ; This way the minibuffer won't display the input as a suggestion.
             (must-match-p prompt-buffer)))
   (initialize-modes prompt-buffer))
+
+(export-always 'make-prompt-buffer)
+(define-function make-prompt-buffer (append
+                                     '(&rest args)
+                                     `(&key (window (current-window))
+                                            ,@(public-initargs 'prompt-buffer)))
+  "TODO: Complete me!"
+  (let* ((initargs (alex:remove-from-plist args :window)) ; TODO: Make window a slot or prompt-buffer? That would simplify `make-prompt-buffer' args.
+         (prompt-buffer (apply #'make-instance 'prompt-buffer
+                               initargs)))
+    (update-display prompt-buffer)
+    (push prompt-buffer (active-prompt-buffers window))
+    (show)                              ; TODO: Show should probably take an argument, no?
+    ;; TODO: Add method that returns if there is only 1 source with no filter.
+    ;; (apply #'show
+    ;;        (unless (prompter:filter prompt-buffer)
+    ;;          ;; We don't need so much height since there is no suggestion to display.
+    ;;          (list :height (minibuffer-open-single-line-height (current-window)))))
+    ))
 
 (defun show (&key (prompt-buffer (first (active-prompt-buffers (current-window)))) height)
   "Show the last active prompt-buffer, if any."
@@ -109,7 +136,7 @@ A prompt query is typically done as follows:
      (or height
          (prompt-buffer-open-height (current-window))))))
 
-(defun hide (prompt-buffer)
+(defun hide-prompt-buffer (prompt-buffer) ; TODO: Rename `hide'
   "Hide PROMPT-BUFFER and display next active one, if any."
   (match (cleanup-function prompt-buffer)
     ((guard f f) (funcall-safely f)))
@@ -141,10 +168,12 @@ A prompt query is typically done as follows:
           (:div :id "container"
                 (:div :id "prompt-input"
                       (:span :id "prompt" "")
+                      (:span :id "prompt-extra" "")
                       (:input :type "text" :id "input"))
                 (:div :id "suggestions" ""))))))
 
 (defmethod generate-prompt-html ((prompt-buffer prompt-buffer))
+  ;; TODO: Print suggestion count in "prompt-extra".
   (with-slots (suggestions marked-suggestions) prompt-buffer
     (format nil "~a~a:"
             (input-prompt prompt-buffer)
@@ -167,73 +196,45 @@ A prompt query is typically done as follows:
               (t
                "[?]")))))
 
-(defmethod generate-suggestion-html ((minibuffer minibuffer)) ; TODO: Generate suggestions as table.
-  (with-slots (suggestions suggestion-cursor) minibuffer
-    (let ((lines (max-lines minibuffer)))
-      (when (>= (- suggestion-cursor (suggestion-head minibuffer)) lines)
-        (setf (suggestion-head minibuffer)
-              (min
-               (+ (suggestion-head minibuffer) 1)
-               (length suggestions))))
-      (when (< (- suggestion-cursor (suggestion-head minibuffer)) 0)
-        (setf (suggestion-head minibuffer)
-              (max
-               (- (suggestion-head minibuffer) 1)
-               0)))
-      (markup:markup
-       (:ul (loop repeat lines
-                  for i from (suggestion-head minibuffer)
-                  for suggestion in (nthcdr i suggestions)
-                  collect
-                  (markup:markup
-                   (:li :class (let ((selected-p (= i suggestion-cursor))
-                                     (marked-p (member suggestion (marked-suggestions minibuffer)))
-                                     (head-p (= i (suggestion-head minibuffer))))
-                                 (str:join " " (delete-if #'null (list (and marked-p "marked")
-                                                                       (and selected-p "selected")
-                                                                       (and head-p "head")))))
-                        (match (object-display suggestion)
-                          ((guard s (not (str:emptyp s))) s)
-                          (_ " "))))))))))
+(defmethod generate-suggestion-html ((prompt-buffer prompt-buffer)) ; TODO: Generate suggestions as table.
+  ;; TODO: Finish me!
+  )
 
 (defmethod update-display ((prompt-buffer prompt-buffer))
-  (flet ((update-input-buffer-display ((prompt-buffer prompt-buffer))
-           "Update the display for the input buffer including the prompt and
-completion count."
-           (with-slots (suggestions marked-suggestions) prompt-buffer
-             (let ((input-text (generate-input-html prompt-buffer))
-                   (prompt-text (generate-prompt-html prompt-buffer)))
-               (evaluate-script
-                prompt-buffer
-                (ps:ps
-                  (setf (ps:chain document (get-element-by-id "prompt") |innerHTML|)
-                        (ps:lisp prompt-text))
-                  (setf (ps:chain document (get-element-by-id "input-buffer") |innerHTML|)
-                        (ps:lisp input-text)))))))
-         (update-suggestions-display ((prompt-buffer prompt-buffer))
-           (let ((suggestion-html (generate-suggestion-html prompt-buffer)))
-             (evaluate-script
-              prompt-buffer
-              (ps:ps
-                (setf (ps:chain document (get-element-by-id "suggestions") |innerHTML|)
-                      (ps:lisp suggestion-html)))))))
-    (update-input-buffer-display prompt-buffer)
-    (update-suggestions-display prompt-buffer)))
-
-
-(defmethod selection ((minibuffer minibuffer))
-  "Return the selected suggestions.
-If no suggestion is marked, return the currently selected suggestion."
-
-  (with-slots (suggestions suggestion-cursor)
-      minibuffer
-    (and suggestions
-         (object-string (nth suggestion-cursor suggestions)))))
+  ;; TODO: Finish me!
+  )
 
 (export-always 'get-marked-suggestions)
-(defmethod get-marked-suggestions ((minibuffer minibuffer))
+(defmethod get-marked-suggestions ((prompt-buffer prompt-buffer))
   "Return the list of strings for the marked suggestion in the minibuffer."
-  (mapcar #'object-string (marked-suggestions minibuffer)))
+  (mapcar #'object-string (marked-suggestions () minibuffer)))
+
+(export-always 'prompt)
+(define-function prompt (append '(&rest args)
+                                `(&key prompt-buffer
+                                       ,@(public-initargs 'prompt-buffer)))
+  "Open the prompt buffer, ready for user input.
+ARGS are passed to the prompt-buffer constructor.
+Example use:
+
+\(prompt
+  :filter #'my-suggestion-filter)
+
+See the documentation of `prompt-buffer' to know more about the options."
+  ;; TODO: Filter out args that don't fit prompter.
+  (let ((prompt (apply #'prompter:make args)))
+    (ffi-within-renderer-thread
+     *browser*
+     (lambda ()
+       ;; TODO: Pass right `args' to prompt-buffer.
+       (apply 'make-prompt-buffer (append (list :prompt prompt)
+                                            args))))
+    ;; Wait until it's destroyed and get the selections from `return-selection'.
+    (calispel:fair-alt
+      ((calispel:? (results prompt-buffer) results)
+       results)
+      ((calispel:? (interrupt-channel prompt-buffer))
+       (error 'nyxt-prompt-buffer-canceled)))))
 
 (export-always 'prompter-if-confirm)    ; TODO: Rename to `if-confirm' once `minibuffer' is gone.
 (defmacro prompter-if-confirm (prompt yes-form &optional no-form)
