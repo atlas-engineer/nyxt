d85e3b84f3e1b99c7ca66549bb881062fdab7410
Pierre Neidhardt <mail@ambrevar.xyz>
2020-12-21 18:06:11 +0100

    prompt-buffer-mode: Simplify return-selection, add cancel-input, remove bindings.
    Let's restore the bindings later.

--- source/prompt-buffer-mode.lisp
+++ source/prompt-buffer-mode.lisp
@@ -12,76 +12,19 @@
     (define-scheme "prompt-buffer"
       scheme:cua
       (list
-       "hyphen" 'self-insert-minibuffer
-       "space" 'self-insert-minibuffer
-       "C-f" 'cursor-forwards
-       "M-f" 'cursor-forwards-word
-       "C-right" 'cursor-forwards-word
-       "C-b" 'cursor-backwards
-       "M-b" 'cursor-backwards-word
-       "C-left" 'cursor-backwards-word
-       "M-d" 'delete-forwards-word
-       "M-backspace" 'delete-backwards-word
-       "right" 'cursor-forwards
-       "left" 'cursor-backwards
-       "C-d" 'delete-forwards
-       "delete" 'delete-forwards
-       "backspace" 'delete-backwards
-       "C-a" 'cursor-beginning
-       "home" 'cursor-beginning
-       "C-e" 'cursor-end
-       "end" 'cursor-end
-       "C-k" 'kill-line
-       "return" 'return-selection
-       "C-return" 'return-input
-       "C-g" 'cancel-input
-       "escape" 'cancel-input
        "C-n" 'select-next
-       "C-p" 'select-previous
-       "button4" 'select-previous
-       "button5" 'select-next
-       "down" 'select-next
-       "up" 'select-previous
-       "C-v" 'minibuffer-paste
-       "C-y" 'minibuffer-paste
-       "C-w" 'copy-suggestion
-       "C-c" 'copy-suggestion
-       "tab" 'insert-suggestion
-       "M-h" 'minibuffer-history
-       "C-space" 'minibuffer-toggle-mark
-       "shift-space" 'minibuffer-toggle-mark-backwards
-       "M-space" 'minibuffer-toggle-mark
-       "M-a" 'minibuffer-mark-all
-       "M-u" 'minibuffer-unmark-all
-       "M-m" 'minibuffer-toggle-mark-all))
+       "return" 'return-selection))
     ;; TODO: We could have VI bindings for the minibuffer too.
     ;; But we need to make sure it's optional + to have an indicator
     ;; for the mode.
     )))
 
-(define-command return-selection (&optional (minibuffer (current-minibuffer)))
-  "Return with minibuffer selection."
-  (let ((result))
-    (with-slots (must-match-p nyxt::suggestions nyxt::suggestion-cursor
-                 invisible-input-p
-                 multi-selection-p nyxt::marked-suggestions)
-        minibuffer
-      (match (or nyxt::marked-suggestions
-                 (and nyxt::suggestions
-                      (list (nth nyxt::suggestion-cursor nyxt::suggestions)))
-                 (and (not must-match-p)
-                      (list (input-buffer minibuffer))))
-        ((guard nyxt::suggestions nyxt::suggestions)
-         ;; Note that "immediate input" is also in suggestions, so it's caught here.
-         (setf nyxt::suggestions
-               (mapcar (lambda (suggestion) (if (stringp suggestion)
-                                                (str:replace-all " " " " suggestion)
-                                                suggestion))
-                       nyxt::suggestions))
-         (setf result (if multi-selection-p
-                          nyxt::suggestions
-                          (first nyxt::suggestions))))
-        (nil (when invisible-input-p
-               (setf result (input-buffer minibuffer))))))
-    (quit-minibuffer minibuffer)
-    (calispel:! (channel minibuffer) result)))
+(define-command return-selection (&optional (prompt-buffer (current-prompt-buffer)))
+  "Have the PROMT-BUFFER return the selection, then quit."
+  (prompter:return-selection (nyxt:prompter prompt-buffer))
+  (hide-prompt-buffer prompt-buffer))
+
+(define-command cancel-input (&optional (prompt-buffer (current-prompt-buffer)))
+  "Close the prompt-buffer without further action."
+  (prompter:destructor (nyxt:prompter prompt-buffer))
+  (hide-prompt-buffer prompt-buffer))
