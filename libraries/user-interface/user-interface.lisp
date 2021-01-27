a7bf17d1303c6e867f74e3952cbf7f7d8f4867b2
John Mercouris <john@mercouris.email>
2021-01-27 18:12:09 +0100

    user-interface.lisp: create ui-element class
    every ui element has a unique ID
    every ui element has a parent buffer so that it can propgate updates
    to the web view

--- libraries/user-interface/user-interface.lisp
+++ libraries/user-interface/user-interface.lisp
@@ -3,15 +3,38 @@
 
 (in-package :user-interface)
 
-(defclass button ()
+(defvar *id* 0 "Counter used to generate a unique ID.")
+
+(defun unique-id ()
+  (incf *id*))
+
+(defclass ui-element ()
+  ((id :accessor id)
+   (buffer :accessor buffer :initarg :buffer
+           :documentation "Buffer where element is drawn.")))
+
+(defmethod initialize-instance :after ((element ui-element) &key)
+  (setf (id element) (unique-id)))
+
+(defmethod object-string ((element ui-element))
+  (cl-markup:markup*
+   (object-expression element)))
+
+(defclass button (ui-element)
   ((text :initform "" :accessor text :initarg :text)
    (url :initform "" :accessor url :initarg :url)))
 
-(defmethod object-string ((button button))
-  (cl-markup:markup
-   (:a :class "button" :href (url button) (text button))))
-
 (defmethod object-expression ((button button))
-  (let ((url (url button))
-        (text (text button)))
-    `(:a :class "button" :href ,url ,text)))
+  `(:a :class "button" :href ,(url button) ,(text button)))
+
+(defclass paragraph (ui-element)
+  ((text :initform "" :initarg :text)))
+
+(defmethod (setf text) (text (paragraph paragraph))
+  (setf (slot-value paragraph 'text) text))
+
+(defmethod object-expression ((paragraph paragraph))
+  `(:p :id ,(id paragraph) ,(text paragraph)))
+
+(defmethod text ((paragraph paragraph))
+  (slot-value paragraph 'text))
