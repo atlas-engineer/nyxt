784fece64d63362ed0990060ed05a985fed79ecf
Pierre Neidhardt <mail@ambrevar.xyz>
2021-01-28 10:39:49 +0100

    libraries/history-tree: Consistently use "history" instead of "tree".

--- libraries/history-tree/tests/tests.lisp
+++ libraries/history-tree/tests/tests.lisp
@@ -9,29 +9,29 @@
 
 (prove:plan nil)
 
-(defun make-tree1 ()
-  (let ((tree (htree:make)))
+(defun make-history1 ()
+  (let ((history (htree:make)))
     (dolist (url '(
                    "http://example.root"
                    "http://example.root/A"
                    "http://example.root/A1"))
-      (htree:add-child url tree))
-    (htree:back tree)
-    (htree:add-child "http://example.root/A2" tree)
-    (htree:back tree 2)
-    (htree:add-child "http://example.root/B" tree)
-    (htree:add-child "http://example.root/B1" tree)
-    (htree:back tree)
-    (htree:add-child "http://example.root/B2" tree)
-    tree))
+      (htree:add-child url history))
+    (htree:back history)
+    (htree:add-child "http://example.root/A2" history)
+    (htree:back history 2)
+    (htree:add-child "http://example.root/B" history)
+    (htree:add-child "http://example.root/B1" history)
+    (htree:back history)
+    (htree:add-child "http://example.root/B2" history)
+    history))
 
-(defun make-tree2 ()
-  (let ((tree (htree:make)))
-    (htree:add-child "http://example.root" tree)
-    (htree:add-child "http://example.root/A" tree)
-    (htree:back tree)
-    (htree:add-child "http://example.root/B" tree)
-    tree))
+(defun make-history2 ()
+  (let ((history (htree:make)))
+    (htree:add-child "http://example.root" history)
+    (htree:add-child "http://example.root/A" history)
+    (htree:back history)
+    (htree:add-child "http://example.root/B" history)
+    history))
 
 (prove:subtest "Single entry"
   (let ((history (htree:make))
@@ -59,19 +59,19 @@
               url2)))
 
 (prove:subtest "Simple branching tree tests."
-  (prove:is (htree:value (htree:current-owner-node (make-tree1)))
+  (prove:is (htree:value (htree:current-owner-node (make-history1)))
             "http://example.root/B2"))
 
 (prove:subtest "History depth."
-  (prove:is (htree:depth (make-tree1))
+  (prove:is (htree:depth (make-history1))
             2))
 
 (prove:subtest "History size."
-  (prove:is (htree:size (make-tree1))
+  (prove:is (htree:size (make-history1))
             7))
 
 (prove:subtest "All contiguous history nodes for current owner."
-  (prove:is (htree:all-contiguous-owned-nodes-data (make-tree1))
+  (prove:is (htree:all-contiguous-owned-nodes-data (make-history1))
             '("http://example.root"
               "http://example.root/B"
               "http://example.root/B2" "http://example.root/B1"
@@ -80,7 +80,7 @@
 
 (prove:subtest "Traverse all history."
   (prove:is (htree:all-contiguous-owned-nodes-data
-             (htree:back (make-tree1)))
+             (htree:back (make-history1)))
             '("http://example.root"
               "http://example.root/B"
               "http://example.root/B2" "http://example.root/B1"
@@ -93,7 +93,7 @@
                "http://example.root/A2"
               (htree:go-to-child
                "http://example.root/A"
-               (htree:back (make-tree1) 2))))
+               (htree:back (make-history1) 2))))
             '("http://example.root"
               "http://example.root/B"
               "http://example.root/B2" "http://example.root/B1"
@@ -102,28 +102,28 @@
 
 (prove:subtest "Traverse parents."
   (prove:is (htree:all-parents-data
-             (htree:back (make-tree1)))
+             (htree:back (make-history1)))
             '("http://example.root")))
 
 (prove:subtest "Traverse forward children."
   (prove:is (htree:all-forward-children-data
-             (htree:back (make-tree1)))
+             (htree:back (make-history1)))
             '("http://example.root/B2")))
 
 (prove:subtest "Traverse all children."
   (prove:is (htree:all-children-data
-             (htree:back (make-tree1)))
+             (htree:back (make-history1)))
             '("http://example.root/B2" "http://example.root/B1")))
 
 (prove:subtest "Move node to forward-child on add."
-  (let ((tree (make-tree2)))
-    (prove:is (htree:value (htree:current-owner-node tree))
+  (let ((history (make-history2)))
+    (prove:is (htree:value (htree:current-owner-node history))
               "http://example.root/B")
-    (htree:back tree)
-    (prove:is (htree:value (htree:current-owner-node tree))
+    (htree:back history)
+    (prove:is (htree:value (htree:current-owner-node history))
               "http://example.root")
-    (htree:add-child "http://example.root/A" tree)
-    (prove:is (htree:value (htree:current-owner-node tree))
+    (htree:add-child "http://example.root/A" history)
+    (prove:is (htree:value (htree:current-owner-node history))
               "http://example.root/A")))
 
 (define-class web-page ()
@@ -230,44 +230,44 @@
 ;; 2. Remove 2nd owner.  Test if all these nodes got garbage collected, but not
 ;; the default branch nodes.
 (prove:subtest "Owner deletion"
-  (let ((tree (htree:make)))
+  (let ((history (htree:make)))
     (dolist (url '("http://example.root"
                    "http://example.root/R"
                    "http://example.root/R1"
                    "http://example.root/R2"))
-      (htree:add-child url tree))
-    (htree:add-owner tree "parent-owner")
-    (htree:set-current-owner tree "parent-owner")
-    (htree:add-child "http://parent/A" tree)
-    (htree:add-child "http://parent/A1" tree)
-    (htree:back tree)
-    (htree:add-child "http://parent-child/A2" tree)
+      (htree:add-child url history))
+    (htree:add-owner history "parent-owner")
+    (htree:set-current-owner history "parent-owner")
+    (htree:add-child "http://parent/A" history)
+    (htree:add-child "http://parent/A1" history)
+    (htree:back history)
+    (htree:add-child "http://parent-child/A2" history)
 
-    (htree:add-owner tree "child-owner" :creator-identifier "parent-owner")
-    (htree:set-current-owner tree "child-owner")
-    (htree:add-child "http://child/A3a" tree)
-    (htree:back tree)
-    (htree:add-child "http://child/A3b" tree)
+    (htree:add-owner history "child-owner" :creator-identifier "parent-owner")
+    (htree:set-current-owner history "child-owner")
+    (htree:add-child "http://child/A3a" history)
+    (htree:back history)
+    (htree:add-child "http://child/A3b" history)
 
-    (prove:is (length (htree:nodes (htree:owner tree "parent-owner")))
+    (prove:is (length (htree:nodes (htree:owner history "parent-owner")))
               3)
-    (prove:is (length (htree:all-current-branch-nodes tree))
+    (prove:is (length (htree:all-current-branch-nodes history))
               5)
-    (prove:is (length (htree:nodes (htree:owner tree htree:+default-owner+)))
+    (prove:is (length (htree:nodes (htree:owner history htree:+default-owner+)))
               4)
 
-    (htree:delete-owner tree "child-owner")
+    (htree:delete-owner history "child-owner")
 
-    (prove:isnt (htree:current-owner-identifier tree)
+    (prove:isnt (htree:current-owner-identifier history)
                 "child-owner")
-    (prove:is (htree:owner tree "child-owner")
+    (prove:is (htree:owner history "child-owner")
               nil)
-    (prove:is (length (htree:nodes (htree:owner tree "parent-owner")))
+    (prove:is (length (htree:nodes (htree:owner history "parent-owner")))
               3)
-    (htree:set-current-owner tree "parent-owner")
-    (prove:is (length (htree:all-current-branch-nodes tree))
+    (htree:set-current-owner history "parent-owner")
+    (prove:is (length (htree:all-current-branch-nodes history))
               5)
-    (prove:is (length (htree:nodes (htree:owner tree htree:+default-owner+)))
+    (prove:is (length (htree:nodes (htree:owner history htree:+default-owner+)))
               4)
 
     (dolist (url-owner (list '("http://parent/A" "parent-owner")
@@ -276,17 +276,17 @@
                              '("http://child/A3a" nil)
                              '("http://child/A3b" nil)))
       (prove:is (alexandria:hash-table-keys
-                 (htree:bindings (first (htree:find-nodes tree (first url-owner)))))
+                 (htree:bindings (first (htree:find-nodes history (first url-owner)))))
                 (if (second url-owner)
-                    (list (htree:owner tree (second url-owner)))
+                    (list (htree:owner history (second url-owner)))
                     nil)))
 
-    (htree:delete-owner tree "parent-owner")
-    (prove:is (htree:current-owner-identifier tree)
+    (htree:delete-owner history "parent-owner")
+    (prove:is (htree:current-owner-identifier history)
               htree:+default-owner+)
-    (prove:is (htree:owner tree "parent-owner")
+    (prove:is (htree:owner history "parent-owner")
               nil)
-    (prove:is (hash-table-count (htree:entries tree))
+    (prove:is (hash-table-count (htree:entries history))
               9)
 
     (maphash (lambda (entry nodes)
@@ -298,10 +298,10 @@
                                  (htree:value entry)
                                  (length nodes))))
 
-             (htree:entries tree))))
+             (htree:entries history))))
 
 (prove:subtest "Visit all nodes until distant node"
-  (let* ((history (make-tree1))
+  (let* ((history (make-history1))
          (creator (htree:current-owner-identifier history))
          (distant-node-value "http://example.root/A1")
          (distant-node (first (htree:find-nodes history distant-node-value))))
