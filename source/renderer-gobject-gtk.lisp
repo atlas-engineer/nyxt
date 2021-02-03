52f6449f04d4124c190bf7604ec932abd245e46b
John Mercouris <john@mercouris.email>
2021-02-03 12:39:19 +0100

    renderer-gobject-gtk.lisp: add notice explaining file behavior

--- source/renderer-gobject-gtk.lisp
+++ source/renderer-gobject-gtk.lisp
@@ -1,6 +1,16 @@
 ;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
+;;;; This file produces two warnings due to redefinitions of
+;;;; renderer-thread-p and ffi-initialize.
+;;;;
+;;;; This is necessary to modify the behavior of renderer-gtk to use
+;;;; gobject to launch/manage the main GTK thread. When/if this
+;;;; renderer will fully utilize gobject introspection (by taking the
+;;;; changes made on the respectively named branch, and reapplying
+;;;; them), then these errors will disappear, as there will be no
+;;;; reliance on renderer-gtk (and thus, no redefinition).
+
 (in-package :nyxt)
 
 (defun renderer-thread-p ()
