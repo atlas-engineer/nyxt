97d91f9a61ddb9693226e649df7155cf4c1b2059
Pierre Neidhardt <mail@ambrevar.xyz>
2021-02-03 20:22:46 +0100

    tests: Fix global history tests.
    Note that now history is written to in make-internal-buffer, so the history
    storage must be tweaked before making the test buffer.

--- tests/test-global-history.lisp
+++ tests/test-global-history.lisp
@@ -8,38 +8,37 @@
 (plan nil)
 
 (subtest "Global history"
-  (let* ((*browser* (make-instance 'user-browser))
-         (buffer (nyxt::make-internal-buffer)))
-    ;; We need to make sure nothing will be restored.
-    (nyxt:with-current-buffer buffer
-      (let ((path (setf (history-path buffer)
-                        (make-instance 'history-data-path
-                                       :basename "history"
-                                       :dirname "/tmp/nyxt-history-test"))))
-        (nyxt::history-add (quri:uri "http://example.org"))
-        (is (length (htree:all-data (nyxt:get-data path)))
-            1
-            "history has 1 entry")
-        (let ((entry (first (htree:all-data (nyxt:get-data path)))))
-          (is (url entry)
-              (quri:uri "http://example.org")
-              :test #'quri:uri=
-              "value has quri:uri")
-          (is (title entry)
-              ""
-              "value has no title"))
-        (nyxt::history-add (quri:uri "http://example.org") :title "foo")
-        (is (length (htree:all-data (nyxt:get-data path)))
-            1
-            "history has still 1 entry after adding same URI")
-        (let ((entry (first (htree:all-data (nyxt:get-data path)))))
-          (is (title entry)
-              "foo"
-              "value now has title"))
-        (nyxt::history-add (quri:uri "http://example.org/sub"))
-        (is (length (htree:all-data (nyxt:get-data path)))
-            2
-            "history now has 2 entries")
-        (uiop:delete-file-if-exists (expand-path (history-path buffer)))))))
+  (let* ((*browser* (make-instance 'user-browser)))
+    ;; Set profile to private to inhibit serialization / deserialization.
+    ;; TODO: We should still test serialization and deserialization.
+    (setf *global-data-profile* (make-instance 'private-data-profile))
+    (let ((buffer (nyxt::make-internal-buffer)))
+      (nyxt:with-current-buffer buffer
+        (let ((path (history-path buffer)))
+          (nyxt::history-add (quri:uri "http://example.org"))
+          (is (length (htree:all-data (nyxt:get-data path)))
+              1
+              "history has 1 entry")
+          (let ((entry (first (htree:all-data (nyxt:get-data path)))))
+            (is (url entry)
+                (quri:uri "http://example.org")
+                :test #'quri:uri=
+                "value has quri:uri")
+            (is (title entry)
+                ""
+                "value has no title"))
+          (nyxt::history-add (quri:uri "http://example.org") :title "foo")
+          (is (length (htree:all-data (nyxt:get-data path)))
+              1
+              "history has still 1 entry after adding same URI")
+          (let ((entry (first (htree:all-data (nyxt:get-data path)))))
+            (is (title entry)
+                "foo"
+                "value now has title"))
+          (nyxt::history-add (quri:uri "http://example.org/sub"))
+          (is (length (htree:all-data (nyxt:get-data path)))
+              2
+              "history now has 2 entries")
+          (uiop:delete-file-if-exists (expand-path (history-path buffer))))))))
 
 (finalize)
