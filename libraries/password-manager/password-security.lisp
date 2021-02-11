4b58658bc59b2ca3342cd5cd5d644f2e4733882e
John Mercouris <john@mercouris.email>
2021-02-11 15:49:16 +0100

    password-security.lisp: remove uneeded :initform

--- libraries/password-manager/password-security.lisp
+++ libraries/password-manager/password-security.lisp
@@ -7,7 +7,7 @@
 ;;; on BSD and Darwin systems to interface with the system keychain
 
 (define-class security-interface (password-interface)
-  ((executable :initform (executable-find "security")))
+  ((executable (executable-find "security")))
   (:export-class-name-p t)
   (:export-accessor-names-p t)
   (:accessor-name-transformer (hu.dwim.defclass-star:make-name-transformer name)))
