27a763939a445fecaade7c180fe311b313477cb5
John Mercouris <john@mercouris.email>
2021-02-11 15:43:49 +0100

    password-keepassxc.lisp: use execute macro

--- libraries/password-manager/password-keepassxc.lisp
+++ libraries/password-manager/password-keepassxc.lisp
@@ -22,41 +22,38 @@
 
 (defmethod list-passwords ((password-interface keepassxc-interface))
   (let* ((st (make-string-input-stream (master-password password-interface)))
-         (output (uiop:run-program (list (executable password-interface)
-                                         "ls" (password-file password-interface))
-                                   :input st :output '(:string :stripped t))))
+         (output (execute password-interface (list "ls" (password-file password-interface))
+                          :input st :output '(:string :stripped t))))
     (remove "Recycle Bin/" (rest (cl-ppcre:split "\\n" output)) :test #'equal)))
 
 (defmethod clip-password ((password-interface keepassxc-interface) &key password-name service)
   (declare (ignore service))
   (with-input-from-string (st (master-password password-interface))
-    (uiop:run-program (list (executable password-interface)
-                            "clip"
-                            (password-file password-interface)
-                            password-name
-                            ;; Timeout for password
-                            (format nil "~a" (sleep-timer password-interface)))
-                      :input st :output '(:string :stripped t))))
+    (execute password-interface (list "clip"
+                                      (password-file password-interface)
+                                      password-name
+                                      ;; Timeout for password
+                                      (format nil "~a" (sleep-timer password-interface)))
+             :input st :output '(:string :stripped t))))
 
 (defmethod save-password ((password-interface keepassxc-interface) &key password-name password service)
   (declare (ignore service))
   (with-input-from-string (st (format nil "~a~C~a"
                                       (master-password password-interface)
                                       #\newline password))
-    (uiop:run-program (list (executable password-interface)
-                            "add" "--password-prompt" (password-file password-interface)
-                            (if (str:emptyp password-name)
-                                "--generate"
-                                password-name))
-                      :input st)))
+    (execute password-interface (list "add" "--password-prompt" (password-file password-interface)
+                                      (if (str:emptyp password-name)
+                                          "--generate"
+                                          password-name))
+             :input st)))
 
 (defmethod password-correct-p ((password-interface keepassxc-interface))
   (when (master-password password-interface)
     (handler-case
         (let* ((st (make-string-input-stream (master-password password-interface)))
-               (output (uiop:run-program (list (executable password-interface)
-                                               "ls" (password-file password-interface))
-                                         :input st :output '(:string :stripped t))))
+               (output (execute password-interface (list (executable password-interface)
+                                                         "ls" (password-file password-interface))
+                                :input st :output '(:string :stripped t))))
           (remove "Recycle Bin/" (rest (cl-ppcre:split "\\n" output)) :test #'equal))
       (uiop/run-program:subprocess-error ()
         nil))))
