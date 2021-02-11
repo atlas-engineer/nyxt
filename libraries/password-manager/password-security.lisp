ea204b11b29a0b207f81f309a78e198a6f06c0d4
John Mercouris <john@mercouris.email>
2021-02-11 14:05:38 +0100

    password-security.lisp: utilize executable slot of password-interface

--- libraries/password-manager/password-security.lisp
+++ libraries/password-manager/password-security.lisp
@@ -6,21 +6,18 @@
 ;;; Provide an interface to the command line "security" program used
 ;;; on BSD and Darwin systems to interface with the system keychain
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (export '*security-cli-program*))
-(defvar *security-cli-program* nil
-  "The path to the executable.")
-
 (defclass security-interface (password-interface) ())
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (export 'make-security-interface))
 (defun make-security-interface ()
-  (unless *security-cli-program*
-    (setf *security-cli-program* (executable-find "security")))
-  (when *security-cli-program*
-    (make-instance 'security-interface)))
+  (make-instance 'security-interface))
+
 (push #'make-security-interface interface-list)
+
+(defmethod initialize-instance :after ((password-interface security-interface) &key)
+  (unless (slot-boundp password-interface 'executable)
+    (setf (executable password-interface) (executable-find "security"))))
 
 (defmethod list-passwords ((password-interface security-interface))
   (error "Listing passwords not supported by security interface."))
@@ -33,7 +30,7 @@
      "password: " ""
      (nth-value 1
                 (uiop:run-program
-                 (list *security-cli-program* "find-internet-password"
+                 (list (executable password-interface) "find-internet-password"
                        "-a" password-name "-s" service "-g")
                  :error-output '(:string :stripped t)))))))
 
