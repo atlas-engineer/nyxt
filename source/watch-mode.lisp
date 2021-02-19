ef9796e430a3890ee3688fbc3e3aa09c1c0fb119
André Alexandre Gomes <andremegafone@gmail.com>
2021-02-19 11:53:59 +0300

    watch-mode.lisp: second implemenation

--- source/watch-mode.lisp
+++ source/watch-mode.lisp
@@ -7,46 +7,40 @@
   intervals."))
 (in-package :nyxt/watch-mode)
 
+(defun seconds-from-user-input ()
+  "Read from the minibuffer numerical time inputs and collate them into
+seconds."
+  (let* ((time-units '("days" "hours" "minutes" "seconds"))
+         (to-seconds-alist (pairlis time-units '(86400 3600 60 1)))
+         (active-time-units (prompt-minibuffer
+                             :input-prompt "Time unit(s)"
+                             :multi-selection-p t
+                             :suggestion-function (lambda (minibuffer)
+                                                    (fuzzy-match
+                                                     (input-buffer minibuffer)
+                                                     time-units))))
+         (times (mapcar (lambda (unit)
+                          (parse-integer
+                           (prompt-minibuffer
+                            :input-prompt (format nil "Time interval (~a)" unit)
+                            :hide-suggestion-count-p t)
+                           :junk-allowed t))
+                        active-time-units))
+         (to-seconds-multipliers
+           (mapcar
+            (lambda (elem) (cdr (assoc elem to-seconds-alist :test 'string-equal)))
+            active-time-units)))
+    (prompt-minibuffer
+     ;; TODO better way to format the string?
+     :input-prompt (format nil "Confirm refreshing every ~:@{~{~d ~}~a~:@}?"
+                           (list times active-time-units)))
+    (apply '+ (mapcar (lambda (time multiplier) (* time multiplier))
+                      times to-seconds-multipliers))))
+
 (define-mode watch-mode ()
   "Reload the current buffer at regular time intervals."
-  ((thread-label (str:concat "watch-mode-" (id (current-buffer)))
-                 :export nil
-                 :documentation "Label that identifies a thread.")
-   (time-interval
-    (let* ((time-units '("days" "hours" "minutes" "seconds"))
-           (to-seconds-alist (pairlis time-units '(86400 3600 60 1)))
-           (active-time-units (prompt-minibuffer
-                               :input-prompt "Time unit(s)"
-                               :multi-selection-p t
-                               :suggestion-function (lambda (minibuffer) time-units)))
-           (times (mapcar (lambda (unit)
-                            (parse-integer
-                             (prompt-minibuffer
-                              :input-prompt (format nil "Time interval (~a)" unit)
-                              :hide-suggestion-count-p t)
-                             :junk-allowed t))
-                          active-time-units))
-           (to-seconds-multipliers
-             (mapcar
-              (lambda (elem) (cdr (assoc elem to-seconds-alist :test 'string-equal)))
-              active-time-units)))
-      (prompt-minibuffer
-       ;; TODO better way to format the string?
-       :input-prompt (format nil "Confirm refreshing every ~:@{~{~d ~}~a~:@}?"
-                             (list times active-time-units)))
-      (apply '+ (mapcar (lambda (time multiplier) (* time multiplier))
-                        times to-seconds-multipliers)))
-    :type number
-    :documentation "Time interval in seconds.")
-   (destructor
-    (lambda (mode)
-      (let ((threads (bt:all-threads)))
-        (bt:destroy-thread
-         (nth (position (thread-label mode)
-                        (mapcar 'bt:thread-name threads) :test 'string-equal)
-              threads)))))
-   (constructor
-    (lambda (mode) (bt:make-thread
-               (lambda () (loop (buffer-load (url (buffer mode)))
-                           (sleep (time-interval mode))))
-               :name (thread-label mode))))))
+  ((thread (let ((secs (seconds-from-user-input)))
+             (bt:make-thread (lambda () (loop (reload-current-buffer)
+                                         (sleep secs))))))
+   (destructor (lambda (mode) (bt:destroy-thread (thread mode))))
+   (constructor (lambda (mode) (thread mode)))))
