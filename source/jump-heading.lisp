5a8580c355e44f32f625da4309467d884d1b4fa8
John Mercouris <john@mercouris.email>
2021-02-23 17:08:51 +0100

    jump-heading.lisp: use new prompt buffer

--- source/jump-heading.lisp
+++ source/jump-heading.lisp
@@ -9,47 +9,44 @@
   (:documentation "A heading. The inner-text must not be modified, so
   than we can jump to the anchor of the same name."))
 
-(defmethod nyxt:object-string ((heading heading))
-  "Cleaned-up text of this heading, to show the user in the minibuffer.
-For example, Wikipedia ones end with '[edit]'. We strip what comes after the first bracket."
-  (with-slots (inner-text) heading
-    (subseq inner-text 0 (position #\[ inner-text))))
+(defmethod title ((heading heading))
+  (subseq (inner-text heading) 0 (position #\[ (inner-text heading))))
 
-(defmethod nyxt:object-display ((heading heading))
-  "Same as `nyxt:object-string'."
-  (nyxt:object-string heading))
+(defmethod prompter:object-properties ((heading heading))
+  (list :title (title heading)))
 
-(defun make-headings (list/str)
-  "Make a list `heading's from a list of strings."
-  (mapcar (lambda (text)
-            (make-instance 'heading :inner-text text))
-          list/str))
+(defun get-headings ()
+  (pflet ((get-headings ()
+           (defun qsa (context selector)
+             "Alias of document.querySelectorAll"
+             (ps:chain context (query-selector-all selector)))
+           (let ((headings (qsa document "h1, h2, h3, h4, h5, h6")))
+             (ps:chain |json| (stringify
+                               (loop for heading in headings
+                                     collect (ps:chain heading inner-text)))))))
+    (mapcar (lambda (i) (make-instance 'heading :inner-text i))
+            (cl-json:decode-json-from-string (get-headings)))))
 
-(define-parenscript get-headings ()
-  (defun qsa (context selector)
-    "Alias of document.querySelectorAll"
-    (ps:chain context (query-selector-all selector)))
-  (let ((headings (qsa document "h1, h2, h3, h4, h5, h6")))
-    (ps:chain |json| (stringify
-                        (loop for heading in headings
-                           collect (ps:chain heading inner-text))))))
-
-(define-parenscript paren-jump-to-heading (&key heading-inner-text)
+(define-parenscript scroll-page-to-heading (heading)
   (defun qsa (context selector)
     "Alias of document.querySelectorAll"
     (ps:chain context (query-selector-all selector)))
   (let ((headings (qsa document "h1, h2, h3, h4, h5, h6")))
     (loop for heading in headings do
-         (when (equal (ps:lisp heading-inner-text) (ps:chain heading inner-text))
-           (ps:chain heading (scroll-into-view t))))))
+             (when (equal (ps:lisp (inner-text heading))
+                          (ps:chain heading inner-text))
+               (ps:chain heading (scroll-into-view t))))))
+
+(define-class heading-source (prompter:source)
+  ((prompter:name "Headings")
+   (prompter:must-match-p t)
+   (prompter:follow-p t)
+   (prompter:persistent-action #'scroll-page-to-heading)
+   (prompter:initial-suggestions (get-headings))
+   (prompter:actions '(scroll-page-to-heading))))
 
 (define-command jump-to-heading ()
   "Jump to a particular heading, of type h1, h2, h3, h4, h5, or h6."
-  (let* ((headings (get-headings))
-         (heading (prompt-minibuffer
-                   :input-prompt "Jump to heading"
-                   :suggestion-function (lambda (minibuffer)
-                                          (fuzzy-match
-                                           (input-buffer minibuffer)
-                                           (make-headings (cl-json:decode-json-from-string headings)))))))
-    (paren-jump-to-heading :heading-inner-text (inner-text heading))))
+  (prompt
+   :prompt "Jump to heading:"
+   :sources (list (make-instance 'heading-source))))
