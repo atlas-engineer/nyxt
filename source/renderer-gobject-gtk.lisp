ce8f23f9576ec9bbc57de33f860c647bffd936a8
Jeff Bowman <jeff.t.bowman@gmail.com>
2021-02-24 17:13:49 -0600

    Fix for 1173, uses glib:g-set-prgname
    As recommended in the issue, changed to use the
    glib:g-set-prgname. The WM_CLASS after the change is reported as:
    
    WM_CLASS(STRING) = "nyxt", "Sbcl"

--- source/renderer-gobject-gtk.lisp
+++ source/renderer-gobject-gtk.lisp
@@ -19,7 +19,7 @@
   (defmethod ffi-initialize ((browser gtk-browser) urls startup-timestamp)
     (log:info "Initializing Gobject-GTK Interface")
     (bt:make-thread (lambda ()
-                      (gdk:gdk-set-program-class "nyxt")
+                      (glib:g-set-prgname "nyxt")
                       (gir:invoke ((gir:ffi "Gtk") 'main)))
                     :name "main thread")
     (finalize browser urls startup-timestamp)))
