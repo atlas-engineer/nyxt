dd64513fbdf81004691ef1028afb4a480096ccd2
Pierre Neidhardt <mail@ambrevar.xyz>
2021-03-09 14:04:54 +0100

    command: Remove `command-function' now that commands are funcallable and we have `fn'.

--- source/buffer.lisp
+++ source/buffer.lisp
@@ -1001,7 +1001,7 @@ MODES should be a list symbols, each possibly returned by `mode-name'."
   (dolist (mode (uiop:ensure-list modes))
     (let ((command (mode-command mode)))
       (if command
-          (funcall-safely (name command) :buffer buffer :activate nil) ; TODO: Make commands funcallable.
+          (funcall-safely command :buffer buffer :activate nil)
           (log:warn "Mode command ~a not found." mode)))))
 
 (export-always 'enable-modes)
@@ -1012,7 +1012,7 @@ ARGS are passed to the mode command."
   (dolist (mode (uiop:ensure-list modes))
     (let ((command (mode-command mode)))
       (if command
-          (apply #'funcall-safely (name command) :buffer buffer :activate t args) ; TODO: Make commands funcallable.
+          (apply #'funcall-safely command :buffer buffer :activate t args)
           (log:warn "Mode command ~a not found." mode)))))
 
 (define-class active-mode-source (prompter:source)
