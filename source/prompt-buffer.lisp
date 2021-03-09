4c2e5da9a35d68bf7e7bb792508a5c106111de4e
John Mercouris <john@mercouris.email>
2021-03-09 19:12:28 +0100

    prompt-buffer.lisp: remove duplicated function (exists in if-confirm.lisp)

--- source/prompt-buffer.lisp
+++ source/prompt-buffer.lisp
@@ -381,26 +381,6 @@ See the documentation of `prompt-buffer' to know more about the options."
         ((calispel:? interrupt-channel)
          (error 'nyxt-prompt-buffer-canceled))))))
 
-(export-always 'prompter-if-confirm)    ; TODO: Rename to `if-confirm' once `minibuffer' is gone.
-(defmacro prompter-if-confirm (prompt yes-form &optional no-form)
-  "Ask the user for confirmation before executing either YES-FORM or NO-FORM.
-YES-FORM is executed on  \"yes\" answer, NO-FORM -- on \"no\".
-PROMPT is a list fed to `format nil'.
-
-Example usage defaulting to \"no\":
-
-\(let ((*yes-no-choices* '(:no \"no\" :yes \"yes\")))
-  (if-confirm (\"Are you sure to kill ~a buffers?\" count)
-     (delete-buffers)))"
-  ;; TODO: Can we keep the `*yes-no-choices*' customization option?
-  `(let ((answer (prompt
-                  :input-prompt (format nil ,@prompt)
-                  :suggestion-function (yes-no-suggestion-filter)
-                  :hide-suggestion-count-p t)))
-     (if (confirmed-p answer)
-         ,yes-form
-         ,no-form)))
-
 (defmethod prompter:object-properties ((prompt-buffer prompt-buffer))
   (list :prompt (prompter:prompt prompt-buffer)
         :input (prompter:input prompt-buffer)))
