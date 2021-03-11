32e4f38b7a2ac850a9cc53ec475d9e61f34d3ced
André Alexandre Gomes <andremegafone@gmail.com>
2021-03-11 18:10:05 +0300

    external-editor.lisp: concatenate optional input to temporary file

--- source/external-editor.lisp
+++ source/external-editor.lisp
@@ -3,13 +3,21 @@
 
 (in-package :nyxt)
 
-(defun get-text-from-external-editor ()
-  "Open an external editor to capture text. Please note, this function
-is blocking, invoke on a separate thread when possible."
-  (uiop:with-temporary-file (:directory (uiop:xdg-data-home +data-root+) :pathname p)
-    (log:debug "External Editor: ~a opening: ~a" (external-editor-program *browser*) p)
-    (uiop:run-program (list (external-editor-program *browser*) (uiop:native-namestring p))
-                      :ignore-error-status t)
+(defun open-with-external-editor (&optional input-text)
+  "Edit `input-text' using `external-editor-program'.
+Create a temporary file and return its content.  The editor runs synchronously
+so invoke on a separate thread when possible."
+  (uiop:with-temporary-file (:directory (uiop:xdg-data-home +data-root+)
+                             :pathname p)
+    (when (> (length input-text) 0)
+      (with-open-file (f p :direction :io
+                           :if-exists :append)
+        (write-sequence input-text f)))
+    (log:debug "External Editor: ~a opening: ~a"
+               (external-editor-program *browser*) p)
+    ;; launch-program runs asynchronously but it misbehaves
+    (uiop:run-program (list (external-editor-program *browser*)
+                               (uiop:native-namestring p)) :ignore-error-status t)
     (uiop:read-file-string p)))
 
 (define-command fill-input-from-external-editor ()
