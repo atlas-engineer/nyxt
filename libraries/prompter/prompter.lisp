6e0eadc41817bc7aa5cfeb72f44c3da8161cbf54
Pierre Neidhardt <mail@ambrevar.xyz>
2021-03-15 08:30:49 +0100

    libraries/prompter: Remove spurious `or' in resolve-selection.

--- libraries/prompter/prompter.lisp
+++ libraries/prompter/prompter.lisp
@@ -320,13 +320,13 @@ If STEPS is negative, go forward and selection first suggestion."
                     (union marks suggestion-values)
                     (intersection marks suggestion-values))))))))))
 
-(defun resolve-selection (prompter)
+(defun resolve-selection (prompter)     ; TODO: Write tests for this!
   "Return the list of selected values.
 If there is no marks, the current selection value is returned as a list of one element.
 For instance, if the selected element value is NIL, this returns '(NIL).
 If there is no element, NIL is returned."
-  (or (or (all-marks prompter)
-          (mapcar #'value (uiop:ensure-list (selected-suggestion prompter))))
+  (or (all-marks prompter)
+      (mapcar #'value (uiop:ensure-list (selected-suggestion prompter)))
       (and (not (must-match-p (selected-source prompter))) ; TODO: Remove when we remove `must-match-p'.
            (slot-value prompter 'input))))
 
