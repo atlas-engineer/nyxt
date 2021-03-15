21f12f274cadfa604e7a9ca2b478aae28e833d5d
André Alexandre Gomes <andremegafone@gmail.com>
2021-03-15 13:00:05 +0300

    external-editor.lisp: superior i/o interaction with EDITOR.
    The current implementation follows the following rationale:
    
    - the currently active input field (with tags INPUT or TEXTAREA) are
    selected;
    
    - that piece of text (the empty string is contemplated) is passed to the
    external editor through %copy (which returns the selection using JS) and
    gets edited as a temporary file;
    
    - the edited text is sent back via %paste (which will overwrite the
    selection via JS);
    
    - the caret is placed at the end of the active input field.
    
    Emacs nits:
    
    If you use Emacs as a server and set EDITOR to emacsclient, then you can
    press C-x # when you're done editing the input field.  That you get back
    to Nyxt effortlessly.  You can find out more by reading dedicated
    section in the info pages---evaluate (info "(emacs) Invoking
    emacsclient") in Emacs.
    
    If you want to leverage the above approach you might find useful to add
    the following to your emacs init file.
    
    (setq server-temp-file-regexp "\\`/tmp/Re\\|/draft\\'\\|\\.tmp")
    
    It tells that files ending in ".*tmp" should be considered temporary
    files.  That way, the temporary files generated by
    `uiop:with-temporary-file' will be treated as such.

--- source/external-editor.lisp
+++ source/external-editor.lisp
@@ -17,23 +17,30 @@ so invoke on a separate thread when possible."
                (external-editor-program *browser*) p)
     ;; launch-program runs asynchronously but it misbehaves
     (uiop:run-program (list (external-editor-program *browser*)
-                               (uiop:native-namestring p)) :ignore-error-status t)
+                            (uiop:native-namestring p))
+                      :ignore-error-status t)
     (uiop:read-file-string p)))
 
 (define-parenscript select-input-field ()
   (let ((active-tag (ps:chain document active-element tag-name)))
     (when (or (string= active-tag "INPUT")
               (string= active-tag "TEXTAREA"))
+      ;; (ps:chain document (exec-command "selectAll"))
       (ps:chain document active-element (select)))))
 
-(define-parenscript undo-selection ()
-  (ps:chain window (get-selection) (remove-all-ranges)))
+;; this could be an optional exiting behaviour
+;; (define-parenscript undo-selection ()
+;;   (ps:chain window (get-selection) (remove-all-ranges)))
+
+(define-parenscript set-caret-on-end ()
+  (ps:chain document (get-selection) (collapse-to-end)))
 
 ;; known issues:
 ;; fails on duckduckgo's search bar because it loses its focus
 (define-command fill-input-from-external-editor ()
   "Edit the current input field using `external-editor-program'."
-  (select-input-field)
   (bt:make-thread
-   (lambda () (%paste :input-text (open-with-external-editor (%copy)))
-         (undo-selection))))
+   (lambda ()
+     (select-input-field)
+     (%paste :input-text (open-with-external-editor (%copy)))
+     (set-caret-on-end))))
