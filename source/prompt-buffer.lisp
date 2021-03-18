c5b367b1605ef2d84683663dcc4141628bdb0a6b
Artyom Bologov <aartaka@protonmail.com>
2021-03-19 00:51:07 +0500

    prompt-buffer: Make user-prompt-buffer instead of prompt-buffer.

--- source/prompt-buffer.lisp
+++ source/prompt-buffer.lisp
@@ -351,7 +351,7 @@ See the documentation of `prompt-buffer' to know more about the options."
       (ffi-within-renderer-thread
        *browser*
        (lambda ()
-         (let ((prompt-buffer (apply #'make-instance 'prompt-buffer
+         (let ((prompt-buffer (apply #'make-instance 'user-prompt-buffer
                                      (append args
                                              (list
                                               :window (current-window)
