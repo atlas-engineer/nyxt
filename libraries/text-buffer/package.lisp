e9d08212b559cd8b28aa7766daf475ccda50a198
Pierre Neidhardt <mail@ambrevar.xyz>
2021-03-24 18:29:12 +0100

    libraries/text-buffer: Use consistent define-package syntax.

--- libraries/text-buffer/package.lisp
+++ libraries/text-buffer/package.lisp
@@ -1,11 +1,9 @@
 ;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
-
-
 (in-package :cl-user)
 
-(defpackage :text-buffer
-  (:use :common-lisp)
-  (:export :text-buffer
-           :cursor))
+(uiop:define-package text-buffer
+  (:use #:common-lisp)
+  (:export #:text-buffer
+           #:cursor))
