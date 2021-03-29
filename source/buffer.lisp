1aac1db5bbf418f8081cb749e8d2b091519a7600
John Mercouris <john@mercouris.email>
2021-03-29 11:58:13 +0200

    editor-mode.lisp: set default initialize-display advising user
    why: user will not know what to do, this should give them a hint that
    they need to configure an editor  mode
    
    buffer.lisp: set default modes to include editor-mode so that they can
    see a prompt about needing to configure an editor mode

--- source/buffer.lisp
+++ source/buffer.lisp
@@ -330,7 +330,7 @@ Must be one of `:always' (accept all cookies), `:never' (reject all cookies),
 
 (define-class editor-buffer (internal-buffer)
   ((file :documentation "The file being edited.")
-   (default-modes '(base-mode))
+   (default-modes '(base-mode editor-mode))
    (url (quri:uri "editor-buffer"))
    (title "editor-buffer"))
   (:export-class-name-p t)
