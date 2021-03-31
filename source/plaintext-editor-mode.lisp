26fb7e933450569e34ca51a918344b22f19cedf6
Artyom Bologov <aartaka@protonmail.com>
2021-03-31 20:00:36 +0500

    Add plaintext-editor-mode enabled by default for editor.

---
+++ source/plaintext-editor-mode.lisp
@@ -0,0 +1,39 @@
+;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
+(in-package :nyxt/editor-mode)
+
+(define-mode plaintext-editor-mode (editor-mode)
+  "Mode for basic plaintext editing."
+  ((style (cl-css:css
+           '(("#editor"
+              :position "absolute"
+              :top "0"
+              :right "0"
+              :bottom "0"
+              :left "0"
+              :width "100%"
+              :min-height "100vh"))))
+   (constructor
+    (lambda (mode)
+      (initialize-display mode)))))
+
+(defmethod initialize-display ((editor plaintext-editor-mode))
+  (let* ((content (markup:markup
+                   (:head (:style (style editor)))
+                   (:body
+                    (:textarea :id "editor" :name "editor" :autofocus t))))
+         (insert-content (ps:ps (ps:chain document (write (ps:lisp content))))))
+    (ffi-buffer-evaluate-javascript-async (buffer editor) insert-content)))
+
+(defmethod set-content ((editor plaintext-editor-mode) content)
+  (with-current-buffer (buffer editor)
+    (pflet ((set-content (content)
+                         (setf (ps:chain document (query-selector "#editor") value)
+                               (ps:lisp content))))
+      (set-content content))))
+
+(defmethod get-content ((editor plaintext-editor-mode))
+  (with-current-buffer (buffer editor)
+    (pflet ((get-content () (ps:chain document (query-selector "#editor") value)))
+      (get-content))))
