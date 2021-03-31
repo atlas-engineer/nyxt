a3575b454184347c8002db156c74e89d2cab0577
John Mercouris <john@mercouris.email>
2021-03-31 17:36:08 +0200

    plaintext-editor-mode.lisp: edit style

--- source/plaintext-editor-mode.lisp
+++ source/plaintext-editor-mode.lisp
@@ -6,14 +6,19 @@
 (define-mode plaintext-editor-mode (editor-mode)
   "Mode for basic plaintext editing."
   ((style (cl-css:css
-           '(("#editor"
+           '(("body"
+              :margin 0)
+             ("#editor"
+              :margin 0
               :position "absolute"
               :top "0"
               :right "0"
               :bottom "0"
               :left "0"
-              :width "100%"
-              :min-height "100vh"))))
+              :border "none"
+              :outline "none"
+              :padding "5px"
+              :autofocus "true"))))
    (constructor
     (lambda (mode)
       (initialize-display mode)))))
