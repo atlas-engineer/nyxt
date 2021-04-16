79198bf8dde1f65159085807cbf3ce7244e72582
John Mercouris <john@mercouris.email>
2021-04-16 19:11:53 +0200

    expedition-mode.lisp: first draft! :-)

---
+++ source/expedition-mode.lisp
@@ -0,0 +1,63 @@
+;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
+(uiop:define-package :nyxt/expedition-mode
+  (:use :common-lisp :nyxt)
+  (:import-from #:keymap #:define-key #:define-scheme)
+  (:import-from #:class-star #:define-class)
+  (:documentation "Traverse a list of links."))
+(in-package :nyxt/expedition-mode)
+
+(define-mode expedition-mode ()
+  "Mode for traversing a set of URLs."
+  ((urls (list))
+   (index 0 :documentation "The index of the current element in URLs.")
+   (keymap-scheme
+    (define-scheme "expedition"
+      scheme:cua
+      (list
+       "C-]" 'expedition-next
+       "C-[" 'expedition-previous)
+      scheme:emacs
+      (list
+       "M-n" 'expedition-next
+       "M-p" 'expedition-previous)))))
+
+(defun current-expedition ()
+  (find-submode (current-buffer) 'expedition-mode))
+
+(define-command expedition-next (&key (expedition (current-expedition)))
+  "Go to the next URL in the expedition."
+  (if (> (length (urls expedition)) (+ 1 (index expedition)))
+      (progn
+        (incf (index expedition))
+        (buffer-load (nth (index expedition) (urls expedition))))
+      (echo "End of expedition.")))
+
+(define-command expedition-previous (&key (expedition (current-expedition)))
+  "Go to the previous URL in the expedition."
+  (if (> (index expedition) 0)
+      (progn
+        (decf (index expedition))
+        (buffer-load (nth (index expedition) (urls expedition))))
+      (echo "Start of expedition.")))
+
+(define-command nyxt::select-frame-expedition (&key (buffer (current-buffer)))
+  "Run an expedition through a set of URLs selected with a rectangle."
+  (let ((urls (reverse
+               (prompt
+                :default-modes '(prompt-buffer-mode nyxt/web-mode::frame-select-mode)
+                :prompt "Start expedition with the following links:"
+                :sources (list (make-instance 'nyxt/web-mode::frame-source
+                                              :buffer buffer
+                                              :multi-selection-p t
+                                              :channel (make-instance 'calispel:channel)))
+                :after-destructor
+                (lambda ()
+                  (with-current-buffer buffer
+                    (nyxt/web-mode::frame-element-clear)))))))
+    (set-current-buffer
+     (expedition-mode
+      :urls urls
+      :buffer (make-buffer :title ""
+                           :url (first urls))))))
