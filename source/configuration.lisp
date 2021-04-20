97c9594d6cd430feb3ef1db9793fa56c0122a0ce
Pierre Neidhardt <mail@ambrevar.xyz>
2021-04-20 13:26:50 +0200

    configuration: Warn when a non-buffer is passed to with-current-buffer.

--- source/configuration.lisp
+++ source/configuration.lisp
@@ -40,10 +40,13 @@
   "Execute BODY in a context in which `current-buffer' returns BUFFER."
   ;; We `unwind-protect' to restore the right buffer when nesting this macro.
   `(let ((old-%buffer %buffer))
-     (unwind-protect
-          (let ((%buffer ,buffer))
-            ,@body)
-       (setf %buffer old-%buffer))))
+     (if (buffer-p buffer)
+         (unwind-protect
+              (let ((%buffer ,buffer))
+                ,@body)
+           (setf %buffer old-%buffer))
+         ;; TODO: Raise error instead?
+         (log:warn "Expected buffer, got ~a"))))
 
 (defun user-class-name (class-sym)
   (intern (str:concat "USER-" (string class-sym))
