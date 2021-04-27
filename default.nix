14ec09d915ffc4e07bde167b937a2cadf6a88595
John Mercouris <john@mercouris.email>
2021-04-27 17:03:42 +0200

    default.nix: no longer need to rename nyxt-ext

--- default.nix
+++ default.nix
@@ -10,10 +10,6 @@ pkgs.nyxt.overrideAttrs (drv: {
   src = drv.src.overrideAttrs (drv: {
     inherit src;
     name = pkgs.lib.replaceStrings [drv.meta.version] [version] drv.name;
-
-    # Pending nixpkgs changes
-    installPhase = builtins.replaceStrings ["nyxt-ext"] ["nyxt"] drv.installPhase;
-
   });
   inherit version;
 })
