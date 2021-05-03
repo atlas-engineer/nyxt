3cd000fa1876b07b15b0e488b4bb7e5394159c30
John Mercouris <john@mercouris.email>
2021-05-03 16:37:41 +0200

    renderer-script.lisp: define nyxt/parenscript macros at top of package
    they can be/are used within other renderer scripts

--- source/renderer-script.lisp
+++ source/renderer-script.lisp
@@ -1,6 +1,51 @@
 ;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
+(in-package :nyxt/parenscript)
+
+(defpsmacro qs (context selector)
+  "Alias of document.querySelector"
+  `(chain ,context (query-selector ,selector)))
+
+(defpsmacro qsa (context selector)
+  "Alias of document.querySelectorAll"
+  `(chain ,context (query-selector-all ,selector)))
+
+(defpsmacro insert-at (tag input-text)
+  "Insert text at a tag."
+  `(let ((origin (chain ,tag selection-start))
+         (end (chain ,tag selection-end)))
+     (setf (chain ,tag value)
+           (+ (chain ,tag value (substring 0 origin))
+              ,input-text
+              (chain ,tag value
+                     (substring end
+                                (chain ,tag value length)))))
+     (if (= origin end)
+         (progn
+           (setf (chain ,tag selection-start) (+ origin (chain ,input-text length)))
+           (setf (chain ,tag selection-end) (chain ,tag selection-start)))
+         (progn
+           (setf (chain ,tag selection-start) origin)
+           (setf (chain ,tag selection-end) (+ origin (chain ,input-text length)))))))
+
+(defpsmacro element-drawable-p (element)
+  "Is the element drawable?"
+  `(if (or (chain ,element offset-width)
+           (chain ,element offset-height)
+           (chain ,element (get-client-rects) length))
+       t nil))
+
+(defpsmacro element-in-view-port-p (element)
+  "Is the element in the view port?"
+  `(let* ((rect (chain ,element (get-bounding-client-rect))))
+     (if (and (>= (chain rect top) 0)
+              (>= (chain rect left) 0)
+              (<= (chain rect right) (chain window inner-width))
+              (<= (chain rect bottom) (chain window inner-height)))
+         t nil)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package :nyxt)
 
 (export-always 'define-parenscript)
@@ -90,47 +135,3 @@ BODY must return the HTML markup as a string."
       ,buffer-var)
      (set-current-buffer ,buffer-var)
      ,buffer-var))
-
-(in-package :nyxt/parenscript)
-
-(defpsmacro qs (context selector)
-  "Alias of document.querySelector"
-  `(chain ,context (query-selector ,selector)))
-
-(defpsmacro qsa (context selector)
-  "Alias of document.querySelectorAll"
-  `(chain ,context (query-selector-all ,selector)))
-
-(defpsmacro insert-at (tag input-text)
-  "Insert text at a tag."
-  `(let ((origin (chain tag selection-start))
-         (end (chain ,tag selection-end)))
-     (setf (chain ,tag value)
-           (+ (chain ,tag value (substring 0 origin))
-              ,input-text
-              (chain ,tag value
-                     (substring end
-                                (chain ,tag value length)))))
-     (if (= origin end)
-         (progn
-           (setf (chain ,tag selection-start) (+ origin (ps:chain ,input-text length)))
-           (setf (chain ,tag selection-end) (ps:chain ,tag selection-start)))
-         (progn
-           (setf (chain ,tag selection-start) origin)
-           (setf (chain ,tag selection-end) (+ origin (ps:chain ,input-text length)))))))
-
-(defpsmacro element-drawable-p (element)
-  "Is the element drawable?"
-  `(if (or (ps:chain ,element offset-width)
-           (ps:chain ,element offset-height)
-           (ps:chain ,element (get-client-rects) length))
-       t nil))
-
-(defpsmacro element-in-view-port-p (element)
-  "Is the element in the view port?"
-  `(ps:let* ((rect (ps:chain ,element (get-bounding-client-rect))))
-     (if (and (>= (ps:chain rect top) 0)
-              (>= (ps:chain rect left) 0)
-              (<= (ps:chain rect right) (ps:chain window inner-width))
-              (<= (ps:chain rect bottom) (ps:chain window inner-height)))
-         t nil)))
