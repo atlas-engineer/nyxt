107049c957f24d2418b61b20a34eed444057d266
Pierre Neidhardt <mail@ambrevar.xyz>
2021-05-05 14:30:49 +0200

    build-scripts/: Add nyxt-quicklisp.scm to easily test Quicklisp setup on Guix.

---
+++ build-scripts/nyxt-quicklisp.scm
@@ -0,0 +1,144 @@
+;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
+;; Test with
+;;
+;;  guix environment -l build-scripts/nyxt-quicklisp.scm --pure --ad-hoc openssl -- bash -c 'make all && make install DESTDIR=/tmp/nyxt-output/'
+
+;; Notes: openssl is required for cl-cookie, etc.  For some reason, it's not
+;; dragged into the environment when added as propagated-input.
+
+(use-modules (ice-9 match)
+             (ice-9 popen)
+             (ice-9 rdelim)
+             (srfi srfi-1)
+             (srfi srfi-26)
+             ((guix build utils) #:select (with-directory-excursion))
+             (guix gexp)
+             (guix packages)
+             ((guix licenses) #:prefix license:)
+             (guix licenses)
+             (guix git-download)
+             (guix build-system gnu)
+             (guix build-system glib-or-gtk)
+             (gnu packages)
+             (gnu packages base)
+             (gnu packages c)
+             (gnu packages commencement)
+             (gnu packages glib)
+             (gnu packages lisp)
+             (gnu packages lisp-xyz)
+             (gnu packages gnome)
+             (gnu packages gstreamer)
+             (gnu packages gtk)
+             (gnu packages pkg-config)
+             (gnu packages version-control)
+             (gnu packages webkit))
+
+(define %source-dir (dirname (dirname (current-filename))))
+
+(define git-file?
+  (let* ((pipe (with-directory-excursion %source-dir
+                 (open-pipe* OPEN_READ "git" "ls-files")))
+         (files (let loop ((lines '()))
+                  (match (read-line pipe)
+                    ((? eof-object?)
+                     (reverse lines))
+                    (line
+                     (loop (cons line lines))))))
+         (status (close-pipe pipe)))
+    (lambda (file stat)
+      (match (stat:type stat)
+        ('directory
+         #t)
+        ((or 'regular 'symlink)
+         (any (cut string-suffix? <> file) files))
+        (_
+         #f)))))
+
+(define (nyxt-git-version)              ; Like Nyxt's `+version+'.
+  (let* ((pipe (with-directory-excursion %source-dir
+                 (open-pipe* OPEN_READ "git" "describe" "--always" "--tags")))
+         (version (read-line pipe)))
+    (close-pipe pipe)
+    version))
+
+(define-public nyxt
+  (package
+    (name "nyxt")
+    (version (nyxt-git-version))
+    (source (local-file %source-dir #:recursive? #t #:select? git-file?))
+    (build-system gnu-build-system)     ; TODO: Use glib-or-gtk-build-system instead?
+    (arguments
+     `(#:make-flags (list "nyxt" "NYXT_INTERNAL_QUICKLISP=false"
+                          (string-append "DESTDIR=" (assoc-ref %outputs "out"))
+                          "PREFIX=")
+       #:strip-binaries? #f             ; Stripping breaks SBCL binaries.
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (add-before 'build 'fix-common-lisp-cache-folder
+           (lambda _
+             (setenv "HOME" "/tmp")
+             #t))
+         (add-before 'build 'set-version
+           (lambda _
+             (setenv "NYXT_VERSION" ,version)
+             #t))
+         (add-before 'check 'configure-tests
+           (lambda _
+             (setenv "NYXT_TESTS_NO_NETWORK" "1")
+             (setenv "NYXT_TESTS_ERROR_ON_FAIL" "1")
+             #t))
+         (add-after 'install 'wrap-program
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let* ((bin (string-append (assoc-ref outputs "out") "/bin/nyxt"))
+                    (glib-networking (assoc-ref inputs "glib-networking"))
+                    (libs '("gsettings-desktop-schemas"))
+                    (path (string-join
+                           (map (lambda (lib)
+                                  (string-append (assoc-ref inputs lib) "/lib"))
+                                libs)
+                           ":"))
+                    (gi-path (string-join
+                              (map (lambda (lib)
+                                     (string-append (assoc-ref inputs lib)
+                                                    "/lib/girepository-1.0"))
+                                   libs)
+                              ":"))
+                    (xdg-path (string-join
+                               (map (lambda (lib)
+                                      (string-append (assoc-ref inputs lib) "/share"))
+                                    libs)
+                               ":")))
+               (wrap-program bin
+                 `("GIO_EXTRA_MODULES" prefix
+                   (,(string-append glib-networking "/lib/gio/modules")))
+                 `("GI_TYPELIB_PATH" prefix (,gi-path))
+                 `("LD_LIBRARY_PATH" ":" prefix (,path))
+                 `("XDG_DATA_DIRS" ":" prefix (,xdg-path)))
+               #t))))))
+    (native-inputs
+     `(("sbcl" ,sbcl)
+       ;; To generate the right version in Nyxt, unneeded from 2.0 onwards:
+       ("git" ,git-minimal)))
+    (inputs
+     `(;; System deps
+       ("gcc" ,gcc-toolchain)           ; Needed for cl-iolib.
+       ("glib" ,glib)
+       ("glib-networking" ,glib-networking)
+       ("gsettings-desktop-schemas" ,gsettings-desktop-schemas)
+       ("gtk" ,gtk+)                    ; For the main loop.
+       ("webkitgtk" ,webkitgtk)         ; Required when we use its typelib.
+       ("gobject-introspection" ,gobject-introspection)))
+    (propagated-inputs
+     ;; For iolib, etc.
+     `(("libfixposix" ,libfixposix)))
+    (synopsis "Extensible web-browser in Common Lisp")
+    (home-page "https://nyxt.atlas.engineer")
+    (description "Nyxt is a keyboard-oriented, extensible web-browser
+designed for power users.  The application has familiar Emacs and VI
+key-bindings and is fully configurable and extensible in Common Lisp.")
+    (license license:bsd-3)))
+
+nyxt
