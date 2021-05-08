9b7f84b4597723bd096a762c76eb78667d526573
Artyom Bologov <aartaka@protonmail.com>
2021-05-08 21:21:49 +0500

    dom,jump-heading: Remove inner-text in favor of plump:text.

--- source/dom.lisp
+++ source/dom.lisp
@@ -96,9 +96,3 @@
 (export-always 'named-parse)
 (defun named-parse (input)
   (name-dom-elements (plump:parse input)))
-
-(export-always 'inner-text)
-(defmethod inner-text ((element plump:node))
-  (if (plump:text-node-p element)
-      (plump:text element)
-      (apply #'str:concat (map 'list #'inner-text (plump:children element)))))
