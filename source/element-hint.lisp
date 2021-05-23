9b29b6fd9589bf32039572cb0ca01ced5155dbc7
Artyom Bologov <aartaka@protonmail.com>
2021-05-23 22:20:27 +0500

    element-hint: Define smaller methods for input sub-classes.

--- source/element-hint.lisp
+++ source/element-hint.lisp
@@ -218,12 +218,14 @@
 (defmethod %follow-hint ((button nyxt/dom:button-element))
   (click-element :nyxt-identifier (get-nyxt-id button)))
 
+(defmethod %follow-hint ((checkbox nyxt/dom:checkbox-element))
+  (check-element :nyxt-identifier (get-nyxt-id checkbox)))
+
+(defmethod %follow-hint ((radio nyxt/dom:radio-element))
+  (check-element :nyxt-identifier (get-nyxt-id radio)))
+
 (defmethod %follow-hint ((input nyxt/dom:input-element))
-  (str:string-case (plump:get-attribute "type" input)
-    ("button" (click-element :nyxt-identifier (get-nyxt-id input)))
-    ("radio" (check-element :nyxt-identifier (get-nyxt-id input)))
-    ("checkbox" (check-element :nyxt-identifier (get-nyxt-id input)))
-    (otherwise (focus-element :nyxt-identifier (get-nyxt-id input)))))
+  (focus-element :nyxt-identifier (get-nyxt-id input)))
 
 (defmethod %follow-hint ((textarea nyxt/dom:textarea-element))
   (focus-element :nyxt-identifier (get-nyxt-id textarea)))
