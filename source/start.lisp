664d895399d5644e8160d136a317c3d3f778d73b
Artyom Bologov <aartaka@protonmail.com>
2021-06-04 02:32:10 +0500

    start: Handle SIGTERM & SIGINT gracefully.

--- source/start.lisp
+++ source/start.lisp
@@ -190,6 +190,11 @@ before running this command."
           (when p
             (file-position stream p)))))))
 
+(cffi:defcallback handle-interrupt
+    :void ((signum :int) (siginfo :pointer) (ptr :pointer))
+  (declare (ignore signum siginfo ptr))
+  (quit))
+
 (export-always 'entry-point)
 (defun entry-point ()
   "Read the CLI arguments and start the browser.
@@ -201,6 +206,17 @@ Don't run this from a REPL, prefer `start' instead."
                      (opts:arg-parser-failed #'handle-malformed-cli-arg))
         (opts:get-opts))
     (setf *run-from-repl-p* nil)             ; Not a REPL.
+    (let ((interrupt-sigaction (cffi:foreign-alloc '(:struct isys::sigaction))))
+      ;; Mimics https://www.systutorials.com/catching-the-signal-sent-by-kill-in-c-on-linux/
+      (isys:memset interrupt-sigaction 0 (cffi:foreign-type-size '(:struct isys::sigaction)))
+      (setf (cffi:foreign-slot-value
+             interrupt-sigaction '(:struct isys:sigaction) 'isys::sigaction)
+            (cffi:callback handle-interrupt))
+      (setf (cffi:foreign-slot-value
+             interrupt-sigaction '(:struct isys:sigaction) 'isys::flags)
+            isys:sa-siginfo)
+      (isys:sigaction isys:sigint interrupt-sigaction (cffi:null-pointer))
+      (isys:sigaction isys:sigterm interrupt-sigaction (cffi:null-pointer)))
     (apply #'start (append options (list :urls free-args)))))
 
 (declaim (ftype (function ((or null trivial-types:pathname-designator)
