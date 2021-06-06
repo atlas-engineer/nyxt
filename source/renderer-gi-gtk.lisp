6184884b48b7cacdc51d104cb2299c26437a73d8
midchildan <git@midchildan.org>
2021-06-06 18:10:54 +0900

    renderer-gi-gtk.lisp: fix initialization on macOS
    This fix is similar to d0a4161a369ab9db050c69b36a924f6b48c73270.

--- source/renderer-gi-gtk.lisp
+++ source/renderer-gi-gtk.lisp
@@ -18,16 +18,24 @@
   (defun renderer-thread-p ()
     (string= "main thread" (bt:thread-name (bt:current-thread))))
   (defmethod ffi-initialize ((browser gtk-browser) urls startup-timestamp)
+    "gtk:within-main-loop handles all the GTK initialization. On
+    GNU/Linux, Next could hang after 10 minutes if it's not
+    used. Conversely, on Darwin, if gtk:within-main-loop is used, no
+    drawing happens. Drawing operations on Darwin MUST originate from
+    the main thread, which the GTK main loop is not guaranteed to be
+    on."
     (log:debug "Initializing GI-GTK Interface")
     (setf gtk-running-p t)
-    (let ((main-thread (bt:make-thread
-                        (lambda ()
-                          (with-protect ("Error on GTK thread: ~a" :condition)
-                            (glib:g-set-prgname "nyxt")
-                            #+GTK-3-4
-                            (gdk:gdk-set-program-class "Nyxt")
-                            (gir:invoke ((gir:ffi "Gtk" "3.0") 'main))))
-                        :name "main thread")))
-      (finalize browser urls startup-timestamp)
-      (unless *run-from-repl-p*
-        (bt:join-thread main-thread)))))
+    (flet ((main-func ()
+             (with-protect ("Error on GTK thread: ~a" :condition)
+               (glib:g-set-prgname "nyxt")
+               #+GTK-3-4
+               (gdk:gdk-set-program-class "Nyxt")
+               (gir:invoke ((gir:ffi "Gtk" "3.0") 'main)))))
+          (finalize browser urls startup-timestamp)
+          #-darwin
+          (let ((main-thread (bt:make-thread #'main-func :name "main thread")))
+            (unless *run-from-repl-p*
+              (bt:join-thread main-thread)))
+          #+darwin
+          (main-func))))
