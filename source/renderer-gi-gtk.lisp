d9fc849bd2df0c5089f51faecf7f8fa14984ae52
John Mercouris <john@mercouris.email>
2021-06-07 17:19:43 +0200

    renderer-gi-gtk.lisp: update documentation string

--- source/renderer-gi-gtk.lisp
+++ source/renderer-gi-gtk.lisp
@@ -18,12 +18,8 @@
   (defun renderer-thread-p ()
     (string= "main thread" (bt:thread-name (bt:current-thread))))
   (defmethod ffi-initialize ((browser gtk-browser) urls startup-timestamp)
-    "gtk:within-main-loop handles all the GTK initialization. On
-    GNU/Linux, Next could hang after 10 minutes if it's not
-    used. Conversely, on Darwin, if gtk:within-main-loop is used, no
-    drawing happens. Drawing operations on Darwin MUST originate from
-    the main thread, which the GTK main loop is not guaranteed to be
-    on."
+    "On GNU/Linux we can create a separate thread to launch the GTK
+interface. On Darwin, we must run the GTK thread on the main thread."
     (log:debug "Initializing GI-GTK Interface")
     (setf gtk-running-p t)
     (flet ((main-func ()
