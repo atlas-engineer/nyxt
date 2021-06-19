4f9365288367664b5c13ce682f9a57fed3e993d5
Artyom Bologov <aartaka@protonmail.com>
2021-06-19 21:06:36 +0500

    proxy-mode: Use web-buffer for an example of proxy-setting.

--- source/proxy-mode.lisp
+++ source/proxy-mode.lisp
@@ -23,7 +23,7 @@ Example to use Tor as a proxy both for browsing and downloading:
                                          :allowlist '(\"localhost\" \"localhost:8080\")
                                          :proxied-downloads-p t))))
 
-\(define-configuration buffer
+\(define-configuration web-buffer
   ((default-modes (append '(proxy-mode) %slot-default%))))"
   ((proxy (make-instance 'nyxt:proxy
                          :url (quri:uri "socks5://localhost:9050")
