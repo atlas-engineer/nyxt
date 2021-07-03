5000d07e58946102c1afaa1779f9e159189d8a53
Pedro Delfino <p.delfino01@gmail.com>
2021-07-03 12:25:06 -0300

    history.lisp: minor advances to use selected history entries before bookmarking what is over the threshold.

--- source/history.lisp
+++ source/history.lisp
@@ -107,6 +107,34 @@ class."
             (if-confirm ("Bookmark ~a?" url-address)
                         (bookmark-url :url url-address)))))))
 
+(defun tweaked-bookmark-frequent-visit (history-entries-selected)
+  "Add frequently visited URLs that are not included in the bookmarks.  The
+   sample space of visited URLs is received as an argument and previously
+   defined by the user. "
+  (labels ((urls-visited-over-threshold (threshold)
+             "The local function "`urls-visited-over-thresold" returns all URLs
+             instances which were visited more times than the threshold."
+             (let* ((history-entries-above-threshold
+                      (remove-if-not #'(lambda (e) (> (implicit-visits e) threshold))
+                                     history-entries-selected)))
+               (mapcar #'(lambda (e) (url e)) history-entries-above-threshold)))
+           (bookmarked-url-p (url)
+             "The local function  "`bookmarked-url-p" returns the URL
+            itself if it is new to the bookmark list and NIL if it is
+            already there."
+             (let ((bookmarks-address-list
+                     (mapcar #'(lambda (e) (url e))
+                             (with-data-unsafe (bookmarks (bookmarks-path (current-buffer)))
+                               bookmarks))))
+               (if (member url bookmarks-address-list :test #'quri:uri=)
+                   nilq
+                   url))))
+    (dolist (url (urls-visited-over-threshold (url->bookmark-visit-threshold *browser*)))
+      (let ((url-address (render-url url)))
+        (if (bookmarked-url-p url)
+            (if-confirm ("Bookmark ~a?" url-address)
+                        (bookmark-url :url url-address)))))))
+
 (define-command bookmark-frequent-history-entries (&key (buffer (current-buffer)))
   "Prompts the user for history entries (the user can select them all), and then
 call bookmark-frequent-visit over the selection."
@@ -119,7 +147,8 @@ call bookmark-frequent-visit over the selection."
       (dolist (entry entries)
         (format t "~S ~S ~S ~%" entry entries history)
         entries))
-     entries))
+    (with-data-access (history (history-path buffer))
+      (tweaked-bookmark-frequent-visit entries))))
 
 (declaim (ftype (function (quri:uri &key (:title string) (:buffer buffer)) t) history-add))
 (defun history-add (url &key (title "") (buffer (current-buffer)))
