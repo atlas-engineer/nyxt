c8473b5858a9ec99d7f5212f29d6c34fa4634116
John Mercouris <john@mercouris.email>
2021-07-06 21:51:12 +0200

    window.lisp: reformat window class
    when elements are multiple lines, indent them down so that they are at
    the same position, gives them more horizontal space
    
    additionally, remove example, it is no longer accurate. it suffices to
    say that the status-formatter accepts a window and returns a string

--- source/window.lisp
+++ source/window.lisp
@@ -7,80 +7,64 @@
 
 (define-class window ()
   ((id "")
-   (title 'window-default-title
-          :type (or function function-symbol)
-          :documentation "Title of the window.
+   (title
+    'window-default-title
+    :type (or function function-symbol)
+    :documentation "Title of the window.
 It's a function of the window argument that returns the title as a string.")
    (active-buffer :accessor nil :reader active-buffer :export nil)
-   (active-prompt-buffers '()
-                          :export nil
-                          :documentation "The stack of current prompt buffers.")
-   (panel-buffers (list)
-                  :export nil
-                  :documentation "A list of panel buffers appearing on the window.")
-   (key-stack '()
-              :documentation "A stack that keeps track of the key chords a user has pressed.")
-   (last-key nil
-             :export nil
-             :type (or null keymap:key)
-             :documentation "Last pressed key.  Useful for `self-insert'.")
-   (fullscreen-p nil
-                 :export nil
-                 :type boolean
-                 :documentation "Whether the window is displayed in
-fullscreen.")
-   ;; TODO: each frame should have a status buffer, not each window
-   (status-buffer :export nil)
-   (message-buffer-height 16
-                          :documentation "The height of the message buffer in pixels.")
-   (message-buffer-style (cl-css:css
-                          '((body
-                             :font-size "12px"
-                             :padding 0
-                             :padding-left "4px"
-                             :margin 0))))
-   (prompt-buffer-open-height 256
-                              :documentation "The height of the prompt buffer when open.")
-   (input-dispatcher #'dispatch-input-event
-                     :documentation "Function to process input events.
+   (active-prompt-buffers
+    '()
+    :export nil
+    :documentation "The stack of current prompt buffers.")
+   (panel-buffers
+    (list)
+    :export nil
+    :documentation "A list of panel buffers appearing on the window.")
+   (key-stack
+    '()
+    :documentation "A stack of the key chords a user has pressed.")
+   (last-key
+    nil
+    :export nil
+    :type (or null keymap:key)
+    :documentation "Last pressed key.  Useful for `self-insert'.")
+   (fullscreen-p
+    nil
+    :export nil
+    :type boolean
+    :documentation "Whether the window is displayed in fullscreen.")
+   (status-buffer
+    :export nil)
+   (message-buffer-height
+    16
+    :documentation "The height of the message buffer in pixels.")
+   (message-buffer-style
+    (cl-css:css
+     '((body
+        :font-size "12px"
+        :padding 0
+        :padding-left "4px"
+        :margin 0))))
+   (prompt-buffer-open-height
+    256
+    :documentation "The height of the prompt buffer when open.")
+   (input-dispatcher
+    #'dispatch-input-event
+    :documentation "Function to process input events.
 It takes EVENT, BUFFER, WINDOW and PRINTABLE-P parameters.
 Cannot be null.")
-   (window-set-buffer-hook (make-hook-window-buffer)
-                                  :type hook-window-buffer
-                                  :documentation "Hook run before `window-set-buffer' takes effect.
+   (window-set-buffer-hook
+    (make-hook-window-buffer)
+    :type hook-window-buffer
+    :documentation "Hook run before `window-set-buffer' takes effect.
 The handlers take the window and the buffer as argument.")
-   (status-formatter #'format-status
-                     :type (function (window) string)
-                     :documentation "Function of a window argument that returns
+   (status-formatter
+    #'format-status
+    :type (function (window) string)
+    :documentation "Function of a window argument that returns
 a string to be printed in the status view.
-Cannot be null.
-
-Example formatter that prints the buffer indices over the total number of buffers:
-
-\(defun my-format-status (window)
-  (let* ((buffer (current-buffer window))
-         (buffer-count (1+ (or (position buffer
-                                         (sort (buffer-list)
-                                               #'string<
-                                               :key #'id))
-                               0))))
-    (str:concat
-     (markup:markup
-      (:b (format nil \"[~{~a~^ ~}]\"
-                  (mapcar (lambda (m) (str:replace-all \"-mode\" \"\"
-                                                       (str:downcase
-                                                        (class-name (class-of m)))))
-                          (modes buffer)))))
-     (format nil \" (~a/~a) \"
-             buffer-count
-             (length (buffer-list)))
-     (format nil \"~a~a — ~a\"
-            (if (and (web-buffer-p buffer)
-                     (eq (slot-value buffer 'load-status) :loading))
-                \"(Loading) \"
-                \"\")
-            (render-url (url buffer))
-            (title buffer)))))")
+Cannot be null.")
    (window-delete-hook (make-hook-window)
                        :type hook-window
                        :documentation "Hook run after `ffi-window-delete' takes effect.
