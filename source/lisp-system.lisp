3f4fbe9cff5048e0a7d7aecff63317c5b1e4b3d2
John Mercouris <john@mercouris.email>
2021-07-15 15:49:21 +0200

    lisp-system.lisp: use spinneret

--- source/lisp-system.lisp
+++ source/lisp-system.lisp
@@ -6,23 +6,23 @@
 (define-command list-systems ()
   "List systems available via Quicklisp."
   (with-current-html-buffer (buffer "*Systems*" 'base-mode)
-    (markup:markup
-     (:style (style buffer))
-     (:h1 "Systems")
-     (:p "Listing of all available Quicklisp systems.")
-     (:body
-      (loop for system in (ql:system-list)
-            collect
-               (let ((name (ql-dist:short-description system))
-                     (size (format nil "~a" (ql-dist:archive-size (ql-dist:preference-parent system))))
-                     (dependencies (format nil "~a" (ql-dist:required-systems system))))
-                 (markup:markup (:div
-                                 (:h2 name)
-                                 (:p "Size: " size)
-                                 (:p "Requires: " dependencies)
-                                 (:p (:a :class "button"
-                                         :href (lisp-url `(ql:quickload ,name)) "Load"))
-                                 (:hr)))))))))
+    (spinneret:with-html-string
+      (:style (style buffer))
+      (:h1 "Systems")
+      (:p "Listing of all available Quicklisp systems.")
+      (:body
+       (loop for system in (ql:system-list)
+             collect
+                (let ((name (ql-dist:short-description system))
+                      (size (format nil "~a" (ql-dist:archive-size (ql-dist:preference-parent system))))
+                      (dependencies (format nil "~a" (ql-dist:required-systems system))))
+                  (:div
+                   (:h2 name)
+                   (:p "Size: " size)
+                   (:p "Requires: " dependencies)
+                   (:p (:a :class "button"
+                           :href (lisp-url `(ql:quickload ,name)) "Load"))
+                   (:hr))))))))
 
 (define-class quicklisp-source (prompter:source)
   ((prompter:name "Quicklisp systems")
