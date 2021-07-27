666e0ef311e680f8fd1e720df093f7444c5f9941
Pierre Neidhardt <mail@ambrevar.xyz>
2021-07-27 12:56:44 +0200

    build-scripts/sync-submodules-with-guix: Init.

---
+++ build-scripts/sync-submodules-with-guix.scm
@@ -0,0 +1,70 @@
+;; Run with
+;;   guix repl -- ./build-scripts/sync-submodules-with-guix.scm
+
+(use-modules (srfi srfi-1)
+             (srfi srfi-2)
+             (gnu packages web-browsers)
+             (guix packages)
+             (guix git-download)
+             (guix build utils)
+             (ice-9 string-fun))
+
+(let* ((cache '())
+       (all-deps (lambda (pkg)
+                   (if (package? pkg)
+                       (delete-duplicates
+                        (append (map cadr (package-inputs pkg))
+                                (map cadr (package-native-inputs pkg))
+                                (map cadr (package-propagated-inputs pkg))))
+                       '()))))
+  (define (all-recursive-deps pkg)
+    (if (and (not (member pkg cache))
+             (package? pkg))
+        (begin
+          (set! cache (cons pkg cache))
+          (cons pkg (apply append (map all-recursive-deps (all-deps pkg)))))
+        '()))
+
+  (define (git-commit? s)
+    (= 40 (string-length s)))
+
+  (define (git-url->name url)
+    (string-replace-substring
+     (string-replace-substring (basename url) ".git" "") "/" ""))
+
+  (define (sbcl-deps pkg)
+    "Return a list of (URL REF COMMIT?).
+REF is a commit or a branch name or a tag.
+In this context, Git conflates 'branch name' and 'tag' as the same thing.
+COMMIT? is either #:commit or #:branch."
+    (filter (lambda (pkg)
+              (and
+               (and-let* ((source (package-source pkg))
+                          (method (origin-method source))
+                          (name (procedure-name method)))
+                 (eq? 'git-fetch name))
+               (string-prefix? "sbcl-" (package-name pkg))))
+            (all-recursive-deps nyxt)))
+
+  (define (package-repo-ref pkg)
+    (let* ((origin (origin-uri (package-source pkg)))
+           (commit (git-reference-commit origin)))
+
+      (list (git-reference-url origin)
+            (git-reference-commit origin)
+            (if (git-commit? commit) #:commit #:branch))))
+
+  (define (add-submodule ref)
+    (pk ref)
+    (let ((path (string-append "_build/submodules/"
+                               (git-url->name (first ref)))))
+      (invoke (append '("git" "submodule" "add")
+                      (if (eq? (list-ref ref 2) #:branch)
+                          '("--depth" "1" "-b" (second ref))
+                          '())
+                      (list (first ref) path)))
+      (when (eq? (list-ref ref 2) #:commit)
+        (invoke "git" "-C" path "checkout" (second ref)))))
+
+  ;; (pk (map package-repo-ref (sbcl-deps nyxt)))
+  (map add-submodule (map package-repo-ref (sbcl-deps nyxt))))
