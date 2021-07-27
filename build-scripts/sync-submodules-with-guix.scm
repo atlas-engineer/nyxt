11f73fc141f9ea288d68f9720d196c53d7e85753
Pierre Neidhardt <mail@ambrevar.xyz>
2021-07-27 14:49:03 +0200

    build-scripts/sync-submodules-with-guix: Fix and don't error on existing module.

--- build-scripts/sync-submodules-with-guix.scm
+++ build-scripts/sync-submodules-with-guix.scm
@@ -9,21 +9,23 @@
              (guix build utils)
              (ice-9 string-fun))
 
-(let* ((cache '())
-       (all-deps (lambda (pkg)
-                   (if (package? pkg)
-                       (delete-duplicates
-                        (append (map cadr (package-inputs pkg))
-                                (map cadr (package-native-inputs pkg))
-                                (map cadr (package-propagated-inputs pkg))))
-                       '()))))
-  (define (all-recursive-deps pkg)
-    (if (and (not (member pkg cache))
-             (package? pkg))
-        (begin
-          (set! cache (cons pkg cache))
-          (cons pkg (apply append (map all-recursive-deps (all-deps pkg)))))
+(let* ((cache '()))
+  (define (all-deps pkg)
+    (if (package? pkg)
+        (delete-duplicates
+         (append (map cadr (package-inputs pkg))
+                 (map cadr (package-native-inputs pkg))
+                 (map cadr (package-propagated-inputs pkg))))
         '()))
+
+  (define (all-recursive-deps pkg)
+    (delete-duplicates
+     (if (and (not (member pkg cache))
+              (package? pkg))
+         (begin
+           (set! cache (cons pkg cache))
+           (cons pkg (apply append (map all-recursive-deps (all-deps pkg)))))
+         '())))
 
   (define (git-commit? s)
     (= 40 (string-length s)))
@@ -33,9 +35,6 @@
      (string-replace-substring (basename url) ".git" "") "/" ""))
 
   (define (sbcl-deps pkg)
-    "Return a list of (URL REF).
-REF is a commit or a branch name or a tag.
-In this context, Git conflates 'branch name' and 'tag' as the same thing."
     (filter (lambda (pkg)
               (and
                (and-let* ((source (package-source pkg))
@@ -46,20 +45,31 @@ In this context, Git conflates 'branch name' and 'tag' as the same thing."
             (all-recursive-deps nyxt)))
 
   (define (package-repo-ref pkg)
+    "Return a list of (URL REF).
+REF is a commit or a branch name or a tag.
+In this context, Git conflates 'branch name' and 'tag' as the same thing."
     (let* ((origin (origin-uri (package-source pkg)))
            (commit (git-reference-commit origin)))
 
       (list (git-reference-url origin)
-            (git-reference-commit origin)
-            (if (git-commit? commit) #:commit #:branch))))
+            (git-reference-commit origin))))
+
+  (define (all-refs pkg)
+    "Return all unique repo refs of PKG."
+    (delete-duplicates (map package-repo-ref (sbcl-deps pkg))))
 
   (define (add-submodule ref)
     (pk ref)
     (let ((path (string-append "_build/"
                                (git-url->name (first ref)))))
-      (apply invoke (append '("git" "submodule" "add")
-                            (list (first ref) path)))
-      (invoke "git" "-C" path "checkout" (second ref))))
+      (let ((code
+             (with-error-to-port (%make-void-port "w")
+               (lambda ()
+                 (system* "git" "submodule" "status" path)))))
+        (unless (zero? code)
+          (apply invoke (append '("git" "submodule" "add")
+                                (list (first ref) path)))
+          (invoke "git" "-C" path "checkout" (second ref))))))
 
-  ;; (pk (map package-repo-ref (sbcl-deps nyxt)))
-  (map add-submodule (map package-repo-ref (sbcl-deps nyxt))))
+  ;; (pk (all-refs nyxt))
+  (map add-submodule (all-refs nyxt)))
