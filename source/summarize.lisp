d4e1d1eb12e0ebef8e4da8c00866b56490b567b0
John Mercouris <john@mercouris.email>
2021-07-29 13:50:58 +0200

    summarize.lisp: reformat summarize-buffer

--- source/summarize.lisp
+++ source/summarize.lisp
@@ -4,13 +4,13 @@
   "Summarize the current buffer by creating a new summary buffer."
   (with-current-html-buffer (output (format nil "*Summary ~a*" (title buffer)) 'base-mode)
     (let ((contents
-            (map 'list (lambda (e)
-                         (plump:text e))
-                 (clss:select "p" (document-model (current-mode 'web))))))
+            (serapeum:string-join
+             (map 'list (lambda (e) (plump:text e))
+                  (clss:select "p" (document-model (current-mode 'web))))
+             " ")))
       (spinneret:with-html-string
         (:style (style output))
         (:h1 "Summary for: " (title buffer))
         (:ul
-         (loop for point in (analysis:summarize-text (serapeum:string-join contents " ")
-                                                     :summary-length summary-length)
+         (loop for point in (analysis:summarize-text contents :summary-length summary-length)
                collect (:li point)))))))
