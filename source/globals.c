207ec78f937081886a3ea894f4a5dbe5904646eb
Artyom Bologov <aartaka@protonmail.com>
2021-08-02 12:16:28 +0500

    *.c: Remove debugging code.

--- source/globals.c
+++ source/globals.c
@@ -17,19 +17,11 @@ extensions_data_add_from_json_root(JsonNode *root, WebKitWebPage *web_page)
                 const char *name = members->data,
                         *permissions = json_node_get_string(
                                 json_object_get_member(obj, name));
-                g_print("Name is \"%s\", permissions is \"%s\"\n", name, permissions);
-                g_print("EXTENSIONS_DATA is %p\n", EXTENSIONS_DATA);
-                g_print("Extension will be allocated\n");
                 extension = malloc(sizeof(ExtensionData));
-                g_print("Extension allocated\n");
                 extension->name = (char*) name;
-                g_print("Extension name set: \"%s\"\n", name);
                 extension->permissions = (char*) permissions;
-                g_print("Extension permissions set: \"%s\"\n", permissions);
                 extension->world = webkit_script_world_new_with_name(name);
-                g_print("Extension world created: %p\n", extension->world);
                 g_hash_table_insert(EXTENSIONS_DATA, (void*) name, extension);
-                g_print("Extension created\n");
         }
 }
 
--- source/nyxt.c
+++ source/nyxt.c
@@ -11,18 +11,7 @@ user_message_received (WebKitWebPage     *web_page,
                        WebKitUserMessage *message,
                        gpointer           user_data)
 {
-        WebKitFrame *frame = webkit_web_page_get_main_frame(web_page);
-        if (hello_world == NULL)
-                hello_world = webkit_script_world_new_with_name("hello");
-        JSCContext *hello_context = webkit_frame_get_js_context_for_script_world(frame, hello_world);
-        int hello = jsc_value_to_int32(jsc_context_evaluate(hello_context, "foo()", -1));
-        g_print ("hello is \"%d\"\n", hello);
         return FALSE;
-}
-
-static int foo(void)
-{
-    return 2;
 }
 
 static void
@@ -35,25 +24,6 @@ inject_apis (char* extension_name, ExtensionData *data, void *user_data)
 static void
 document_loaded_callback (WebKitWebPage *web_page)
 {
-        WebKitFrame *frame = webkit_web_page_get_main_frame(web_page);
-        if (hello_world == NULL)
-                hello_world = webkit_script_world_new_with_name("hello");
-        JSCContext *hello_context = webkit_frame_get_js_context_for_script_world(frame, hello_world);
-        JSCValue *hello_fn;
-        if (jsc_value_is_undefined(jsc_context_evaluate(hello_context, "foo", -1))) {
-                hello_fn = jsc_value_new_function(
-                        hello_context, "foo", G_CALLBACK(foo), NULL, NULL,
-                        G_TYPE_INT, 0, G_TYPE_NONE);
-                jsc_context_set_value(hello_context, "foo", hello_fn);
-                g_print ("Defined foo\n");
-        } else {
-                hello_fn = jsc_context_evaluate(hello_context, "foo", -1);
-        }
-        int hello = jsc_value_to_int32(jsc_value_function_call(hello_fn, G_TYPE_NONE));
-        g_print ("Page %lu loaded for \"%s\", hello is \"%d\"\n",
-                 webkit_web_page_get_id (web_page),
-                 webkit_web_page_get_uri (web_page),
-                 hello);
         g_hash_table_foreach (EXTENSIONS_DATA, inject_apis, NULL);
 }
 
