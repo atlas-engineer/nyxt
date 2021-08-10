7afaf9e929919d9bae5f5e3541750c445b570c08
John Mercouris <john@mercouris.email>
2021-08-10 13:56:24 +0200

    Revert "Revert renaming of "non-hypenated modes""
    This reverts commit f77594f5c17bc89a6c62b3a4021f0c4089253aef.
    
    we can now do this!

--- source/no-webgl-mode.lisp
+++ source/no-webgl-mode.lisp
@@ -1,16 +1,16 @@
 ;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
-(uiop:define-package :nyxt/nowebgl-mode
+(uiop:define-package :nyxt/no-webgl-mode
   (:use :common-lisp  :nyxt)
   (:documentation "Disable WebGL."))
-(in-package :nyxt/nowebgl-mode)
+(in-package :nyxt/no-webgl-mode)
 
-(define-mode nowebgl-mode ()
+(define-mode no-webgl-mode ()
   "Disable WebGL in current buffer."
   ((previous-webgl-setting nil
                            :documentation "The state of WebGL
- before nowebgl-mode was enabled.")
+ before no-webgl-mode was enabled.")
    (destructor
     (lambda (mode)
       (ffi-buffer-enable-webgl (buffer mode) (previous-webgl-setting mode))))
