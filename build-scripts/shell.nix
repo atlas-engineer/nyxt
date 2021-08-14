45bda8c0344b819f858c4402890d5cd19a3a764d
John Mercouris <john@mercouris.email>
2021-08-14 20:59:38 +0200

    shell.nix: add comment explaining purpose

--- build-scripts/shell.nix
+++ build-scripts/shell.nix
@@ -1,7 +1,9 @@
 # SPDX-FileCopyrightText: Atlas Engineer LLC
 # SPDX-License-Identifier: BSD-3-Clause
 
-# This file is meant to be used with the nyxt/gi-gtk system
+# This file is meant to be used with the nyxt/gi-gtk system. Use this
+# file to open a nix shell with the required dependencies to run SBCL
+# and load nyxt/gi-gtk.
 
 { pkgs ? import <nixpkgs> {} } :
 with builtins;
