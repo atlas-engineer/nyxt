4704439f3f83ababede4ad42e6c7974b384dcf2e
Pedro Delfino <p.delfino01@gmail.com>
2021-08-21 19:36:14 -0300

    web-mode: refactor code to insert auxiliary ps function inside main function.

--- source/web-mode.lisp
+++ source/web-mode.lisp
@@ -552,16 +552,15 @@ Otherwise go forward to the only child."
     (copy-to-clipboard input)
     (echo "Text copied.")))
 
-(define-parenscript %copy-placeholder ()
-  (ps:chain document active-element placeholder))
-
 (define-command copy-placeholder ()
   "Copy placeholder text to clipboard."
-  (let ((current-value (%copy-placeholder)))
-    (if (eq current-value :undefined)
-        (echo "No active selected placeholder.")
-        (progn (copy-to-clipboard current-value)
-               (echo "Placeholder copied.")))))
+  (pflet ((copy-placeholder ()
+            (ps:chain document active-element placeholder)))
+    (let ((current-value (copy-placeholder)))
+      (if (eq current-value :undefined)
+          (echo "No active selected placeholder.")
+          (progn (copy-to-clipboard current-value)
+                 (echo "Placeholder copied."))))))
 
 (define-class autofill-source (prompter:source)
   ((prompter:name "Autofills")
