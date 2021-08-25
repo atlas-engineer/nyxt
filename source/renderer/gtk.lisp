9a0dc4a668e4d94980fb6cea975cfb9f426a5972
Artyom Bologov <aartaka@protonmail.com>
2021-08-25 10:41:53 +0500

    renderers/gtk: Ignore request in resource-load-started.

--- source/renderer/gtk.lisp
+++ source/renderer/gtk.lisp
@@ -1505,7 +1505,7 @@ See `gtk-browser's `modifier-translator' slot."
       (reply-user-message buffer message))
     t)
   (connect-signal buffer "resource-load-started" (view resource request)
-    (declare (ignore request))
+    (declare (ignore request resource))
     (let ((extensions (sera:filter #'nyxt/web-extensions::extension-p
                                    (modes buffer))))
       (alex:when-let ((extension (or (and (background-buffer-p buffer)
