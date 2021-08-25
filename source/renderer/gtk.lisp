61736ab8f1a04bcb1773639169af39e9194d5535
Artyom Bologov <aartaka@protonmail.com>
2021-08-25 17:04:16 +0500

    renderers/gtk: Parse version in a way supporting stable releases.

--- source/renderer/gtk.lisp
+++ source/renderer/gtk.lisp
@@ -1400,7 +1400,9 @@ See `gtk-browser's `modifier-translator' slot."
              `(("name" . "Nyxt")
                ("vendor" . "Atlas Engineer LLC")
                ("version" ,(first nyxt-version))
-               ("build" ,(third nyxt-version)))))))
+               ("build" ,(if (rest nyxt-version)
+                             (third nyxt-version)
+                             "")))))))
         ("storage.local.get"
          (wrap-in-channel (storage-local-get buffer message-params)))
         ("storage.local.set"
