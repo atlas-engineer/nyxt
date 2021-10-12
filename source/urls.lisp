74349deccf9870545409a6f7a9f5cf355d1c776d
Artyom Bologov <aartaka@protonmail.com>
2021-10-12 14:05:00 +0500

    urls: echo-warning on deprecation in lisp-url.

--- source/urls.lisp
+++ source/urls.lisp
@@ -231,6 +231,7 @@ Example:
 (export-always 'lisp-url)
 (defun lisp-url (&rest args)
   (declare (ignore args))
+  (echo-warning "Lisp URLs are no longer doing anything")
   "")
 
 (export-always 'parse-about-url)
