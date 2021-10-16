720204be82619bed18009e1d6c1cdb4170bd9100
Artyom Bologov <aartaka@protonmail.com>
2021-10-16 20:42:14 +0500

    browser (startup): Use the buffer instead of (current-buffer).

--- source/browser.lisp
+++ source/browser.lisp
@@ -312,7 +312,7 @@ restored."
                                     nil)))
                    (:always-restore
                     (get-data (history-path buffer))
-                    (restore-history-buffers (history-path (current-buffer))))
+                    (restore-history-buffers (history-path buffer)))
                    (:never-restore
                     (log:info "Not restoring session.")
                     (get-data (history-path buffer))
