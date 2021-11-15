89c35c77efcf6e58c7a9f54d0c6fd95931cadd0f
Artyom Bologov <aartaka@protonmail.com>
2021-11-16 00:43:12 +0500

    browser,mode/web: Docstring and warning fixes.

--- source/browser.lisp
+++ source/browser.lisp
@@ -755,49 +755,37 @@ sometimes yields the wrong result."
 (define-ffi-generic ffi-set-tracking-prevention (buffer value))
 (define-ffi-generic ffi-buffer-copy (buffer)
   (:method ((buffer buffer))
-    ;; TODO: Abstract this to a sibling of `with-current-buffer'?
-    (let ((switching-buffers-p
-            (not (eq buffer (current-buffer)))))
-      (when switching-buffers-p
-        (switch-buffer :id (id buffer)))
+    (with-current-buffer buffer
+      ;; On some systems like Xorg, clipboard pasting happens just-in-time.  So if we
+      ;; copy something from the context menu 'Copy' action, upon pasting we will
+      ;; retrieve the text from the GTK thread.  This is prone to create
+      ;; dead-locks (e.g. when executing a Parenscript that acts upon the clipboard).
+      ;;
+      ;; To avoid this, we can 'flush' the clipboard to ensure that the copied text
+      ;; is present the clipboard and need not be retrieved from the GTK thread.
+      ;; TODO: Do we still need to flush now that we have multiple threads?
+      ;; (trivial-clipboard:text (trivial-clipboard:text))
       (let ((input (%copy)))
         (copy-to-clipboard input)
-        (echo "Text copied: ~s" input))
-      (when switching-buffers-p
-        (switch-buffer-last)))))
+        (echo "Text copied: ~s" input)))))
 (define-ffi-generic ffi-buffer-paste (buffer)
   (:method ((buffer buffer))
-    (let ((switching-buffers-p
-            (not (eq buffer (current-buffer)))))
-      (when switching-buffers-p
-        (switch-buffer :id (id buffer)))
-      (%paste)
-      (when switching-buffers-p
-        (switch-buffer-last)))))
+    (with-current-buffer buffer
+      (%paste))))
 (define-ffi-generic ffi-buffer-cut (buffer)
   (:method ((buffer buffer))
-    (let ((switching-buffers-p
-            (not (eq buffer (current-buffer)))))
-      (when switching-buffers-p
-        (switch-buffer :id (id buffer)))
+    (with-current-buffer buffer
       (let ((input (%cut)))
         (when input
           (copy-to-clipboard input)
-          (echo "Cut the text : ~s" input)))
-      (when switching-buffers-p
-        (switch-buffer-last)))))
+          (echo "Text cut: ~s" input))))))
 (define-ffi-generic ffi-buffer-select-all (buffer)
   (:method ((buffer buffer))
-    (let ((switching-buffers-p
-            (not (eq buffer (current-buffer)))))
-      (when switching-buffers-p
-        (switch-buffer :id (id buffer)))
-      (%select-all)
-      (when switching-buffers-p
-        (switch-buffer-last)))))
+    (with-current-buffer buffer
+      (%select-all))))
 (define-ffi-generic ffi-buffer-undo (buffer)
   (:method ((buffer buffer))
-    (echo-warning "Undoing the editingis not yet implemented for this port.")))
+    (echo-warning "Undoing the edits is not yet implemented for this renderer.")))
 (define-ffi-generic ffi-buffer-redo (buffer)
   (:method ((buffer buffer))
-    (echo-warning "Redoing the editingis not yet implemented for this port.")))
+    (echo-warning "Redoing the edits is not yet implemented for this renderer.")))
