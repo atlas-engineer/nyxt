db57ef43478fc3768b2f3529617f4760d33ea925
aartaka <aartaka@protonmail.com>
2022-01-06 12:01:57 +0500

    gopher: Simplify rendering and enable menu items.

--- source/gopher.lisp
+++ source/gopher.lisp
@@ -3,36 +3,29 @@
 
 (in-package :nyxt)
 
-(defgeneric gopher-line->html (line parent)
+(defgeneric gopher-line->html (line)
   (:documentation "Transform a gopher line to a reasonable HTML representation."))
 
 (defgeneric render-gopher-contents (contents)
-  (:documentation "Produce a page content string given CONTENTS."))
+  (:documentation "Produce a page content string/array given CONTENTS.
+Second return value should be the MIME-type of the content."))
 
-(defmethod gopher-line->html ((line cl-gopher:gopher-line) parent)
-  (declare (ignore parent))
+(defmethod gopher-line->html ((line cl-gopher:gopher-line))
   (spinneret:with-html-string
     (:pre (cl-gopher:display-string line))))
 
-(defmethod gopher-line->html ((line cl-gopher:info-message) parent)
-  (declare (ignore parent))
+(defmethod gopher-line->html ((line cl-gopher:info-message))
   (spinneret:with-html-string
     (if (str:emptyp (cl-gopher:display-string line))
         (:br)
-        (:pre (cl-gopher:display-string line)))))
+        (:pre :style "padding: 0; margin: 0; border-radius: 0;"
+              (cl-gopher:display-string line)))))
 
-(defmethod gopher-line->html ((line cl-gopher:submenu) parent)
-  (let* ((selector (cl-gopher:selector line))
-         (url (str:concat "gopher://"
-                          (cl-gopher:hostname line)
-                          selector
-                          (if (str:ends-with-p "/" selector) "" "/")))
-         (lines (cl-gopher:lines parent)))
-    (spinneret:with-html-string
-     (:a :onclick (ps:ps (setf (ps:chain window location href)
-                               (ps:lisp url)))
-         (cl-gopher:display-string line))
-      (:br))))
+(defmethod gopher-line->html ((line cl-gopher:submenu))
+  (spinneret:with-html-string
+    (:a :class "button" :href (cl-gopher:uri-for-gopher-line line)
+        (cl-gopher:display-string line))
+    (:br)))
 
 (defmethod render-gopher-contents ((contents cl-gopher:text-file-contents))
   (values (str:join +newline+ (cl-gopher:lines contents)) "text/plain"))
@@ -40,4 +33,4 @@
 (defmethod render-gopher-contents ((contents cl-gopher:submenu-contents))
   (spinneret:with-html-string
     (loop for line in (cl-gopher:lines contents)
-          collect (:raw (gopher-line->html line contents)))))
+          collect (:raw (gopher-line->html line)))))
