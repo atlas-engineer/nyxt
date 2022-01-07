5e0f6c46d0a55511b6566679745008c00a025ed0
aartaka <aartaka@protonmail.com>
2022-01-07 13:32:09 +0500

    mode/summarize: Fix summarize-buffer.

--- source/mode/summarize.lisp
+++ source/mode/summarize.lisp
@@ -1,16 +1,17 @@
 (in-package :nyxt/web-mode)
 
-(nyxt::define-internal-page-command summarize-buffer (&key (summary-length 5) (buffer (current-buffer)))
-  (output (format nil "*Summary ~a*" (title buffer)) 'base-mode)
+(nyxt::define-internal-page-command summarize-buffer (&key (summary-length 5) (id (id (current-buffer))))
+  (output (format nil "*Summary ~a*" (title (nyxt::buffers-get id))) 'base-mode)
   "Summarize the current buffer by creating a new summary buffer."
-  (let ((contents
-          (serapeum:string-join
-           (map 'list (lambda (e) (plump:text e))
-                (clss:select "p" (document-model buffer)))
-           " ")))
-    (spinneret:with-html-string
-      (:style (style output))
-      (:h1 "Summary for: " (title buffer))
-      (:ul
-       (loop for point in (analysis:summarize-text contents :summary-length summary-length)
-             collect (:li point))))))
+  (let ((buffer (nyxt::buffers-get id)))
+    (let ((contents
+            (serapeum:string-join
+             (map 'list (lambda (e) (plump:text e))
+                  (clss:select "p" (document-model buffer)))
+             " ")))
+      (spinneret:with-html-string
+        (:style (style output))
+        (:h1 "Summary for: " (title buffer))
+        (:ul
+         (loop for point in (analysis:summarize-text contents :summary-length summary-length)
+               collect (:li point)))))))
