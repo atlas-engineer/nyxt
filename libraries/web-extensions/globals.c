c95c94c44fbe036681f59befc4b78eb6aaddb4ae
aartaka <aartaka@protonmail.com>
2022-01-12 12:42:21 +0500

    libraries/web-extensions/globals.c: Add newlines to TODO printing.

--- libraries/web-extensions/globals.c
+++ libraries/web-extensions/globals.c
@@ -316,7 +316,7 @@ get_result (unsigned long int data_index, int check_only)
 JSCValue *
 todo_method_callback (GPtrArray *args, void *user_data)
 {
-        g_print("%s is not yet implemented.", (char *) user_data);
+        g_print("%s is not yet implemented.\n", (char *) user_data);
         return jsc_value_new_undefined(jsc_context_get_current());
 }
 
@@ -331,7 +331,7 @@ todo_method_callback (GPtrArray *args, void *user_data)
 JSCValue *
 todo_property_callback (void *instance, void *user_data)
 {
-        g_print("%s is not yet implemented.", (char *) user_data);
+        g_print("%s is not yet implemented.\n", (char *) user_data);
         return jsc_value_new_undefined(jsc_context_get_current());
 }
 
