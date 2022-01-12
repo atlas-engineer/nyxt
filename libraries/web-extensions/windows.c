a7d9b3551e0624c69d8be84a126c6543c9b472f8
aartaka <aartaka@protonmail.com>
2022-01-12 13:58:45 +0500

    libraries/web-extensions/windows: Add skeleton of windows API.

---
+++ libraries/web-extensions/windows.c
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: Atlas Engineer LLC
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "globals.h"
+#include "windows.h"
+
+void inject_windows_api (char* extension_name)
+{
+        JSCContext *context = get_extension_context(IS_PRIVILEGED ? NULL : extension_name);
+        MAKE_CLASS(context, Windows, "windows");
+
+        MAKE_EVENT(context, "windows", "onCreated");
+        MAKE_EVENT(context, "windows", "onRemoved");
+        MAKE_EVENT(context, "windows", "onFocusChanged");
+
+        TODO_METHOD(context, windows, get);
+        TODO_METHOD(context, windows, getCurrent);
+        TODO_METHOD(context, windows, getLastFocused);
+        TODO_METHOD(context, windows, getAll);
+        TODO_METHOD(context, windows, create);
+        TODO_METHOD(context, windows, update);
+        TODO_METHOD(context, windows, remove);
+
+        jsc_value_object_set_property(
+                jsc_context_evaluate(context, "browser", -1), "windows",
+                jsc_context_evaluate(context, "windows", -1));
+}
