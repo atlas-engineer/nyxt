bee4631c374e918c668dc45400877131fb14f774
aartaka <aartaka@protonmail.com>
2022-01-17 18:15:07 +0500

    Update phos to the multiple-values API and use it in renderer/gtk.

--- source/renderer/gtk.lisp
+++ source/renderer/gtk.lisp
@@ -715,23 +715,23 @@ See `gtk-browser's `modifier-translator' slot."
   (sera:mvlet* ((url (webkit:webkit-uri-scheme-request-get-uri request))
                 (buffer (find (webkit:webkit-uri-scheme-request-get-web-view request)
                               (buffer-list) :key #'gtk-object))
-                (status body (phos/gemini:request url)))
+                (status meta body (phos/gemini:request url)))
     (flet ((make-gemini-error-page (title)
              (spinneret:with-html-string
                (:h1 title)
-               (:pre (second status)))))
-      (case (first status)
+               (:pre meta))))
+      (case status
         ((:input :sensitive-input)
          (let ((text (quri:url-encode
                       (handler-case
-                          (prompt1 :prompt (second status)
+                          (prompt1 :prompt meta
                             :sources (list (make-instance 'prompter:raw-source))
-                            :invisible-input-p (eq (first status) :sensitive-input))
+                            :invisible-input-p (eq status :sensitive-input))
                         (nyxt-prompt-buffer-canceled () "")))))
            (buffer-load (str:concat url "?" text) :buffer buffer)))
-        (:success (if (str:starts-with-p "text/gemini" (second status))
+        (:success (if (str:starts-with-p "text/gemini" meta)
                       (gemini-render body)
-                      (values body (second status))))
+                      (values body meta)))
         ((:temporary-failure :server-unavailable :cgi-error :proxy-error :slow-down
                              :permanent-failure :not-found :gone :proxy-request-refused :bad-request)
          (make-gemini-error-page
