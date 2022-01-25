ab5dde6d0f16661c016976d92c0dc1a9c984fd87
Artyom Bologov <aartaka@protonmail.com>
2022-01-25 13:15:45 +0500

    browser: Move *after-startup-hook* later in the startup.

--- source/browser.lisp
+++ source/browser.lisp
@@ -324,7 +324,6 @@ prevents otherwise."))
                (startup browser urls)))))))
   ;; Set `init-time' at the end of finalize to take the complete startup time
   ;; into account.
-  (hooks:run-hook *after-startup-hook*)
   (setf (slot-value *browser* 'init-time)
         (local-time:timestamp-difference (local-time:now) startup-timestamp))
   (setf (slot-value *browser* 'ready-p) t))
@@ -364,6 +363,7 @@ restored."
       (restore-session)
       (load-start-urls urls)
       (ffi-buffer-delete dummy))
+    (hooks:run-hook *after-startup-hook*)
     (funcall* (startup-error-reporter-function *browser*))))
 
 ;; Catch a common case for a better error message.
