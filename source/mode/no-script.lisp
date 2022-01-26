ed70510bc0b36a031910348ebf8a58d445f6b5f3
Pierre Neidhardt <mail@ambrevar.xyz>
2022-01-26 13:30:24 +0100

    mode/no-script: Automatically enable JavaScript for internal buffers.

--- source/mode/no-script.lisp
+++ source/mode/no-script.lisp
@@ -7,10 +7,16 @@
 (in-package :nyxt/no-script-mode)
 
 (define-mode no-script-mode ()
-  "Disable Javascript in current buffer."
+  "Disable Javascript in current buffer.
+If the current URL is an internal one (for instance 'nyxt://...'), JavaScript is enabled.
+It's automatically disabled when loading a non-internal URL."
   ((destructor
     (lambda (mode)
       (ffi-buffer-enable-javascript-markup (buffer mode) t)))
    (constructor
     (lambda (mode)
-      (ffi-buffer-enable-javascript-markup (buffer mode) nil)))))
+      (declare (ignore mode))
+      (echo "Reload the buffer for no-script-mode to take effect.")))))
+
+(defmethod nyxt:on-signal-load-started ((mode no-script-mode) url)
+  (ffi-buffer-enable-javascript-markup (buffer mode) (internal-url-p url)))
