d51a4bb602df5eb73eab6c1a3cb9bb0b2c9ca35c
Artyom Bologov <aartaka@protonmail.com>
2022-01-27 15:49:50 +0500

    Replace prompt-buffer-channel with prompt-buffer-ready-hook.

--- tests/renderer-online/set-url.lisp
+++ tests/renderer-online/set-url.lisp
@@ -37,13 +37,14 @@
                         (calispel:? buffer-loaded-channel 10)
                         (nyxt:set-url))))
           (declare (ignorable thread))
-          (sleep 1) ; waiting for set-url thread
-          (calispel:? (nyxt::prompt-buffer-channel (nyxt:current-window)))
-          (nyxt::set-prompt-buffer-input url)
-          (prompter:all-ready-p (nyxt:current-prompt-buffer))
-          (nyxt/prompt-buffer-mode:return-selection)
-          (once-on (buffer-loaded-hook (current-buffer)) buffer
-              (calispel:! url-channel (nyxt:render-url (nyxt:url buffer))))
+          (on (prompt-buffer-ready-hook *browser*)
+              (prompt-buffer)
+            (sleep 1)
+            (nyxt::set-prompt-buffer-input url prompt-buffer)
+            (prompter:all-ready-p prompt-buffer)
+            (nyxt/prompt-buffer-mode:return-selection prompt-buffer)
+            (once-on (buffer-loaded-hook (current-buffer)) buffer
+              (calispel:! url-channel (nyxt:render-url (nyxt:url buffer)))))
           ;; (bt:join-thread thread) ; TODO: Make sure thread always returns.
           )))
     (nyxt:start :no-init t :no-auto-config t
