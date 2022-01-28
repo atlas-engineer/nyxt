13a2216fddb4e903dce94d7b7976da30e2c04f70
Artyom Bologov <aartaka@protonmail.com>
2022-01-28 14:38:42 +0500

    tests/package: Import prove:*debug-on-error* shadowing Nyxt one.

--- tests/package.lisp
+++ tests/package.lisp
@@ -4,5 +4,6 @@
 (uiop:define-package nyxt/tests
   (:use #:common-lisp #:trivia)
   (:use #:nyxt)
+  (:shadowing-import-from #:prove #:*debug-on-error*)
   (:use #:prove)
   (:import-from #:class-star #:define-class))
