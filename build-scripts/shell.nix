aa7772726efae3293c9ecc03fa0ea2bed89392a4
John Mercouris <john@mercouris.email>
2022-02-02 16:48:17 -0600

    shell.nix: update to remove stdenv.lib -> lib
    https://github.com/NixOS/nixpkgs/pull/108934

--- build-scripts/shell.nix
+++ build-scripts/shell.nix
@@ -54,19 +54,19 @@ stdenv.mkDerivation {
     pkgs.webkitgtk
   ];
 
-  LD_LIBRARY_PATH = with stdenv.lib; "${makeLibraryPath [ pkgs.gsettings-desktop-schemas.out
-                                                          pkgs.enchant.out
-                                                          pkgs.glib-networking.out
-                                                          pkgs.webkitgtk
-                                                          pkgs.gobjectIntrospection
-                                                          pkgs.pkg-config.out
-                                                          pkgs.gtk3
-                                                          pkgs.pango.out
-                                                          pkgs.cairo.out
-                                                          pkgs.gdk-pixbuf.out
-                                                          pkgs.glib.out
-                                                          pkgs.libfixposix.out
-                                                          pkgs.libressl.out ]};";
+  LD_LIBRARY_PATH = with lib; "${makeLibraryPath [ pkgs.gsettings-desktop-schemas.out
+                                                   pkgs.enchant.out
+                                                   pkgs.glib-networking.out
+                                                   pkgs.webkitgtk
+                                                   pkgs.gobjectIntrospection
+                                                   pkgs.pkg-config.out
+                                                   pkgs.gtk3
+                                                   pkgs.pango.out
+                                                   pkgs.cairo.out
+                                                   pkgs.gdk-pixbuf.out
+                                                   pkgs.glib.out
+                                                   pkgs.libfixposix.out
+                                                   pkgs.libressl.out ]};";
 
   GIO_MODULE_DIR = "${pkgs.glib-networking.out}/lib/gio/modules/";
   GIO_EXTRA_MODULES = "${pkgs.glib-networking.out}/lib/gio/modules/";
