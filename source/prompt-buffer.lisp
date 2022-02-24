atlas-engineer/nyxt

commit d51a4bb602df5eb73eab6c1a3cb9bb0b2c9ca35c

Replace prompt-buffer-channel with prompt-buffer-ready-hook.

source/prompt-buffer.lisp
@@ -185,7 +185,7 @@ See also `hide-prompt-buffer'."
   ;; TODO: Add method that returns if there is only 1 source with no filter.
   (when prompt-buffer
     (push prompt-buffer (active-prompt-buffers (window prompt-buffer)))
-    (calispel:! (prompt-buffer-channel (window prompt-buffer)) prompt-buffer)
+    (hooks:run-hook (prompt-buffer-ready-hook *browser*) prompt-buffer)
     (prompt-render prompt-buffer)
     (run-thread "Show prompt watcher"
       (let ((prompt-buffer prompt-buffer))
@@ -203,8 +203,6 @@ See also `show-prompt-buffer'."
     ;; Note that PROMPT-BUFFER is not necessarily first in the list, e.g. a new
     ;; prompt-buffer was invoked before the old one reaches here.
     (alex:deletef (active-prompt-buffers window) prompt-buffer)
-    ;; The channel values are irrelevant, so is the element order:
-    (calispel:? (prompt-buffer-channel (window prompt-buffer)) 0)
     (if (resumable-p prompt-buffer)
         (flet ((prompter= (prompter1 prompter2)
                  (and (string= (prompter:prompt prompter1)

source/window.lisp
@@ -21,13 +21,6 @@ It's a function of the window argument that returns the title as a string.")
     (list)
     :export nil
     :documentation "A list of panel buffers appearing on the window.")
-   (prompt-buffer-channel
-    (make-channel) ; TODO: Rename `prompt-buffer-ready-channel'?
-    :export nil
-    :documentation "A channel one may listen to if waiting
-for the prompt buffer to be available.
-You should not rely on the value of this channel.
-The channel is popped when a prompt buffer is hidden.")
    (key-stack
     '()
     :documentation "A stack of the key chords a user has pressed.")

source/browser.lisp
@@ -188,6 +188,11 @@ The handlers take the buffer as argument.")
     :documentation "Hook run after the `prompt-buffer'
 class is instantiated and before initializing the prompt-buffer modes.
 The handlers take the prompt-buffer as argument.")
+   (prompt-buffer-ready-hook
+    (make-instance 'hook-prompt-buffer)
+    :type hook-prompt-buffer
+    :documentation "A hook one can attach to waiting for the prompt buffer to be available.
+The handlers take the prompt-buffer as argument.")
    (before-download-hook
     (make-instance 'hook-download)
     :type hook-download

tests/renderer-online/set-url.lisp
@@ -37,13 +37,14 @@
                         (calispel:? buffer-loaded-channel 10)
                         (nyxt:set-url))))
           (declare (ignorable thread))
-          (sleep 1) ; waiting for set-url thread
-          (calispel:? (nyxt::prompt-buffer-channel (nyxt:current-window)))
-          (nyxt::set-prompt-buffer-input url)
-          (prompter:all-ready-p (nyxt:current-prompt-buffer))
-          (nyxt/prompt-buffer-mode:return-selection)
-          (once-on (buffer-loaded-hook (current-buffer)) buffer
-              (calispel:! url-channel (nyxt:render-url (nyxt:url buffer))))
+          (on (prompt-buffer-ready-hook *browser*)
+              (prompt-buffer)
+            (sleep 1)
+            (nyxt::set-prompt-buffer-input url prompt-buffer)
+            (prompter:all-ready-p prompt-buffer)
+            (nyxt/prompt-buffer-mode:return-selection prompt-buffer)
+            (once-on (buffer-loaded-hook (current-buffer)) buffer
+              (calispel:! url-channel (nyxt:render-url (nyxt:url buffer)))))
           ;; (bt:join-thread thread) ; TODO: Make sure thread always returns.
           )))
     (nyxt:start :no-init t :no-auto-config t

commit 2ecdf69df7f50908445ce5eaebbcec7c06b66b27

prompt-buffer: Restore prompt-buffer-channel.
d51a4bb602df5eb73eab6c1a3cb9bb0b2c9ca35c mistakenly confused it for a hook
point, it is actually necessary to synchronize the show/hide functions.

source/prompt-buffer.lisp
@@ -185,7 +185,7 @@ See also `hide-prompt-buffer'."
   ;; TODO: Add method that returns if there is only 1 source with no filter.
   (when prompt-buffer
     (push prompt-buffer (active-prompt-buffers (window prompt-buffer)))
-    (hooks:run-hook (prompt-buffer-ready-hook *browser*) prompt-buffer)
+    (calispel:! (prompt-buffer-channel (window prompt-buffer)) prompt-buffer)
     (prompt-render prompt-buffer)
     (run-thread "Show prompt watcher"
       (let ((prompt-buffer prompt-buffer))
@@ -193,7 +193,8 @@ See also `hide-prompt-buffer'."
     (ffi-window-set-prompt-buffer-height
      (window prompt-buffer)
      (or height
-         (prompt-buffer-open-height (window prompt-buffer))))))
+         (prompt-buffer-open-height (window prompt-buffer))))
+    (hooks:run-hook (prompt-buffer-ready-hook *browser*) prompt-buffer)))
 
 (defun hide-prompt-buffer (prompt-buffer)
   "Hide PROMPT-BUFFER, display next active one.
@@ -203,6 +204,8 @@ See also `show-prompt-buffer'."
     ;; Note that PROMPT-BUFFER is not necessarily first in the list, e.g. a new
     ;; prompt-buffer was invoked before the old one reaches here.
     (alex:deletef (active-prompt-buffers window) prompt-buffer)
+    ;; The channel values are irrelevant, so is the element order:
+    (calispel:? (prompt-buffer-channel (window prompt-buffer)) 0)
     (if (resumable-p prompt-buffer)
         (flet ((prompter= (prompter1 prompter2)
                  (and (string= (prompter:prompt prompter1)

source/window.lisp
@@ -21,6 +21,13 @@ It's a function of the window argument that returns the title as a string.")
     (list)
     :export nil
     :documentation "A list of panel buffers appearing on the window.")
+   (prompt-buffer-channel
+    (make-channel) ; TODO: Rename `prompt-buffer-ready-channel'?
+    :export nil
+    :documentation "A channel one may listen to if waiting
+for the prompt buffer to be available.
+You should not rely on the value of this channel.
+The channel is popped when a prompt buffer is hidden.")
    (key-stack
     '()
     :documentation "A stack of the key chords a user has pressed.")