66913729755293b559264357574ce7e9e407b08b
Pierre Neidhardt <mail@ambrevar.xyz>
2022-02-25 13:29:58 +0100

    renderer/gtk-clipboard: Add missing license header.

--- source/renderer/gtk-clipboard.lisp
+++ source/renderer/gtk-clipboard.lisp
@@ -1,3 +1,6 @@
+;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
 (in-package :gtk)
 
 (defcfun ("gtk_clipboard_wait_for_text" gtk-clipboard-wait-for-text)
