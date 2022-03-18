db5b183acfca7c68df6085edad809fbdf462fc90
aartaka <aartaka@protonmail.com>
2022-03-18 21:15:37 +0500

    mode/web: Export user-script and user-styles methods.

--- source/mode/user-script.lisp
+++ source/mode/user-script.lisp
@@ -31,10 +31,12 @@
     (lambda (mode)
       (inject-user-styles (user-scripts mode) (buffer mode))))))
 
+(export-always 'user-scripts)
 (defmethod (setf user-scripts) (new-value (mode web-mode))
   (inject-user-scripts new-value (buffer mode))
   (setf (slot-value mode 'user-scripts) new-value))
 
+(export-always 'user-styles)
 (defmethod (setf user-styles) (new-value (mode web-mode))
   (inject-user-styles new-value (buffer mode))
   (setf (slot-value mode 'user-styles) new-value))
