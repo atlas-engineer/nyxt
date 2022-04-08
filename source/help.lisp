a66fbe137ca26fc20895c71bcfd959e7d44b720f
aartaka <aartaka@protonmail.com>
2022-04-08 11:26:08 +0500

    Revert "help: Remove copyright -- it's implicit."
    This reverts commit bef34040a457462be389e3f934bac57f968e3c2c.

--- source/help.lisp
+++ source/help.lisp
@@ -847,7 +847,7 @@ The version number is stored in the clipboard."
                      :min-width "100px")
                     (.container
                      :min-height "100%")
-                    (.version
+                    (.copyright
                      :position "absolute"
                      :bottom "1em"
                      :right "1em"))))
@@ -903,8 +903,10 @@ The version number is stored in the clipboard."
                    :type "submit"
                    :onclick (ps:ps (nyxt/ps:lisp-eval '(set-url :prefill-current-url-p nil)))
                    "Start searching!")))
-     (:p :class "version"
-         (format nil "Nyxt/~a ~a" +renderer+ +version+)))))
+     (:p :class "copyright"
+         (format nil "Nyxt/~a ~a" +renderer+ +version+)
+         (:br)
+         (format nil "© Atlas Engineer LLC, ~a" (local-time:timestamp-year (local-time:now)))))))
 
 (define-internal-page-command-global manual ()
     (buffer "*Manual*" 'nyxt/help-mode:help-mode)
