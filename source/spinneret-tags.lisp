60580aa1cc6235f7e37580ebe98af448ee8d489c
aartaka <aartaka@protonmail.com>
2022-04-12 21:45:07 +0500

    spinneret-tags: Add :nscript and :nstyle tags for unescaped CSS/JS.

--- source/spinneret-tags.lisp
+++ source/spinneret-tags.lisp
@@ -8,3 +8,9 @@
   `(:raw (if (nyxt:html-string-p (progn ,@body))
              (progn ,@body)
              (escape-string (progn ,@body)))))
+
+(deftag :nstyle (body attrs &key &allow-other-keys)
+  `(:style ,@attrs (:raw ,@body)))
+
+(deftag :nscript (body attrs &key &allow-other-keys)
+  `(:script ,@attrs (:raw ,@body)))
