339dfe885071dd0435561e29cbc57dff02ec2a7c
André A. Gomes <andremegafone@gmail.com>
2022-04-14 22:27:24 +0100

    mode/help: Bind keys.
    The documentation interface becomes easier to navigate.

--- source/mode/help.lisp
+++ source/mode/help.lisp
@@ -10,4 +10,21 @@
 
 (define-mode help-mode ()
   "Mode for displaying documentation."
-  ((rememberable-p nil)))
+  ((rememberable-p nil)
+   ;; add 'Type "?" for help' at the top of the web-buffer
+   ;; (constructor
+   ;;  (lambda (mode)
+   ;;    (...)))
+   (keymap-scheme
+    (define-scheme "help-mode"
+      scheme:cua
+      (list
+       "q" 'nyxt::delete-current-buffer
+       "n" 'nyxt/web-mode::next-heading
+       "p" 'nyxt/web-mode::previous-heading
+       "m" 'nyxt/web-mode:jump-to-heading
+       "s" 'nyxt/web-mode::search-buffer
+       "t" 'nyxt::headings-panel
+       ;; TODO local help-mode keybindings don't appear when describe-bindings
+       ;; is called.  Why?  %define-internal-page?
+       "?" 'nyxt::describe-bindings)))))
