b9096adb0a3153f59aab007d5ad183ec2ca42bcb
Pierre Neidhardt <mail@ambrevar.xyz>
2022-04-18 11:39:43 +0200

    Structure auto-init-file and replace append-configuration with auto-configure.
    Previously, append-configuration and configure-slot were overlapping in
    functionality.
    Now configure-slot is only for interactive use, while append-configuration does
    the actual work.
    
    auto-init-file is now fully leveraging nfiles.
    Its content is also more structured into different form types so that we can
    avoid duplicates and find existing values.

--- source/configuration.lisp
+++ source/configuration.lisp
@@ -11,7 +11,23 @@
   (:export-class-name-p t)
   (:accessor-name-transformer (class*:make-name-transformer name)))
 
-(define-class auto-init-file (init-file) ; TODO: Be consistent here for 3.0!
+(define-class slot-form ()
+  ((name nil
+         :type symbol)
+   (value nil
+         :type t))
+  (:export-class-name-p t)
+  (:accessor-name-transformer (class*:make-name-transformer name)))
+
+(define-class class-form ()
+  ((class-name nil
+               :type symbol)
+   (forms '()
+          :type (maybe (cons (or cons slot-form) *))))
+  (:export-class-name-p t)
+  (:accessor-name-transformer (class*:make-name-transformer name)))
+
+(define-class auto-init-file (nfiles:config-file nyxt-lisp-file) ; TODO: Be consistent here for 3.0!
   ((nfiles:base-path #p"auto-config")
    (command-line-option :auto-init
                         :accessor nil
@@ -70,6 +86,109 @@ This is set globally so that extensions can be loaded even if there is no
     (:tree ,(nfiles:expand *extensions-directory*))
     (:tree ,(nfiles:expand *source-directory*))
     :inherit-configuration))
+
+
+
+(defun read-init-form-slot (class-name sexp)
+  "Return 2 values:
+- the slot name;
+- the slot value.
+Return NIL if not a slot setting."
+  (when (and (= 3 (length sexp))
+             (eq (first sexp) 'setf)
+             (eq (first (second sexp)) 'slot-value)
+             (eq (second (second sexp)) class-name))
+    (let ((slot-name (third (second sexp)))
+          (slot-value (third sexp)))
+      (values slot-name slot-value))))
+
+(defun write-init-form-slot (class-name slot-form)
+  `(setf (slot-value ,class-name ',(name slot-form)) ,(value slot-form)))
+
+(defun read-init-form-class (form)
+  "Return:
+- the class name
+- the list of forms, either `slot-form' or a raw s-exp.
+Return NIL if not a class form."
+  (alex:when-let ((class-name (when (eq (second form) 'customize-instance)
+                                (second (first (find-if #'consp form))))))
+    (let ((body (sera:nlet lp ((sexp form))
+                  (if (consp (first sexp))
+                      (rest sexp)
+                      (lp (rest sexp))))))
+      (values class-name
+              (mapcar (lambda (sexp)
+                        (multiple-value-bind (name value)
+                            (read-init-form-slot class-name sexp)
+                          (if name
+                              (make-instance 'slot-form
+                                             :name name
+                                             :value value)
+                              sexp)))
+                      body)))))
+
+(defun write-init-form-class (class-form)
+  `(defmethod customize-instance ((,(class-name class-form) ,(class-name class-form)))
+     ,@(mapcar (lambda (form)
+                 (if (slot-form-p form)
+                     (write-init-form-slot (class-name class-form) form)
+                     form))
+               (forms class-form))))
+
+;; TODO: Instantiate directly in read-init-*?
+(defmethod nfiles:deserialize ((profile nyxt-profile) (file auto-init-file) raw-content &key)
+  (flet ((make-init-form (form)
+           (multiple-value-bind (name forms)
+               (read-init-form-class form)
+             (if name
+                 (make-instance 'class-form
+                                :class-name name
+                                :forms forms)
+                 form))))
+    (loop
+      for form = (read raw-content nil :eof)
+      until (eq form :eof)
+      collect (make-init-form form))))
+
+(defmethod nfiles:serialize ((profile nyxt-profile) (file auto-init-file) stream &key)
+  (dolist (form (nfiles:content file))
+    (write
+     (if (class-form-p form)
+         (write-init-form-class form)
+         form)
+     :stream stream)
+    (write-string +newline+ stream)))
+
+(defmethod nfiles:write-file ((profile nyxt-profile) (file auto-init-file) &key)
+  (let ((*print-case* :downcase)
+        (*package* (find-package :nyxt-user)))
+    (log:info "Writing auto configuration to ~s." (nfiles:expand file))
+    (call-next-method)))
+
+(defun auto-configure (&key form class-name slot (slot-value nil slot-value-p))
+  (nfiles:with-file-content (config *auto-config-file*)
+    (if class-name
+        (flet ((ensure-class-form (class-name)
+                 (or (when config
+                       (find-if (sera:eqs class-name) (sera:filter #'class-form-p config) :key #'class-name))
+                     (sera:lret ((form (make-instance 'class-form :class-name class-name)))
+                       (alex:appendf config (list form)))))
+               (ensure-slot-form (class-form slot)
+                 (or (find-if (sera:eqs slot) (sera:filter #'slot-form-p (forms class-form)) :key #'name)
+                     (sera:lret ((form (make-instance 'slot-form :name slot)))
+                       (alex:appendf (forms class-form) (list form)))))
+               (delete-slot-form (class-form slot)
+                 (delete-if (sera:eqs slot) (sera:filter #'slot-form-p (forms class-form)) :key #'name)))
+          (let ((class-form (ensure-class-form class-name)))
+            (if slot
+                (if slot-value-p
+                    (sera:lret ((slot-form (ensure-slot-form class-form slot)))
+                      (setf (value slot-form) slot-value))
+                    (setf (forms class-form) (delete-slot-form class-form slot)))
+                (alex:appendf (forms class-form) (list form)))))
+        (alex:appendf config (list form)))))
+
+
 
 (defparameter %buffer nil)              ; TODO: Make a monad?
 
