af434f1d11e5507c9f2af7ac4be94237c0b6e677
aartaka <aartaka@protonmail.com>
2022-04-19 12:01:24 +0500

    Revert "source/start: Quit on any condition, rid of CFFI signal handling."
    This reverts commit e33a7cd9fc8d7a4b6515fde4b4bc0884ae239030.
    
    The reason is it was catching other conditions and exiting on initfile errors,
    not only on SIGTERMs.

--- source/start.lisp
+++ source/start.lisp
@@ -209,6 +209,11 @@ before running this command."
           (when p
             (file-position stream p)))))))
 
+(cffi:defcallback handle-interrupt
+    :void ((signum :int) (siginfo :pointer) (ptr :pointer))
+  (declare (ignore signum siginfo ptr))
+  (quit))
+
 (export-always 'entry-point)
 (defun entry-point ()
   "Read the CLI arguments and start the browser.
@@ -221,9 +226,18 @@ Don't run this from a REPL, prefer `start' instead."
                      (opts:arg-parser-failed #'handle-malformed-cli-arg))
         (opts:get-opts))
     (setf *run-from-repl-p* nil)             ; Not a REPL.
-    (handler-case
-        (apply #'start (append options (list :urls free-args)))
-      (condition () (quit)))))
+    (let ((interrupt-sigaction (cffi:foreign-alloc '(:struct isys::sigaction))))
+      ;; Mimics https://www.systutorials.com/catching-the-signal-sent-by-kill-in-c-on-linux/
+      (isys:memset interrupt-sigaction 0 (cffi:foreign-type-size '(:struct isys::sigaction)))
+      (setf (cffi:foreign-slot-value
+             interrupt-sigaction '(:struct isys:sigaction) 'isys::sigaction)
+            (cffi:callback handle-interrupt))
+      (setf (cffi:foreign-slot-value
+             interrupt-sigaction '(:struct isys:sigaction) 'isys::flags)
+            isys:sa-siginfo)
+      (isys:sigaction isys:sigint interrupt-sigaction (cffi:null-pointer))
+      (isys:sigaction isys:sigterm interrupt-sigaction (cffi:null-pointer)))
+    (apply #'start (append options (list :urls free-args)))))
 
 (-> load-lisp
     ((or null trivial-types:pathname-designator) &key (:package (or null package)))
