f175b79ea658da043fb2bf4865841b953baebcea
Pierre Neidhardt <mail@ambrevar.xyz>
2022-04-29 16:36:33 +0200

    mode: Fix toggler warning on CCL + make list it globally.

--- source/mode.lisp
+++ source/mode.lisp
@@ -20,31 +20,34 @@
     ;; FIXME: SBCL `slot-value' returns a list, while CCL returns the boolean.  Why?
     (if (alex:ensure-car (slot-value class 'toggler-command-p))
         (sera:lret ((command (make-command
-                              name
-                              `(lambda (&rest args
-                                        &key
-                                          ;; TODO: Shall we have a function that returns the focused
-                                          ;; buffer?  `focused-buffer'?  `current-buffer*'?  Rename
-                                          ;; `current-buffer' to `current-view-buffer' and add
-                                          ;; `current-buffer' for this task?
-                                          (buffer (or (current-prompt-buffer) (current-buffer)))
-                                          (activate t explicit?)
-                                        &allow-other-keys)
-                                 ,(format nil "Toggle ~a." name)
-                                 (let ((existing-instance (find-submode ',name buffer)))
-                                   (unless explicit?
-                                     (setf activate (or (not existing-instance)
-                                                        (not (enabled-p existing-instance)))))
-                                   (if activate
-                                       ;; TODO: Shall we pass args to `make-instance' or `enable'?
-                                       ;; Have 2 args parameters?
-                                       (enable (or existing-instance
-                                                   (apply #'make-instance ',name
-                                                          :buffer buffer
-                                                          args)))
-                                       (when existing-instance
-                                         (disable existing-instance))))))))
-
+                                 name
+                                 `(lambda (&rest args
+                                           &key
+                                             ;; TODO: Shall we have a function that returns the focused
+                                             ;; buffer?  `focused-buffer'?  `current-buffer*'?  Rename
+                                             ;; `current-buffer' to `current-view-buffer' and add
+                                             ;; `current-buffer' for this task?
+                                             (buffer (or (current-prompt-buffer) (current-buffer)))
+                                             (activate t explicit?)
+                                           &allow-other-keys)
+                                    ,(format nil "Toggle ~a." name)
+                                    ;; On CCL `find-submode' would trigger a
+                                    ;; warning because the class is not findable
+                                    ;; when toggler is defined.
+                                    (let ((existing-instance (find ',name (modes buffer) :key #'sera:class-name-of)))
+                                      (unless explicit?
+                                        (setf activate (or (not existing-instance)
+                                                           (not (enabled-p existing-instance)))))
+                                      (if activate
+                                          ;; TODO: Shall we pass args to `make-instance' or `enable'?
+                                          ;; Have 2 args parameters?
+                                          (enable (or existing-instance
+                                                      (apply #'make-instance ',name
+                                                             :buffer buffer
+                                                             args)))
+                                          (when existing-instance
+                                            (disable existing-instance)))))
+                               :global)))
           (setf (fdefinition name) command))
         (delete-command name))))
 
