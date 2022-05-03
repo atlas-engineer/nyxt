1a347cb9badabf2838a2f7b2d51bc74a8646e7a4
André A. Gomes <andremegafone@gmail.com>
2022-05-03 23:27:00 +0100

    mode/element-hint: Simplify parenscript.

--- source/mode/element-hint.lisp
+++ source/mode/element-hint.lisp
@@ -448,10 +448,10 @@ visible nosave active buffer."
   "Toggle the on-screen element hints transparency."
   (with-current-buffer buffer
     (peval (ps:dolist (element (nyxt/ps:qsa document ".nyxt-hint"))
-               (if (or (= (ps:chain element style opacity) "1")
-                       (= (ps:chain element style opacity) ""))
-                   (setf (ps:chain element style opacity) "0.2")
-                   (setf (ps:chain element style opacity) "1.0"))))))
+             (if (or (= (ps:@ element style opacity) "1")
+                     (= (ps:@ element style opacity) ""))
+                 (setf (ps:@ element style opacity) "0.2")
+                 (setf (ps:@ element style opacity) "1.0"))))))
 
 (define-command scroll-to-hint (&key (buffer (current-buffer)))
   "Show the selected hint on screen."
