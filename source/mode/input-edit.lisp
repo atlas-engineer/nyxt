0a277142d089f86862872ef85fb434043da670f1
aartaka <aartaka@protonmail.com>
2022-05-05 20:30:59 +0500

    mode/input-edit: Make input-edit-mode togglable.
    This mode is made to be togglable by the user.

--- source/mode/input-edit.lisp
+++ source/mode/input-edit.lisp
@@ -146,5 +146,4 @@ necessary."
        "M-b" 'cursor-backwards-word
        "C-d" 'delete-forwards
        "M-backspace" 'delete-backwards-word
-       "M-d" 'delete-forwards-word))))
-  (:toggler-command-p nil))
+       "M-d" 'delete-forwards-word)))))
