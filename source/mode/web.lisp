87e7ec98f02a80cc42339d2288b228d26f78fc4e
Pierre Neidhardt <mail@ambrevar.xyz>
2022-05-13 16:38:57 +0200

    mode: Merge mode/scroll.lisp into mode/web.lisp.
    scroll.lisp is trivial and web.lisp is now much simpler too.

--- source/mode/web.lisp
+++ source/mode/web.lisp
@@ -325,4 +325,44 @@ ELEMENT-SCRIPT is a Parenscript script that is passed to `ps:ps'."
       (when (background-buffer-p buffer)
         (ffi-buffer-delete buffer)))))
 
+(define-command scroll-to-top ()
+  "Scroll to the top of the current page."
+  (peval (ps:chain window (scroll-by 0 (- (ps:chain document document-element scroll-height))))))
+
+(define-command scroll-to-bottom ()
+  "Scroll to the bottom of the current page."
+  (peval (ps:chain window (scroll-by 0 (ps:chain document document-element scroll-height)))))
+
+(define-command scroll-down (&key (scroll-distance (scroll-distance (current-buffer))))
+  "Scroll down the current page.
+The amount scrolled is determined by the buffer's `scroll-distance'."
+  (peval (ps:chain window (scroll-by 0 (ps:lisp scroll-distance)))))
+
+(define-command scroll-up (&key (scroll-distance (scroll-distance (current-buffer))))
+  "Scroll up the current page.
+The amount scrolled is determined by the buffer's `scroll-distance'."
+  (peval (ps:chain window (scroll-by 0 (ps:lisp (- scroll-distance))))))
+
+(define-command scroll-left (&key (horizontal-scroll-distance
+                                   (horizontal-scroll-distance (current-buffer))))
+  "Scroll left the current page.
+The amount scrolled is determined by the buffer's `horizontal-scroll-distance'."
+  (peval (ps:chain window (scroll-by (ps:lisp (- horizontal-scroll-distance)) 0))))
+
+(define-command scroll-right (&key (horizontal-scroll-distance
+                                    (horizontal-scroll-distance (current-buffer))))
+  "Scroll right the current page.
+The amount scrolled is determined by the buffer's `horizontal-scroll-distance'."
+  (peval (ps:chain window (scroll-by (ps:lisp horizontal-scroll-distance) 0))))
+
+(define-command scroll-page-down ()
+  "Scroll down by one page height."
+  (peval (ps:chain window (scroll-by 0 (* (ps:lisp (page-scroll-ratio (current-buffer)))
+                                          (ps:@ window inner-height))))))
+
+(define-command scroll-page-up ()
+  "Scroll up by one page height."
+  (peval (ps:chain window (scroll-by 0 (- (* (ps:lisp (page-scroll-ratio (current-buffer)))
+                                             (ps:@ window inner-height)))))))
+
 (pushnew 'web-mode nyxt::%default-modes)
