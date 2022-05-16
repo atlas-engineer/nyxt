10806075f931c7876382028afd6f284078df2b80
Pierre Neidhardt <mail@ambrevar.xyz>
2022-05-16 09:08:02 +0200

    Move element-hint-prompt-buffer-mode package to a separate file.
    It's good practice to have only one package per file, also to not confuse
    SLIME / SLY.

---
+++ source/mode/element-hint-prompt-buffer.lisp
@@ -0,0 +1,31 @@
+;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
+(uiop:define-package :nyxt/element-hint-prompt-buffer-mode
+  (:use :common-lisp :nyxt)
+  (:import-from #:keymap #:define-key #:define-scheme)
+  (:documentation "Prompt-buffer mode for element hints."))
+(in-package :nyxt/element-hint-prompt-buffer-mode)
+
+(define-command toggle-hints-transparency (&key (buffer (current-buffer)))
+  "Toggle the on-screen element hints transparency."
+  (with-current-buffer buffer
+    (peval (ps:dolist (element (nyxt/ps:qsa document ".nyxt-hint"))
+             (if (or (= (ps:@ element style opacity) "1")
+                     (= (ps:@ element style opacity) ""))
+                 (setf (ps:@ element style opacity) "0.2")
+                 (setf (ps:@ element style opacity) "1.0"))))))
+
+(define-command scroll-to-hint (&key (buffer (current-buffer)))
+  "Show the selected hint on screen."
+  (with-current-buffer buffer
+    (nyxt/element-hint-mode::highlight-selected-hint :element (current-suggestion-value) :scroll t)))
+
+(define-mode element-hint-prompt-buffer-mode (nyxt/prompt-buffer-mode:prompt-buffer-mode)
+  "Prompt buffer mode for element hinting."
+  ((keymap-scheme
+    (define-scheme "element-hint"
+      scheme:cua
+      (list
+       "M-i" 'toggle-hints-transparency
+       "C-l" 'scroll-to-hint)))))
--- source/mode/element-hint.lisp
+++ source/mode/element-hint.lisp
@@ -455,32 +455,3 @@ modes."
                          do (download buffer (url link))
                             (sleep 0.25)))
                  :selector "a, img")))
-
-(uiop:define-package :nyxt/element-hint-prompt-buffer-mode ; TODO: Move to separate file.
-  (:use :common-lisp :nyxt)
-  (:import-from #:keymap #:define-key #:define-scheme)
-  (:documentation "Prompt-buffer mode for element hints."))
-(in-package :nyxt/element-hint-prompt-buffer-mode)
-
-(define-command toggle-hints-transparency (&key (buffer (current-buffer)))
-  "Toggle the on-screen element hints transparency."
-  (with-current-buffer buffer
-    (peval (ps:dolist (element (nyxt/ps:qsa document ".nyxt-hint"))
-             (if (or (= (ps:@ element style opacity) "1")
-                     (= (ps:@ element style opacity) ""))
-                 (setf (ps:@ element style opacity) "0.2")
-                 (setf (ps:@ element style opacity) "1.0"))))))
-
-(define-command scroll-to-hint (&key (buffer (current-buffer)))
-  "Show the selected hint on screen."
-  (with-current-buffer buffer
-    (nyxt/element-hint-mode::highlight-selected-hint :element (current-suggestion-value) :scroll t)))
-
-(define-mode element-hint-prompt-buffer-mode (nyxt/prompt-buffer-mode:prompt-buffer-mode)
-  "Prompt buffer mode for element hinting."
-  ((keymap-scheme
-    (define-scheme "element-hint"
-      scheme:cua
-      (list
-       "M-i" 'toggle-hints-transparency
-       "C-l" 'scroll-to-hint)))))
