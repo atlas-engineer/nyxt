aad6cf64dcb9753327305b5924d8df897263b136
Pierre Neidhardt <mail@ambrevar.xyz>
2022-05-24 11:21:27 +0200

    libraries/keymap: Revert test tweak.

--- libraries/keymap/test-package.lisp
+++ libraries/keymap/test-package.lisp
@@ -6,10 +6,3 @@
 (uiop:define-package keymap/tests
   (:use #:common-lisp)
   (:import-from #:keymap))
-
-(in-package :keymap/tests)
-
-(defmacro keymap-test (name &body body)
-  `(prove:subtest ,name
-     (let ((keymap:*default-bound-type* '(or keymap:keymap t)))
-       ,@body)))
