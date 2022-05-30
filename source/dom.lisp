80df6f8fa475597e1a784e4561153badaa3dd5f0
Pierre Neidhardt <mail@ambrevar.xyz>
2022-05-30 10:11:22 +0200

    dom: Use nyxt:define-package.

--- source/dom.lisp
+++ source/dom.lisp
@@ -1,15 +1,9 @@
 ;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
-(uiop:define-package :nyxt/dom
-  (:use :common-lisp :nyxt)
-  (:import-from #:class-star #:define-class)
-  (:import-from #:serapeum
-                #:export-always
-                #:->)
+(nyxt:define-package :nyxt/dom
   (:documentation "Nyxt-specific DOM classes and functions operating on them."))
 (in-package :nyxt/dom)
-(use-nyxt-package-nicknames)
 
 ;; TODO: Factor out into a library?
 
