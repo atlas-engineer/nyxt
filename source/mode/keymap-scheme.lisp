atlas-engineer/nyxt

commit d066b13633fc90f422dcb006b67f10ae5dfd9e96

Change visible modes on status buffer.
All modes that inherit from prompt-buffer-mode are hidden.

source/mode/keymap-scheme.lisp
@@ -9,7 +9,8 @@ This is used so that only one mode at a time may be enabled."))
 (define-mode keymap-scheme-mode ()
   "Mode from which all modes setting the buffer `keymap-scheme-name' should inherit.
 This is used so that only one mode at a time may be enabled."
-  ((rememberable-p nil)
+  ((visible-in-status-p nil)
+   (rememberable-p nil)
    (scheme-name
     scheme:cua
     :documentation "The scheme to enable.")

source/mode/annotate.lisp
@@ -9,7 +9,8 @@ Annotations are persisted to disk."))
 (define-mode annotate-mode ()
   "Annotate document with arbitrary comments.
 Annotations are persisted to disk, see the `annotations-file' mode slot."
-  ((annotations-file
+  ((visible-in-status-p nil)
+   (annotations-file
     (make-instance 'annotations-file)
     :type annotations-file
     :documentation "The file where to save annotations.")))

source/mode/autofill.lisp
@@ -11,7 +11,8 @@
 
 (define-mode autofill-mode ()
   "Mode to fill forms more rapidly."
-  ((rememberable-p t)
+  ((visible-in-status-p nil)
+   (rememberable-p t)
    (autofills
     (list (make-autofill :name "Name" :fill "My Name")
           (make-autofill :name "Hello Printer"

source/mode/bookmark-frequent-visits.lisp
@@ -7,7 +7,8 @@
 
 (define-mode bookmark-frequent-visits-mode ()
   "Mode to bookmark frequently visited URLs while navigating the web."
-  ((threshold 20)))
+  ((visible-in-status-p nil)
+   (threshold 20)))
 
 (defmethod enable ((mode bookmark-frequent-visits-mode) &key)
   (nyxt:on-signal-load-finished mode (url (current-buffer))))

source/mode/bookmark.lisp
@@ -21,7 +21,8 @@
 (define-mode bookmark-mode ()
   "Manage bookmarks.
 Bookmarks can be persisted to disk, see the `bookmarks-file' mode slot."
-  ((bookmarks-file
+  ((visible-in-status-p nil)
+   (bookmarks-file
     (make-instance 'bookmarks-file)
     :type bookmarks-file
     :documentation "The file where the system will create/save the bookmarks.")

source/mode/bookmarklets.lisp
@@ -14,7 +14,8 @@ the content of HTML pages."))
 
 (define-mode bookmarklets-mode ()
   "Mode for 'bookmarklets' commands.
-By default, this mode does nothing but expose the default bookmarklets.")
+By default, this mode does nothing but expose the default bookmarklets."
+  ((visible-in-status-p nil)))
 
 (defmacro nyxt::define-bookmarklet-command (name documentation source) ; TODO: Should it really belong to the `nyxt' package?
   "Define a bookmarklet command, the source can either be a JavaScript string to

source/mode/buffer-listing.lisp
@@ -7,7 +7,8 @@
 
 (define-mode buffer-listing-mode ()
   "Mode for buffer-listing."
-  ((rememberable-p nil))
+  ((visible-in-status-p nil)
+   (rememberable-p nil))
   (:toggler-command-p nil))
 
 (define-internal-page-command-global list-buffers (&key (cluster nil)

source/mode/certificate-exception.lisp
@@ -14,7 +14,8 @@ See the `add-domain-to-certificate-exceptions' command.")
   "Enable ignoring of certificate errors.
 This can apply to specific buffers.
 See the `add-domain-to-certificate-exceptions' command."
-  ((certificate-exceptions *default-certificate-exceptions*
+  ((visible-in-status-p nil)
+   (certificate-exceptions *default-certificate-exceptions*
                            :type list-of-strings)))
 
 (defmethod enable ((mode certificate-exception-mode) &key)

commit 107d5bcf1446b8e798f16ab41fc26493f4e3d17c

mode/keymap-scheme: Fix mode visibility inheriting from keymap-scheme.
Fix bug introduced in d066b1363.

source/mode/keymap-scheme.lisp
@@ -9,8 +9,7 @@ This is used so that only one mode at a time may be enabled."))
 (define-mode keymap-scheme-mode ()
   "Mode from which all modes setting the buffer `keymap-scheme-name' should inherit.
 This is used so that only one mode at a time may be enabled."
-  ((visible-in-status-p nil)
-   (rememberable-p nil)
+  ((rememberable-p nil)
    (scheme-name
     scheme:cua
     :documentation "The scheme to enable.")