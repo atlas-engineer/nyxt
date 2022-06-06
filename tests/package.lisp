52d7bda84fce406d925832e24bd5a7eb6c466b24
Pierre Neidhardt <mail@ambrevar.xyz>
2022-06-06 15:47:30 +0200

    tests: Use nyxt:define-package.

--- tests/package.lisp
+++ tests/package.lisp
@@ -1,9 +1,6 @@
 ;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
-(uiop:define-package nyxt/tests
-  (:use #:common-lisp #:trivia)
-  (:use #:nyxt)
+(nyxt:define-package nyxt/tests
   (:shadowing-import-from #:prove #:*debug-on-error*)
-  (:use #:prove)
-  (:import-from #:class-star #:define-class))
+  (:use #:prove))
