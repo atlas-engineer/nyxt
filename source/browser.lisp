d912ef0b399b316210458404d6ff45fdcd95b811
Pierre Neidhardt <mail@ambrevar.xyz>
2022-06-07 11:00:19 +0200

    browser: Re-fix focused-buffer docstring.

--- source/browser.lisp
+++ source/browser.lisp
@@ -568,7 +568,7 @@ Return BUFFER."
 
 (export-always 'focused-buffer)
 (defun focused-buffer (&optional (window (current-window)) )
-  "Return the currently  focused buffer."
+  "Return the currently focused buffer."
   ;; TODO: Add message-buffer when we have the slot in `window'.
   (find-if #'ffi-focused-p
            (list (first (active-prompt-buffers window))
