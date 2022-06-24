623cba0234368363c53ccc02745dcbd22f8635d7
André A. Gomes <andremegafone@gmail.com>
2022-06-24 16:44:41 +0100

    Delete keymap library.
    Moved to https://github.com/atlas-engineer/nkeymaps.

--- libraries/keymap/scheme-names.lisp
+++
@@ -1,9 +0,0 @@
-;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
-;;;; SPDX-License-Identifier: BSD-3-Clause
-
-(in-package :keymap/scheme)
-
-(defvar cua (make-scheme-name "cua"))
-(defvar emacs (make-scheme-name "emacs" cua))
-(defvar vi-normal (make-scheme-name "vi-normal" cua emacs))
-(defvar vi-insert (make-scheme-name "vi-insert" cua))
