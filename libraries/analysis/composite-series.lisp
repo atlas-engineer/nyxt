e6fba44212f87db02047c14dd5e30b35e35dbeee
John Mercouris <john@mercouris.email>
2022-07-05 18:38:18 -0500

    composite-series.lisp: add-record records instances a sequence appears

--- libraries/analysis/composite-series.lisp
+++ libraries/analysis/composite-series.lisp
@@ -7,4 +7,5 @@
   ((record :accessor record :initform (make-hash-table :test #'equal))))
 
 (defmethod add-record ((model sequence-model) sequence)
-  (print "record added"))
+  (setf (gethash sequence (record model))
+        (incf (gethash sequence (record model) 0))))
