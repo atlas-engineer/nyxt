51c7797d28feff99e341a7af94fdfb8f18ce6b04
André A. Gomes <andremegafone@gmail.com>
2022-07-05 17:49:58 +0100

    mode/process: Adhere to new slot style.

--- source/mode/process.lisp
+++ source/mode/process.lisp
@@ -15,28 +15,34 @@ Possible applications:
 - Live tracking of filesystem/data in a file/directory."
   ((visible-in-status-p nil)
    (rememberable-p nil)
-   (path-url nil
-             :type (or quri:uri null)
-             :documentation "The path to where `process-mode' needs to track things at.
+   (path-url
+    nil
+    :type (or quri:uri null)
+    :documentation "The path to where `process-mode' needs to track things at.
 Is not necessarily the same as current buffer URL.")
-   (firing-condition t
-                     :type (or boolean (function (quri:uri process-mode)))
-                     :documentation "The condition for the action firing.
+   (firing-condition
+    t
+    :type (or boolean (function (quri:uri process-mode)))
+    :documentation "The condition for the action firing.
 Can be boolean (T to always fire, NIL to never fire), or function over URL and mode instance.
 If it is/returns :RETURN, the process is stopped.")
-   (action nil
-           :type (or (function (quri:uri process-mode)) null)
-           :documentation "The action (function) to do with file URL and `process-mode' instance.")
-   (cleanup nil
-            :type (or (function (quri:uri process-mode)) null)
-            :documentation "Function to run when process ends.
+   (action
+    nil
+    :type (or (function (quri:uri process-mode)) null)
+    :documentation "The action (function) to do with file URL and `process-mode' instance.")
+   (cleanup
+    nil
+    :type (or (function (quri:uri process-mode)) null)
+    :documentation "Function to run when process ends.
 Accepts the path to the acted-on document and `process-mode' instance.")
-   (thread nil
-           :type (or bt:thread null)
-           :export nil
-           :documentation "The thread that `action' happen in.")
-   (thread-terminated-p nil
-                        :documentation "Has the thread been terminated?"))
+   (thread
+    nil
+    :type (or bt:thread null)
+    :export nil
+    :documentation "The thread that `action' happen in.")
+   (thread-terminated-p
+    nil
+    :documentation "Has the thread been terminated?"))
   (:toggler-command-p nil))
 
 (defmethod enable ((mode process-mode) &key)
