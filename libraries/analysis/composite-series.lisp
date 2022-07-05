8a49fd64dbe492d6ca86728033ef4b5d351c293f
John Mercouris <john@mercouris.email>
2022-07-05 18:35:49 -0500

    composite-series.lisp: create class to represent model

--- libraries/analysis/composite-series.lisp
+++ libraries/analysis/composite-series.lisp
@@ -2,3 +2,9 @@
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
 (in-package :analysis)
+
+(defclass sequence-model ()
+  ((record :accessor record :initform (make-hash-table :test #'equal))))
+
+(defmethod add-record ((model sequence-model) sequence)
+  (print "record added"))
