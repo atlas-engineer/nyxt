a61ade5c39356807210b70fd4a6a50d078a2c781
John Mercouris <john@mercouris.email>
2022-07-05 19:20:09 -0500

    composite-series.lisp: deal with adding-records with only one possible outcome

--- libraries/analysis/composite-series.lisp
+++ libraries/analysis/composite-series.lisp
@@ -6,6 +6,18 @@
 (defclass sequence-model ()
   ((record :accessor record :initform (make-hash-table :test #'equal))))
 
+(defclass element ()
+  ((occurrence-count :accessor occurrence-count :initform 0)
+   (element :accessor element :initarg :element)))
+
+(defmethod increment ((element element))
+  (setf (occurrence-count element) (incf (occurrence-count element))))
+
 (defmethod add-record ((model sequence-model) sequence)
-  (setf (gethash sequence (record model))
-        (incf (gethash sequence (record model) 0))))
+  (let* ((list-but-last-element (butlast sequence))
+         (last-element (car (last sequence)))
+         (record-result (setf (gethash list-but-last-element (record model))
+                              (gethash list-but-last-element (record model)
+                                       (make-instance 'element
+                                                      :element last-element)))))
+    (increment record-result)))
