d6bb47a333319e961cabdc6816257175dcfbb5cd
André A. Gomes <andremegafone@gmail.com>
2022-07-06 18:43:02 +0100

    mode/watch: Review docstring.

--- source/mode/watch.lisp
+++ source/mode/watch.lisp
@@ -40,6 +40,6 @@
         (buffer-load (nyxt/process-mode:path-url mode) :buffer (buffer mode))))))
 
 (define-command-global watch-buffer (&optional (buffer (current-buffer)))
-  "Reload BUFFER at a prompted interval"
+  "Reload BUFFER at a prompted interval."
   (let ((interval (seconds-from-user-input)))
     (enable-modes 'watch-mode buffer (list :repeat-interval interval))))
