2528a22b3d5b135c1ea384eb8eed9bc24d011d91
John Mercouris <john@mercouris.email>
2022-07-05 19:45:58 -0500

    composite-series.lisp: generalize code to work with multiple edges/results

--- libraries/analysis/composite-series.lisp
+++ libraries/analysis/composite-series.lisp
@@ -4,20 +4,27 @@
 (in-package :analysis)
 
 (defclass sequence-model ()
-  ((record :accessor record :initform (make-hash-table :test #'equal))))
+  ((sequence-look-up :accessor sequence-look-up :initform (make-hash-table :test #'equal))))
 
-(defclass element ()
-  ((occurrence-count :accessor occurrence-count :initform 0)
+(defclass node ()
+  ((edges :accessor edges :initform (make-hash-table :test #'equal))
+   (occurrence-count :accessor occurrence-count :initform 0)
    (element :accessor element :initarg :element)))
 
-(defmethod increment ((element element))
-  (setf (occurrence-count element) (incf (occurrence-count element))))
+(defmethod add-edge ((from-node node) (to-node node))
+  (alexandria:ensure-gethash (element to-node)
+                             (edges from-node) to-node))
+
+(defmethod list-edge-elements ((node node))
+  (mapcar #'element (alexandria:hash-table-values (edges node))))
+
+(defmethod increment ((node node))
+  (setf (occurrence-count node) (incf (occurrence-count node))))
 
 (defmethod add-record ((model sequence-model) sequence)
   (let* ((list-but-last-element (butlast sequence))
          (last-element (car (last sequence)))
-         (record-result (setf (gethash list-but-last-element (record model))
-                              (gethash list-but-last-element (record model)
-                                       (make-instance 'element
-                                                      :element last-element)))))
-    (increment record-result)))
+         (leaf (alexandria:ensure-gethash list-but-last-element
+                                          (sequence-look-up model)
+                                          (make-instance 'node))))
+    (increment (add-edge leaf (make-instance 'node :element last-element)))))
