a6b5d76784a61bf25e473cf48719585f3077d702
André A. Gomes <andremegafone@gmail.com>
2022-07-12 21:07:49 +0300

    mode/prompt-buffer: Move basic keybindings to default keyscheme.

--- source/mode/prompt-buffer.lisp
+++ source/mode/prompt-buffer.lisp
@@ -25,6 +25,10 @@ Return-actions can be listed and run with `return-selection-over-action' (bound 
     (define-keyscheme-map "prompt-buffer" ()
       keyscheme:default
       (list
+       "down" 'select-next
+       "up" 'select-previous
+       "button5" 'select-next
+       "button4" 'select-previous
        "escape" 'cancel-input
        "return" 'return-selection
        "M-return" 'return-selection-over-action
@@ -45,10 +49,6 @@ Return-actions can be listed and run with `return-selection-over-action' (bound 
 
       keyscheme:cua
       (list
-       "down" 'select-next
-       "up" 'select-previous
-       "button5" 'select-next
-       "button4" 'select-previous
        "home" 'select-first
        "end" 'select-last
        "pagehome" 'select-first
