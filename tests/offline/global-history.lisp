d32b369b18c753b1a618b2e611e62f0342f90857
Pierre Neidhardt <mail@ambrevar.xyz>
2022-07-23 06:31:37 +0200

    tests: Fix global-history test to not load local history.

--- tests/offline/global-history.lisp
+++ tests/offline/global-history.lisp
@@ -13,37 +13,37 @@
   nil)
 
 (define-test global-history ()
-  (let* ((*browser* (make-instance 'browser)))
-    ;; Set profile to nosave to inhibit serialization / deserialization.
-    ;; TODO: We should still test serialization and deserialization.
-    (setf *global-profile* (make-instance 'test-profile))
-    (let ((buffer (nyxt::make-buffer)))
-      (nyxt:with-current-buffer buffer
-        (let ((file (history-file buffer)))
-          (nyxt::history-add (quri:uri "http://example.org"))
-          ;; history has 1 entry
-          (assert-eq 1
-                     (length (htree:all-data (files:content file))))
-          (let ((entry (first (htree:all-data (files:content file)))))
-            ;; "value has quri:uri"
-            (assert-equality #'quri:uri=
-                             (quri:uri "http://example.org")
-                             (url entry))
-            ;; "value has no title"
-            (assert-equal ""
-                          (title entry)))
-          (nyxt::history-add (quri:uri "http://example.org") :title "foo")
-          ;; "history has still 1 entry after adding same URL"
-          (assert-eq 1
-                     (length (htree:all-data (files:content file))))
-          (assert-eq 2
-                     (nyxt::implicit-visits (first (htree:all-data (files:content file)))))
-          (let ((entry (first (htree:all-data (files:content file)))))
-            ;; "value now has title"
-            (assert-equal "foo"
-                          (title entry)))
-          (nyxt::history-add (quri:uri "http://example.org/sub"))
-          ;; "history now has 2 entries"
-          (assert-eq 2
-                     (length (htree:all-data (files:content file))))
-          (uiop:delete-file-if-exists (files:expand (history-file buffer))))))))
+  ;; Set profile to nosave to inhibit serialization / deserialization.
+  ;; TODO: We should still test serialization and deserialization.
+  (let* ((nyxt:*global-profile* (make-instance 'test-profile))
+         (*browser* (make-instance 'browser))
+         (buffer (nyxt::make-buffer)))
+    (nyxt:with-current-buffer buffer
+      (let ((file (history-file buffer)))
+        (nyxt::history-add (quri:uri "http://example.org"))
+        ;; history has 1 entry
+        (assert-eq 1
+                   (length (htree:all-data (files:content file))))
+        (let ((entry (first (htree:all-data (files:content file)))))
+          ;; "value has quri:uri"
+          (assert-equality #'quri:uri=
+                           (quri:uri "http://example.org")
+                           (url entry))
+          ;; "value has no title"
+          (assert-equal ""
+                        (title entry)))
+        (nyxt::history-add (quri:uri "http://example.org") :title "foo")
+        ;; "history has still 1 entry after adding same URL"
+        (assert-eq 1
+                   (length (htree:all-data (files:content file))))
+        (assert-eq 2
+                   (nyxt::implicit-visits (first (htree:all-data (files:content file)))))
+        (let ((entry (first (htree:all-data (files:content file)))))
+          ;; "value now has title"
+          (assert-equal "foo"
+                        (title entry)))
+        (nyxt::history-add (quri:uri "http://example.org/sub"))
+        ;; "history now has 2 entries"
+        (assert-eq 2
+                   (length (htree:all-data (files:content file))))
+        (uiop:delete-file-if-exists (files:expand (history-file buffer)))))))
