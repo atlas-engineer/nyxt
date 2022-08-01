4844a5beab365a0a4139af99c2333dd3091eb160
André A. Gomes <andremegafone@gmail.com>
2022-08-01 14:07:30 +0300

    build-scripts/nyxt: Add guix dependencies.

--- build-scripts/nyxt.scm
+++ build-scripts/nyxt.scm
@@ -164,6 +164,7 @@
           cl-nhooks
           cl-nkeymaps
           cl-osicat ; Not needed for SBCL, remove it in Guix upstream package.
+          cl-ospm
           cl-parenscript
           cl-phos
           cl-plump
