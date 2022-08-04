645e6a74779a065044a1b1d93e35c2f8db071907
aartaka <aartaka@protonmail.com>
2022-08-04 12:37:09 +0400

    changelog: Mention apply-all-matching-auto-rules-p.

--- source/changelog.lisp
+++ source/changelog.lisp
@@ -475,7 +475,18 @@ SLY install.")
    (:li "New " (:nxref :class-name 'nyxt/remembrance-mode:remembrance-mode)
         " to automatically cache the visited page content. "
         "The cache can be looked up and the page textual content can be displayed even offline."
-        "See " (:nxref :function 'nyxt/remembrance-mode:recollect-visited-page) "."))
+        "See " (:nxref :function 'nyxt/remembrance-mode:recollect-visited-page) ".")
+   (:li (:code "auto-mode")
+        " is incorporated into Nyxt core, with its settings residing in "
+        (:nxref :class-name 'modable-buffer) ".")
+   (:ul
+    (:li "The new "
+         (:nxref :slot 'apply-all-matching-auto-rules-p :class-name 'modable-buffer)
+         " allows you to apply all the matching auto-rules instead of the most
+specific one.")
+    (:li "There are default rules for Gopher, Gemini, and Nyxt-internal-pages.")
+    (:li "The rules file is now moved to auto-rules.lisp (instead of the old
+auto-mode-rules.lisp)")))
 
   (:h3 "Bindings")
   (:ul
