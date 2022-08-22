f6c33ead42ea8ae56b2fb3549a589ed5d2687fdf
André A. Gomes <andremegafone@gmail.com>
2022-08-22 10:53:04 +0300

    mode/hint: Refactor filtering.

--- source/mode/hint.lisp
+++ source/mode/hint.lisp
@@ -214,7 +214,8 @@ For instance, to include images:
   ((prompter:name "Hints")
    (prompter:selection-actions-enabled-p t)
    (prompter:filter
-    (if (auto-return-p (current-prompt-buffer))
+    (if (and (auto-follow-hints-p (find-submode 'hint-mode))
+             (fit-to-prompt-p (find-submode 'hint-mode)))
         (lambda (suggestion source input)
           (declare (ignore source))
           (str:starts-with-p input
