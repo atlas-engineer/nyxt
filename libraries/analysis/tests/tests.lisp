adbad4b1557aa8119b5c23530c9d88beb9008876
John Mercouris <john@mercouris.email>
2022-09-20 01:56:30 -0500

    tests.lisp: test a single markov chain length

--- libraries/analysis/tests/tests.lisp
+++ libraries/analysis/tests/tests.lisp
@@ -6,5 +6,7 @@
   (:use #:common-lisp #:lisp-unit2 :analysis))
 (in-package :analysis/tests)
 
-(define-test tomato-test ()
-  (lisp-unit2:assert-equal "tomato" "tomato"))
+(define-test test-single-length ()
+  (let ((model (make-instance 'analysis::sequence-model)))
+    (analysis::add-record model '(1 2))
+    (lisp-unit2:assert-equal (analysis::element (analysis::predict model '(1))) 2)))
