cbabe944eda7e4f377c88cd6fe7d4cad96a69e91
André A. Gomes <andremegafone@gmail.com>
2022-10-26 16:45:22 +0300

    libraries/nyxt-asdf/user-systems: Delete spurious whitespace.

--- libraries/nyxt-asdf/user-systems.lisp
+++ libraries/nyxt-asdf/user-systems.lisp
@@ -6,4 +6,4 @@
 (export-always 'nyxt-user-system)
 (defclass nyxt-user-system (asdf:system) ()
   (:documentation "Specialized systems for Nyxt users."))
-(import 'nyxt-user-system  :asdf-user)
+(import 'nyxt-user-system :asdf-user)
