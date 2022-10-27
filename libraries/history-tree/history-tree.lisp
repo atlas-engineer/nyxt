b6124a82b0f7a7cf52e24e54aeb77d7bcf2464fc
André A. Gomes <andremegafone@gmail.com>
2022-10-27 10:20:31 +0300

    libraries/history-tree/history-tree: Delete spurious whitespace.

--- libraries/history-tree/history-tree.lisp
+++ libraries/history-tree/history-tree.lisp
@@ -851,8 +851,6 @@ If nodes are still associated to entry, do nothing."
         (let ((matching-entry (make-entry history data)))
           (remhash matching-entry (entries history)))))))
 
-
-
 (deftype non-negative-integer ()
   `(integer 0 ,most-positive-fixnum))
 
