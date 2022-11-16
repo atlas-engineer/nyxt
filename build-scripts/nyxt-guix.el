atlas-engineer/nyxt

commit 86bf1805bb289b921a0c320e8d837f9b241918b9

Prefer rest to cdr.
As according to our programming convention in documents/README.

build-scripts/nyxt-guix.el
@@ -177,7 +177,7 @@ implementation.  Example:
           (let ((status (apply #'call-process
                                (car command)
                                nil (list output t) nil
-                               (cdr command))))
+                               (rest command))))
             (if (= status 0)
                 (kill-buffer output)
               (error "Nyxt Guix environment creation failed, see %S." output)

libraries/analysis/analysis.lisp
@@ -91,11 +91,11 @@ operations available, some examples:
                   collect (cons word (term-frequency-inverse-document-frequency
                                       document document-collection word)))
             #'>
-            :key #'cdr)
+            :key #'rest)
       (sort (loop for word in (dictionary document)
                   collect (cons word (term-frequency document word)))
             #'>
-            :key #'cdr)))
+            :key #'rest)))
 
 (defun extract-keywords (text &key (limit 5))
   "Extract keywords from a string of text."

libraries/nyxt-asdf/systems.lisp
@@ -68,7 +68,7 @@ pathname."
                                     :key #'namestring
                                     :test #'string-equal)))
                    (if pair
-                       (setf (cdr pair) (list value))
+                       (setf (rest pair) (list value))
                        (push (list key value)
                              (logical-pathname-translations host))))))
           (set-alist logical-path path-translation)

source/inspector.lisp
@@ -133,7 +133,7 @@ values in help buffers, REPL and elsewhere."))
          (:tbody
           (:tr
            (dolist (e value)
-             (:td (:raw (value->html (cdr e) t))))))))
+             (:td (:raw (value->html (rest e) t))))))))
        ((and (types:property-list-p value)
              ;; Stricter understanding of property lists:
              ;; -- Even length.

source/mode/watch.lisp
@@ -24,7 +24,7 @@
                         active-time-units))
          (to-seconds-multipliers
            (mapcar
-            (lambda (elem) (cdr (assoc elem to-seconds-alist :test 'string-equal)))
+            (lambda (elem) (rest (assoc elem to-seconds-alist :test 'string-equal)))
             active-time-units)))
     (echo "Refreshing every ~:@{~{~d ~}~a~:@}"
           (list times active-time-units))

source/web-extensions.lisp
@@ -106,8 +106,8 @@ JSON is the parsed extension manifest."
                                     (abs (- optimal-height b))))
                                :key (compose #'parse-integer #'symbol-name #'first))))))
     (error ()
-      (cdr (first (and (gethash "icons" json)
-                       (alex:hash-table-alist (gethash "icons" json))))))))
+      (rest (first (and (gethash "icons" json)
+                        (alex:hash-table-alist (gethash "icons" json))))))))
 
 (defun encode-browser-action-icon (json extension-directory)
   "Return the proper <img> HTML with the embedded browser action icon.

commit abf268701885a0bbc59ee103b139c854d0620c25

build-scripts/nyxt-guix: Fix bug.
Wrongly introduced in 86bf1805bb289b921a0c320e8d837f9b241918b9.

build-scripts/nyxt-guix.el
@@ -177,7 +177,7 @@ implementation.  Example:
           (let ((status (apply #'call-process
                                (car command)
                                nil (list output t) nil
-                               (rest command))))
+                               (cdr command))))
             (if (= status 0)
                 (kill-buffer output)
               (error "Nyxt Guix environment creation failed, see %S." output)