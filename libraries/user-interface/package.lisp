4c6c9e32cf700280c94cafea3d0db27752b0ced4
André A. Gomes <andremegafone@gmail.com>
2022-11-18 11:43:29 +0200

    libraries/user-interface: Use export-always.
    In accordance to the style guide.

--- libraries/user-interface/package.lisp
+++ libraries/user-interface/package.lisp
@@ -2,15 +2,4 @@
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
 (uiop:define-package :user-interface
-  (:use :cl)
-  (:export #:id
-           #:buffer
-           #:update
-           #:text
-           #:action
-           #:to-html-string
-           #:paragraph
-           #:progress-bar
-           #:button
-           #:percentage
-           #:connect))
+  (:use :cl))
