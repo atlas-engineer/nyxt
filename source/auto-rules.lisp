atlas-engineer/nyxt

commit eea221eabe33b11e4882eddd0f177ed22ec9030d

Uniformize return-actions binding to no-op.

source/auto-rules.lisp
@@ -290,7 +290,7 @@ For the storage format see the comment in the header of your `auto-rules-file'."
                         (make-instance 'prompter:raw-source
                                        :name "New URL")
                         (make-instance 'global-history-source
-                                       :return-actions '())))))
+                                       :return-actions #'identity))))
     (when (typep url 'nyxt::history-entry)
       (setf url (url url)))
     (add-modes-to-auto-rules
@@ -322,7 +322,7 @@ For the storage format see the comment in the header of your `auto-rules-file'."
                         (make-instance 'prompter:raw-source
                                        :name "New URL")
                         (make-instance 'global-history-source
-                                       :return-actions '())))))
+                                       :return-actions #'identity)))))
     (setf url (url url))
     (add-modes-to-auto-rules (url-infer-match url)
                              :include (rememberable-of (modes (current-buffer)))

source/history.lisp
@@ -122,7 +122,7 @@ then become available for deletion with `delete-history-entry'."
   (let ((buffers (or (alex:ensure-list buffer)
                      (prompt :prompt "Reset histories of buffer(s)"
                              :sources (make-instance 'buffer-source
-                                                     :return-actions '())))))
+                                                     :return-actions #'identity)))))
     (files:with-file-content (history (history-file (current-buffer)))
       (dolist (buffer buffers)
         (htree:reset-owner history (id buffer))))))

source/mode/annotate.lisp
@@ -180,7 +180,7 @@ make-instance."
           (prompt
            :prompt "Show annotation(s)"
            :sources (make-instance 'annotation-source
-                                   :return-actions nil))))
+                                   :return-actions #'identity))))
     (render-annotations selected-annotations)))
 
 (define-internal-page-command-global show-annotations ()

source/mode/diff.lisp
@@ -30,10 +30,10 @@ the highest standard on accessibility."))
 (define-internal-page-command-global diff
     (&key (old-buffer-id (id (prompt1 :prompt "Old buffer"
                                       :sources (make-instance 'buffer-source
-                                                              :return-actions  nil))))
+                                                              :return-actions #'identity))))
           (new-buffer-id (id (prompt1 :prompt "New buffer"
                                       :sources (make-instance 'buffer-source
-                                                              :return-actions  nil)))))
+                                                              :return-actions #'identity)))))
     (diff-buffer "*diff*" 'diff-mode)
   "Show difference between two buffers"
   (let ((old-html (ffi-buffer-get-document (nyxt::buffers-get old-buffer-id)))

source/mode/document.lisp
@@ -504,7 +504,7 @@ of buffers."
                   :prompt "Select headings from buffers"
                   :sources (make-instance 'buffer-source
                                           :multi-selection-p t
-                                          :return-actions nil))))
+                                          :return-actions #'identity))))
     (prompt
      :prompt "Jump to heading"
      :sources (loop for buffer in buffers

source/mode/remembrance.lisp
@@ -373,7 +373,7 @@ See also `auto-cache-on-load-p' in `remembrance-mode'."
   (let ((buffer (or buffer
                     (prompt1 :prompt "View changes for buffer"
                              :sources (make-instance 'buffer-source
-                                                     :return-actions '()))))
+                                                     :return-actions #'identity))))
         (mode (find-submode 'remembrance-mode)))
     (alex:when-let ((doc (find-url (url buffer) mode)))
       ;; TODO: Display in internal page.
@@ -388,7 +388,7 @@ BUFFER can be a list of buffers."
   (let ((buffers (or (alex:ensure-list buffer)
                      (prompt :prompt "Cache content of buffers"
                              :sources (make-instance 'buffer-source
-                                                     :return-actions '())))))
+                                                     :return-actions #'identity)))))
     (dolist (buffer buffers)
       (buffer->cache buffer (find-submode 'remembrance-mode)))))
 

source/mode/search-buffer.lisp
@@ -262,7 +262,7 @@ Example:
   "Search multiple buffers."
   (let ((buffers (prompt :prompt "Search buffer(s)"
                          :sources (make-instance 'buffer-source ; TODO: Define class?
-                                                 :return-actions '()
+                                                 :return-actions #'identity
                                                  :multi-selection-p t))))
     (prompt
      :prompt "Search text"

commit 066e05c8354a00c3f33e7b1b86e90041995b46fe

auto-rules: Fix typo.
Mistake introduced in eea221eabe33b11e4882eddd0f177ed22ec9030d.

source/auto-rules.lisp
@@ -290,7 +290,7 @@ For the storage format see the comment in the header of your `auto-rules-file'."
                         (make-instance 'prompter:raw-source
                                        :name "New URL")
                         (make-instance 'global-history-source
-                                       :return-actions #'identity))))
+                                       :return-actions #'identity)))))
     (when (typep url 'nyxt::history-entry)
       (setf url (url url)))
     (add-modes-to-auto-rules