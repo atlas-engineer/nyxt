atlas-engineer/nyxt

commit d063ee71aea013e6c6f78353da158d2ca51ef930

mode/document(next-heading): Only scroll if there's a next heading.

source/mode/document.lisp
@@ -453,8 +453,10 @@ ID is a buffer `id'."
 (define-command next-heading (&optional (buffer (current-buffer)))
   "Scroll to the next heading of the BUFFER."
   (sera:and-let* ((headings (get-headings :buffer buffer))
-                  (current (current-heading buffer)))
-    (scroll-page-to-heading (elt headings (1+ (position (element current) headings :key #'element))))))
+                  (current (current-heading buffer))
+                  (pos (position (element current) headings :key #'element))
+                  (_ (not (< (1+ pos) (length headings)))))
+    (scroll-page-to-heading (elt headings (1+ pos)))))
 
 (define-command previous-heading (&optional (buffer (current-buffer)))
   "Scroll to the previous heading of the BUFFER."

commit d19d7fe8fb1f44d3ec46f58d89c817fb1f5c1568

mode/document(next-heading): Fix bug.
Bug introduced in d063ee71aea013e6c6f78353da158d2ca51ef930.

source/mode/document.lisp
@@ -452,19 +452,23 @@ ID is a buffer `id'."
   (set-current-buffer (buffer heading) :focus nil)
   (nyxt/dom:scroll-to-element (element heading)))
 
+(defun scroll-page-to-n-headings (n &optional (buffer (current-buffer)))
+  "Scroll to the N adjacent heading of the BUFFER."
+  (sera:and-let* ((headings (get-headings :buffer buffer))
+                  (new-position (+ n
+                                   (position (element (current-heading buffer))
+                                             headings
+                                             :key #'element)))
+                  (_ (< 0 new-position (1- (length headings)))))
+    (scroll-page-to-heading (elt headings new-position))))
+
 (define-command next-heading (&optional (buffer (current-buffer)))
   "Scroll to the next heading of the BUFFER."
-  (sera:and-let* ((headings (get-headings :buffer buffer))
-                  (current (current-heading buffer))
-                  (pos (position (element current) headings :key #'element))
-                  (_ (not (< (1+ pos) (length headings)))))
-    (scroll-page-to-heading (elt headings (1+ pos)))))
+  (scroll-page-to-n-headings 1 buffer))
 
 (define-command previous-heading (&optional (buffer (current-buffer)))
   "Scroll to the previous heading of the BUFFER."
-  (sera:and-let* ((headings (get-headings :buffer buffer))
-                  (current (current-heading buffer)))
-    (scroll-page-to-heading (elt headings (1- (position (element current) headings :key #'element))))))
+  (scroll-page-to-n-headings -1 buffer))
 
 (define-class heading-source (prompter:source)
   ((prompter:name "Headings")