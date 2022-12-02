d753dd322f03c095ad4723df74293168f32372dc
Artyom Bologov <aartaka@protonmail.com>
2022-12-02 13:04:51 +0400

    window: Set default prompt height to a third of window height.

--- source/window.lisp
+++ source/window.lisp
@@ -77,10 +77,11 @@ the generic functions on `status-buffer'.  Finally set the `window'
         :margin 0)))
    (prompt-buffer-open-height
     ;; Ensures that 10 suggestions are vertically shown, while the last entry
-    ;; appears truncated thus making it clear that more follow.
-    ;; It's also set exactly to a third of the default Nyxt window height
-    ;; (768:256).
-    256
+    ;; appears truncated thus making it clear that more follow. It's also set
+    ;; exactly to a third of Nyxt window height.
+    :unbound
+    :reader nil
+    :writer t
     :documentation "The height of the prompt buffer when open.")
    (input-dispatcher
     'dispatch-input-event
@@ -123,6 +124,12 @@ The handlers take the window as argument."))
     (setf (id window) (new-id))
     (setf (slot-value browser 'last-active-window) window))
   window)
+
+(defmethod prompt-buffer-open-height ((window window))
+  (if (slot-boundp window 'prompt-buffer-open-height)
+      (slot-value window 'prompt-buffer-open-height)
+      (setf (slot-value window 'prompt-buffer-open-height)
+            (round (/ (ffi-height window) 3)))))
 
 (defmethod print-object ((window window) stream)
   (print-unreadable-object (window stream :type t :identity t)
