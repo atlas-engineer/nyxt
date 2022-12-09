37a9574aaef375b99820559efb93d696a851b793
André A. Gomes <andremegafone@gmail.com>
2022-12-09 18:05:46 +0200

    tests/offline/prompt-buffer: Rewrite tests.

--- tests/offline/prompt-buffer.lisp
+++ tests/offline/prompt-buffer.lisp
@@ -6,18 +6,41 @@
 (unless lparallel:*kernel* (setf lparallel:*kernel*
                                  (lparallel:make-kernel (or (serapeum:count-cpus) 1))))
 
-(define-class twice-source (prompter:source)
-  ((prompter:name "Twice-repeated suggestions")))
+(define-class test-source (prompter:source)
+  ;; The number of suggestions is only relevant to access the algorithm's
+  ;; performance, so it suffices to use a single suggestion for general testing.
+  ((prompter:name "Test")
+   (prompter:constructor '("suggestion"))))
 
-(defmethod prompter:object-attributes ((object string) (source twice-source))
-  `(("Once" ,object)
-    ("Twice" ,(str:concat object object))))
+(defun set-test-source-object-attributes (lst)
+  "Set `prompter:object-attributes' following LST properties:
+Its length LST dictates the number of attributes.
+Each element dictates the number of times a suggestion is concatenated."
+  (defmethod prompter:object-attributes ((object string) (source test-source))
+    (do ((index 0 (1+ index))
+         (result '() (cons `("Attribute" ,(str:repeat (nth index lst) object))
+                           result)))
+        ((eq index (length lst)) (nreverse result)))))
 
-(define-test smart-attribute-widths ()
-  (assert-equal
-   (list (round (/ 100 3))
-         (round (* 2 (/ 100 3))))
-   (nyxt::attribute-widths
-    (make-instance 'twice-source
-                   :constructor (mapcar (curry #'format nil "~R")
-                                        (alex:iota 100))))))
+;; TODO Add to lisp-unit2.
+(defun random-in-range (start end)
+  (+ start (random (+ 1 (- end start)))))
+
+(define-test constant-size-attributes ()
+  (let ((n-attributes (random-in-range 1 10)))
+    ;; How to shup up the fact that the method is being redefined?
+    (set-test-source-object-attributes (make-list n-attributes :initial-element 1))
+    (assert-equal (make-list n-attributes :initial-element (/ 100 n-attributes))
+                  (nyxt::attribute-widths (make-instance 'test-source)))))
+
+(define-test ratio-between-two-attributes ()
+  (let ((ratio (random-in-range 1 4)))
+    (set-test-source-object-attributes `(1 ,ratio))
+    (assert-equal (mapcar (curry #'* 100)
+                          (list (/ 1 (1+ ratio)) (/ ratio (1+ ratio))))
+                  (nyxt::attribute-widths (make-instance 'test-source)))
+    ;; Test symmetry.
+    (set-test-source-object-attributes `(,ratio 1))
+    (assert-equal (mapcar (curry #'* 100)
+                          (list (/ ratio (1+ ratio)) (/ 1 (1+ ratio))))
+                  (nyxt::attribute-widths (make-instance 'test-source)))))
