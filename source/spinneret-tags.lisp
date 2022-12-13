3b884c1e4a213cb4aa2d8495da047e5045e2941b
aartaka <aartaka@protonmail.com>
2022-12-13 18:16:54 +0400

    spinneret(:ninput): Calculate rows based on body size.
    <textarea> uses body as the content, intead of value attribute for <input>.

--- source/spinneret-tags.lisp
+++ source/spinneret-tags.lisp
@@ -242,7 +242,7 @@ Evaluates (via `nyxt/ps:lisp-eval') the BODY in BUFFER when clicked."
     ,@attrs
     ,title))
 
-(deftag :ninput (body attrs &key value (rows (length (str:lines value :omit-nulls nil))) &allow-other-keys)
+(deftag :ninput (body attrs &key (rows (length (str:lines body :omit-nulls nil))) &allow-other-keys)
   "Nicely styled <textarea> with a reasonable number of ROWS to accommodate the VALUE."
   `(:textarea.input
     :rows ,rows
