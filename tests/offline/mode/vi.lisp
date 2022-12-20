16dba7fb7c35fd7bbc8f17ffa3e4538492de840b
André A. Gomes <andremegafone@gmail.com>
2022-12-20 22:58:59 +0200

    tests/offline/mode/vi: Add keyscheme check.

--- tests/offline/mode/vi.lisp
+++ tests/offline/mode/vi.lisp
@@ -4,9 +4,19 @@
 (in-package :nyxt/tests)
 
 (define-test toggle-vi-modes ()
-  (let ((buffer (make-instance 'input-and-modable-buffer)))
+  (let* ((buffer (make-instance 'input-and-modable-buffer))
+         (default-keyscheme (nkeymaps:name (keyscheme buffer))))
     (with-current-buffer buffer
       (assert-true (enable-modes* 'nyxt/vi-mode:vi-normal-mode buffer))
+      (assert-string= "vi-normal"
+                      (nkeymaps:name (keyscheme buffer)))
       (assert-true (disable-modes* 'nyxt/vi-mode:vi-normal-mode buffer))
+      (assert-string= default-keyscheme
+                      (nkeymaps:name (keyscheme buffer)))
+
       (assert-true (enable-modes* 'nyxt/vi-mode:vi-insert-mode buffer))
-      (assert-true (disable-modes* 'nyxt/vi-mode:vi-insert-mode buffer)))))
+      (assert-string= "vi-insert"
+                      (nkeymaps:name (keyscheme buffer)))
+      (assert-true (disable-modes* 'nyxt/vi-mode:vi-insert-mode buffer))
+      (assert-string= default-keyscheme
+                      (nkeymaps:name (keyscheme buffer))))))
