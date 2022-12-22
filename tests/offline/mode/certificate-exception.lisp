d98408bc9a8de47cfdea63384b71e6855c4d12a3
André A. Gomes <andremegafone@gmail.com>
2022-12-22 10:23:10 +0200

    tests/offline/mode/certificate-exception: Simplify test.

--- tests/offline/mode/certificate-exception.lisp
+++ tests/offline/mode/certificate-exception.lisp
@@ -4,7 +4,7 @@
 (in-package :nyxt/tests)
 
 (define-test toggle-certificate-exception-mode ()
-  (let ((buffer (make-instance 'network-and-modable-buffer)))
+  (let ((buffer (make-instance 'modable-buffer)))
     (with-current-buffer buffer
       (assert-true (enable-modes* 'nyxt/certificate-exception-mode:certificate-exception-mode
                                   buffer))
