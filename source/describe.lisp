atlas-engineer/nyxt

commit ecb9c46bcc2bff34925cebb86201f1b384639e51

describe(-function): Deprecate format-arglist in favor of arglist.

source/describe.lisp
@@ -424,25 +424,6 @@ turned into links to their respective description page."
           (:h1 (format nil "~s" variable))
           (:p "Unbound")))))
 
-(defun format-arglist (arglist)
-  (if arglist
-      (multiple-value-bind (required optional rest keywords aok? aux key?)
-          (alex:parse-ordinary-lambda-list arglist
-                                           :normalize-optional nil
-                                           :normalize-keyword nil)
-        (declare (ignore aux aok? key?))
-        (with-output-to-string (s)
-          (when required
-            (format s "~{~a~^ ~}~&" required))
-          (when optional
-            (format s "&optional ~{~s~^ ~}~&"
-                    optional))
-          (when rest
-            (format s "&rest ~a~&" rest))
-          (when keywords
-            (format s "&key ~{~s~^ ~}~&" keywords))))
-      "()"))
-
 (defun format-function-type (function-type)
   (match function-type
     ((list 'function argument-types return-types)
@@ -467,8 +448,7 @@ For generic functions, describe all the methods."
                  (spinneret:with-html-string
                    (:raw (resolve-backtick-quote-links (documentation input 'function) (symbol-package input)))
                    (:h2 "Argument list")
-                   (:p (:pre (let ((*package* (symbol-package input)))
-                               (format-arglist (mopu:function-arglist input)))))
+                   (:p (:pre (prini-to-string (arglist input) :package (symbol-package input))))
                    (when (sym:command-symbol-p input)
                      (let* ((key-keymap-pairs (nth-value 1 (keymaps:binding-keys input (all-keymaps))))
                             (key-keymapname-pairs (mapcar (lambda (pair)

commit 80a834cf5f774c1ac70654b18eb7b4f34aa45b08

describe: Fix describe-function.
Bug introduced by commit ecb9c46bcc2bff34925cebb86201f1b384639e51.

source/describe.lisp
@@ -512,9 +512,7 @@ For generic functions, describe all the methods."
                     (:raw (resolve-backtick-quote-links (documentation method 't)
                                                         (symbol-package (mopu:method-name method))))
                     (:h4 "Argument list")
-                    (:p (:pre
-                         (let ((*package* (symbol-package input)))
-                           (format-arglist (closer-mop:method-lambda-list method)))))
+                    (:p (:pre (prini-to-string (arglist input) :package (symbol-package input))))
                     (alex:when-let* ((definition (swank:find-definition-for-thing method))
                                      (not-error-p (null (getf definition :error)))
                                      (file (first (rest (getf definition :location)))))