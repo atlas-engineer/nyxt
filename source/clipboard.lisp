0ab4b77539b5158907ea0716cb51ea059f451a5c
Artyom Bologov <aartaka@protonmail.com>
2023-02-02 21:06:42 +0400

    clipboard(ring-insert-clipboard): Returns (maybe string).

--- source/clipboard.lisp
+++ source/clipboard.lisp
@@ -3,8 +3,7 @@
 
 (in-package :nyxt)
 
-(-> ring-insert-clipboard (containers:ring-buffer-reverse) string)
-
+(-> ring-insert-clipboard (containers:ring-buffer-reverse) (maybe string))
 (export-always 'ring-insert-clipboard)
 (defun ring-insert-clipboard (ring)
   "Check if clipboard-content is most recent entry in RING.
