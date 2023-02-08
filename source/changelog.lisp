ARL-Cookie/gin

Make attribute widths adjustments optional.

source/changelog.lisp
@@ -577,8 +577,9 @@ auto-mode-rules.lisp)."))
         " allows to revisit the \"hub\" URLs you often visit, instead of adding them to history anew.")
    (:li "nyxt:// pages can be linked from the outside Internet due to scheme security
 settings changes.")
-   (:li "Prompt buffer attribute columns adjust to their content, allowing for
-a better overview of lengthy attributes."))
+   (:li "With " (:nxref :slot 'auto-attribute-width-p :class-name 'prompt-buffer)
+        " on, prompt buffer attribute columns adjust to their content, allowing
+for a better overview of lengthy attributes."))
 
   (:h3 "Bindings")
   (:ul

atlas-engineer/nyxt

source/changelog.lisp
@@ -580,8 +580,9 @@ settings changes.")
    (:li "When started with " (:code "--remote") " and without " (:code "--quit")
         ", Nyxt now reads s-expression from standard input and sends it to the remote process. "
         "This avoids the performance penalty of a new process startup on each iteration.")
-   (:li "Prompt buffer attribute columns adjust to their content, allowing for
-a better overview of lengthy attributes."))
+   (:li "With " (:nxref :slot 'dynamic-attribute-width-p :class-name 'prompt-buffer)
+        " on, prompt buffer attribute columns adjust to their content, allowing
+for a better overview of lengthy attributes."))
 
   (:h3 "Bindings")
   (:ul