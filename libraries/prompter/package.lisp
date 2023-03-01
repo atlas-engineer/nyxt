730f476574fa769877280f13e193555c28253d23
Andre A. Gomes <andremegafone@gmail.com>
2023-03-01 11:20:50 +0200

    libraries/prompter/package: Initialize lparallel:*kernel*.

--- libraries/prompter/package.lisp
+++ libraries/prompter/package.lisp
@@ -7,6 +7,9 @@
   (:import-from :serapeum #:export-always))
 (in-package prompter)
 
+(unless lparallel:*kernel* (setf lparallel:*kernel*
+                                 (lparallel:make-kernel (or (serapeum:count-cpus) 1))))
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (trivial-package-local-nicknames:add-package-local-nickname :alex :alexandria)
   (trivial-package-local-nicknames:add-package-local-nickname :sera :serapeum))
