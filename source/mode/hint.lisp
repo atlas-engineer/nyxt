6e63bf5eadc75d4849e23c00a2922db8802cf045
Andre A. Gomes <andremegafone@gmail.com>
2023-04-11 14:10:32 +0300

    mode/hint: Remove unhighlight-selected-hint.
    It used to be called in mode/search-buffer, but not anymore.

--- source/mode/hint.lisp
+++ source/mode/hint.lisp
@@ -206,15 +206,6 @@ For instance, to include images:
       (when (ps:lisp scroll)
         (ps:chain %element (scroll-into-view (ps:create block "center")))))))
 
-(export-always 'unhighlight-selected-hint)
-(define-parenscript unhighlight-selected-hint ()
-  ;; There should be, at most, a unique element with the
-  ;; "nyxt-select-hint" class.
-  ;; querySelectAll, unlike querySelect, handles the case when none are
-  ;; found.
-  (ps:dolist (selected-hint (nyxt/ps:qsa document ".nyxt-select-hint"))
-    (ps:chain selected-hint class-list (remove "nyxt-select-hint"))))
-
 (define-parenscript-async set-hint-visibility (hint state)
   "Set visibility STATE of HINT element.
 
