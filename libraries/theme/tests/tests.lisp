516a3d5eaad4cdf646602ef6b81338e20e9fa2e3
Andre A. Gomes <andremegafone@gmail.com>
2023-04-12 10:49:26 +0300

    libraries/theme/tests/tests: Fix indentation.

--- libraries/theme/tests/tests.lisp
+++ libraries/theme/tests/tests.lisp
@@ -20,42 +20,42 @@
 
 (define-test basic-css-substitution ()
   (assert-string= "a{background-color:black;color:yellow;}"
-                              (let ((lass:*pretty* nil))
-                                (theme:themed-css *theme*
-                                  `(a
-                                    :background-color ,theme:background
-                                    :color ,theme:primary)))))
+                  (let ((lass:*pretty* nil))
+                    (theme:themed-css *theme*
+                      `(a
+                        :background-color ,theme:background
+                        :color ,theme:primary)))))
 
 (define-test multi-rule/multi-color-substitution ()
   (assert-string= "a{background-color:black;color:yellow;}body{background-color:yellow;color:white;}h1{color:magenta;}"
-                              (let ((lass:*pretty* nil))
-                                (theme:themed-css *theme*
-                                  `(a
-                                    :background-color ,theme:background
-                                    :color ,theme:primary)
-                                  `(body
-                                    :background-color ,theme:primary
-                                    :color ,theme:on-background)
-                                  `(h1
-                                    :color ,theme:accent)))))
+                  (let ((lass:*pretty* nil))
+                    (theme:themed-css *theme*
+                      `(a
+                        :background-color ,theme:background
+                        :color ,theme:primary)
+                      `(body
+                        :background-color ,theme:primary
+                        :color ,theme:on-background)
+                      `(h1
+                        :color ,theme:accent)))))
 
 (define-test irregular-args ()
   (assert-string=  "body{background-color:yellow;color:magenta !important;}"
-                               (let ((lass:*pretty* nil))
-                                 (theme:themed-css *theme*
-                                   `(body
-                                     :background-color ,theme:primary
-                                     :color ,theme:accent "!important")))))
+                   (let ((lass:*pretty* nil))
+                     (theme:themed-css *theme*
+                       `(body
+                         :background-color ,theme:primary
+                         :color ,theme:accent "!important")))))
 
 (define-test quasi-quoted-form ()
   (assert-string= "body{color:black;background-color:yellow;}"
-                              (let ((lass:*pretty* nil))
-                                (theme:themed-css *theme*
-                                  `(body
-                                    :color ,(if (theme:dark-p theme:theme)
-                                                theme:background
-                                                theme:on-background)
-                                    :background-color ,theme:primary)))))
+                  (let ((lass:*pretty* nil))
+                    (theme:themed-css *theme*
+                      `(body
+                        :color ,(if (theme:dark-p theme:theme)
+                                    theme:background
+                                    theme:on-background)
+                        :background-color ,theme:primary)))))
 
 (defun assert-contrast-ratio (color1 color2 &optional (min-contrast 7.0))
   (assert-true (>= (theme:contrast-ratio color1 color2)
