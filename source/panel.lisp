e18d7d25a23af3fd7e79786d053b6384efbf5966
Artyom Bologov <aartaka@protonmail.com>
2023-06-20 16:20:57 +0400

    panel: Add close button ID.

--- source/panel.lisp
+++ source/panel.lisp
@@ -76,6 +76,7 @@ The main difference is that their command toggles the panel."))
                         (:raw contents)
                         (let ((buffer (find-panel-buffer (name page))))
                           (:button.button
+                           :id "close"
                            :style "position: absolute; top: 1em; right: 1em;"
                            :title "Close this panel buffer"
                            :onclick (ps:ps (nyxt/ps:lisp-eval
