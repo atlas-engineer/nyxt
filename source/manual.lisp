bdd8f0088572a8ec56d6956857df03a4e5ea9d24
Andre A. Gomes <andremegafone@gmail.com>
2023-06-26 19:47:16 +0300

    manual: Fix compilation error.

--- source/manual.lisp
+++ source/manual.lisp
@@ -193,12 +193,7 @@ shifts.  For instance if " (:code "C-x C-F") " fails to match anything " (:code 
 documentation.  Bookmarks can also be used as search engines, see the
 corresponding section.")
         (:p "Nyxt comes with default search engines for "
-            (:code (format nil "~{~a~^, ~}"
-                           (mapcar (lambda (engine)
-                                     (quri:uri-host (quri:uri (getf engine :search-url))))
-                                   (rest (getf (mopu:slot-properties 'context-buffer 'search-engines)
-                                               :initform)))))
-            ". "
+            (:code "en.wikipedia.org, duckduckgo.com, search.atlas.engineer") ". "
             "The following example shows one way to add new search engines.")
         (:ncode
           '(defvar *my-search-engines*
