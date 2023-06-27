099f4368f26a185ea21079d317c5f6f73861b574
Andre A. Gomes <andremegafone@gmail.com>
2023-06-27 16:39:15 +0300

    manual: Fix auto-generation of default search engines.

--- source/manual.lisp
+++ source/manual.lisp
@@ -193,7 +193,14 @@ shifts.  For instance if " (:code "C-x C-F") " fails to match anything " (:code 
 documentation.  Bookmarks can also be used as search engines, see the
 corresponding section.")
         (:p "Nyxt comes with default search engines for "
-            (:code "en.wikipedia.org, duckduckgo.com, search.atlas.engineer") ". "
+            (:code
+             (format nil "~{~a~^, ~}"
+                     (mapcar (lambda (engine) (quri:uri-host (quri:uri (getf engine
+                                                                        :search-url))))
+                             (rest (third (getf (mopu:slot-properties 'context-buffer
+                                                                      'search-engines)
+                                                :initform))))))
+            ". "
             "The following example shows one way to add new search engines.")
         (:ncode
           '(defvar *my-search-engines*
