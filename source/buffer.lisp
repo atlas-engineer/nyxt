atlas-engineer/nyxt

commit 4009036bed2ebea8783041990b8827533dfb1c25

Propagate the new theme.
Uniformize the code style of the style slot.  It resembles CSS and becomes
readable.

All hoverable elements are distinguishable by opacity.

source/buffer.lisp
@@ -30,73 +30,70 @@ various parts, such as the path of all data files.")
    (url-at-point (quri:uri ""))
    (title "")
 
-   (style
-    (theme:themed-css
-        (theme *browser*)
-
-      (body
-       :color theme:text
-       :background-color theme:background
-       :margin-left "20px"
-       :margin-top "20px")
-      ("h1,h2,h3,h4,h5,h6"
-       :color theme:primary
-       :font-family theme:font-family)
-      (hr
-       :height "3px"
-       :border-radius "2px"
-       :border-width "0"
-       :color theme:secondary
-       :background-color theme:secondary)
-      (button
-       :background "none"
-       :color "inherit"
-       :border "none"
-       :padding 0
-       :font "inherit"
-       :outline "inherit")
-      (.button
-       :display "inline-block"
-       :background-color theme:secondary
-       :color theme:background
-       :text-decoration "none"
-       :border-radius "2px"
-       :padding "6px"
-       :margin "2px")
-      (.link
-       :all "unset"
-       :text-decoration "underline"
-       :display "inline"
-       :color theme:primary)
-      (".link:hover"
-       :color theme:text)
-      (.accent
-       :background-color theme:accent)
-      (|.button:hover|
-       :color theme:text)
-      (|.button:visited|
-       :color theme:background)
-      (|.button:active|
-       :color theme:background)
-      (a
-       :color theme:primary)
-      (pre
-       :overflow "auto"
-       :color theme:text
-       :background-color theme:quaternary
-       :border-radius "2px"
-       :padding "5px")
-      ("table, th, td"
-       :border-color theme:quaternary
-       :border-collapse "collapse"
-       :border-width "1px"
-       :border-style "solid"
-       :color theme:text
-       :background-color theme:background)
-      (th
-       :background-color theme:primary
-       :color theme:background
-       :text-align "left")))
+   (style (theme:themed-css (theme *browser*)
+            (body
+             :background-color theme:background
+             :color theme:on-background
+             :margin-left "20px"
+             :margin-top "20px")
+            ("h1,h2,h3,h4,h5,h6"
+             :color theme:primary
+             :font-family theme:font-family)
+            (hr
+             :background-color theme:secondary
+             :color theme:on-secondary
+             :height "3px"
+             :border-radius "2px"
+             :border-width "0")
+            (button
+             :background "transparent"
+             :color "inherit"
+             :border "none"
+             :padding 0
+             :font "inherit"
+             :outline "inherit")
+            (.button
+             :background-color theme:primary
+             :color theme:on-primary
+             :display "inline-block"
+             :text-decoration "none"
+             :border-radius "2px"
+             :padding "6px"
+             :margin "2px")
+            (.link
+             :all "unset"
+             :text-decoration "underline"
+             :display "inline"
+             :color theme:primary)
+            (".link:hover"
+             :opacity 0.5)
+            (.accent
+             :color theme:accent)
+            (|.button:hover|
+             :opacity 0.5)
+            (|.button:visited|
+             :color theme:background)
+            (|.button:active|
+             :color theme:background)
+            (a
+             :color theme:primary)
+            (pre
+             :overflow "auto"
+             :color theme:on-background
+             :background-color theme:secondary
+             :border-radius "2px"
+             :padding "5px")
+            ("table, th, td"
+             :border-color theme:secondary
+             :border-collapse "collapse"
+             :border-width "1px"
+             :border-style "solid"
+             :background-color theme:background
+             :color theme:on-background)
+            (th
+             :background-color theme:primary
+             :color theme:on-primary
+             :text-align "left")))
    (buffer-delete-hook                  ; TODO: Should we move this to `context-buffer'?
     (make-instance 'hook-buffer)
     :type hook-buffer
@@ -580,35 +577,34 @@ store them somewhere and `ffi-buffer-delete' them once done."))
    (style (theme:themed-css (theme *browser*)
             (body
              :background-color theme:background
-             :color theme:text
+             :color theme:on-background
              :margin "0"
              :padding "10px"
              :border-style "solid"
              :border-width "0px 1px"
-             :border-color theme:tertiary)
+             :border-color theme:secondary)
             ("h1,h2,h3,h4,h5,h6"
              :font-family theme:font-family
              :font-weight 500)
             (a
              :color theme:primary)
             (button
-             :background "none"
+             :background "transparent"
              :color "inherit"
              :border "none"
              :padding 0
              :font "inherit"
              :outline "inherit")
             (.button
+             :background-color theme:primary
+             :color theme:on-primary
              :display "inline-block"
-             :background-color "darkgray"
-             :color theme:background
              :text-decoration "none"
              :border-radius "2px"
              :padding "6px"
-             :margin-left "2px"
-             :margin-right "2px")
+             :margin "2px")
             (|.button:hover|
-             :color theme:text)
+             :opacity 0.5)
             (|.button:visited|
              :color theme:background)
             (|.button:active|
@@ -630,108 +626,101 @@ store them somewhere and `ffi-buffer-delete' them once done."))
    (glyph-mode-presentation-p
     nil
     :documentation "Display the modes as a list of glyphs.")
-   (style
-    (theme:themed-css (theme *browser*)
-      (body
-       :color theme:text
-       :background theme:tertiary
-       :font-size "14px"
-       :color theme:text
-       :padding 0
-       :margin 0
-       :line-height "20px")
-      (.loader
-       :border-width "2px"
-       :border-style "solid"
-       :border-color "transparent"
-       :border-top-color theme:accent
-       :border-left-color theme:accent
-       :border-radius "50%"
-       :display "inline-block"
-       :width "7px"
-       :height "7px"
-       :animation "spin 1s linear infinite")
-      ("@keyframes spin"
-       ("0%" :transform "rotate(0deg)")
-       ("100%" :transform "rotate(360deg)"))
-      (".arrow-right"
-       :clip-path "polygon(0 0, calc(100% - 10px) 0, 100% 50%, calc(100% - 10px) 100%, 0 100%)"
-       :margin-right "-10px")
-      (".arrow-left"
-       :clip-path "polygon(10px 0, 100% 0, 100% 100%, 10px 100%, 0% 50%)"
-       :margin-left "-10px")
-      ("#container"
-       :display "grid"
-       ;; Columns: controls, url, tabs, modes
-       :grid-template-columns "90px minmax(auto, 30ch) 1fr 220px"
-       :overflow-y "hidden")
-      ("#controls"
-       :font-size "16px"
-       :font-weight "700"
-       :background-color theme:primary
-       :color theme:background
-       :padding-left "5px"
-       :overflow "hidden"
-       :white-space "nowrap"
-       :z-index "4")
-      ("#scheme-mode"
-       :padding-right "10px"
-       :padding-left "10px"
-       :text-align "center"
-       :z-index "3")
-      ("#url"
-       :color theme:background
-       :background-color theme:secondary
-       :min-width "100px"
-       :text-overflow "ellipsis"
-       :overflow-x "hidden"
-       :white-space "nowrap"
-       :padding-right "10px"
-       :padding-left "15px"
-       :z-index "2")
-      ("#tabs"
-       :color theme:background
-       :background-color theme:tertiary
-       :min-width "100px"
-       :white-space "nowrap"
-       :overflow-x "scroll"
-       :text-align "left"
-       :padding-left "15px"
-       :padding-right "10px"
-       :z-index "1")
-      ("#tabs::-webkit-scrollbar"
-       :display "none")
-      (.tab
-       :color theme:background
-       :white-space "nowrap"
-       :text-decoration "none"
-       :padding-left "5px"
-       :padding-right "5px")
-      (".tab:hover"
-       :color theme:text)
-      ("#modes"
-       :background-color theme:secondary
-       :color theme:background
-       :text-align "right"
-       :padding-left "10px"
-       :padding-right "5px"
-       :overflow-x "scroll"
-       :white-space "nowrap"
-       :z-index "2")
-      ("#modes::-webkit-scrollbar"
-       :display "none")
-      (button
-       :background "none"
-       :color "inherit"
-       :border "none"
-       :padding 0
-       :font "inherit"
-       :outline "inherit")
-      (.button
-       :color theme:background
-       :text-decoration "none")
-      (|.button:hover|
-       :color theme:text))))
+   (style (theme:themed-css (theme *browser*)
+            (body
+             :line-height "20px"
+             :font-size "14px"
+             :padding 0
+             :margin 0)
+            (.loader
+             :border-width "2px"
+             :border-style "solid"
+             :border-color "transparent"
+             :border-top-color theme:accent
+             :border-left-color theme:accent
+             :border-radius "50%"
+             :display "inline-block"
+             :width "7px"
+             :height "7px"
+             :animation "spin 1s linear infinite")
+            ("@keyframes spin"
+             ("0%" :transform "rotate(0deg)")
+             ("100%" :transform "rotate(360deg)"))
+            (".arrow-right"
+             :clip-path "polygon(0 0, calc(100% - 10px) 0, 100% 50%, calc(100% - 10px) 100%, 0 100%)"
+             :margin-right "-10px")
+            (".arrow-left"
+             :clip-path "polygon(10px 0, 100% 0, 100% 100%, 10px 100%, 0% 50%)"
+             :margin-left "-10px")
+            ("#container"
+             :display "grid"
+             ;; Columns: controls, url, tabs, modes
+             :grid-template-columns "90px minmax(auto, 30ch) 1fr 220px"
+             :overflow-y "hidden")
+            ("#controls"
+             :background-color theme:primary
+             :color theme:on-primary
+             ;; :font-size "16px"
+             :font-weight "700"
+             :padding-left "5px"
+             :overflow "hidden"
+             :white-space "nowrap"
+             :z-index "3"
+             )
+            ("#url"
+             :background-color theme:secondary
+             :color theme:on-secondary
+             :min-width "100px"
+             :text-overflow "ellipsis"
+             :overflow-x "hidden"
+             :white-space "nowrap"
+             :padding-right "10px"
+             :padding-left "15px"
+             :z-index "2"
+             )
+            ("#tabs"
+             :background-color theme:primary
+             :color theme:on-primary
+             :min-width "100px"
+             :white-space "nowrap"
+             :overflow-x "scroll"
+             :text-align "left"
+             :padding-left "15px"
+             :padding-right "10px"
+             :z-index "1"
+             )
+            ("#tabs::-webkit-scrollbar"
+             :display "none")
+            (.tab
+             :color theme:background
+             :white-space "nowrap"
+             :text-decoration "none"
+             :padding-left "5px"
+             :padding-right "5px")
+            (".tab:hover"
+             :opacity 0.5)
+            ("#modes"
+             :background-color theme:secondary
+             :color theme:on-secondary
+             :text-align "right"
+             :padding-left "10px"
+             :padding-right "5px"
+             :overflow-x "scroll"
+             :white-space "nowrap"
+             :z-index "2"
+             )
+            ("#modes::-webkit-scrollbar"
+             :display "none")
+            (button
+             :background "transparent"
+             :color "inherit"
+             :text-decoration "transparent"
+             :border "transparent"
+             :padding 0
+             :font "inherit"
+             :outline "inherit")
+            (|.button:hover|
+             :opacity 0.5))))
   (:export-class-name-p t)
   (:export-accessor-names-p t)
   (:export-predicate-name-p t)

source/mode/download.lisp
@@ -125,33 +125,32 @@ appearance in the buffer when they are setf'd."
 (define-mode download-mode ()
   "Display list of downloads."
   ((rememberable-p nil)
-   (style
-       (theme:themed-css (theme *browser*)
-         (".download"
-          :margin-top "10px"
-          :padding-left "5px"
-          :background-color theme:background
-          :color theme:text
-          :brightness "80%"
-          :border-radius "3px")
-         (".download-url"
-          :overflow "auto"
-          :white-space "nowrap")
-         (".download-url a"
-          :font-size "small"
-          :color theme:text)
-         (".status p"
-          :display "inline-block"
-          :margin-right "10px")
-         (".progress-bar-container"
-          :height "20px"
-          :width "100%")
-         (".progress-bar-base"
-          :height "100%"
-          :background-color theme:secondary)
-         (".progress-bar-fill"
-          :height "100%"
-          :background-color theme:tertiary))))
+   (style (theme:themed-css (theme *browser*)
+            (".download"
+             :background-color theme:background
+             :color theme:on-background
+             :margin-top "10px"
+             :padding-left "5px"
+             :brightness "80%"
+             :border-radius "3px")
+            (".download-url"
+             :overflow "auto"
+             :white-space "nowrap")
+            (".download-url a"
+             :color theme:on-background
+             :font-size "small")
+            (".status p"
+             :display "inline-block"
+             :margin-right "10px")
+            (".progress-bar-container"
+             :height "20px"
+             :width "100%")
+            (".progress-bar-base"
+             :background-color theme:secondary
+             :height "100%")
+            (".progress-bar-fill"
+             :background-color theme:secondary
+             :height "100%"))))
   (:toggler-command-p nil))
 
 

source/help.lisp
@@ -64,8 +64,8 @@ CLASS is a class symbol."
              (spinneret:with-html-string
                (:p (:button :class "button"
                             :style (format nil "background-color: ~a; color: ~a"
-                                           (theme:primary-color (symbol-value theme-symbol))
-                                           (theme:background-color (symbol-value theme-symbol)))
+                                           (theme:accent-color (symbol-value theme-symbol))
+                                           (theme:on-accent-color (symbol-value theme-symbol)))
                             :onclick (ps:ps (nyxt/ps:lisp-eval
                                              (:title "set-theme")
                                              (nyxt::auto-configure
@@ -75,13 +75,10 @@ CLASS is a class symbol."
                             text))
                (:p "Colors:")
                (:dl
-                (loop for (name color text-color) in '(("Text" theme:text-color theme:background-color)
-                                                       ("Accent" theme:accent-color theme:background-color)
-                                                       ("Primary" theme:primary-color theme:background-color)
-                                                       ("Secondary" theme:secondary-color theme:background-color)
-                                                       ("Tertiary" theme:tertiary-color theme:text-color)
-                                                       ("Quaternary" theme:quaternary-color theme:text-color)
-                                                       ("Background" theme:background-color theme:text-color))
+                (loop for (name color text-color) in '(("Background" theme:background-color theme:on-background-color)
+                                                       ("Accent" theme:accent-color theme:on-accent-color)
+                                                       ("Primary" theme:primary-color theme:on-primary-color)
+                                                       ("Secondary" theme:secondary-color theme:on-secondary-color))
                       collect (:dt name ": ")
                       collect (:dd (:span :style (format nil "background-color: ~a; color: ~a; border-radius: 0.2em"
                                                          (slot-value (symbol-value theme-symbol) color)
@@ -123,14 +120,16 @@ disabling compositing, you will need to restart Nyxt."))
 (define-command print-bindings-cheatsheet ()
   "Print the buffer with the list of all known bindings for the current buffer
 optimizing the use of space."
-  (nyxt::html-set-style
-   (theme:themed-css (theme *browser*)
-     (h3 :font-size "10px"
-         :font-family theme:font-family
-         :font-weight 500)
-     (tr :font-size "7px")
-     (div :display inline-block))
-   (describe-bindings))
+  (nyxt::html-set-style (theme:themed-css (theme *browser*)
+                          (h3
+                           :font-size "10px"
+                           :font-family theme:font-family
+                           :font-weight 500)
+                          (tr
+                           :font-size "7px")
+                          (div
+                           :display inline-block))
+                        (describe-bindings))
   (nyxt/document-mode:print-buffer))
 
 (defun tls-help (buffer url)
@@ -242,9 +241,9 @@ The version number is stored in the clipboard."
            :title "Chat with developers and other Nyxt users."
            "Chat")))
      (:main
-      (:h1 "Nyxt")
+      (:h1 :class "accent" "Nyxt")
       (:i "The Internet on your terms.")
-      (:p (:button :class "button accent"
+      (:p (:button :class "button"
                    :type "submit"
                    :onclick (ps:ps (nyxt/ps:lisp-eval
                                     (:title "search")
@@ -306,14 +305,14 @@ System information is also saved into the clipboard."
                  (:p (format nil "No bookmarks in ~s." (files:expand (nyxt/bookmark-mode:bookmarks-file mode)))))))))
     (let ((dashboard-style (theme:themed-css (theme *browser*)
                              (body
-                              :color theme:text
                               :background-color theme:background
+                              :color theme:on-background
                               :margin-top 0
                               :margin-bottom 0)
                              ("#title"
                               :font-size "400%")
                              ("#subtitle"
-                              :color theme:tertiary)
+                              :color theme:secondary)
                              (.section
                               :border-style "solid none none none"
                               :border-color theme:secondary
@@ -321,7 +320,7 @@ System information is also saved into the clipboard."
                               :overflow "scroll"
                               :min-height "150px")
                              ("h3"
-                              :color theme:tertiary)
+                              :color theme:secondary)
                              ("ul"
                               :list-style-type "circle"))))
       (spinneret:with-html-string

source/mode/bookmark.lisp
@@ -61,7 +61,7 @@ Bookmarks can be persisted to disk, see the `bookmarks-file' mode slot."
    (style (theme:themed-css (theme *browser*)
             ("summary"
              :background-color theme:secondary
-             :color theme:background
+             :color theme:on-secondary
              :font-size "14px"
              :padding "12px"
              :margin "6px"
@@ -73,9 +73,9 @@ Bookmarks can be persisted to disk, see the `bookmarks-file' mode slot."
              :margin-left "8px")
             ;; Taken from buffer.lisp to save space for big bookmark lists.
             (button
-             :display "inline-block"
              :background-color theme:secondary
-             :color theme:background
+             :color theme:on-secondary
+             :display "inline-block"
              :text-decoration "none"
              :border-radius "2px"
              :padding "6px"

source/mode/diff.lisp
@@ -11,17 +11,17 @@
   ((rememberable-p nil)
    (style (theme:themed-css (theme *browser*)
             (".nyxt-diff-insert"
-             :text-decoration "none"
-             :background-color "#bbeabb")
+             :background-color "#bbeabb"
+             :text-decoration "none")
             ("ins.nyxt-diff-replace"
-             :text-decoration "none"
-             :background-color "#bbeabb")
+             :background-color "#bbeabb"
+             :text-decoration "none")
             (".nyxt-diff-delete"
-             :text-decoration "none"
-             :background-color "#efcbcf")
+             :background-color "#efcbcf"
+             :text-decoration "none")
             ("del.nyxt-diff-replace"
-             :text-decoration "none"
-             :background-color "#efcbcf"))
+             :background-color "#efcbcf"
+             :text-decoration "none"))
           :documentation "Diff colours for its visual representation.
 They're based on the modus-operandi theme by Protesilaos Stavrou, which follows
 the highest standard on accessibility."))

source/mode/document.lisp
@@ -563,7 +563,7 @@ of buffers."
                           :left "0"
                           :right "0"
                           :bottom "0"
-                          :background theme:text
+                          :background theme:on-background
                           :z-index #.(1- (expt 2 31)))))
         (selection-rectangle-style (theme:themed-css (theme *browser*)
                                      ("#nyxt-rectangle-selection"
@@ -572,8 +572,8 @@ of buffers."
                                       :left "0"
                                       :border-style "dotted"
                                       :bordeer-width "1px"
-                                      :border-color theme:text
-                                      :background-color theme:text
+                                      :border-color theme:on-background
+                                      :background-color theme:on-background
                                       :opacity 0.05
                                       :z-index #.(1- (expt 2 30))))))
     (pflet ((add-overlay (overlay-style selection-rectangle-style)

source/mode/hint.lisp
@@ -16,8 +16,8 @@
    (box-style (theme:themed-css (theme *browser*)
                 (".nyxt-hint"
                  :background-color theme:primary
+                 :color theme:on-primary
                  :opacity 0.8
-                 :color theme:background
                  :font-weight "bold"
                  :padding "0px 3px 0px 3px"
                  :border-radius "2px"
@@ -26,7 +26,7 @@
    (highlighted-box-style (theme:themed-css (theme *browser*)
                             (".nyxt-hint.nyxt-highlight-hint"
                              :background-color theme:accent
-                             :color theme:background))
+                             :color theme:on-accent))
                           :documentation "The style of highlighted boxes, e.g. link hints.")
 
    (hints-alphabet "ABCDEFGHIJKLMNOPQRSTUVWXYZ"

source/mode/history-tree.lisp
@@ -9,60 +9,59 @@
   "Mode for history-tree listing."
   ((visible-in-status-p nil)
    (rememberable-p nil)
-   (style
-    (theme:themed-css (theme *browser*)
-      (body
-       :color theme:text
-       :background theme:background
-       :line-height "initial")
-      (* :margin 0
-         :padding 0
-         :list-style "none")
-      (".current-buffer a"
-       :color theme:text)
-      (".current-buffer a:hover"
-       :color theme:primary)
-      (".other-buffer a"
-       :color theme:primary)
-      (".other-buffer a:hover"
-       :color theme:secondary)
-      (li
-       :white-space "nowrap")
-      ("ul li"
-       :margin-left "15px"
-       :position "relative"
-       :padding-left "5px")
-      ("ul li::before"
-       :content "' '"
-       :position "absolute"
-       :width "1px"
-       :background-color theme:text
-       :color theme:background
-       :top "5px"
-       :bottom "-12px"
-       :left "-10px")
-      ("body > ul > li:first-child::before"
-       :top "12px")
-      ("ul li:not(:first-child):last-child::before"
-       :display "none")
-      ("ul li:only-child::before"
-       :display "list-item"
-       :content "' '"
-       :position "absolute"
-       :width "1px"
-       :background-color theme:text
-       :color theme:background
-       :top "5px"
-       :bottom "7px"
-       :height "7px"
-       :left "-10px")
-      ("ul li::after"
-       :content "' '"
-       :position "absolute"
-       :left "-10px"
-       :width "10px"
-       :height "1px"
-       :background-color theme:text
-       :color theme:background
-       :top "12px"))))
+   (style (theme:themed-css (theme *browser*)
+            (* :margin 0
+               :padding 0
+               :list-style "none")
+            (body
+             :background theme:background
+             :color theme:on-background
+             :line-height "initial")
+            (".current-buffer a"
+             :color theme:on-background)
+            (".current-buffer a:hover"
+             :opacity 0.5)
+            (".other-buffer a"
+             :color theme:primary)
+            (".other-buffer a:hover"
+             :opacity 0.5)
+            (li
+             :white-space "nowrap")
+            ("ul li"
+             :margin-left "15px"
+             :position "relative"
+             :padding-left "5px")
+            ("ul li::before"
+             :content "' '"
+             :position "absolute"
+             :width "1px"
+             :background-color theme:on-background ; is this right???
+             :color theme:background
+             :top "5px"
+             :bottom "-12px"
+             :left "-10px")
+            ("body > ul > li:first-child::before"
+             :top "12px")
+            ("ul li:not(:first-child):last-child::before"
+             :display "none")
+            ("ul li:only-child::before"
+             :display "list-item"
+             :content "' '"
+             :position "absolute"
+             :width "1px"
+             :background-color theme:on-background ; is this right???
+             :color theme:background
+             :top "5px"
+             :bottom "7px"
+             :height "7px"
+             :left "-10px")
+            ("ul li::after"
+             :content "' '"
+             :position "absolute"
+             :left "-10px"
+             :width "10px"
+             :height "1px"
+             :background-color theme:on-background ; is this right?
+             :color theme:background
+             :top "12px"))))
   (:toggler-command-p nil))

commit 598561e5e2e39f2f2d5e1c8e1fca806ea2ed3325

Fix progress bar styling.
Bug introduced in commit 4009036bed2ebea8783041990b8827533dfb1c25, when
refactoring the theme library.

source/buffer.lisp
@@ -732,10 +732,10 @@ store them somewhere and `ffi-buffer-delete' them once done."))
               :height "20px"
               :width "100%")
             `(".progress-bar-base"
-              :background-color ,theme:primary
+              :background-color ,theme:secondary
               :height "100%")
             `(".progress-bar-fill"
-              :background-color ,theme:secondary
+              :background-color ,theme:primary
               :height "100%"))))
   (:export-class-name-p t)
   (:export-accessor-names-p t)

source/mode/download.lisp
@@ -187,10 +187,10 @@ appearance in the buffer when they are setf'd."
         :height "20px"
         :width "100%")
       `(".progress-bar-base"
-        :background-color ,theme:primary
+        :background-color ,theme:secondary
         :height "100%")
       `(".progress-bar-fill"
-        :background-color ,theme:secondary
+        :background-color ,theme:primary
         :height "100%"))))
   (:toggler-command-p nil))
 