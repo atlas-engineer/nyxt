01dda2b92c71fb08c549368943427c41fa642565
John Mercouris <john@mercouris.email>
2023-08-02 21:29:50 +0300

    global.lisp: Remove unused variable.

--- source/global.lisp
+++ source/global.lisp
@@ -178,4 +178,4 @@ Return nil on error."
     (when (and commits (not (zerop commits)))
       (push-feature "UNSTABLE"))))
 
-(defvar +logo-svg+ (alexandria:read-file-into-string (asdf:system-relative-pathname :nyxt "assets/nyxt.svg")))
+
