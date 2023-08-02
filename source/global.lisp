846983b0bdd818f06b5570b4c3c413e500a8fd3d
John Mercouris <john@mercouris.email>
2023-08-02 23:08:28 +0300

    global.lisp: Load static files into *static-data*.

--- source/global.lisp
+++ source/global.lisp
@@ -178,4 +178,13 @@ Return nil on error."
     (when (and commits (not (zerop commits)))
       (push-feature "UNSTABLE"))))
 
+(export-always '*static-data*)
+(defvar *static-data* (make-hash-table :test 'equal)
+  "Static data for usage in Nyxt.")
 
+;; Load all static files into the Lisp Image.
+(mapcar (lambda (static-file)
+          (setf (gethash (asdf:component-name static-file) *static-data*)
+                (alexandria:read-file-into-byte-vector
+                 (asdf:component-pathname static-file))))
+        (nyxt/static:collect-static-file-components :nyxt/assets))
