84ee1b280adcf090a8b8e5a0540589faa60e2736
John Mercouris <john@mercouris.email>
2023-09-14 16:46:12 -0400

    *.lisp: Use new pretty-binding-keys function.

--- source/input.lisp
+++ source/input.lisp
@@ -18,8 +18,8 @@
          (keymaps (cons (override-map buffer)
                         (delete nil (mapcar #'keymap modes)))))
     (unwind-protect
-         (or (first (keymaps:binding-keys fn keymaps))
-             "UNBOUND")
+         (or (first (keymaps:pretty-binding-keys fn keymaps :print-style (keymaps:name (keyscheme buffer))))
+             "unbound")
       (unless current-buffer
         (buffer-delete buffer)))))
 
