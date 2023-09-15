85d6c6cbfc66b14e5c5041f7dabfd87760ebd2d8
Andre A. Gomes <andremegafone@gmail.com>
2023-09-15 16:59:44 +0300

    buffer: Tweak message buffer style.
    The font size is set according to the height of the message buffer.

--- source/buffer.lisp
+++ source/buffer.lisp
@@ -1022,7 +1022,8 @@ Also see `panel-page'."))
             `(body
               :background-color ,theme:background-alt
               :color ,theme:on-background-alt
-              :font-size "12px"
+              :font-size "75vh"
+              :line-height "100vh"
               :padding 0
               :padding-left "4px"
               :margin 0))))
