atlas-engineer/nyxt

commit f38fec13d71feea3c411457f5305ee0531872c66

mode/annotate..cruise-control: Document a fraction of modes.
This also:
- Renames nyxt/bookmark-frequent-visits-mode package to
  nyxt/mode/bookmark-frequent-visits.
- Purges the signature of set-url-from-bookmark.
- Changes add-domain-to-certificate-exceptions arglist to &key for
  consistency
- Changes the syntax of define-configuration in examples in the
  respective modes to keyword class designators (buffer -> :buffer)
  for ease of configuration.

source/mode/bookmark.lisp
@@ -2,7 +2,15 @@
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
 (nyxt:define-package :nyxt/mode/bookmark
-    (:documentation "Manage bookmarks."))
+  (:documentation "Manage bookmarks.
+The main object is `bookmark-entry'. The main function to add a bookmark is
+`bookmark-add'. Commands utilizing it are:
+- `bookmarks-panel' (panel command).
+- `list-bookmarks' (internal page).
+- `bookmark-current-url', `bookmark-buffer-url', `bookmark-url', and
+  `bookmark-hint' to add bookmarks. `delete-bookmark' to remove.
+- `set-url-from-bookmark'.
+- `import-bookmarks-from-html'."))
 (in-package :nyxt/mode/bookmark)
 
 ;;; We don't use CL-prevalence to serialize / deserialize bookmarks for a couple for reasons:
@@ -20,7 +28,14 @@
 (export-always 'bookmark-mode)
 (define-mode bookmark-mode ()
   "Manage bookmarks.
-Bookmarks can be persisted to disk, see the `bookmarks-file' mode slot."
+Bookmarks can be persisted to disk, see the `bookmarks-file' mode slot.
+
+- `bookmark-current-url', `bookmark-buffer-url', `bookmark-url', and
+  `bookmark-hint' to add bookmarks. `delete-bookmark' to remove them.
+- `set-url-from-bookmark' to open a bookmark.
+- `list-bookmarks' to list bookmarks as a separate page.
+- `bookmarks-panel' to show all bookmarks as a panel.
+- `import-bookmarks-from-html' to import the HTML-formatted bookmarks."
   ((visible-in-status-p nil)
    (bookmarks-file
     (make-instance 'bookmarks-file)
@@ -206,7 +221,7 @@ In particular, we ignore the protocol (e.g. HTTP or HTTPS does not matter)."
       (tags existing))))
 
 (define-command bookmark-current-url (&optional (buffer (current-buffer)))
-  "Bookmark the URL of BUFFER."
+  "Bookmark the URL of the current BUFFER."
   (if (url-empty-p (url buffer))
       (echo "Buffer has no URL.")
       (let ((tags (prompt
@@ -232,7 +247,7 @@ In particular, we ignore the protocol (e.g. HTTP or HTTPS does not matter)."
         (echo "Bookmarked ~a." (render-url (url buffer))))))
 
 (define-command bookmark-buffer-url ()
-  "Bookmark the currently opened page(s) in the active buffer."
+  "Bookmark the page(s) currently opened in the existing buffers."
   (prompt
    :prompt "Bookmark URL from buffer(s)"
    :sources (make-instance 'buffer-source
@@ -242,8 +257,7 @@ In particular, we ignore the protocol (e.g. HTTP or HTTPS does not matter)."
 (define-command bookmark-url
     (&key (url (ignore-errors (quri:uri (prompt1
                                          :prompt "Bookmark URL"
-                                         :sources (make-instance 'prompter:raw-source
-                                                                 :name "New URL"))))))
+                                         :sources (url-sources (current-buffer) (compose #'url #'identity)))))))
   "Prompt for a URL to bookmark."
   (if (not (valid-url-p url))
       (echo "Invalid URL '~a'" url)
@@ -260,8 +274,8 @@ In particular, we ignore the protocol (e.g. HTTP or HTTPS does not matter)."
         (bookmark-add url :tags tags :title title))))
 
 (define-command delete-bookmark (&optional urls-or-bookmark-entries)
-  "Delete bookmark(s) matching URLS-OR-BOOKMARK-ENTRIES.
-URLS is either a list or a single element."
+  "Delete bookmark(s) matching the chosen URLS-OR-BOOKMARK-ENTRIES.
+URLS-OR-BOOKMARK-ENTRIES could be a list or a single URL/`bookmark-entry'."
   (if urls-or-bookmark-entries
       (files:with-file-content (bookmarks (bookmarks-file (current-buffer)))
         (setf bookmarks
@@ -279,29 +293,30 @@ URLS is either a list or a single element."
                                               :enable-marks-p t))))
         (delete-bookmark entries))))
 
-(define-command set-url-from-bookmark
-    (&key (actions-on-return
-           (list #'buffer-load*
-                 (lambda-command new-buffer-load (suggestion-values)
-                   "Load bookmark(s) in new buffer(s)."
-                   (mapc (lambda (url) (make-buffer :url (url url))) (rest suggestion-values))
-                   (make-buffer-focus :url (url (first suggestion-values))))
-                 (lambda-command copy-url* (suggestions)
-                   "Copy bookmark URL."
-                   (trivial-clipboard:text (render-url (url (first suggestions)))))
-                 'delete-bookmark)))
+(define-command set-url-from-bookmark ()
   "Set the URL for the current buffer from a bookmark.
-With marks, open the first bookmark in the current buffer, the rest in
-background buffers."
+With marks, open the first bookmark in the current buffer, and the rest in other
+buffers in the background."
   (prompt
    :prompt "Open bookmark(s)"
-   :sources (make-instance 'bookmark-source
-                           :actions-on-return actions-on-return)))
+   :sources (make-instance
+             'bookmark-source
+             :actions-on-return
+             (list #'buffer-load*
+                   (lambda-command new-buffer-load (suggestion-values)
+                     "Load bookmark(s) in new buffer(s)."
+                     (mapc (lambda (url) (make-buffer :url (url url))) (rest suggestion-values))
+                     (make-buffer-focus :url (url (first suggestion-values))))
+                   (lambda-command copy-url* (suggestions)
+                     "Copy bookmark URL."
+                     (trivial-clipboard:text (render-url (url (first suggestions)))))
+                   'delete-bookmark))))
 
 (export-always 'list-bookmarks)
 (define-internal-page-command-global list-bookmarks ()
     (bookmarks-buffer "*Bookmarks*")
-  "List all bookmarks in a new buffer."
+  "List all bookmarks in a new buffer.
+Splits bookmarks into groups by tags."
   (let ((bookmarks (group-bookmarks bookmarks-buffer)))
     (spinneret:with-html-string
       (:nstyle (style (find-submode 'bookmark-mode (current-buffer)))) ; TODO: Make sure this is the right buffer
@@ -413,7 +428,7 @@ background buffers."
                        :sources (make-instance
                                  'nyxt/mode/file-manager:file-source
                                  :extensions '("html")))))
-  "Import bookmarks from an HTML-FILE."
+  "Import bookmarks from an HTML-FILE with bookmarks from other browsers."
   (if (and (uiop:file-exists-p html-file)
            (equal (pathname-type html-file) "html"))
       (with-open-file (in-html html-file :external-format :utf-8)

source/mode/annotate.lisp
@@ -3,12 +3,29 @@
 
 (nyxt:define-package :nyxt/mode/annotate
   (:documentation "Mode to annotate documents.
-Annotations are persisted to disk."))
+
+Important pieces of functionality are (together with an obvious
+`annotate-mode'):
+- `annotation' and its subclasses: `url-annotation' and `snippet-annotation'.
+- Commands:
+  - `annotate-current-url'.
+  - `annotate-highlighted-text'.
+  - `show-annotation'.
+  - `show-annotations'.
+  - `show-annotations-for-current-url'."))
 (in-package :nyxt/mode/annotate)
 
 (define-mode annotate-mode ()
   "Annotate document with arbitrary comments.
-Annotations are persisted to disk, see the `annotations-file' mode slot."
+Annotations are persisted to disk, see the `annotations-file' mode slot.
+
+Commands are:
+- `annotate-current-url' and `annotate-highlighted-text' to create
+  annotations.
+- `show-annotation' and `show-annotations' to show the annotations
+  unconditionally.
+- And `show-annotations-for-current-url' to show the ones associated with the
+  current page."
   ((visible-in-status-p nil)
    (annotations-file
     (make-instance 'annotations-file)
@@ -33,18 +50,26 @@ Annotations are persisted to disk, see the `annotations-file' mode slot."
     :type (list-of string))
    (date (time:now)))
   (:export-class-name-p t)
-  (:export-accessor-names-p t))
+  (:export-accessor-names-p t)
+  (:documentation "An umbrella annotation type.
+Should not be instantiated on its own. Instead, use `url-annotation' and
+`snippet-annotation'."))
 
 (define-class url-annotation (annotation)
   ((url nil)
    (page-title ""))
   (:export-class-name-p t)
-  (:export-accessor-names-p t))
+  (:export-accessor-names-p t)
+  (:documentation "Annotation for a page with a certain URL.
+Command to create one is `annotate-current-url'."))
 
 (define-class snippet-annotation (url-annotation)
   ((snippet nil :documentation "The snippet of text being annotated."))
   (:export-class-name-p t)
-  (:export-accessor-names-p t))
+  (:export-accessor-names-p t)
+  (:documentation "Annotation in relation to a text on a certain page.
+The page is handled by underlying `url-annotation', while the snippet is
+extracted by `annotate-highlighted-text' command."))
 
 ;; TODO: Wrap in <div>s with special CSS classes.
 (defmethod render ((annotation url-annotation))

source/mode/autofill.lisp
@@ -2,7 +2,18 @@
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
 (nyxt:define-package :nyxt/mode/autofill
-    (:documentation "Mode to fill forms more rapidly."))
+  (:documentation "Mode to fill forms more rapidly.
+
+The whole API is centered around `autofill' class and its slots:
+- `name' as the descriptive name of the autofill. Accessed with `autofill-name'.
+- and `fill' as the actual content-designating thing (string or a
+  function). Accessed with `autofill-fill'.
+
+`autofill' is funcallable. When funcallable, returns the string produced by the
+autofill.
+
+Then, the command eponymously called `autofill' actually fills (with
+`ffi-buffer-paste') the contents into the page."))
 (in-package :nyxt/mode/autofill)
 
 (export-always 'make-autofill)
@@ -10,7 +21,7 @@
   (apply #'make-instance 'autofill args))
 
 (define-mode autofill-mode ()
-  "Mode to fill forms more rapidly."
+  "Mode to fill forms more rapidly (with `autofill' command)."
   ((visible-in-status-p nil)
    (rememberable-p t)
    (autofills

source/mode/blocker.lisp
@@ -2,7 +2,15 @@
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
 (nyxt:define-package :nyxt/mode/blocker
-    (:documentation "Block resource queries for listed hosts."))
+  (:documentation "Block resource queries for listed hosts.
+`blocker-mode' relies on:
+- `hostlist' as the hostlist representation.
+- `*default-hostlist*' as the most reliable hostlist.
+- `load-hostlists' as the function forcing hostlist update and the user-space
+  `update-hostlists' commands relying on it.
+
+- `request-resource-block' as the hook handler that does all the automagic
+  blocking."))
 (in-package :nyxt/mode/blocker)
 
 ;; TODO: Add convenient interface to block hosts depending on the current URL.
@@ -40,7 +48,7 @@ See the `hostlist' class documentation."
 (define-mode blocker-mode ()
   "Enable blocking of listed hosts.
 To customize the list of blocked hosts, set the `hostlists' slot.
-See the `hostlist' class documentation.
+To force hostlist update, use `update-hostlists'.
 
 Example:
 
@@ -54,7 +62,7 @@ Example:
   \"Blocker mode with custom hosts from `*my-blocked-hosts*'.\"
   ((nyxt/mode/blocker:hostlists (list *my-blocked-hosts* nyxt/mode/blocker:*default-hostlist*))))
 
-\(define-configuration buffer
+\(define-configuration :buffer
   ((default-modes (append '(my-blocker-mode) %slot-default%))))"
   ((hostlists (list *default-hostlist*))
    (blocked-hosts

source/mode/bookmark-frequent-visits.lisp
@@ -1,14 +1,23 @@
 ;;;; SPDX-FileCopyrightText: Atlas Engineer LLC
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
-(nyxt:define-package :nyxt/bookmark-frequent-visits
-    (:documentation "Mode to bookmark frequently visited URLs."))
-(in-package :nyxt/bookmark-frequent-visits)
+(nyxt:define-package :nyxt/mode/bookmark-frequent-visits
+  (:documentation "Mode to bookmark frequently visited URLs.
+The substance of it is `bookmark-frequent-visit' function."))
+(in-package :nyxt/mode/bookmark-frequent-visits)
 
 (define-mode bookmark-frequent-visits-mode ()
-  "Mode to bookmark frequently visited URLs while navigating the web."
+  "Mode to bookmark frequently visited URLs while navigating the web.
+Does not need any setup and commands: it bookmarks the visited pages once they
+hit a `threshold'. To change it to a different threshold (e.g. 50), do
+
+\(define-configuration :bookmark-frequent-visits-mode
+  \"Change the threshold of `bookmark-frequent-visits-mode' to not clutter bookmarks too much.\"
+  ((threshold 50)))"
   ((visible-in-status-p nil)
-   (threshold 20)))
+   (threshold
+    20
+    :documentation "The number of hits after which to ")))
 
 (defmethod enable ((mode bookmark-frequent-visits-mode) &key)
   (nyxt:on-signal-load-finished mode (url (current-buffer))))

source/mode/bookmarklets.lisp
@@ -8,13 +8,17 @@
 ;;;; here: https://www.squarefree.com/bookmarklets/copyright.html
 
 (nyxt:define-package :nyxt/mode/bookmarklets
-  (:documentation "Easily create 'bookmarklets' (JavaScript snippets) to alter
-the content of HTML pages."))
+  (:documentation "Collection of 'bookmarklets' (JavaScript snippets) to interact with web pages.
+
+All bookmarklets are defined with `define-bookmarklet-command' and
+`define-bookmarklet-command-global'. All the defined bookmarklets are `command's
+and also global functions. Which means: they can have `:around' and other
+qualified methods to modify their behavior."))
 (in-package :nyxt/mode/bookmarklets)
 
 (define-mode bookmarklets-mode ()
   "Mode for 'bookmarklets' commands.
-By default, this mode does nothing but expose the default bookmarklets."
+By default, this mode does nothing but expose the default bookmarklet commands."
   ((visible-in-status-p nil)))
 
 (sera:eval-always

source/mode/buffer-listing.lisp
@@ -2,21 +2,24 @@
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
 (nyxt:define-package :nyxt/mode/buffer-listing
-    (:documentation "Mode for buffer-listings"))
+  (:documentation "Mode for buffer listing.
+See `buffer-listing-mode'."))
 (in-package :nyxt/mode/buffer-listing)
 
 (define-mode buffer-listing-mode ()
-  "Mode for buffer-listing."
+  "Mode for buffer-listing.
+Hosts `list-buffers' page and `buffers-panel' panel."
   ((visible-in-status-p nil))
   (:toggler-command-p nil))
 
 (define-internal-page-command-global list-buffers (&key (cluster nil)
                                                   linear-view-p) ; TODO: Document `cluster'.
     (listing-buffer "*Buffers*" 'nyxt/mode/buffer-listing:buffer-listing-mode)
-  "Show a buffer listing all buffer trees.
-Buffers have relationships.  When a buffer is spawned from another one (e.g. by
-middle-clicking on a link), the new buffer is a child buffer.
-This kind of relationships creates 'trees' of buffers.
+  "Show all buffers and their interrelations.
+
+When a buffer is spawned from another one (e.g. by middle-clicking on a link),
+the new buffer is a child buffer. This kind of relationships creates 'trees' of
+buffers.
 
 With LINEAR-VIEW-P, list buffers linearly instead."
   (labels ((cluster-buffers ()

source/mode/certificate-exception.lisp
@@ -2,7 +2,10 @@
 ;;;; SPDX-License-Identifier: BSD-3-Clause
 
 (nyxt:define-package :nyxt/mode/certificate-exception
-    (:documentation "Control which invalid certificates to accept or reject."))
+  (:documentation "Control which invalid certificates to accept or reject.
+Important parts are:
+- `*default-certificate-exceptions*' variable.
+- and `add-domain-to-certificate-exceptions' command."))
 (in-package :nyxt/mode/certificate-exception)
 
 (export-always '*default-certificate-exceptions*)
@@ -12,7 +15,7 @@ See the `add-domain-to-certificate-exceptions' command.")
 
 (define-mode certificate-exception-mode ()
   "Control which invalid certificates to accept or reject.
-This can apply to specific buffers.
+This applies to specific buffers.
 See the `add-domain-to-certificate-exceptions' command."
   ((visible-in-status-p nil)
    (certificate-exceptions *default-certificate-exceptions*
@@ -24,7 +27,7 @@ See the `add-domain-to-certificate-exceptions' command."
 (defmethod disable ((mode certificate-exception-mode) &key)
   (setf (certificate-exceptions (buffer mode)) nil))
 
-(define-command add-domain-to-certificate-exceptions (&optional (buffer (current-buffer)))
+(define-command add-domain-to-certificate-exceptions (&key (buffer (current-buffer)))
   "Add the current hostname to the buffer's certificate exception list.
 This is only effective if `certificate-exception-mode' is enabled.
 

commit dbc7b952668891cb3f92b66a7898b93f7f31b7cf

mode/bookmark: Use custom sources in `bookmark-url` prompt.
Fix bug introduced in f38fec13d71feea3c411457f5305ee0531872c66.

source/mode/bookmark.lisp
@@ -261,9 +261,17 @@ against date, given `prompter:active-attributes-keys' configuration."))
                            :actions-on-return (lambda-mapped-command bookmark-current-url))))
 
 (define-command bookmark-url
-    (&key (url (ignore-errors (quri:uri (prompt1
-                                         :prompt "Bookmark URL"
-                                         :sources (url-sources (current-buffer) (compose #'url #'identity)))))))
+    (&key (url (prompt1
+                :prompt "Bookmark URL"
+                :sources (list
+                          (make-instance 'new-url-or-search-source
+                                         :actions-on-return (lambda-mapped-command url))
+                          (make-instance 'buffer-source
+                                         :actions-on-return (lambda-mapped-command url))
+                          (make-instance 'global-history-source
+                                         :actions-on-return (lambda-mapped-command url))
+                          (make-instance 'bookmark-source
+                                         :actions-on-return (lambda-mapped-command url))))))
   "Prompt for a URL to bookmark."
   (if (not (valid-url-p url))
       (echo "Invalid URL '~a'" url)