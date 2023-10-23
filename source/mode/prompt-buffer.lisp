atlas-engineer/nyxt

commit 5d4d7fa8c070f2440aaa5f918d15dae12ba47495

prompt-buffer.lisp: add button to toggle attributes for source

source/prompt-buffer.lisp
@@ -473,6 +473,11 @@ This does not redraw the whole prompt buffer, unlike `prompt-render'."
                                            (sera:single sources))
                                       "display:none;"
                                       "display:revert")
+                           (:nbutton
+                             :text "⚙"
+                             :title "Toggle columns"
+                             :buffer prompt-buffer
+                             (funcall (sym:resolve-symbol :toggle-attributes-display :command)))
                            (prompter:name source)
                            (if (prompter:hide-suggestion-count-p source)
                                ""

commit 6cb941d5cffcb88a307a3980896759ea8c71f619

Add optional source argument to `toggle-attributes-display`.
Make the new argument optional and default to `(current-source prompt-buffer)`.
Pass source to `toggle-attributes-display` button in
`prompt-render-suggestions`.

Fixes bug introduced in 5d4d7fa8c070f2440aaa5f918d15dae12ba47495.

source/mode/prompt-buffer.lisp
@@ -303,20 +303,18 @@ current unmarked suggestion."
                 :test #'equal)
         suggestion-values)))
 
-(define-command-prompt toggle-attributes-display (prompt-buffer)
+(define-command-prompt toggle-attributes-display (prompt-buffer
+                                                  &key (source (current-source prompt-buffer)))
   "Prompt for which prompter attributes to display."
   (let ((attributes (prompt :prompt "Mark attributes to display"
-                            :sources (make-instance
-                                      'attribute-source
-                                      :marks (intersection
-                                              (prompter:active-attributes-keys
-                                               (current-source prompt-buffer))
-                                              (prompter:attributes-keys-non-default
-                                               (current-source prompt-buffer))
-                                              :test #'string=)
-                                      :constructor (prompter:attributes-keys-non-default
-                                                    (current-source prompt-buffer))))))
-    (setf (prompter:active-attributes-keys (current-source prompt-buffer))
+                            :sources
+                            (make-instance
+                             'attribute-source
+                             :marks (intersection (prompter:active-attributes-keys source)
+                                                  (prompter:attributes-keys-non-default source)
+                                                  :test #'string=)
+                             :constructor (prompter:attributes-keys-non-default source)))))
+    (setf (prompter:active-attributes-keys source)
           attributes)
     (prompt-render-suggestions prompt-buffer)))
 

source/prompt-buffer.lisp
@@ -523,7 +523,8 @@ This does not redraw the whole prompt buffer, use `prompt-render' for that."
                              :text "⚙"
                              :title "Toggle attributes display"
                              :buffer prompt-buffer
-                             '(funcall (sym:resolve-symbol :toggle-attributes-display :command)))
+                             `(funcall (sym:resolve-symbol :toggle-attributes-display :command)
+                                       :source ,source))
                            (prompter:name source)
                            (if (prompter:hide-suggestion-count-p source)
                                ""