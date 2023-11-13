atlas-engineer/nyxt

commit 017eed79c241b8a2c4fa0a0cbb56c56446a45f47

Add ffi-buffer-cookie-policy.

source/browser.lisp
@@ -1053,6 +1053,7 @@ sometimes yields the wrong reasult."
 (define-ffi-method ffi-inspector-show ((buffer buffer)))
 (define-ffi-method ffi-print-status ((window window) text))
 (define-ffi-method ffi-print-message ((window window) message))
+(define-ffi-method ffi-buffer-cookie-policy ((buffer buffer) value))
 
 (define-class-type browser)
 (declaim (type (browser-type) *browser-class*))

source/renderer-gtk.lisp
@@ -724,6 +724,18 @@ requested a reload."
        (ps:ps (setf (ps:@ document Body |innerHTML|)
                     (ps:lisp text)))))))
 
+(defmethod ffi-buffer-cookie-policy ((buffer gtk-buffer) value)
+  "VALUE is one of`:always', `:never' or `:no-third-party'."
+  (let* ((context (webkit:webkit-web-view-web-context (gtk-object buffer)))
+         (cookie-manager (webkit:webkit-web-context-get-cookie-manager context)))
+    (webkit:webkit-cookie-manager-set-accept-policy
+     cookie-manager
+     (match value
+       (:accept :webkit-cookie-policy-accept-always)
+       (:never :webkit-cookie-policy-accept-never)
+       (:no-third-party :webkit-cookie-policy-accept-no-third-party)))
+    buffer))
+
 (defstruct webkit-history-entry
   title
   uri

commit 6c7de66516c331187087e3d35d55ca690097b5a7

Fix cookie-policy valid values.
Bug introduced in 017eed79c241b8a2c4fa0a0cbb56c56446a45f47.

Closes #3251.

Note that the facilities that rely on ffi-buffer-cookie-policy are implemented
in a non renderer-agnostic fashion.

source/browser.lisp
@@ -204,8 +204,7 @@ You can store and restore sessions manually to various files with
     :no-third-party
     :type cookie-policy
     :documentation "Cookie policy of new buffers.
-Must be one of `:always' (accept all cookies), `:never' (reject all cookies),
-`:no-third-party' (accept cookies for current website only).")
+Valid values are `:accept', `:never' and `:no-third-party'.")
    ;; Hooks follow:
    (after-init-hook
     (make-instance 'hook-browser)

source/foreign-interface.lisp
@@ -270,10 +270,8 @@ The `style' of the `message-buffer' is honored."
 In particular, this should understand Punycode.")
 
 (define-ffi-generic ffi-buffer-cookie-policy (buffer)
-  "Return the cookie 'accept' policy, one of of`:always',
-`:never' or `:no-third-party'.
-
-Setf-able with the same aforementioned values."
+  "Return the cookie policy.
+Setf-able.  Valid values are determined by the `cookie-policy' type."
   (:setter-p t))
 
 (define-ffi-generic ffi-preferred-languages (buffer)

source/help.lisp
@@ -349,11 +349,11 @@ to the next."
                  :class-name 'browser
                  :slot 'default-cookie-policy
                  :slot-value :no-third-party))
-              '(:always "Always accept"
+              '(:accept "Always accept"
                 (nyxt::auto-configure
                  :class-name 'browser
                  :slot 'default-cookie-policy
-                 :slot-value :always))
+                 :slot-value :accept))
               '(:never "Never accept"
                 (nyxt::auto-configure
                  :class-name 'browser

source/renderer/gtk.lisp
@@ -2096,17 +2096,19 @@ custom (the specified proxy) and none."
                  callback
                  (cffi:null-pointer))))))
         (calispel:? result-channel))))
-(defmethod (setf ffi-buffer-cookie-policy) (cookie-policy (buffer gtk-buffer))
-  "VALUE is one of`:always', `:never' or `:no-third-party'."
+(defmethod (setf ffi-buffer-cookie-policy) (value (buffer gtk-buffer))
+  "Set the cookie policy to VALUE.
+Valid values are determined by the `cookie-policy' type."
   (let* ((context (webkit:webkit-web-view-web-context (gtk-object buffer)))
          (cookie-manager (webkit:webkit-web-context-get-cookie-manager context)))
-    (setf (ffi-buffer-cookie-policy cookie-manager) cookie-policy)
+    (setf (ffi-buffer-cookie-policy cookie-manager) value)
     buffer))
-(defmethod (setf ffi-buffer-cookie-policy) (cookie-policy (cookie-manager webkit:webkit-cookie-manager))
-  "VALUE is one of`:always', `:never' or `:no-third-party'."
+(defmethod (setf ffi-buffer-cookie-policy) (value (cookie-manager webkit:webkit-cookie-manager))
+  "Set the cookie policy to VALUE.
+Valid values are determined by the `cookie-policy' type."
   (webkit:webkit-cookie-manager-set-accept-policy
    cookie-manager
-   (match cookie-policy
+   (match value
      (:accept :webkit-cookie-policy-accept-always)
      (:never :webkit-cookie-policy-accept-never)
      (:no-third-party :webkit-cookie-policy-accept-no-third-party))))

source/types.lisp
@@ -42,14 +42,10 @@ Unlike `(cons TYPE *)', it checks all the elements.
   "Alist of dotted conses."
   `(list-of (cons ,key-type ,(or value-type key-type))))
 
-;; FIXME: Maybe define it at use site, or even inline it there?
 (export-always 'cookie-policy)
 (deftype cookie-policy ()
-  "Enum of cookie policies:
-- allow :ALWAYS,
-- allow :NEVER,
-- allow all, but :NO-THIRD-PARTY."
-  `(member :always :never :no-third-party))
+  "The type of cookie policy to apply."
+  `(member :accept :never :no-third-party))
 
 ;; The following types represent the positional arguments documented at
 ;; https://developer.mozilla.org/en-US/docs/Web/API/Selection/modify#parameters