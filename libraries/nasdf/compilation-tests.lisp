97f7556eab25f5727e75a7cb8f917afe6ff90c09
Andre A. Gomes <andremegafone@gmail.com>
2023-12-14 21:50:48 +0200

    libraries/nasdf/compilation-tests: Fix logic behind subpackages.
    The previous implementation relied on the notion of a proper subpcakage.  In the
    new implementation, a package is a subpackage of itself.
    
    From this results that, when checking for unbound exports or undocumented
    exports, the targeted package is checked too.

--- libraries/nasdf/compilation-tests.lisp
+++ libraries/nasdf/compilation-tests.lisp
@@ -69,15 +69,12 @@ The logic is:
                        (gethash s (symbol-value (find-symbol "*MACRO-TOPLEVEL*" :parenscript)))))
         (push s result)))))
 
-(defun subpackage-p (subpackage package)
-  "Return non-nil if SUBPACKAGE is a sub-package of PACKAGE.
-A sub-package has a name that starts with that of PACKAGE followed by a '/' separator."
-  (not (null
-        (uiop:string-prefix-p (uiop:strcat (package-name package) "/")
-                              (package-name subpackage)))))
-
 (defun list-subpackages (package)
-  (remove-if (lambda (pkg) (not (subpackage-p pkg package))) (list-all-packages)))
+  "Return a list of sub-packages of PACKAGE.
+The sub-package relationship is determined by whether a common prefix exists."
+  (remove-if (lambda (pkg) (not (uiop:string-prefix-p (package-name package) pkg)))
+             (list-all-packages)
+             :key #'package-name))
 
 (defun list-undocumented-exports (package)
   (let ((result '())
