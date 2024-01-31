6b33f00a6a84daef2447b58ad5aaa326f04de659
Andre A. Gomes <andremegafone@gmail.com>
2024-01-31 15:17:51 +0200

    mode/plaintext-editor: Review documentation.

--- source/mode/plaintext-editor.lisp
+++ source/mode/plaintext-editor.lisp
@@ -6,12 +6,8 @@
 (define-mode plaintext-editor-mode (editor-mode)
   "Mode for basic plaintext editing.
 
-It renders the file in a single textarea HTML element.
-
-To enable it, add this to your configuration file:
-
-\(define-configuration nyxt/mode/editor::editor-buffer
-  ((default-modes (cons 'nyxt/mode/editor::plaintext-editor-mode %slot-value%))))"
+It renders the file in a single textarea HTML element.  Enabled by default for
+`editor-buffer's."
   ((visible-in-status-p nil)
    (rememberable-p nil)
    (style (theme:themed-css (theme *browser*)
