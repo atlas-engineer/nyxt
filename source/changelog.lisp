c531b3cc9d464c43fc5534330c4909a6ee9bb4db
Andre A. Gomes <andremegafone@gmail.com>
2024-04-04 16:26:57 +0300

    changelog: Mention common-settings crash fix.

--- source/changelog.lisp
+++ source/changelog.lisp
@@ -96,6 +96,7 @@ elements are scaled accordingly.")
 (define-version "3.11.6"
   (:nsection :title "Bug fixes"
     (:ul
+     (:li "Fix crash on " (:a :href (nyxt-url 'common-settings) "."))
      (:li "Fix warnings raised by " (:nxref :command 'pick-color) "."))))
 
 (define-version "3.11.5"
