bd1b770a55e6ac5df4ad6af6746f9ae5bc88ff5b
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-05-31 07:58:40 +0300

    Test redirect with REDUCE-TRACKING-MODE

--- tests/offline/mode/reduce-tracking.lisp
+++ tests/offline/mode/reduce-tracking.lisp
@@ -4,9 +4,16 @@
 (in-package :nyxt/tests)
 
 (define-test toggle-reduce-tracking-mode ()
-  (let ((buffer (make-instance 'network-and-modable-buffer)))
+  (let ((buffer (make-instance 'network-and-modable-buffer))
+        (url-pre  (quri:uri "https://example.com/query?foo=bar&twclid=1&redirect=https://example.org/foo&s_cid=123"))
+        (url-post (quri:uri "https://example.com/query?foo=bar&redirect=https://example.org/foo")))
     (with-current-buffer buffer
       (assert-true (enable-modes* 'nyxt/mode/reduce-tracking:reduce-tracking-mode
                                   buffer))
+      (assert-equality
+       #'quri:uri= url-post
+       (url (nhooks:run-hook
+             (request-resource-hook buffer)
+             (make-instance 'request-data :url url-pre))))
       (assert-true (disable-modes* 'nyxt/mode/reduce-tracking:reduce-tracking-mode
                                    buffer)))))
