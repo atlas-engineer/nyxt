45a24966a1daae5b4bcdba192367d84fdf1c8507
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-05-31 07:24:52 +0300

    Mention REDUCE-TRACKING-MODE fix in the changelog

--- source/changelog.lisp
+++ source/changelog.lisp
@@ -99,7 +99,10 @@ elements are scaled accordingly.")
 (define-version "3.11.7"
   (:nsection :title "Bug fixes"
     (:ul
-     (:li "Fix query handling when invoking command" (:nxref :command 'set-url) "."))))
+     (:li "Fix query handling when invoking command" (:nxref :command 'set-url) ".")
+     (:li "Fix redirect while "
+          (:nxref :class-name 'nyxt/mode/reduce-tracking:reduce-tracking-mode)
+          " is active"))))
 
 (define-version "3.11.6"
   (:nsection :title "Bug fixes"
