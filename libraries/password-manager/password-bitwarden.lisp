2a5e94c341485cd3efd2ab940b969b4f9d5dd70b
Ed Tavinor <edtavinor@posteo.de>
2024-07-08 19:13:32 +0200

    added bitwarden integration

---
+++ libraries/password-manager/password-bitwarden.lisp
@@ -0,0 +1,121 @@
+(in-package :password)
+
+(define-class bitwarden-interface (password-interface)
+  ((password :accessor password :type string :initform "")
+   (sessionid :accessor sessionid :type string :initform ""))
+  (:export-class-name-p t)
+  (:export-accessor-names-p t))
+
+(push 'bitwarden-interface *interfaces*)
+
+(defparameter *bitwarden-host* "http://localhost:8087")
+
+(defun get-password ()
+  (let ((p1 (nyxt:prompt :prompt "enter password" :sources (make-instance 'prompter:raw-source) :invisible-input-p t))
+	(p2 (nyxt:prompt :prompt "please confirm" :sources (make-instance 'prompter:raw-source)
+			 :invisible-input-p t)))
+    (if (string= (first p1) (first p2)) (first p1)
+	(progn
+	  (nyxt:echo-warning "passwords do not match")
+	  (get-password)))))
+
+(defun get-sessionid (output)
+  (let ((j (njson:decode output)))
+    (gethash "raw" (gethash "data" j))))
+
+(defun get-password-list (output)
+  (let ((json-ht (njson:decode output)))
+    (coerce (gethash "data" (gethash "data" json-ht)) 'list)))
+
+(defun my/get-passwords (sessionid)
+  (format *error-output* "reading passwords over rest~%")
+  (handler-case
+      (let* ((output
+	       (dex:get (format nil "~a/list/object/items" *bitwarden-host*)
+			:headers `(("sessionid" . sessionid)
+				   ("Content-Type" . "application/json"))))
+	     (passwords (get-password-list output)))
+	(format *error-output* "~a passwords read~%" (length passwords))
+	passwords)
+    (error (c)
+      (nyxt:echo-warning (format nil "could not list-passwords: ~a~%" c)))))
+
+(defun get-password-names (passwords)
+  (mapcar (lambda (ht) (gethash "name" ht)) passwords))
+
+(defmethod connect ((password-interface bitwarden-interface))
+  (with-slots (password sessionid) password-interface
+    (format *error-output* "calling connect. current value of session-id: ~a~%" sessionid)
+    (if (not (zerop (length sessionid)))
+	t
+	(handler-case (let ((output (dex:post (format nil "~a/unlock" *bitwarden-host*)
+					      :content (format nil "{\"password\": \"~a\"}" (get-password))
+					      :headers '(("Content-Type" . "application/json")))))
+			(format *error-output* output)
+			(setf sessionid (get-sessionid output))
+			(format *error-output* "session-id: ~a~%" sessionid) 
+			t)
+	  (error (c)
+	    (nyxt:echo-warning "login failed: " c))))))
+
+(defmethod list-passwords ((password-interface bitwarden-interface))
+  (when (connect password-interface)
+    (with-slots (sessionid) password-interface
+      (handler-case (let* ((password-seq (my/get-passwords sessionid)))
+		      (get-password-names password-seq))
+	(error (c)
+	  (nyxt:echo-warning (format nil "could not list-passwords: ~a~%" c)))))))
+
+(defun get-login-field (pw field)
+  (let* ((login-block (gethash "login" pw)))
+    (gethash field login-block)))
+		       
+(defun name-matcher (name)
+  (lambda (pw)
+    (let* ((other-name (gethash "name" pw)))
+      (string= name other-name))))
+
+
+(defun login-field-matcher (field name)
+  (lambda (pw)
+    (let* ((password-name (get-login-field pw field)))
+      (string= name password-name))))
+
+(defmethod clip-password ((password-interface bitwarden-interface) &key password-name service)
+  (declare (ignore service))
+  (format *error-output* "getting password for entry with name ~a~%" password-name)
+  (when (connect password-interface)
+    (format *error-output* "connection successful~%")
+    (handler-case
+	(with-slots (sessionid) password-interface
+	  (let* ((passwords (my/get-passwords sessionid))
+		 (matching (remove-if-not (name-matcher password-name) passwords))
+		 (password (get-login-field (first matching) "password")))
+	    (clip-password-string password-interface password)))
+      (error (c)
+	(format *error-output* "could not clip-password: ~a~%" c)
+	(nyxt:echo-warning (format nil "could not clip-password: ~a~%" c))))))
+
+
+(defmethod clip-username ((password-interface bitwarden-interface) &key password-name service)
+  (declare (ignore service))
+  (when (connect password-interface)
+    (handler-case
+	(with-slots (sessionid) password-interface
+	  (let* ((passwords (my/get-passwords sessionid))
+		 (matching (remove-if-not (name-matcher password-name) passwords))
+		 (username (get-login-field (first matching) "username")))
+	    (trivial-clipboard:text username)))
+      (error (c)
+	(format *error-output* "could not clip-username: ~a~%" c)
+	(nyxt:echo-warning (format nil "could not clip-username: ~a~%" c))))))
+
+
+(defmethod save-password ((password-interface bitwarden-interface)
+                          &key password-name username password service)
+  (declare (ignore service))
+  (nyxt:echo-warning "saving passwords not yet supported"))
+
+(defmethod password-correct-p ((password-interface bitwarden-interface))
+  (connect password-interface)
+  t)
