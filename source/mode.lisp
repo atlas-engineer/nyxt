atlas-engineer/nyxt

commit 76b9789cf102864fb7dd929716a47a1d3eeea3d2

Review print-object method.

source/mode.lisp
@@ -223,13 +223,7 @@ When unset, it corresponds to the mode name."
   (setf (slot-value mode 'glyph) glyph))
 
 (defmethod print-object ((mode mode) stream)
-  (if *print-escape*
-      (print-unreadable-object (mode stream :type t :identity t))
-      (let ((name (symbol-name (sera:class-name-of mode)))
-            (suffix "-MODE"))
-        (format stream "~(~a~)" (sera:string-replace
-                                 suffix name ""
-                                 :start (- (length name ) (length suffix)))))))
+  (print-unreadable-object (mode stream :type t)))
 
 (sym:define-symbol-type mode (class)
   (when-let ((class (find-class sym:%symbol% nil)))

source/auto-rules.lisp
@@ -81,7 +81,7 @@
   all the other modes disabled."))
 
 (defmethod print-object ((rule auto-rule) stream)
-  (print-unreadable-object (rule stream :type t :identity t)
+  (print-unreadable-object (rule stream :type t)
     (princ (sera:ellipsize (format nil "~a" (test rule)) 40) stream)))
 
 (defvar *default-auto-rules* '()

source/buffer.lisp
@@ -596,8 +596,8 @@ Conceptually, it's similar to \"private windows\" in popular browsers but the
 scope is that of buffers."))
 
 (defmethod print-object ((buffer buffer) stream)
-  (print-unreadable-object (buffer stream :type t :identity t)
-    (format stream "~a" (id buffer))))
+  (print-unreadable-object (buffer stream :type t)
+    (format stream "~a ~a" (id buffer) (url buffer))))
 
 (defmethod (setf url) :around (value (buffer document-buffer))
   (declare (ignore value))

source/migration.lisp
@@ -35,8 +35,8 @@ This is useful to delay the evaluation of the tip until it's rendered."))
 `symbols' are the offending ones deprecated in `version'."))
 
 (defmethod print-object ((object suggestion) stream)
-  (print-unreadable-object (object stream :type t :identity t)
-    (format stream "~a" (symbols  object))))
+  (print-unreadable-object (object stream :type t)
+    (format stream "~a" (symbols object))))
 
 (defun suggestion= (suggestion1 suggestion2)
   (and (string= (version suggestion1)

source/urls.lisp
@@ -80,7 +80,7 @@ content. In case something goes wrong, runs `error-callback'.")
   (:metaclass user-class))
 
 (defmethod print-object ((scheme scheme) stream)
-  (print-unreadable-object (scheme stream :type t :identity t)
+  (print-unreadable-object (scheme stream :type t)
     (format stream "~a" (name scheme))))
 
 (defvar *schemes* (sera:dict)

source/window.lisp
@@ -87,8 +87,8 @@ The handlers take the window as argument."))
   window)
 
 (defmethod print-object ((window window) stream)
-  (print-unreadable-object (window stream :type t :identity t)
-    (format stream "~a" (id window))))
+  (print-unreadable-object (window stream :type t)
+    (format stream "~a ~a" (id window) (titler window))))
 
 (defmethod titler ((window window))
   "Return the title of WINDOW."

commit 5271b549280e4e2924ef7cfd60f48ef172506926

mode: Fix printed representation.
Bug introduced in 76b9789cf102864fb7dd929716a47a1d3eeea3d2.

source/mode.lisp
@@ -223,7 +223,9 @@ When unset, it corresponds to the mode name."
   (setf (slot-value mode 'glyph) glyph))
 
 (defmethod print-object ((mode mode) stream)
-  (print-unreadable-object (mode stream :type t)))
+  (format stream "~@(~a~)"
+          (sera:drop-suffix "-MODE"
+                            (symbol-name (sera:class-name-of mode)))))
 
 (sym:define-symbol-type mode (class)
   (when-let ((class (find-class sym:%symbol% nil)))