73316569fb0df4653be3fcda7aff72887d82b0b0
Yuva <94527987+yuvashrikarunakaran@users.noreply.github.com>
2025-01-11 22:16:32 +0530

    Update .dir-locals.el
    updated

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,80 @@
+# Keep in sync with https://github.com/flathub/engineer.atlas.Nyxt/.
+app-id: engineer.atlas.Nyxt-WebKitGTK
+runtime: org.gnome.Platform
+runtime-version: '46'
+sdk: org.gnome.Sdk
+rename-icon: nyxt
+rename-appdata-file: nyxt.metainfo.xml
+rename-desktop-file: nyxt.desktop
+command: nyxt
+
+finish-args:
+  - --device=dri
+  - --share=network
+  - --share=ipc
+  - --socket=wayland
+  - --socket=fallback-x11
+  - --socket=pulseaudio
+  - --socket=cups
+  - --filesystem=host
+  # Allow invoking host commands via flatpak-spawn --host <command> <command-args>
+  - --talk-name=org.freedesktop.Flatpak
+  # See http://sbcl.org/getting.html
+  - --env=SBCL_HOME=/app/lib/sbcl
+
+modules:
+  - name: sbcl
+    buildsystem: simple
+    sources:
+      - type: archive
+        url: http://prdownloads.sourceforge.net/sbcl/sbcl-2.4.11-x86-64-linux-binary.tar.bz2
+        sha256: e7c62c37f39a8b73663b575af0b2b733fd0a993b79a1a2262f81446e2a08b0f9
+        only-arches: [x86_64]
+    build-commands:
+      - INSTALL_ROOT=${FLATPAK_DEST} sh install.sh
+
+  - name: xsel
+    buildsystem: autotools
+    sources:
+      - type: git
+        url: https://github.com/kfish/xsel.git
+        tag: 1.2.1
+
+  - name: wl-clipboard
+    buildsystem: meson
+    config-opts:
+      - -Dzshcompletiondir=no
+      - -Dfishcompletiondir=no
+    sources:
+      - type: git
+        url: https://github.com/bugaevc/wl-clipboard.git
+        tag: v2.2.2
+
+  - name: libfixposix
+    buildsystem: autotools
+    sources:
+      - type: git
+        url: https://github.com/sionescu/libfixposix.git
+        tag: v0.5.2
+      - type: script
+        dest-filename: autogen.sh
+        commands:
+          - autoreconf -fi
+    cleanup:
+      - /lib/pkgconfig
+      - /include
+      - '*.h'
+      - '*.pc'
+      - '*.la'
+
+  - name: nyxt
+    buildsystem: simple
+    sources:
+      - type: dir
+        path: .
+    build-commands:
+      - make install DESTDIR=${FLATPAK_DEST} PREFIX=
+
 ((nil . ((fill-column . 80)
          (project-vc-ignores . ("./_build"))))
  (org-mode . ((org-edit-src-content-indentation 0)))
@@ -22,7 +99,7 @@
                                                               keyword)
                                                             (concat prefix "-" keyword))
                                                       t)
-                                                     "\\s-+\\(" lisp-mode-symbol-regexp "\\)"))
+                                                     "\\s-+(\\(" lisp-mode-symbol-regexp "\\)"))
                                    2))))))
                ;; This adds the argument to the list of imenu known keywords.
                (enhance-imenu-lisp
