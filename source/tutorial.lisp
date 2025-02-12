b9de70aaeba601492b02440a6ff228f4064775a1
John Mercouris <john@mercouris.email>
2025-02-11 22:19:56 -0600

    tutorial: Remove section on buffer history.
    The history is no longer controlled on a per-buffer basis. It *will* be, but not now.

--- source/tutorial.lisp
+++ source/tutorial.lisp
@@ -147,21 +147,6 @@ by matching against the hint, the URL or the title.")
                                      nyxt/mode/hint:follow-hint-new-buffer-focus
                                      nyxt/mode/hint:follow-hint-new-buffer))))
 
-      (:nsection :title "Using the buffer history"
-        (:p "History is represented as a tree that you can traverse: when you go back
-in history, then follow a new URL, it effectively creates a new branch without
-deleting the old path. The tree makes sure you never lose track of where you've
-been.")
-        (:ul
-         (list-command-information '(nyxt/mode/history:history-forwards
-                                     nyxt/mode/history:history-backwards
-                                     nyxt/mode/history:history-forwards-query
-                                     nyxt/mode/history:history-backwards-query
-                                     nyxt/mode/history:history-forwards-all-query
-                                     nyxt/mode/history:history-all-query)))
-        (:p "You can also view a full tree of the history for a given buffer by
-invoking the command 'buffer-history-tree'."))
-
       (:nsection :title "Incremental Search"
         (:p "Nyxt's search is incremental, i.e. it begins as soon as you type
 the first character of the search string.  A single or multiple buffers can be
