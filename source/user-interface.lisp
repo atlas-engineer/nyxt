da75ccae709a1635153df06b05918339f9cb6295
John Mercouris <john@mercouris.email>
2025-03-19 16:20:24 -0500

    user-interface: Format for width.

--- source/user-interface.lisp
+++ source/user-interface.lisp
@@ -18,11 +18,13 @@
   (ffi-buffer-evaluate-javascript-async
    (user-interface:buffer progress-bar)
    (ps:ps
-     (setf (ps:chain document
-                     (get-element-by-id (ps:lisp (user-interface:id progress-bar)))
-                     style
-                     width)
-           (ps:lisp (format nil "~,1f%" (user-interface:percentage progress-bar)))))))
+     (setf (ps:chain
+            document
+            (get-element-by-id (ps:lisp (user-interface:id progress-bar)))
+            style
+            width)
+           (ps:lisp (format nil "~,1f%"
+                            (user-interface:percentage progress-bar)))))))
 
 (defmethod user-interface:update ((button user-interface:button))
   (ffi-buffer-evaluate-javascript-async
