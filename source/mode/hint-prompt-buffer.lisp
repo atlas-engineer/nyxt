4812000c62824892f84974b15a23b296db1498f7
John Mercouris <john@mercouris.email>
2025-07-15 14:46:07 -0500

    mode/hint-prompt-buffer: Delete toggle-hints-transparency
    This will be readded at some future time when it is not buggy. It is not a
    critical feature.

--- source/mode/hint-prompt-buffer.lisp
+++ source/mode/hint-prompt-buffer.lisp
@@ -5,15 +5,6 @@
   (:documentation "Package for `hint-prompt-buffer-mode', prompt buffer mode for element hints."))
 (in-package :nyxt/mode/hint-prompt-buffer)
 
-(define-command toggle-hints-transparency (&key (buffer (current-buffer)))
-  "Toggle the transparency of all element hints in BUFFER."
-  (ps-eval :buffer buffer
-    (ps:dolist (element (nyxt/ps:qsa document ".nyxt-hint"))
-      (if (or (= (ps:@ element style opacity) "1")
-              (= (ps:@ element style opacity) ""))
-          (setf (ps:@ element style opacity) "0.2")
-          (setf (ps:@ element style opacity) "1.0")))))
-
 (define-command scroll-to-hint (&key (buffer (current-buffer)))
   "Display the current hint and center it in BUFFER."
   (with-current-buffer buffer
@@ -29,5 +20,4 @@ Provides keybindings `toggle-hints-transparency' and `scroll-to-hint'."
     (define-keyscheme-map "hint-prompt-buffer-mode" ()
       keyscheme:default
       (list
-       "M-i" 'toggle-hints-transparency
        "C-l" 'scroll-to-hint)))))
